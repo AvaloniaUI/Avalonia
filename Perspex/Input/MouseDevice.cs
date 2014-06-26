e50a5d197e23d12bcc4abcd963c9776a5912f200
Steven Kirk <grokys@gmail.com>
2014-06-26 11:18:28 +0200

    Fixed header.

--- Perspex/Input/MouseDevice.cs
+++ Perspex/Input/MouseDevice.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="MouseDevice.cs" company="Tricycle">
-// Copyright 2014 Tricycle. All rights reserved.
+// <copyright file="MouseDevice.cs" company="Steven Kirk">
+// Copyright 2014 MIT Licence. See licence.md for more information.
 // </copyright>
 // -----------------------------------------------------------------------
 
