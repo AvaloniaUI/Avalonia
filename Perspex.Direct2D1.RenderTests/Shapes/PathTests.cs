6b20bc7349e041c2b85335e64161f95bcd8520fb
Steven Kirk <steven.kirk@tricycleinc.com>
2014-09-14 05:25:08 +0200

    Added a Path test.

---
+++ Perspex.Direct2D1.RenderTests/Shapes/PathTests.cs
@@ -0,0 +1,45 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PathTests.cs" company="Steven Kirk">
+// Copyright 2014 MIT Licence. See licence.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Perspex.Direct2D1.RenderTests.Shapes
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Perspex.Controls;
+    using Perspex.Layout;
+    using Perspex.Media;
+    using Perspex.Shapes;
+
+    [TestClass]
+    public class PathTests : TestBase
+    {
+        public PathTests()
+            : base(@"Shapes\Path")
+        {
+        }
+
+        [TestMethod]
+        public void Path_100px_Triangle_Centered()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Content = new Path
+                {
+                    Fill = Brushes.Gray,
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 2,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Data = StreamGeometry.Parse("M 0,100 L 100,100 50,0 Z"),
+                }
+            };
+
+            this.RenderToFile(target);
+            this.CompareImages();
+        }
+    }
+}
