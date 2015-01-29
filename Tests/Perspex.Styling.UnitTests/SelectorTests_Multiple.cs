9f68e2f2db46967b35c4c1fb21eceef0a51b57ed
Steven Kirk <steven.kirk@tricycleinc.com>
2015-01-29 14:29:50 +0100

    Reinstated SelectorTests_Multiple.

--- Tests/Perspex.Styling.UnitTests/SelectorTests_Multiple.cs
+++ Tests/Perspex.Styling.UnitTests/SelectorTests_Multiple.cs
@@ -9,45 +9,48 @@ namespace Perspex.Styling.UnitTests
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using Perspex.Controls;
     using Perspex.Styling;
+    using Xunit;
 
-    ////[TestFixture]
-    ////public class SelectorTests_Multiple
-    ////{
-    ////    [Fact]
-    ////    public void Template_Child_Of_Control_With_Two_Classes()
-    ////    {
-    ////        var template = new ControlTemplate(parent =>
-    ////        {
-    ////            return new Border
-    ////            {
-    ////                Id = "border",
-    ////            };
-    ////        });
+    public class SelectorTests_Multiple
+    {
+        [Fact]
+        public void Template_Child_Of_Control_With_Two_Classes()
+        {
+            var template = new ControlTemplate(parent =>
+            {
+                return new Border
+                {
+                    Id = "border",
+                };
+            });
 
-    ////        var control = new Button
-    ////        {
-    ////            Template = template,
-    ////        };
+            var control = new Button
+            {
+                Template = template,
+            };
 
-    ////        var selector = new Selector()
-    ////            .OfType<Button>()
-    ////            .Class("foo")
-    ////            .Class("bar")
-    ////            .Template()
-    ////            .Id("border");
+            control.ApplyTemplate();
 
-    ////        var border = (Border)((IVisual)control).VisualChildren.Single();
-    ////        var values = new List<bool>();
-    ////        var activator = selector.GetActivator(border);
+            var selector = new Selector()
+                .OfType<Button>()
+                .Class("foo")
+                .Class("bar")
+                .Template()
+                .Id("border");
 
-    ////        activator.Subscribe(x => values.Add(x));
+            var border = (Border)((IVisual)control).VisualChildren.Single();
+            var values = new List<bool>();
+            var activator = selector.GetActivator(border);
 
-    ////        Assert.Equal(new[] { false }, values);
-    ////        control.Classes.Add("foo", "bar");
-    ////        Assert.Equal(new[] { false, true }, values);
-    ////        control.Classes.Remove("foo");
-    ////        Assert.Equal(new[] { false, true, false }, values);
-    ////    }
-    ////}
+            activator.Subscribe(x => values.Add(x));
+
+            Assert.Equal(new[] { false }, values);
+            control.Classes.Add("foo", "bar");
+            Assert.Equal(new[] { false, true }, values);
+            control.Classes.Remove("foo");
+            Assert.Equal(new[] { false, true, false }, values);
+        }
+    }
 }
