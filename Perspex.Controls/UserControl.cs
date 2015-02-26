70ae10b07767fff4737ec9f1c5b0c56edd618243
Steven Kirk <grokys@gmail.com>
2015-02-26 01:04:48 +0100

    Added UserControl.

---
+++ Perspex.Controls/UserControl.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="UserControl.cs" company="Steven Kirk">
+// Copyright 2015 MIT Licence. See licence.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Perspex.Controls
+{
+    using System;
+    using Perspex.Styling;
+
+    public class UserControl : ContentControl, IStyleable
+    {
+        Type IStyleable.StyleKey
+        {
+            get { return typeof(ContentControl); }
+        }
+    }
+}
