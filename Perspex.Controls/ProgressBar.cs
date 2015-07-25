08ca269a093e4aff3cb37195c0d998bdb8ed8901
Nelson Carrillo <ncarrillo@harbortouch.com>
2015-07-25 16:40:35 -0400

    Correctly subtracted the minimum from the value and the divisor.

--- Perspex.Controls/ProgressBar.cs
+++ Perspex.Controls/ProgressBar.cs
@@ -16,13 +16,15 @@ namespace Perspex.Controls
     public class ProgressBar : RangeBase
     {
         /// <inheritdoc/>
+        /// 
+        private Border indicator;
         protected override Size ArrangeOverride(Size finalSize)
         {
             var size = base.ArrangeOverride(finalSize);
-            var b = this.Bounds;
-
-            var indicator = this.GetTemplateChild<Border>("PART_Indicator");
-            indicator.Width = Math.Max(this.Minimum, finalSize.Width * (this.Value / this.Maximum));
+            this.indicator = this.indicator ?? this.GetTemplateChild<Border>("PART_Indicator");
+            
+            double percent = this.Maximum == this.Minimum ? 1.0 : (this.Value - this.Minimum) / (this.Maximum - this.Minimum);
+            indicator.Width = finalSize.Width * percent;
 
             return size;
         }
