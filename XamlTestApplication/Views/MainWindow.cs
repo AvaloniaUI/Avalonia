4c4a939684bb703776abce22f14a76b88a619209
Steven Kirk <grokys@gmail.com>
2015-08-25 02:48:31 +0200

    Enable DevTools in XamlTestApplication.

--- XamlTestApplication/Views/MainWindow.cs
+++ XamlTestApplication/Views/MainWindow.cs
@@ -1,10 +1,15 @@
 ﻿namespace XamlTestApplication.Views
 {
     using OmniXaml.AppServices.Mvvm;
+    using Perspex.Diagnostics;
     using Perspex.Xaml.Desktop;
 
     [ViewToken("Main", typeof(MainWindow))]
     public class MainWindow : PerspexWindow
-    {         
+    {
+        public MainWindow()
+        {
+            DevTools.Attach(this);
+        }
     }
 }
