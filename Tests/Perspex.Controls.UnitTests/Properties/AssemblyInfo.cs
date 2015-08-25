eaacfa5ac91593ccd49b24f3089dcccf803cf03e
Steven Kirk <grokys@gmail.com>
2015-08-25 16:47:25 +0200

    Don't run Controls unit tests in parallel.

--- Tests/Perspex.Controls.UnitTests/Properties/AssemblyInfo.cs
+++ Tests/Perspex.Controls.UnitTests/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +35,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Don't run tests in parallel.
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
