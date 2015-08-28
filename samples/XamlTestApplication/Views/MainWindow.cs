c14194bee71d19d058737c89ce07d51e4039ac44
José Manuel Nieto Sánchez <superjmn@outlook.com>
2015-08-28 12:40:47 +0200

    Fixed resource loading. Deleted PerspexXamlLoader (useless).

--- samples/XamlTestApplication/Views/MainWindow.cs
+++ samples/XamlTestApplication/Views/MainWindow.cs
@@ -1,5 +1,11 @@
 ﻿namespace XamlTestApplication.Views
 {
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Reflection;
+    using System.Resources;
+    using OmniXaml;
     using OmniXaml.AppServices.Mvvm;
     using Perspex.Diagnostics;
     using Perspex.Xaml.Desktop;
@@ -16,8 +22,29 @@
 
         private void InitializeComponent()
         {
-            var loader = new PerspexXamlLoader(new PerspexInflatableTypeFactory());
-            loader.Load(this.GetType());
+            var xamlLoader = new XamlLoader(new PerspexParserFactory(new TypeFactory()));
+            var stream = GetStream(new Uri("Views/MainWindow.xaml", UriKind.Relative));
+            xamlLoader.Load(stream, this);
+        }
+
+
+        private static Stream GetStream(Uri resourceLocator)
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var resourceName = assembly.GetName().Name + ".g";
+            var manager = new ResourceManager(resourceName, assembly);
+
+            using (var resourceSet = manager.GetResourceSet(CultureInfo.CurrentCulture, true, true))
+            {
+                var stream = (Stream)resourceSet.GetObject(resourceLocator.ToString(), true);
+
+                if (stream == null)
+                {
+                    throw new IOException($"The requested resource could not be found: {resourceLocator}");
+                }
+
+                return stream;
+            }
         }
     }
 }
