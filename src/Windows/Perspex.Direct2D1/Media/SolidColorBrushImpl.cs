dcdcd9ebefc6a0d9724d5104fc562f78550377be
Nelson Carrillo <ncarrillo@harbortouch.com>
2015-09-05 18:16:13 -0400

    Update SolidColorBrushImpl.cs

--- src/Windows/Perspex.Direct2D1/Media/SolidColorBrushImpl.cs
+++ src/Windows/Perspex.Direct2D1/Media/SolidColorBrushImpl.cs
@@ -10,7 +10,7 @@ namespace Perspex.Direct2D1.Media
     {
         public SolidColorBrushImpl(Perspex.Media.SolidColorBrush brush, SharpDX.Direct2D1.RenderTarget target)
         {
-            this.PlatformBrush = new SharpDX.Direct2D1.SolidColorBrush(target, brush?.Color.ToDirect2D() ?? new SharpDX.Color4());
+            this.PlatformBrush = new SharpDX.Direct2D1.SolidColorBrush(target, brush?.Color.ToDirect2D() ?? new SharpDX.Color4(), new SharpDX.Direct2D1.BrushProperties { Opacity = brush != null ? (float)brush.Opacity : 1.0f, Transform = target.Transform });
         }
     }
 }
