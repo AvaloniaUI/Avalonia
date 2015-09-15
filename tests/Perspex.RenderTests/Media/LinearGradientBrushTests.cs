c10ac8e68d33ca08dcb8c4a61a8745e14c86a666
Steven Kirk <grokys@gmail.com>
2015-09-15 02:19:44 +0200

    Added LinearGradient vertical fill tests.

--- tests/Perspex.RenderTests/Media/LinearGradientBrushTests.cs
+++ tests/Perspex.RenderTests/Media/LinearGradientBrushTests.cs
@@ -23,7 +23,7 @@ namespace Perspex.Direct2D1.RenderTests.Media
         }
 
         [Fact]
-        public void LinearGradientBrush_RedBlue_Fill()
+        public void LinearGradientBrush_RedBlue_Horizontal_Fill()
         {
             Decorator target = new Decorator
             {
@@ -36,6 +36,33 @@ namespace Perspex.Direct2D1.RenderTests.Media
                     {
                         StartPoint = new Point(0, 0.5),
                         EndPoint = new Point(1, 0.5),
+                        GradientStops =
+                        {
+                            new GradientStop { Color = Colors.Red, Offset = 0 },
+                            new GradientStop { Color = Colors.Blue, Offset = 1 }
+                        }
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [Fact]
+        public void LinearGradientBrush_RedBlue_Vertical_Fill()
+        {
+            Decorator target = new Decorator
+            {
+                Padding = new Thickness(8),
+                Width = 200,
+                Height = 200,
+                Child = new Border
+                {
+                    Background = new LinearGradientBrush
+                    {
+                        StartPoint = new Point(0.5, 0),
+                        EndPoint = new Point(0.5, 1),
                         GradientStops =
                         {
                             new GradientStop { Color = Colors.Red, Offset = 0 },
