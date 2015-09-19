f6ef878d86aae6482b90f2efaafd891a3cd0268b
Nikita Tsukanov <keks9n@gmail.com>
2015-09-19 13:30:58 +0300

    build: Publish nuget from ps1

--- nuget/build-appveyor.ps1
+++ nuget/build-appveyor.ps1
@@ -4,4 +4,11 @@ Push-Location $dir
 
 sv version $env:APPVEYOR_BUILD_NUMBER
 sv version 9999.0.$version-nightly
+sv key $env:myget_key
+
+sv file Perspex.$version.nupkg
+
 .\build-version.ps1 $version
+
+
+nuget.exe push $file $key -Source https://www.myget.org/F/perspex-nightly/api/v2/package
