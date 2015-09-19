8e38e64562df8b90e67714caa79a502fc8955cbf
Nikita Tsukanov <keks9n@gmail.com>
2015-09-19 13:43:08 +0300

    build: publish nuget after tests and only from master

--- nuget/build-appveyor.ps1
+++ nuget/build-appveyor.ps1
@@ -2,7 +2,10 @@ $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 Push-Location $dir
 
+
 sv version $env:APPVEYOR_BUILD_NUMBER
+#sv version "1-debug"
+
 sv version 9999.0.$version-nightly
 sv key $env:myget_key
 
@@ -11,4 +14,20 @@ sv file Perspex.$version.nupkg
 .\build-version.ps1 $version
 
 
-nuget.exe push $file $key -Source https://www.myget.org/F/perspex-nightly/api/v2/package
+sv reponame $env:APPVEYOR_REPO_NAME
+sv repobranch $env:APPVEYOR_REPO_BRANCH
+sv pullreq $env:APPVEYOR_PULL_REQUEST_NUMBER
+
+echo "Checking for publishing"
+echo $reponame $repobranch $pullreq
+if ($reponame -eq "Perspex/Perspex")
+{
+    echo Repo name matched
+    if($repobranch -eq "master")
+    {
+        echo Repo branch matched
+        nuget.exe push $file $key -Source https://www.myget.org/F/perspex-nightly/api/v2/package
+    }
+}
+
+
