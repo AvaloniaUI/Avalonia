1d5b5858951d4649d54691cf21577d8bf25d6f27
Nikita Tsukanov <keks9n@gmail.com>
2015-09-28 11:59:29 +0300

    Implemented HotKey attached property.
    It doesn't work, however, because of broken ContentControl (doesn't sets child's parent for some reason)

---
+++ src/Perspex.Input/HotkeyManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Perspex.Input
+{
+    public class HotkeyManager
+    {
+        public static PerspexProperty<KeyGesture> HotKey
+            = PerspexProperty.RegisterAttached<Visual, KeyGesture>("HotKey", typeof (HotkeyManager));
+
+        class HotkeyCommandWrapper : ICommand
+        {
+            public Visual Visual;
+            public PerspexProperty Property;
+
+            ICommand GetCommand() => (ICommand) Visual.GetValue(Property);
+
+            public bool CanExecute(object parameter) => GetCommand()?.CanExecute(parameter) ?? false;
+
+            public void Execute(object parameter) => GetCommand()?.Execute(parameter);
+
+            //Implementation isn't needed in this case
+            public event EventHandler CanExecuteChanged;
+        }
+
+
+        static HotkeyManager()
+        {
+            HotKey.Changed.Subscribe(args =>
+            {
+                if (args.OldValue != null)
+                {
+                    
+                }
+
+
+            });
+        }
+
+    }
+}
---
+++ tests/Perspex.Controls.UnitTests/Utils/HotKeyManagerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Perspex.Controls.Presenters;
+using Perspex.Controls.Templates;
+using Perspex.Input;
+using Perspex.Platform;
+using Perspex.Styling;
+using Xunit;
+
+namespace Perspex.Controls.UnitTests.Utils
+{
+    public class HotKeyManagerTests
+    {
+        [Fact]
+        public void HotKeyManager_Should_Register_And_Unregister_Key_Binding()
+        {
+            using (PerspexLocator.EnterScope())
+            {
+                var windowImpl = new Mock<IWindowImpl>();
+                PerspexLocator.CurrentMutable
+                    .Bind<IWindowImpl>().ToConstant(windowImpl.Object);
+
+                var gesture1 = new KeyGesture {Key = Key.A, Modifiers = InputModifiers.Control};
+                var gesture2 = new KeyGesture {Key = Key.B, Modifiers = InputModifiers.Control};
+
+                var tl = new Window();
+                
+                var button = new Button();
+                HotKeyManager.SetHotKey(button, gesture1);
+
+                //ContentPresenter's parent management is broken for now, so I'm setting parent property directly
+                button.SetValue(Control.ParentProperty, tl);
+                Assert.Equal(gesture1, tl.KeyBindings[0].Gesture);
+
+                HotKeyManager.SetHotKey(button, gesture2);
+                Assert.Equal(gesture2, tl.KeyBindings[0].Gesture);
+
+                button.SetValue(Control.ParentProperty, null);
+
+                Assert.Empty(tl.KeyBindings);
+
+                button.SetValue(Control.ParentProperty, tl);
+
+                Assert.Equal(gesture2, tl.KeyBindings[0].Gesture);
+
+                HotKeyManager.SetHotKey(button, null);
+                Assert.Empty(tl.KeyBindings);
+
+            }
+        }
+    }
+}
