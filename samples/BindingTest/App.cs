4d5abf80e7dbd64833baa16b100afa115de00a4c
Steven Kirk <grokys@gmail.com>
2015-10-02 11:43:08 +0200

    Added a binding test application.

---
+++ samples/BindingTest/App.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Perspex;
+using Perspex.Controls;
+using Perspex.Diagnostics;
+using Perspex.Themes.Default;
+
+namespace BindingTest
+{
+    public class App : Application
+    {
+        public App()
+        {
+            RegisterServices();
+            InitializeSubsystems((int)Environment.OSVersion.Platform);
+            Styles = new DefaultTheme();
+        }
+
+        public static void AttachDevTools(Window window)
+        {
+            DevTools.Attach(window);
+        }
+
+        private static void Main()
+        {
+            var app = new App();
+            var window = new MainWindow();
+            window.Show();
+            app.Run(window);
+        }
+    }
+}
---
+++ samples/BindingTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+
+namespace BindingTest.ViewModels
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        private string _simpleBinding = "Simple Binding";
+
+        public string SimpleBinding
+        {
+            get { return _simpleBinding; }
+            set { this.RaiseAndSetIfChanged(ref _simpleBinding, value); }
+        }
+    }
+}
