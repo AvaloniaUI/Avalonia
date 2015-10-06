c7ace7b6a7b20c1702a806b5298d9847957b20c1
Steven Kirk <grokys@gmail.com>
2015-10-06 21:26:26 +0200

    Allow binding to attached properties.
    Closes #255.

---
+++ src/Markup/Perspex.Markup.Xaml/Context/PerspexAttachableXamlMember.cs
@@ -0,0 +1,33 @@
+// Copyright (c) The Perspex Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using Perspex.Markup.Xaml.Binding;
+using OmniXaml;
+using OmniXaml.Typing;
+using System.Reflection;
+
+namespace Perspex.Markup.Xaml.Context
+{
+    public class PerspexAttachableXamlMember : AttachableXamlMember
+    {
+        public PerspexAttachableXamlMember(string name,
+            XamlType owner,
+            MethodInfo getter,
+            MethodInfo setter,
+            IXamlTypeRepository xamlTypeRepository,
+            ITypeFeatureProvider featureProvider)
+            : base(name, getter, setter, xamlTypeRepository, featureProvider)
+        {
+        }
+
+        protected override IXamlMemberValuePlugin LookupXamlMemberValueConnector()
+        {
+            return new PerspexXamlMemberValuePlugin(this);
+        }
+
+        public override string ToString()
+        {
+            return "Perspex Attachable XAML Member " + base.ToString();
+        }
+    }
+}
