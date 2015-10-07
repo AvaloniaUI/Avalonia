303f473ffe9bc2d4e1fff82ea32f28d4a8c3e658
Nikita Tsukanov <keks9n@gmail.com>
2015-10-07 13:28:57 +0300

    Added CheckDisposed logic to FormattedText

---
+++ src/Perspex.Base/PerspexDisposable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perspex
+{
+    public abstract class PerspexDisposable : IDisposable
+    {
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+            DoDispose();
+        }
+
+        protected void CheckDisposed()
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(GetType().FullName);
+        }
+
+        protected abstract void DoDispose();
+    }
+}
