526ce1e0ccb499dfcc2eff99fc87c0bdede020a3
Steven Kirk <grokys@gmail.com>
2015-10-19 21:35:38 +0200

    Get the PerspexProperty type from the Style.

--- src/Markup/Perspex.Markup.Xaml/Converters/PerspexPropertyTypeConverter.cs
+++ src/Markup/Perspex.Markup.Xaml/Converters/PerspexPropertyTypeConverter.cs
@@ -25,30 +25,38 @@ namespace Perspex.Markup.Xaml.Converters
         public object ConvertFrom(IXamlTypeConverterContext context, CultureInfo culture, object value)
         {
             var s = (string)value;
-            var lastDot = s.LastIndexOf('.');
 
-            if (lastDot == -1)
+            string typeName;
+            string propertyName;
+            Type type;
+
+            ParseProperty(s, out typeName, out propertyName);
+
+            if (typeName == null)
             {
-                throw new NotSupportedException("PerspexProperties must currently be fully qualified.");
+                var styleType = context.TypeRepository.GetXamlType(typeof(Style));
+                var style = (Style)context.TopDownValueContext.GetLastInstance(styleType);
+                type = style.Selector.TargetType;
+
+                if (type == null)
+                {
+                    throw new XamlParseException(
+                        "Could not determine the target type. Please fully qualify the property name.");
+                }
             }
-
-            var typeName = s.Substring(0, lastDot);
-            var propertyName = s.Substring(lastDot + 1);
-            var type = context.TypeRepository.GetByQualifiedName(typeName)?.UnderlyingType;
-            var styleType = context.TypeRepository.GetXamlType(typeof(Style));
-
-            // ATTN: SuperJMN
-            //var style = ((XamlTypeConverterContext)context).TopDownValueContext.GetLastInstance(styleType);
-
-            if (type == null)
+            else
             {
-                throw new XamlParseException($"Could not find type '{typeName}'.");
+                type = context.TypeRepository.GetByQualifiedName(typeName)?.UnderlyingType;
+
+                if (type == null)
+                {
+                    throw new XamlParseException($"Could not find type '{typeName}'.");
+                }
             }
 
             // First look for non-attached property on the type and then look for an attached property.
-            var property = PerspexPropertyRegistry.Instance.GetRegistered(type)
-                .FirstOrDefault(x => x.Name == propertyName);
-
+            var property = PerspexPropertyRegistry.Instance.FindRegistered(type, s);
+            
             if (property == null)
             {
                 property = PerspexPropertyRegistry.Instance.GetAttached(type)
@@ -58,7 +66,7 @@ namespace Perspex.Markup.Xaml.Converters
             if (property == null)
             {
                 throw new XamlParseException(
-                    $"Could not find PerspexProperty '{typeName}'.{propertyName}.");
+                    $"Could not find PerspexProperty '{type.Name}.{propertyName}'.");
             }
 
             return property;
@@ -67,6 +75,26 @@ namespace Perspex.Markup.Xaml.Converters
         public object ConvertTo(IXamlTypeConverterContext context, CultureInfo culture, object value, Type destinationType)
         {
             throw new NotImplementedException();
+        }
+
+        private void ParseProperty(string s, out string typeName, out string propertyName)
+        {
+            var split = s.Split('.');
+
+            if (split.Length == 1)
+            {
+                typeName = null;
+                propertyName = split[0];
+            }
+            else if (split.Length == 2)
+            {
+                typeName = split[0];
+                propertyName = split[1];
+            }
+            else
+            {
+                throw new XamlParseException($"Invalid property name: '{s}'.");
+            }
         }
     }
 }
--- tests/Perspex.Markup.Xaml.UnitTests/Converters/PerspexPropertyConverterTest.cs
+++ tests/Perspex.Markup.Xaml.UnitTests/Converters/PerspexPropertyConverterTest.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) The Perspex Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
+using System;
 using Moq;
 using OmniXaml;
+using OmniXaml.ObjectAssembler.Commands;
 using OmniXaml.TypeConversion;
 using OmniXaml.Typing;
 using Perspex.Markup.Xaml.Converters;
+using Perspex.Styling;
 using Xunit;
 
 namespace Perspex.Markup.Xaml.UnitTests.Converters
@@ -25,6 +28,19 @@ namespace Perspex.Markup.Xaml.UnitTests.Converters
             var target = new PerspexPropertyTypeConverter();
             var context = CreateContext();
             var result = target.ConvertFrom(context, null, "Class1.Foo");
+
+            Assert.Equal(Class1.FooProperty, result);
+        }
+
+        [Fact]
+        public void ConvertFrom_Uses_Selector_TargetType()
+        {
+            var target = new PerspexPropertyTypeConverter();
+            var style = new Style(x => x.OfType<Class1>());
+            var context = CreateContext(style);
+            var result = target.ConvertFrom(context, null, "Foo");
+
+            Assert.Equal(Class1.FooProperty, result);
         }
 
         [Fact]
@@ -33,25 +49,50 @@ namespace Perspex.Markup.Xaml.UnitTests.Converters
             var target = new PerspexPropertyTypeConverter();
             var context = CreateContext();
             var result = target.ConvertFrom(context, null, "AttachedOwner.Attached");
+
+            Assert.Equal(AttachedOwner.AttachedProperty, result);
         }
 
-        private IXamlTypeConverterContext CreateContext()
+        private IXamlTypeConverterContext CreateContext(Style style = null)
         {
             var context = new Mock<IXamlTypeConverterContext>();
+            var topDownValueContext = new Mock<ITopDownValueContext>();
             var typeRepository = new Mock<IXamlTypeRepository>();
             var featureProvider = new Mock<ITypeFeatureProvider>();
             var class1XamlType = new XamlType(typeof(Class1), typeRepository.Object, null, featureProvider.Object);
             var attachedOwnerXamlType = new XamlType(typeof(AttachedOwner), typeRepository.Object, null, featureProvider.Object);
+            context.Setup(x => x.TopDownValueContext).Returns(topDownValueContext.Object);
             context.Setup(x => x.TypeRepository).Returns(typeRepository.Object);
+            topDownValueContext.Setup(x => x.GetLastInstance(It.IsAny<XamlType>())).Returns(style);
             typeRepository.Setup(x => x.GetByQualifiedName("Class1")).Returns(class1XamlType);
             typeRepository.Setup(x => x.GetByQualifiedName("AttachedOwner")).Returns(attachedOwnerXamlType);
             return context.Object;
         }
 
-        private class Class1 : PerspexObject
+        private class Class1 : PerspexObject, IStyleable
         {
             public static readonly PerspexProperty<string> FooProperty =
                 PerspexProperty.Register<Class1, string>("Foo");
+
+            public Classes Classes
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string Name
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public Type StyleKey
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public ITemplatedControl TemplatedParent
+            {
+                get { throw new NotImplementedException(); }
+            }
         }
 
         private class AttachedOwner
