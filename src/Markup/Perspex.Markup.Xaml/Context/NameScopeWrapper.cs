c8b772c4ce7e376ef1773dc00a7a211f2ac45b09
Steven Kirk <grokys@gmail.com>
2015-11-09 21:15:03 +0100

    Proof of concept for OmniXAML issue #57.

---
+++ src/Markup/Perspex.Markup.Xaml/Context/NameScopeWrapper.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) The Perspex Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+namespace Perspex.Markup.Xaml.Context
+{
+    internal class NameScopeWrapper : OmniXaml.INameScope
+    {
+        private Perspex.INameScope _inner;
+
+        public NameScopeWrapper(Perspex.INameScope inner)
+        {
+            _inner = inner;
+        }
+
+        public object Find(string name)
+        {
+            return _inner.Find(name);
+        }
+
+        public void Register(string name, object scopedElement)
+        {
+            _inner.Register(name, scopedElement);
+        }
+
+        public void Unregister(string name)
+        {
+            _inner.Unregister(name);
+        }
+    }
+}
