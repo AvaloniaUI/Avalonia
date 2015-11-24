a511eb57caa80f442cb5802a1b7bb042f2c0fcec
Steven Kirk <grokys@gmail.com>
2015-11-24 01:52:06 +0100

    Fixed skia backend broken by rename.

--- src/Skia/Perspex.Skia/StreamGeometryImpl.cs
+++ src/Skia/Perspex.Skia/StreamGeometryImpl.cs
@@ -99,7 +99,7 @@ namespace Perspex.Skia
                 _currentPoint = startPoint;
             }
 
-            public void BezierTo(Point point1, Point point2, Point point3)
+            public void CubicBezierTo(Point point1, Point point2, Point point3)
             {
                 _elements.Add(new SkiaGeometryElement
                 {
@@ -111,7 +111,7 @@ namespace Perspex.Skia
                 _currentPoint = point3;
             }
 
-            public void QuadTo(Point control, Point endPoint)
+            public void QuadraticBezierTo(Point control, Point endPoint)
             {
                 _elements.Add(new SkiaGeometryElement
                 {
