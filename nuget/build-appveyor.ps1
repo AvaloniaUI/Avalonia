e64e2f1c47edfe4881bb9c1c4f10ae76b4d51ef9
Nikita Tsukanov <keks9n@gmail.com>
2015-11-28 04:56:24 +0300

    Nuget

--- nuget/build-appveyor.ps1
+++ nuget/build-appveyor.ps1
@@ -9,7 +9,7 @@ sv version $env:APPVEYOR_BUILD_NUMBER
 sv version 9999.0.$version-nightly
 sv key $env:myget_key
 
-. "include.ps1"
+. ".\include.ps1"
 .\build-version.ps1 $version
 
 sv reponame $env:APPVEYOR_REPO_NAME
