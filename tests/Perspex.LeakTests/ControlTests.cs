4d13d1313c39c3aa2f4395e9eb26bd92cb96635d
Steven Kirk <grokys@gmail.com>
2015-11-29 17:11:15 +0100

    Added some memory leak unit tests.
    Using JetBrains' dotMemory Unit testing framework. Currently shows that
    TreeView does not get freed.

---
+++ tests/Perspex.LeakTests/ControlTests.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) The Perspex Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.dotMemoryUnit;
+using Perspex.Controls;
+using Perspex.Controls.Templates;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Perspex.LeakTests
+{
+    [DotMemoryUnit(FailIfRunWithoutSupport = false)]
+    public class ControlTests
+    {
+        public ControlTests(ITestOutputHelper atr)
+        {
+            TestApp.Initialize();
+            DotMemoryUnitTestOutput.SetOutputMethod(atr.WriteLine);
+        }
+
+        [Fact]
+        public void Canvas_Is_Freed()
+        {
+            Func<Window> run = () =>
+            {
+                var window = new Window
+                {
+                    Content = new Canvas()
+                };
+
+                // Do a layout and make sure that Canvas gets added to visual tree.
+                window.LayoutManager.ExecuteLayoutPass();
+                Assert.IsType<Canvas>(window.Presenter.Child);
+
+                // Clear the content and ensure the Canvas is removed.
+                window.Content = null;
+                window.LayoutManager.ExecuteLayoutPass();
+                Assert.Null(window.Presenter.Child);
+
+                return window;
+            };
+
+            var result = run();
+
+            dotMemory.Check(memory =>
+                Assert.Equal(0, memory.GetObjects(where => where.Type.Is<Canvas>()).ObjectsCount));
+        }
+
+        [Fact]
+        public void TreeView_Is_Freed()
+        {
+            Func<Window> run = () =>
+            {
+                var nodes = new[]
+                {
+                    new Node
+                    {
+                        Children = new[] { new Node() },
+                    }
+                };
+
+                TreeView target;
+
+                var window = new Window
+                {
+                    Content = target = new TreeView
+                    {
+                        DataTemplates = new DataTemplates
+                    {
+                        new FuncTreeDataTemplate<Node>(
+                            x => new TextBlock { Text = x.Name },
+                            x => x.Children,
+                            x => true)
+                    },
+                        Items = nodes
+                    }
+                };
+
+                // Do a layout and make sure that TreeViewItems get realized.
+                window.LayoutManager.ExecuteLayoutPass();
+                Assert.Equal(1, target.ItemContainerGenerator.Containers.Count());
+
+                // Clear the content and ensure the TreeView is removed.
+                window.Content = null;
+                window.LayoutManager.ExecuteLayoutPass();
+                Assert.Null(window.Presenter.Child);
+
+                return window;
+            };
+
+            var result = run();
+
+            dotMemory.Check(memory =>
+                Assert.Equal(0, memory.GetObjects(where => where.Type.Is<TreeView>()).ObjectsCount));
+        }
+
+        private class Node
+        {
+            public string Name { get; set; }
+            public IEnumerable<Node> Children { get; set; }
+        }
+    }
+}
---
+++ tests/Perspex.LeakTests/TestApp.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) The Perspex Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using Moq;
+using Perspex.Controls.UnitTests;
+using Perspex.Platform;
+using Perspex.Shared.PlatformSupport;
+using Perspex.Themes.Default;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+
+namespace Perspex.LeakTests
+{
+    internal class TestApp : Application
+    {
+        private TestApp()
+        {
+            RegisterServices();
+
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var windowImpl = new Mock<IWindowImpl>();
+            var renderInterface = fixture.Create<IPlatformRenderInterface>();
+
+            PerspexLocator.CurrentMutable
+                .Bind<IAssetLoader>().ToConstant(new AssetLoader())
+                .Bind<IPclPlatformWrapper>().ToConstant(new PclPlatformWrapper())
+                .Bind<IPlatformRenderInterface>().ToConstant(renderInterface)
+                .Bind<IWindowingPlatform>().ToConstant(new WindowingPlatformMock(() => windowImpl.Object));
+
+            Styles = new DefaultTheme();
+        }
+
+        public static void Initialize()
+        {
+            if (Current == null)
+            {
+                new TestApp();
+            }
+        }
+    }
+}
