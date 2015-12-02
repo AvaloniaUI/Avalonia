b9d32d86a823f2d127264e8c8df147c287847865
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-12-02 19:39:34 +0100

    Moved UnitTests to new projects

---
+++ tests/Perspex.Xaml.Interactivity.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) The Perspex Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System.Reflection;
+using Xunit;
+
+[assembly: AssemblyTitle("Perspex.Xaml.Interactivity.UnitTests")]
+
+// Don't run tests in parallel.
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
