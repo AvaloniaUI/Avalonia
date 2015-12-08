7a08eb9df5269cb9d54d44da2651f21e066dbfab
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-12-08 19:35:34 +0100

    Added EventTriggerBehavior test using code behind

--- samples/XamlTestApplicationPcl/Views/MainWindow.cs
+++ samples/XamlTestApplicationPcl/Views/MainWindow.cs
@@ -5,6 +5,7 @@ using Perspex.Controls;
 using Perspex.Controls.Shapes;
 using Perspex.Diagnostics;
 using Perspex.Markup.Xaml;
+using Perspex.Xaml.Interactions.Core;
 using Perspex.Xaml.Interactivity;
 using XamlTestApplication.Behaviors;
 using XamlTestApplication.ViewModels;
@@ -19,10 +20,55 @@ namespace XamlTestApplication.Views
             DataContext = new MainWindowViewModel();
             DevTools.Attach(this);
 
-            // TODO: Remove, only temporary for test of DragPositionBehavior
+            //
+            // TODO: Remove code below used only for temporary test of DragPositionBehavior
+            //
+
+            /*
+            <Interactivity:Interaction.Behaviors>
+                <behaviors:DragPositionBehavior/>
+            </Interactivity:Interaction.Behaviors>
+            */
+
             Interaction.SetBehaviors(
                 this.FindControl<Ellipse>("dragEllipse"), 
                 new BehaviorCollection { new DragPositionBehavior() });
+
+            //
+            // TODO: Remove code below used only for temporary test of EventTriggerBehavior
+            //
+
+            /*
+            <Interactivity:Interaction.Behaviors>
+                <Interactions:EventTriggerBehavior EventName="Click" SourceObject="{Binding ElementName=button}">
+                    <Interactions:ChangePropertyAction TargetObject="{Binding ElementName=DataTriggerText}" PropertyName="Text" Value="Hello!"/>
+                </Interactions:EventTriggerBehavior>
+            </Interactivity:Interaction.Behaviors>
+            */
+
+            var button = this.FindControl<Button>("button");
+
+            var eventTriggerBehavior = new EventTriggerBehavior()
+            {
+                EventName = "Click",
+                SourceObject = button
+            };
+
+            eventTriggerBehavior.SetValue(
+                EventTriggerBehavior.ActionsProperty,
+                new ActionCollection
+                {
+                    new ChangePropertyAction()
+                    {
+                        TargetObject = this.FindControl<TextBox>("DataTriggerText"),
+                        PropertyName = "Text",
+                        Value = "Hello!"
+                    }
+                });
+
+            Interaction.SetBehaviors(
+                button,
+                new BehaviorCollection { eventTriggerBehavior });
         }
 
         private void InitializeComponent()
