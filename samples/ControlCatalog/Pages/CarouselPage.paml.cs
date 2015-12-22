bbe0f5a7a7909620cf4bcc9e96a345f4cbb980b3
Steven Kirk <grokys@gmail.com>
2015-12-22 22:37:40 +0000

    Added control catalog.
    Ported from old branch to incorporate latest master.

---
+++ samples/ControlCatalog/Pages/CarouselPage.paml.cs
@@ -0,0 +1,22 @@
+﻿using Perspex.Controls;
+using Perspex.Markup.Xaml;
+
+namespace ControlCatalog.Pages
+{
+    public class CarouselPage : UserControl
+    {
+        private Carousel carousel;
+        private Button left;
+        private Button right;
+
+        public CarouselPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            PerspexXamlLoader.Load(this);
+        }
+    }
+}
