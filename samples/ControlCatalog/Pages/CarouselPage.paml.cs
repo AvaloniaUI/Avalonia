ce1ef2fd892a7133123a01b83af3806952418f4c
Steven Kirk <grokys@gmail.com>
2015-12-24 00:55:41 +0000

    Make Carousel demo buttons work.

--- samples/ControlCatalog/Pages/CarouselPage.paml.cs
+++ samples/ControlCatalog/Pages/CarouselPage.paml.cs
@@ -5,18 +5,23 @@ namespace ControlCatalog.Pages
 {
     public class CarouselPage : UserControl
     {
-        private Carousel carousel;
-        private Button left;
-        private Button right;
+        private Carousel _carousel;
+        private Button _left;
+        private Button _right;
 
         public CarouselPage()
         {
             this.InitializeComponent();
+            _left.Click += (s, e) => _carousel.Previous();
+            _right.Click += (s, e) => _carousel.Next();
         }
 
         private void InitializeComponent()
         {
             PerspexXamlLoader.Load(this);
+            _carousel = this.FindControl<Carousel>("carousel");
+            _left = this.FindControl<Button>("left");
+            _right = this.FindControl<Button>("right");
         }
     }
 }
