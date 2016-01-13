fd368713324a0ba18618a5fd11c5831bd2700dbd
Steven Kirk <grokys@gmail.com>
2016-01-13 21:19:08 +0100

    Make carousel page transition changable in catalog.

--- samples/ControlCatalog/Pages/CarouselPage.paml.cs
+++ samples/ControlCatalog/Pages/CarouselPage.paml.cs
@@ -1,4 +1,6 @@
-﻿using Perspex.Controls;
+﻿using System;
+using Perspex.Animation;
+using Perspex.Controls;
 using Perspex.Markup.Xaml;
 
 namespace ControlCatalog.Pages
@@ -8,12 +10,14 @@ namespace ControlCatalog.Pages
         private Carousel _carousel;
         private Button _left;
         private Button _right;
+        private DropDown _transition;
 
         public CarouselPage()
         {
             this.InitializeComponent();
             _left.Click += (s, e) => _carousel.Previous();
             _right.Click += (s, e) => _carousel.Next();
+            _transition.SelectionChanged += TransitionChanged;
         }
 
         private void InitializeComponent()
@@ -22,6 +26,23 @@ namespace ControlCatalog.Pages
             _carousel = this.FindControl<Carousel>("carousel");
             _left = this.FindControl<Button>("left");
             _right = this.FindControl<Button>("right");
+            _transition = this.FindControl<DropDown>("transition");
+        }
+
+        private void TransitionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (_transition.SelectedIndex)
+            {
+                case 0:
+                    _carousel.Transition = null;
+                    break;
+                case 1:
+                    _carousel.Transition = new PageSlide(TimeSpan.FromSeconds(0.25));
+                    break;
+                case 2:
+                    _carousel.Transition = new CrossFade(TimeSpan.FromSeconds(0.25));
+                    break;
+            }
         }
     }
 }
