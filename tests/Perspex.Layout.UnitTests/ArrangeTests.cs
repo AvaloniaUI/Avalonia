f7f2fc65b7107fb1cb1b7ef39fa12389a2185ca4
Steven Kirk <grokys@gmail.com>
2016-02-01 16:02:27 +0100

    Make Arrange call Measure if !IsMeasureValid.
    To follow WPF. Also removed force parameter from Arrange and fixed
    Decorator measure invalidation.

---
+++ tests/Perspex.Layout.UnitTests/ArrangeTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) The Perspex Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using Perspex.Controls;
+using Xunit;
+
+namespace Perspex.Layout.UnitTests
+{
+    public class ArrangeTests
+    {
+        [Fact]
+        public void Arrange_With_IsMeasureValid_False_Calls_Measure()
+        {
+            var target = new TestControl();
+
+            Assert.False(target.IsMeasureValid);
+            target.Arrange(new Rect(0, 0, 120, 120));
+            Assert.True(target.IsMeasureValid);
+            Assert.Equal(new Size(120, 120), target.MeasureConstraint);
+        }
+
+        private class TestControl : Border
+        {
+            public Size MeasureConstraint { get; private set; }
+
+            protected override Size MeasureOverride(Size constraint)
+            {
+                MeasureConstraint = constraint;
+                return base.MeasureOverride(constraint);
+            }
+        }
+    }
+}
