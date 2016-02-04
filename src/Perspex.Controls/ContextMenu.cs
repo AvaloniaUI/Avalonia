d2a6b8943415e588001859f59b907c6e4aed706e
danwalmsley <dan@walms.co.uk>
2016-02-04 10:44:25 +0000

    Added initial ContextMenu attachedProperty.

---
+++ src/Perspex.Controls/ContextMenu.cs
@@ -0,0 +1,20 @@
+﻿namespace Perspex.Controls
+{
+    using Primitives;
+
+    public class ContextMenu : SelectingItemsControl
+    {
+        public static readonly AttachedProperty<ContextMenu> MenuProperty =
+       PerspexProperty.RegisterAttached<ContextMenu, TextBlock, ContextMenu>("Menu");
+
+        public static ContextMenu GetMenu(TextBlock element)
+        {
+            return element.GetValue(MenuProperty);
+        }
+
+        public static void SetMenu(TextBlock element, ContextMenu value)
+        {
+            element.SetValue(MenuProperty, value);
+        }
+    }
+}
