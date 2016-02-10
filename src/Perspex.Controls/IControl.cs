b68c417f37fc717888c2994bdb466993b4e1b534
Steven Kirk <grokys@gmail.com>
2016-02-10 21:44:22 +0100

    Expose logical tree events on IControl.

--- src/Perspex.Controls/IControl.cs
+++ src/Perspex.Controls/IControl.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) The Perspex Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
+using System;
 using Perspex.Controls.Templates;
 using Perspex.Input;
 using Perspex.Layout;
@@ -13,6 +14,16 @@ namespace Perspex.Controls
     /// </summary>
     public interface IControl : IVisual, ILogical, ILayoutable, IInputElement, INamed, IStyleable, IStyleHost
     {
+        /// <summary>
+        /// Raised when the control is attached to a rooted logical tree.
+        /// </summary>
+        event EventHandler<LogicalTreeAttachmentEventArgs> AttachedToLogicalTree;
+
+        /// <summary>
+        /// Raised when the control is detached from a rooted logical tree.
+        /// </summary>
+        event EventHandler<LogicalTreeAttachmentEventArgs> DetachedFromLogicalTree;
+
         /// <summary>
         /// Gets or sets the control's styling classes.
         /// </summary>
