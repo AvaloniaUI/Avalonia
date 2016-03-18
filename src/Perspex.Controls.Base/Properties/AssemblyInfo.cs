3306c81de2565d52fcb3400cc04b840cac6b2f1f
Jeremy Koritzinsky <jkoritzinsky@live.com>
2016-03-18 16:51:47 -0500

    Fixed references and started work on fixing unit tests.

--- src/Perspex.Controls.Base/Properties/AssemblyInfo.cs
+++ src/Perspex.Controls.Base/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) The Perspex Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
+using Perspex.Metadata;
 using System.Reflection;
 
 [assembly: AssemblyTitle("Perspex.Controls.Base")]
+
+[assembly: XmlnsDefinition("https://github.com/perspex", "Perspex.Controls.Templates")]
