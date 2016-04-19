ab3237a92310f6d438a8bcacc77800698ac5171d
Dan Walmsley <dan@walms.co.uk>
2016-04-19 22:13:00 +0100

    repro commit for itemscontrol causing memory leak.

---
+++ samples/XamlTestApplicationPcl/ViewModels/EditorViewModel.cs
@@ -0,0 +1,73 @@
+﻿namespace XamlTestApplication.ViewModels
+{
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ShellViewModel : ReactiveObject
+    {
+        private ShellViewModel()
+        {
+            documents = new ObservableCollection<EditorViewModel>();
+
+            AddDocumentCommand = ReactiveCommand.Create();
+            AddDocumentCommand.Subscribe(_ =>
+            {
+                Documents.Add(new EditorViewModel());
+            });
+        }
+
+        public static ShellViewModel Instance = new ShellViewModel();
+
+        private ObservableCollection<EditorViewModel> documents;
+        public ObservableCollection<EditorViewModel> Documents
+        {
+            get { return documents; }
+            set { this.RaiseAndSetIfChanged(ref documents, value); }
+        }
+
+        private EditorViewModel selectedDocument;
+
+        public EditorViewModel SelectedDocument
+        {
+            get { return selectedDocument; }
+            set { this.RaiseAndSetIfChanged(ref selectedDocument, value); }
+        }
+
+
+        public ReactiveCommand<object> AddDocumentCommand { get; }
+    }
+
+    public class EditorViewModel : ReactiveObject
+    {
+        public EditorViewModel()
+        {
+            text = "This is some text.";
+
+            CloseCommand = ReactiveCommand.Create();
+
+            CloseCommand.Subscribe(_ =>
+            {
+                ShellViewModel.Instance.Documents.Remove(this);
+            });
+        }
+
+        ~EditorViewModel()
+        {
+
+        }
+
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { this.RaiseAndSetIfChanged(ref text, value); }
+        }
+
+        public ReactiveCommand<object> CloseCommand { get; }
+    }
+}
