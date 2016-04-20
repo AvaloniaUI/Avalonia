8fa4b1b5966e3f850498fe515d8b236af2759086
Jeremy Koritzinsky <jkoritzinsky@live.com>
2016-04-20 13:14:47 -0500

    Added documentation to ValidatingPropertyAccessorBase

--- src/Markup/Perspex.Markup/Data/Plugins/ValidatingPropertyAccessorBase.cs
+++ src/Markup/Perspex.Markup/Data/Plugins/ValidatingPropertyAccessorBase.cs
@@ -3,6 +3,10 @@ using Perspex.Data;
 
 namespace Perspex.Markup.Data.Plugins
 {
+
+    /// <summary>
+    /// A base class for validating <see cref="IPropertyAccessor"/>s that wraps an <see cref="IPropertyAccessor"/> and forwards method calls to it.
+    /// </summary>
     public abstract class ValidatingPropertyAccessorBase : IPropertyAccessor
     {
         protected readonly WeakReference _reference;
@@ -18,14 +22,22 @@ namespace Perspex.Markup.Data.Plugins
             _callback = callback;
         }
 
+        /// <inheritdoc/>
         public Type PropertyType => _accessor.PropertyType;
 
+        /// <inheritdoc/>
         public object Value => _accessor.Value;
 
+        /// <inheritdoc/>
         public virtual void Dispose() => _accessor.Dispose();
 
+        /// <inheritdoc/>
         public virtual bool SetValue(object value, BindingPriority priority) => _accessor.SetValue(value, priority);
 
+        /// <summary>
+        /// Sends the validation status to the callback specified in construction.
+        /// </summary>
+        /// <param name="status">The validation status.</param>
         protected void SendValidationCallback(ValidationStatus status)
         {
             _callback?.Invoke(status);
