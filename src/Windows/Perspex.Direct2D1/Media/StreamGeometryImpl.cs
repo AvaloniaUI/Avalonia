424d29e3ee12de58fc65d5a80c8fb52f6121f260
donandren <donandren@gmail.com>
2016-05-09 15:50:20 +0300

    fixed some build errors if SharpDX StreamGeometryContextImpl with class names

--- src/Windows/Perspex.Direct2D1/Media/StreamGeometryImpl.cs
+++ src/Windows/Perspex.Direct2D1/Media/StreamGeometryImpl.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) The Perspex Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
-using Perspex.Media;
 using Perspex.Platform;
 using SharpDX.Direct2D1;
 using D2DGeometry = SharpDX.Direct2D1.Geometry;
