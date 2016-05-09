90efeeeb186a0b5b4b7af9ee74ab11d2cea7e8c6
Steven Kirk <grokys@github.com>
2016-05-09 20:07:35 +0200

    Added TODO doc.

--- src/Markup/Perspex.Markup.Xaml/PerspexXamlLoader.cs
+++ src/Markup/Perspex.Markup.Xaml/PerspexXamlLoader.cs
@@ -45,6 +45,13 @@ namespace Perspex.Markup.Xaml
         /// <summary>
         /// Gets the URI of the XAML file currently being loaded.
         /// </summary>
+        /// <remarks>
+        /// TODO: Making this internal for now as I'm not sure that this is the correct
+        /// thing to do, but its needd by <see cref="StyleInclude"/> to get the URL of
+        /// the currently loading XAML file, as we can't use the OmniXAML parsing context
+        /// there. Maybe we need a way to inject OmniXAML context into the objects it's
+        /// constructing?
+        /// </remarks>
         internal static Uri UriContext => s_uriStack.Count > 0 ? s_uriStack.Peek() : null;
 
         /// <summary>
