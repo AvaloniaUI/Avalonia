dd2e5721a669aca7df84bbd5535c2cf20a3c6527
Steven Kirk <grokys@users.noreply.github.com>
2016-05-12 00:47:07 +0200

    Removed "nightly" postfix.

--- nuget/build-appveyor.ps1
+++ nuget/build-appveyor.ps1
@@ -7,7 +7,7 @@ Push-Location $dir
 sv version $env:APPVEYOR_BUILD_NUMBER
 #sv version "1-debug"
 
-sv version 0.2.0.$version-nightly-alpha
+sv version 0.2.0.$version-alpha
 sv key $env:myget_key
 
 . ".\include.ps1"
