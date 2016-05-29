5bbdfc55b4398a8d748e6561991a7ed70f614e2a
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2016-05-29 12:26:48 -0400

    Added a render test.

---
+++ tests/Avalonia.RenderTests/OpacityMaskTests.cs
@@ -0,0 +1,64 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+#if AVALONIA_CAIRO
+namespace Avalonia.Cairo.RenderTests
+#elif AVALONIA_SKIA
+namespace Avalonia.Skia.RenderTests
+#else
+namespace Avalonia.Direct2D1.RenderTests
+#endif
+{
+    public class OpacityMaskTests : TestBase
+    {
+        public OpacityMaskTests()
+            : base("OpacityMask")
+        {
+        }
+
+#if AVALONIA_SKIA
+        [Fact(Skip = "Opacity Masks not supported on Skia")]
+#else
+        [Fact]
+#endif
+        public void Opacity_Mask_Masks_Element()
+        {
+            var target = new Canvas
+            {
+                OpacityMask = new LinearGradientBrush
+                {
+                    StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
+                    EndPoint = new RelativePoint(1, 1, RelativeUnit.Relative),
+                    GradientStops = new List<GradientStop>
+                    {
+                        new GradientStop(Color.FromUInt32(0xffffffff), 0),
+                        new GradientStop(Color.FromUInt32(0x00ffffff), 1)
+                    }
+                },
+                Width = 76,
+                Height = 76,
+                Children = new Avalonia.Controls.Controls
+                {
+                    new Path
+                    {
+                        Width = 32,
+                        Height = 40,
+                        [Canvas.LeftProperty] = 23,
+                        [Canvas.TopProperty] = 18,
+                        Stretch = Stretch.Fill,
+                        Fill = Brushes.Red,
+                        Data = StreamGeometry.Parse("F1 M 27,18L 23,26L 33,30L 24,38L 33,46L 23,50L 27,58L 45,58L 55,38L 45,18L 27,18 Z")
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+    }
+}
