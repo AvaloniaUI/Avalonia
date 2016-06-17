cd863434be087a72772b4ed2f82feaf5f80b0796
Steven Kirk <grokys@gmail.com>
2016-06-17 16:14:05 +0200

    Fix TreeDataTemplate.
    Was calling wrong overload.

--- src/Markup/Avalonia.Markup.Xaml/Templates/TreeDataTemplate.cs
+++ src/Markup/Avalonia.Markup.Xaml/Templates/TreeDataTemplate.cs
@@ -43,7 +43,7 @@ namespace Avalonia.Markup.Xaml.Templates
             if (ItemsSource != null)
             {
                 var obs = new ExpressionObserver(item, ItemsSource.Path);
-                return new InstancedBinding(obs, BindingPriority.Style);
+                return new InstancedBinding(obs, BindingMode.OneWay, BindingPriority.Style);
             }
 
             return null;
