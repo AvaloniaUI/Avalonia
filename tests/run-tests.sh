3bee859be6f35125bcd30d996b0a3c2c42d3b700
Steven Kirk <grokys@gmail.com>
2016-08-05 00:53:35 +0200

    Excluded D2D test from bash test runner.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,9 +3,15 @@
 cd "$(dirname "$0")"
 
 tests=(Avalonia.*.UnitTests/)
+exclude=("*Direct2D*/")
 result=0
 
-for test in "${tests[@]}"; do
+for del in ${exclude[@]}; do
+    tests=(${tests[@]/$del})
+done
+
+for test in ${tests[@]}; do
+    echo Running test $test
     mono ../testrunner/xunit.runner.console.2.1.0/tools/xunit.console.exe ${test}bin/Release/${test%/}.dll  -parallel none
 
     if [ $? -ne 0 ]; then result=1 ; fi
