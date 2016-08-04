bc16abc257e0516022aa151b6f1b5fed6e8ca2ac
Steven Kirk <grokys@gmail.com>
2016-08-05 00:20:50 +0200

    Set return value from run-tests.sh script.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,7 +3,12 @@
 cd "$(dirname "$0")"
 
 tests=(Avalonia.*.UnitTests/)
+result=0
 
 for test in "${tests[@]}"; do
     mono ../testrunner/xunit.runner.console.2.1.0/tools/xunit.console.exe ${test}bin/Release/${test%/}.dll  -parallel none
+
+    if [ $? -ne 0 ]; then result=1 ; fi
 done
+
+exit $result
