369c6a99ddd263a944958b87be60d1cf866c4187
Steven Kirk <grokys@gmail.com>
2016-08-20 19:52:36 +0200

    WIP on templated TabControl.

--- samples/ControlCatalog/Pages/TabControlPage.xaml.cs
+++ samples/ControlCatalog/Pages/TabControlPage.xaml.cs
@@ -8,11 +8,40 @@ namespace ControlCatalog.Pages
         public TabControlPage()
         {
             this.InitializeComponent();
+
+            DataContext = new[]
+            {
+                new TabItemViewModel
+                {
+                    Header = "Arch",
+                    Text = "This is the first templated tab page.",
+                    ImageUrl = "resm:ControlCatalog.Assets.delicate-arch-896885_640.jpg",
+                },
+                new TabItemViewModel
+                {
+                    Header = "Leaf",
+                    Text = "This is the second templated tab page.",
+                    ImageUrl = "resm:ControlCatalog.Assets.maple-leaf-888807_640.jpg",
+                },
+                new TabItemViewModel
+                {
+                    Header = "Disabled",
+                    Text = "You should not see this.",
+                },
+            };
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private class TabItemViewModel
+        {
+            public string Header { get; set; }
+            public string Text { get; set; }
+            public string ImageUrl { get; set; }
+            public bool IsEnabled { get; set; }
         }
     }
 }
