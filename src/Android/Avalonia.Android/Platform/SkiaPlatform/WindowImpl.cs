dec5682dce412f93b3f3bd85c4a697c726e4af78
Dan Walmsley <dan@walms.co.uk>
2016-11-13 23:26:33 +0000

    Completed Andriod IWindowImpl

--- src/Android/Avalonia.Android/Platform/SkiaPlatform/WindowImpl.cs
+++ src/Android/Avalonia.Android/Platform/SkiaPlatform/WindowImpl.cs
@@ -107,6 +107,12 @@ namespace Avalonia.Android.Platform.SkiaPlatform
         public void SetSystemDecorations(bool enabled)
         {
         }
+
+        public void SetIgnoreTaskBarWhenMaximized(bool enable)
+        {
+            //Not supported
+        }
+
         public void Invalidate(Rect rect)
         {
             if (Holder?.Surface?.IsValid == true) base.Invalidate();
