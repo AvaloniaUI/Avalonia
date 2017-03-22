fdcc4be654d6e9b24bb13efb885a29c1492ebd60
Nikita Tsukanov <keks9n@gmail.com>
2017-03-22 15:08:14 +0300

    Handle console input in fbdev mode

--- samples/ControlCatalog.NetCore/Program.cs
+++ samples/ControlCatalog.NetCore/Program.cs
@@ -8,13 +8,22 @@ namespace ControlCatalog.NetCore
     {
         static void Main(string[] args)
         {
-            if (args.Contains("--fbdev"))
-                AppBuilder.Configure<App>()
-                    .InitializeWithLinuxFramebuffer(tl => tl.Content = new MainView());
+            if (args.Contains("--fbdev")) AppBuilder.Configure<App>().InitializeWithLinuxFramebuffer(tl =>
+            {
+                tl.Content = new MainView();
+                System.Threading.ThreadPool.QueueUserWorkItem(_ => ConsoleSilencer());
+            });
             else
                 AppBuilder.Configure<App>()
                     .UsePlatformDetect()
                     .Start<MainWindow>();
+        }
+
+        static void ConsoleSilencer()
+        {
+            Console.CursorVisible = false;
+            while (true)
+                Console.ReadKey();
         }
     }
 }
