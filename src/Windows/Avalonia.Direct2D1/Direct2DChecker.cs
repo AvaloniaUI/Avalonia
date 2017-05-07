804d1b66a192fe98b75309974d9f9f3316081195
Steven Kirk <grokys@gmail.com>
2017-05-07 18:58:51 +0200

    Always allow D2D for the moment.

--- src/Windows/Avalonia.Direct2D1/Direct2DChecker.cs
+++ src/Windows/Avalonia.Direct2D1/Direct2DChecker.cs
@@ -10,20 +10,6 @@ namespace Avalonia.Direct2D1
     class Direct2DChecker : IModuleEnvironmentChecker
     {
         //Direct2D backend doesn't work on some machines anymore
-        public bool IsCompatible
-        {
-            get
-            {
-                try
-                {
-                    Direct2D1Platform.InitializeDirect2D();
-                    return true;
-                }
-                catch
-                {
-                    return false;
-                }
-            }
-        }
+        public bool IsCompatible => true;
     }
 }
