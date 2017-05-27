3d76a5040cedb0f50b3fa2b06d417d906fdee6a7
Steven Kirk <grokys@gmail.com>
2017-05-27 19:09:44 +0200

    Port Direct2D code from master.

--- src/Windows/Avalonia.Direct2D1/Direct2DChecker.cs
+++ src/Windows/Avalonia.Direct2D1/Direct2DChecker.cs
@@ -10,6 +10,20 @@ namespace Avalonia.Direct2D1
     class Direct2DChecker : IModuleEnvironmentChecker
     {
         //Direct2D backend doesn't work on some machines anymore
-        public bool IsCompatible => true;
+        public bool IsCompatible
+        {
+            get
+            {
+                try
+                {
+                    Direct2D1Platform.InitializeDirect2D();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
