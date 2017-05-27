7e1efb33e2d8a7b87223450948cfc718e2f979de
Steven Kirk <grokys@gmail.com>
2017-05-27 11:45:31 +0200

    Added failing render test.
    Deferred rendering does not yet support clipping in `Visual.Render`.

--- tests/Avalonia.RenderTests/Controls/CustomRenderTests.cs
+++ tests/Avalonia.RenderTests/Controls/CustomRenderTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) The Avalonia Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
+using System;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Layout;
@@ -23,6 +24,35 @@ namespace Avalonia.Direct2D1.RenderTests.Controls
         }
 
         [Fact]
+        public async Task Clip()
+        {
+            Decorator target = new Decorator
+            {
+                Padding = new Thickness(8),
+                Width = 200,
+                Height = 200,
+                Child = new CustomRenderer((control, context) =>
+                {
+                    context.FillRectangle(
+                        Brushes.Red,
+                        control.Bounds,
+                        4);
+
+                    using (context.PushClip(control.Bounds.Deflate(20)))
+                    {
+                        context.FillRectangle(
+                            Brushes.Blue,
+                            control.Bounds,
+                            4);
+                    }
+                }),
+            };
+
+            await RenderToFile(target);
+            CompareImages();
+        }
+
+        [Fact]
         public async Task Opacity()
         {
             Decorator target = new Decorator
@@ -30,30 +60,32 @@ namespace Avalonia.Direct2D1.RenderTests.Controls
                 Padding = new Thickness(8),
                 Width = 200,
                 Height = 200,
-                Child = new OpacityRenderer(),
+                Child = new CustomRenderer((control, context) =>
+                {
+                    context.FillRectangle(
+                        Brushes.Red,
+                        control.Bounds,
+                        4);
+
+                    using (context.PushOpacity(0.5))
+                    {
+                        context.FillRectangle(
+                            Brushes.Blue,
+                            control.Bounds.Deflate(20),
+                            4);
+                    }
+                }),
             };
 
             await RenderToFile(target);
             CompareImages();
         }
 
-        class OpacityRenderer : Control
+        class CustomRenderer : Control
         {
-            public override void Render(DrawingContext context)
-            {
-                context.FillRectangle(
-                    Brushes.Red,
-                    Bounds,
-                    4);
-
-                using (context.PushOpacity(0.5))
-                {
-                    context.FillRectangle(
-                        Brushes.Blue,
-                        Bounds.Deflate(20),
-                        4);
-                }
-            }
+            private Action<CustomRenderer, DrawingContext> _render;
+            public CustomRenderer(Action<CustomRenderer, DrawingContext> render) => _render = render;
+            public override void Render(DrawingContext context) => _render(this, context);
         }
     }
 }
