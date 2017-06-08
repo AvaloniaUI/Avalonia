13822754a9a8f568d5a9ade37c5679c9e169105d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-06-08 14:28:46 +0200

    Fix duplicate 'CollectionBehavior' attribute

--- tests/Avalonia.Base.UnitTests/Properties/AssemblyInfo.cs
+++ tests/Avalonia.Base.UnitTests/Properties/AssemblyInfo.cs
@@ -7,5 +7,4 @@ using Xunit;
 [assembly: AssemblyTitle("Avalonia.UnitTests")]
 
 // Don't run tests in parallel.
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
 [assembly: CollectionBehavior(MaxParallelThreads = 1)]
