ca3cc808811f36f4bdf191f99f388d5b4124ebd7
Steven Kirk <grokys@gmail.com>
2017-08-09 01:13:43 +0200

    Fix non-async render test.

--- tests/Avalonia.RenderTests/Controls/TextBlockTests.cs
+++ tests/Avalonia.RenderTests/Controls/TextBlockTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) The Avalonia Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Layout;
 using Avalonia.Media;
@@ -22,7 +23,7 @@ namespace Avalonia.Direct2D1.RenderTests.Controls
         }
 
         [Fact]
-        public void Wrapping_NoWrap()
+        public async Task Wrapping_NoWrap()
         {
             Decorator target = new Decorator
             {
@@ -40,7 +41,7 @@ namespace Avalonia.Direct2D1.RenderTests.Controls
                 }
             };
 
-            RenderToFile(target);
+            await RenderToFile(target);
             CompareImages();
         }
     }
