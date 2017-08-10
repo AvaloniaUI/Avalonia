dc6833c10c32af3c49776357111a10ec6601e36f
Steven Kirk <grokys@gmail.com>
2017-08-10 11:02:40 +0200

    Don't set output dir for render tests.
    It was needed for OpenCover but that's been disabled now.

--- tests/Avalonia.RenderTests/TestBase.cs
+++ tests/Avalonia.RenderTests/TestBase.cs
@@ -53,11 +53,11 @@ namespace Avalonia.Direct2D1.RenderTests
         public TestBase(string outputPath)
         {
 #if AVALONIA_CAIRO
-            string testFiles = Path.GetFullPath(@"..\..\..\tests\TestFiles\Cairo");
+            string testFiles = Path.GetFullPath(@"..\..\..\..\..\tests\TestFiles\Cairo");
 #elif AVALONIA_SKIA
-            string testFiles = Path.GetFullPath(@"..\..\..\tests\TestFiles\Skia");
+            string testFiles = Path.GetFullPath(@"..\..\..\..\..\tests\TestFiles\Skia");
 #else
-            string testFiles = Path.GetFullPath(@"..\..\..\tests\TestFiles\Direct2D1");
+            string testFiles = Path.GetFullPath(@"..\..\..\..\..\tests\TestFiles\Direct2D1");
 #endif
             OutputPath = Path.Combine(testFiles, outputPath);
 
