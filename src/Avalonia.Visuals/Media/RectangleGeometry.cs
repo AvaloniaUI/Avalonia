f05cb39a36bdd8226005a652c97db5c31838894c
Eli Arbel <aelij@arbel.net>
2017-08-20 15:00:18 +0300

    Allow Geometry classes to be instantiated in XAML

--- src/Avalonia.Visuals/Media/RectangleGeometry.cs
+++ src/Avalonia.Visuals/Media/RectangleGeometry.cs
@@ -11,15 +11,50 @@ namespace Avalonia.Media
     public class RectangleGeometry : Geometry
     {
         /// <summary>
+        /// Defines the <see cref="Rect"/> property.
+        /// </summary>
+        public static readonly StyledProperty<Rect> RectProperty =
+            AvaloniaProperty.Register<RectangleGeometry, Rect>(nameof(Rect));
+
+        public Rect Rect
+        {
+            get => GetValue(RectProperty);
+            set => SetValue(RectProperty, value);
+        }
+
+        static RectangleGeometry()
+        {
+            RectProperty.Changed.AddClassHandler<RectangleGeometry>(x => x.RectChanged);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RectangleGeometry"/> class.
+        /// </summary>
+        public RectangleGeometry()
+        {
+            IPlatformRenderInterface factory = AvaloniaLocator.Current.GetService<IPlatformRenderInterface>();
+            PlatformImpl = factory.CreateStreamGeometry();
+        }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="RectangleGeometry"/> class.
         /// </summary>
         /// <param name="rect">The rectangle bounds.</param>
-        public RectangleGeometry(Rect rect)
+        public RectangleGeometry(Rect rect) : this()
         {
-            IPlatformRenderInterface factory = AvaloniaLocator.Current.GetService<IPlatformRenderInterface>();
-            IStreamGeometryImpl impl = factory.CreateStreamGeometry();
+            Rect = rect;
+        }
 
-            using (IStreamGeometryContextImpl context = impl.Open())
+        /// <inheritdoc/>
+        public override Geometry Clone()
+        {
+            return new RectangleGeometry(Rect);
+        }
+
+        private void RectChanged(AvaloniaPropertyChangedEventArgs e)
+        {
+            var rect = (Rect)e.NewValue;
+            using (var context = ((IStreamGeometryImpl)PlatformImpl).Open())
             {
                 context.BeginFigure(rect.TopLeft, true);
                 context.LineTo(rect.TopRight);
@@ -27,14 +62,6 @@ namespace Avalonia.Media
                 context.LineTo(rect.BottomLeft);
                 context.EndFigure(true);
             }
-
-            PlatformImpl = impl;
-        }
-
-        /// <inheritdoc/>
-        public override Geometry Clone()
-        {
-            return new RectangleGeometry(Bounds);
         }
     }
 }
