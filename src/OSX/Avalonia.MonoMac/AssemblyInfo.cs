b909380909d33891f655d95664d47854914763ff
Nikita Tsukanov <keks9n@gmail.com>
2017-09-13 16:47:57 +0300

    [MONOMAC] Fixed assembly info

--- src/OSX/Avalonia.MonoMac/AssemblyInfo.cs
+++ src/OSX/Avalonia.MonoMac/AssemblyInfo.cs
@@ -2,4 +2,5 @@
 using System.Collections.Generic;
 using System.Text;using Avalonia.MonoMac;
 using Avalonia.Platform;
-[assembly: ExportWindowingSubsystem(OperatingSystemType.OSX, 1, "MonoMac", typeof(MonoMacPlatform), nameof(MonoMacPlatform.Initialize))]]
+[assembly: ExportWindowingSubsystem(OperatingSystemType.OSX, 1, "MonoMac", typeof(MonoMacPlatform), nameof(MonoMacPlatform.Initialize))]
+
