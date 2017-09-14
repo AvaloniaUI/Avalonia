562381eecb2f024707e180a5613b0f48a3a0cbe9
Jurjen Biewenga <j.f.biewenga@gmail.com>
2017-09-14 13:58:45 +0200

    Added caching to win32 implementation

--- src/Windows/Avalonia.Win32/ScreenImpl.cs
+++ src/Windows/Avalonia.Win32/ScreenImpl.cs
@@ -18,39 +18,52 @@ namespace Avalonia.Win32
             get => GetSystemMetrics(SystemMetric.SM_CMONITORS);
         }
 
+        private Screen[] allScreens;
         public Screen[] AllScreens
         {
             get
             {
+                if (allScreens == null)
+                {
                     int index = 0;
                     Screen[] screens = new Screen[ScreenCount];
                     EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, (IntPtr monitor, IntPtr hdcMonitor, ref Rect lprcMonitor, IntPtr data) =>
-                                                                                   {
-                                                                                       MONITORINFO monitorInfo = new MONITORINFO();
-                                                                                       if (GetMonitorInfo(monitor, monitorInfo))
-                                                                                       {
-                                                                                           RECT bounds = monitorInfo.rcMonitor;
-                                                                                           RECT workingArea = monitorInfo.rcWork;
-                                                                                           Rect avaloniaBounds = new Rect(bounds.left, bounds.top, bounds.right, bounds.bottom);
-                                                                                           Rect avaloniaWorkArea = new Rect(workingArea.left, workingArea.top, workingArea.right, workingArea.bottom);
-                                                                                           screens[index] = new WinScreen(avaloniaBounds, avaloniaWorkArea, monitorInfo.dwFlags == 1, monitor);
-                                                                                           index++;
-                                                                                       }
-                                                                                       return true;
-                                                                                   }, IntPtr.Zero);
-                    return screens;
+                                                                  {
+                                                                      MONITORINFO monitorInfo = new MONITORINFO();
+                                                                      if (GetMonitorInfo(monitor, monitorInfo))
+                                                                      {
+                                                                          RECT bounds = monitorInfo.rcMonitor;
+                                                                          RECT workingArea = monitorInfo.rcWork;
+                                                                          Rect avaloniaBounds = new Rect(bounds.left, bounds.top, bounds.right, bounds.bottom);
+                                                                          Rect avaloniaWorkArea =
+                                                                              new Rect(workingArea.left, workingArea.top, workingArea.right,
+                                                                                       workingArea.bottom);
+                                                                          screens[index] =
+                                                                              new WinScreen(avaloniaBounds, avaloniaWorkArea, monitorInfo.dwFlags == 1,
+                                                                                            monitor);
+                                                                          index++;
+                                                                      }
+                                                                      return true;
+                                                                  }, IntPtr.Zero);
+                    allScreens = screens;
+                }
+                return allScreens;
             }
+        }
 
+        public void InvalidateScreensCache()
+        {
+            allScreens = null;
         }
         
         public Screen PrimaryScreen
         {
             get
             {
-                foreach (Screen screen in AllScreens)
+                for (var i = 0; i < allScreens.Length; i++)
                 {
-                    if (screen.Primary)
-                        return screen;
+                    if (allScreens[i].Primary)
+                        return allScreens[i];
                 }
 
                 return null;
