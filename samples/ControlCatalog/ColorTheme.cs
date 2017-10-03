d8e991c0d2d6ef6db68cbe99391aea61c0077b35
Dan Walmsley <dan@walms.co.uk>
2017-10-03 13:35:31 +0100

    repro for dynamic resource bug

---
+++ samples/ControlCatalog/ColorTheme.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Media;
+
+namespace ControlCatalog
+{
+    public class ColorTheme
+    {
+        public static readonly ColorTheme VisualStudioLight = new ColorTheme
+        {
+            WindowBorder = Brush.Parse("#9B9FB9"),
+            Background = Brush.Parse("#EEEEF2"),
+            Foreground = Brush.Parse("#1E1E1E"),
+            ForegroundLight = Brush.Parse("#525252"),
+            BorderLight = Brush.Parse("#9B9FB9"),
+            BorderMid = Brush.Parse("#9B9FB9"),
+            BorderDark = Brush.Parse("Red"),
+            ControlLight = Brush.Parse("#9B9FB9"),
+            ControlMid = Brush.Parse("#F5F5F5"),
+            ControlDark = Brush.Parse("#E6E7E8")
+        };
+
+        public static readonly ColorTheme VisualStudioDark = new ColorTheme
+        {
+            WindowBorder = Brush.Parse("#9B9FB9"),
+            Background = Brush.Parse("#FF2D2D30"),
+            Foreground = Brush.Parse("#FFC4C4C4"),
+            ForegroundLight = Brush.Parse("#FF808080"),
+            BorderLight = Brush.Parse("#FFAAAAAA"),
+            BorderMid = Brush.Parse("#FF888888"),
+            BorderDark = Brush.Parse("Green"),
+            ControlLight = Brush.Parse("#FFFFFFFF"),
+            ControlMid = Brush.Parse("#FF3E3E42"),
+            ControlDark = Brush.Parse("#FF252526")
+        };
+
+        public IBrush WindowBorder { get; set; }
+
+        public IBrush Background { get; set; }
+
+        public IBrush Foreground { get; set; }
+
+        public IBrush ForegroundLight { get; set; }
+
+        public IBrush BorderLight { get; set; }
+
+        public IBrush BorderMid { get; set; }
+
+        public IBrush BorderDark { get; set; }
+
+        public IBrush ControlLight { get; set; }
+
+        public IBrush ControlMid { get; set; }
+
+        public IBrush ControlDark { get; set; }
+    }
+}
--- samples/ControlCatalog/MainWindow.xaml.cs
+++ samples/ControlCatalog/MainWindow.xaml.cs
@@ -1,18 +1,51 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 using System;
 
 namespace ControlCatalog
 {
     public class MainWindow : Window
     {
+        private bool set = false;
+
         public MainWindow()
         {
             this.InitializeComponent();
             this.AttachDevTools();
             //Renderer.DrawFps = true;
             //Renderer.DrawDirtyRects = Renderer.DrawFps = true;
+
+            var timer = new DispatcherTimer();
+
+            LoadTheme(ColorTheme.VisualStudioLight);
+
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += (sender, e) =>
+            {
+                if (set)
+                {
+                    LoadTheme(ColorTheme.VisualStudioLight);
+                }
+                else
+                {
+                    LoadTheme(ColorTheme.VisualStudioDark);
+                }
+
+                set = !set;
+            };
+
+            timer.Start();
+        }
+
+        public void LoadTheme(ColorTheme theme)
+        {
+            Resources["ThemeBackgroundBrush"] = theme.Background;
+            Resources["ThemeControlDarkBrush"] = theme.ControlDark;
+            Resources["ThemeControlMidBrush"] = theme.ControlMid;
+            Resources["ThemeForegroundBrush"] = theme.Foreground;
+            Resources["ThemeBorderDarkBrush"] = theme.BorderDark;
         }
 
         private void InitializeComponent()
