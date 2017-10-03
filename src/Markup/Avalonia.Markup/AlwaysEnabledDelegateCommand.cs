05b6ab91c9873885e639e6ba4153b6828dde4edf
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-10-02 22:14:56 -0500

    Disable unused event warning

--- src/Markup/Avalonia.Markup/AlwaysEnabledDelegateCommand.cs
+++ src/Markup/Avalonia.Markup/AlwaysEnabledDelegateCommand.cs
@@ -14,7 +14,9 @@ namespace Avalonia.Markup
             this.action = action;
         }
 
+#pragma warning disable 0067
         public event EventHandler CanExecuteChanged;
+#pragma warning restore 0067
 
         public bool CanExecute(object parameter) => true;
 
