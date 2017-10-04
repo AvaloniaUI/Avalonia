097716141399b0a27a34887e109981a7ff1e9b6c
Nikita Tsukanov <keks9n@gmail.com>
2017-10-04 11:23:48 +0300

    Use AppDomain.GetLoadedAssemblies() for all platforms

--- src/Shared/PlatformSupport/StandardRuntimePlatform.cs
+++ src/Shared/PlatformSupport/StandardRuntimePlatform.cs
@@ -11,13 +11,8 @@ namespace Avalonia.Shared.PlatformSupport
 {
     internal partial class StandardRuntimePlatform : IRuntimePlatform
     {
-
-#if NETCOREAPP2_0
-        public void PostThreadPoolItem(Action cb) =>  ThreadPool.QueueUserWorkItem(_ => cb(), null);
-#else
-        public Assembly[] GetLoadedAssemblies() => AppDomain.CurrentDomain.GetAssemblies();
         public void PostThreadPoolItem(Action cb) => ThreadPool.UnsafeQueueUserWorkItem(_ => cb(), null);
-#endif
+        public Assembly[] GetLoadedAssemblies() => AppDomain.CurrentDomain.GetAssemblies();
         public IDisposable StartSystemTimer(TimeSpan interval, Action tick)
         {
             return new Timer(_ => tick(), null, interval, interval);
