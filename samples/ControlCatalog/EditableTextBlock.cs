d07ff6a0c0db079bc7a25934d31536c0d6642df9
Unknown <dan@walms.co.uk>
2017-10-14 14:25:43 +0100

    add repro for TextPresenter.Text not getting set on initialisation.

---
+++ samples/ControlCatalog/EditableTextBlock.cs
@@ -0,0 +1,112 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.LogicalTree;
+using Avalonia.Metadata;
+using Avalonia.Threading;
+using Avalonia.VisualTree;
+using System;
+
+namespace ControlCatalog
+{
+    public class EditableTextBlock : TemplatedControl
+    {
+        private string _text;
+        private TextBox _textBox;
+
+        public static readonly DirectProperty<EditableTextBlock, string> TextProperty = TextBlock.TextProperty.AddOwner<EditableTextBlock>(
+                o => o.Text,
+                (o, v) => o.Text = v,
+                defaultBindingMode: BindingMode.TwoWay,
+                enableDataValidation: true);        
+
+        [Content]
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                SetAndRaise(TextProperty, ref _text, value);                
+            }
+        }
+
+        public static readonly StyledProperty<bool> InEditModeProperty =
+            AvaloniaProperty.Register<EditableTextBlock, bool>(nameof(InEditMode));
+
+        public bool InEditMode
+        {
+            get { return GetValue(InEditModeProperty); }
+            set { SetValue(InEditModeProperty, value); }
+        }
+
+        protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
+        {
+            base.OnTemplateApplied(e);
+
+            _textBox = e.NameScope.Find<TextBox>("PART_TextBox");
+
+            _textBox.KeyUp += _textBox_KeyUp;            
+        }
+
+        protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e)
+        {
+            base.OnDetachedFromLogicalTree(e);
+
+            _textBox.KeyUp -= _textBox_KeyUp;
+        }
+
+        private void _textBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Escape)
+            {
+                ExitEditMode();
+            }
+        }
+
+        private void EnterEditMode()
+        {
+            InEditMode = true;
+            (VisualRoot as IInputRoot).MouseDevice.Capture(_textBox);
+            _textBox.CaretIndex = Text.Length - 1;
+
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                _textBox.Focus();
+            });    
+        }
+
+        private void ExitEditMode()
+        {
+            InEditMode = false;
+            (VisualRoot as IInputRoot).MouseDevice.Capture(null);
+        }
+
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+
+            if (!InEditMode)
+            {
+                if (e.MouseButton == MouseButton.Middle)
+                {
+                    EnterEditMode();
+                }
+                else if (e.ClickCount == 2)
+                {
+                    EnterEditMode();
+                }
+            }
+            else
+            {
+                var hit = this.InputHitTest(e.GetPosition(this));
+
+                if (!this.IsVisualAncestorOf(hit))
+                {
+                    ExitEditMode();
+                }
+            }
+        }
+    }
+}
--- samples/ControlCatalog/Pages/TextBoxPage.xaml.cs
+++ samples/ControlCatalog/Pages/TextBoxPage.xaml.cs
@@ -1,13 +1,27 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using ReactiveUI;
 
 namespace ControlCatalog.Pages
 {
+    public class TestViewModel : ReactiveObject
+    {
+        private string testText = "Editable TextBlock";
+
+        public string TestText
+        {
+            get { return testText; }
+            set { this.RaiseAndSetIfChanged(ref testText, value); }
+        }
+    }
+
     public class TextBoxPage : UserControl
     {
         public TextBoxPage()
         {
             this.InitializeComponent();
+
+            DataContext = new TestViewModel();
         }
 
         private void InitializeComponent()
