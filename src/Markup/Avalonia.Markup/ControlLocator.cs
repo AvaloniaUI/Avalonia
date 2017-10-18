f4011a9d32788286208036033449e9755cd22e22
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-10-18 14:37:20 -0500

    Fix unsubscription.

--- src/Markup/Avalonia.Markup/ControlLocator.cs
+++ src/Markup/Avalonia.Markup/ControlLocator.cs
@@ -121,7 +121,7 @@ namespace Avalonia.Markup
 
             var detached = Observable.FromEventPattern<LogicalTreeAttachmentEventArgs>(
                 x => relativeTo.DetachedFromLogicalTree += x,
-                x => relativeTo.DetachedFromLogicalTree += x)
+                x => relativeTo.DetachedFromLogicalTree -= x)
                 .Select(x => false);
 
             return attached.Merge(detached).Select(isAttachedToLogicalTree =>
