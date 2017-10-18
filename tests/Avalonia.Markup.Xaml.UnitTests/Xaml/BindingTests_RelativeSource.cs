cc147db28d7f36596ed38d32a4cc601819cea726
Steven Kirk <grokys@gmail.com>
2017-10-18 23:12:54 +0200

    Added more tests.
    One failing.

--- tests/Avalonia.Markup.Xaml.UnitTests/Xaml/BindingTests_RelativeSource.cs
+++ tests/Avalonia.Markup.Xaml.UnitTests/Xaml/BindingTests_RelativeSource.cs
@@ -78,6 +78,32 @@ namespace Avalonia.Markup.Xaml.UnitTests.Xaml
         }
 
         [Fact]
+        public void Binding_To_First_Ancestor_Without_AncestorType_Uses_LogicalTree()
+        {
+            using (UnitTestApplication.Start(TestServices.StyledWindow))
+            {
+                var xaml = @"
+<Window xmlns='https://github.com/avaloniaui'
+        xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+        xmlns:local='clr-namespace:Avalonia.Markup.Xaml.UnitTests.Xaml;assembly=Avalonia.Markup.Xaml.UnitTests'>
+    <Border Name='border1'>
+      <ContentControl Name='contentControl'>
+        <Button Name='button' Content='{Binding Name, RelativeSource={RelativeSource AncestorLevel=1}}'/>
+      </ContentControl>
+    </Border>
+</Window>";
+                var loader = new AvaloniaXamlLoader();
+                var window = (Window)loader.Load(xaml);
+                var contentControl = window.FindControl<ContentControl>("contentControl");
+                var button = window.FindControl<Button>("button");
+
+                window.ApplyTemplate();
+
+                Assert.Equal("contentControl", button.Content);
+            }
+        }
+
+        [Fact]
         public void Binding_To_First_Ancestor_With_Shorthand_Works()
         {
             using (UnitTestApplication.Start(TestServices.StyledWindow))
@@ -103,6 +129,36 @@ namespace Avalonia.Markup.Xaml.UnitTests.Xaml
         }
 
         [Fact]
+        public void Binding_To_First_Ancestor_With_Shorthand_Uses_LogicalTree()
+        {
+            using (UnitTestApplication.Start(TestServices.StyledWindow))
+            {
+                var xaml = @"
+<Window xmlns='https://github.com/avaloniaui'
+        xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+        xmlns:local='clr-namespace:Avalonia.Markup.Xaml.UnitTests.Xaml;assembly=Avalonia.Markup.Xaml.UnitTests'>
+    <Border Name='border'>
+      <ContentControl Name='contentControl'>
+        <Button Name='button' Content='{Binding $parent.Name}'/>
+      </ContentControl>
+    </Border>
+</Window>";
+                var loader = new AvaloniaXamlLoader();
+                var window = (Window)loader.Load(xaml);
+                var contentControl = window.FindControl<ContentControl>("contentControl");
+                var button = window.FindControl<Button>("button");
+
+                window.ApplyTemplate();
+
+                // This isn't needed in `Binding_To_First_Ancestor_Without_AncestorType_Uses_LogicalTree` -
+                // why is it needed to get non-null value here?
+                contentControl.ApplyTemplate();
+
+                Assert.Equal("contentControl", button.Content);
+            }
+        }
+
+        [Fact]
         public void Binding_To_Second_Ancestor_Works()
         {
             using (UnitTestApplication.Start(TestServices.StyledWindow))
@@ -124,6 +180,33 @@ namespace Avalonia.Markup.Xaml.UnitTests.Xaml
                 window.ApplyTemplate();
 
                 Assert.Equal("border1", button.Content);
+            }
+        }
+
+        [Fact]
+        public void Binding_To_Second_Ancestor_With_Shorthand_Uses_LogicalTree()
+        {
+            using (UnitTestApplication.Start(TestServices.StyledWindow))
+            {
+                var xaml = @"
+<Window xmlns='https://github.com/avaloniaui'
+        xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+        xmlns:local='clr-namespace:Avalonia.Markup.Xaml.UnitTests.Xaml;assembly=Avalonia.Markup.Xaml.UnitTests'>
+    <ContentControl Name='contentControl1'>
+      <ContentControl Name='contentControl2'>
+        <Button Name='button' Content='{Binding $parent[1].Name}'/>
+      </ContentControl>
+    </ContentControl>
+</Window>";
+                var loader = new AvaloniaXamlLoader();
+                var window = (Window)loader.Load(xaml);
+                var contentControl1 = window.FindControl<ContentControl>("contentControl1");
+                var contentControl2 = window.FindControl<ContentControl>("contentControl2");
+                var button = window.FindControl<Button>("button");
+
+                window.ApplyTemplate();
+
+                Assert.Equal("contentControl1", button.Content);
             }
         }
 
