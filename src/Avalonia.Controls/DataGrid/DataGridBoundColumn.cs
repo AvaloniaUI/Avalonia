44fd9f59401f1f7611cc3e8e86b88b882e364b67
sdoroff <sdoroff@gmail.com>
2018-03-19 17:17:30 -0400

    Ported the DataGrid control from Silverlight

---
+++ src/Avalonia.Controls/DataGrid/DataGridBoundColumn.cs
@@ -0,0 +1,223 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
+using Avalonia.Media;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Avalonia.Controls
+{
+    /// <summary>
+    /// Represents a <see cref="T:System.Windows.Controls.DataGrid" /> column that can 
+    /// bind to a property in the grid's data source.
+    /// </summary>
+    public abstract class DataGridBoundColumn : DataGridColumn
+    {
+        private IBinding _binding;
+        
+        /// <summary>
+        /// Gets or sets the binding that associates the column with a property in the data source.
+        /// </summary>
+        //TODO
+        //Binding
+        public virtual IBinding Binding
+        {
+            get
+            {
+                return _binding;
+            }
+            set
+            {
+                if (_binding != value)
+                {
+                    if (OwningGrid != null && !OwningGrid.CommitEdit(DataGridEditingUnit.Row, exitEditingMode: true))
+                    {
+                        // Edited value couldn't be committed, so we force a CancelEdit
+                        OwningGrid.CancelEdit(DataGridEditingUnit.Row, raiseEvents: false);
+                    }
+
+                    _binding = value;
+
+                    if (_binding != null)
+                    {
+                        //// Force the TwoWay binding mode if there is a Path present.  TwoWay binding requires a Path.
+                        //if (!String.IsNullOrEmpty(_binding.Path.Path))
+                        //{
+                        //    _binding.Mode = BindingMode.TwoWay;
+                        //}
+
+                        //if (_binding.Converter == null)
+                        //{
+                        //    _binding.Converter = new DataGridValueConverter();
+                        //}
+
+                        //// Setup the binding for validation
+                        //_binding.ValidatesOnDataErrors = true;
+                        //_binding.ValidatesOnExceptions = true;
+                        //_binding.NotifyOnValidationError = true;
+                        //_binding.UpdateSourceTrigger = UpdateSourceTrigger.Explicit;
+
+                        // Apply the new Binding to existing rows in the DataGrid
+                        if (OwningGrid != null)
+                        {
+                            OwningGrid.OnColumnBindingChanged(this);
+                        }
+                    }
+
+                    RemoveEditingElement();
+                }
+            }
+        }
+
+        internal AvaloniaProperty BindingTarget { get; set; }
+
+    }
+
+    /*
+     
+    [StyleTypedProperty(Property = "ElementStyle", StyleTargetType = typeof(FrameworkElement))]
+    [StyleTypedProperty(Property = "EditingElementStyle", StyleTargetType = typeof(FrameworkElement))]
+    */
+
+    #region Binding
+
+
+    /*internal override List<string> CreateBindingPaths()
+    {
+        if (Binding != null && Binding.Path != null)
+        {
+            return new List<string>() { Binding.Path.Path };
+        }
+        return base.CreateBindingPaths();
+    }*/
+
+    /*internal override List<BindingInfo> CreateBindings(FrameworkElement element, object dataItem, bool twoWay)
+    {
+        BindingInfo bindingData = new BindingInfo();
+        if (twoWay && BindingTarget != null)
+        {
+            bindingData.BindingExpression = element.GetBindingExpression(BindingTarget);
+            if (bindingData.BindingExpression != null)
+            {
+                bindingData.BindingTarget = BindingTarget;
+                bindingData.Element = element;
+                return new List<BindingInfo> { bindingData };
+            }
+        }
+        foreach (DependencyProperty bindingTarget in element.GetDependencyProperties(false))
+        {
+            bindingData.BindingExpression = element.GetBindingExpression(bindingTarget);
+            if (bindingData.BindingExpression != null
+                && bindingData.BindingExpression.ParentBinding == Binding)
+            {
+                BindingTarget = bindingTarget;
+                bindingData.BindingTarget = BindingTarget;
+                bindingData.Element = element;
+                return new List<BindingInfo> { bindingData };
+            }
+        }
+        return base.CreateBindings(element, dataItem, twoWay);
+    }*/
+
+    /*internal void SetHeaderFromBinding()
+    {
+        if (OwningGrid != null && OwningGrid.DataConnection.DataType != null
+            && Header == null && Binding != null && Binding.Path != null)
+        {
+            string header = OwningGrid.DataConnection.DataType.GetDisplayName(Binding.Path.Path);
+            if (header != null)
+            {
+                Header = header;
+            }
+        }
+    }*/
+
+    #endregion
+
+    #region ClipBoard
+
+    /// <summary>
+    /// The binding that will be used to get or set cell content for the clipboard.
+    /// If the base ClipboardContentBinding is not explicitly set, this will return the value of Binding.
+    /// </summary>
+    /*public override Binding ClipboardContentBinding
+    {
+        get
+        {
+            return base.ClipboardContentBinding ?? Binding;
+        }
+        set
+        {
+            base.ClipboardContentBinding = value;
+        }
+    }*/
+
+
+
+    #endregion
+
+
+    #region Styles
+
+    /*
+    private Style _elementStyle;
+    private Style _editingElementStyle;
+    */
+
+
+    /// <summary>
+    /// Gets or sets the style that is used when rendering the element that the column displays for a cell in editing mode.
+    /// </summary>
+    /*public Style EditingElementStyle
+    {
+        get
+        {
+            return _editingElementStyle;
+        }
+        set
+        {
+            if (_editingElementStyle != value)
+            {
+                _editingElementStyle = value;
+                // We choose not to update the elements already editing in the Grid here.  They
+                // will get the EditingElementStyle next time they go into edit mode
+            }
+        }
+    }*/
+
+    /// <summary>
+    /// Gets or sets the style that is used when rendering the element that the column displays for a cell 
+    /// that is not in editing mode.
+    /// </summary>
+    /*public Style ElementStyle
+    {
+        get
+        {
+            return _elementStyle;
+        }
+        set
+        {
+            if (_elementStyle != value)
+            {
+                _elementStyle = value;
+                if (OwningGrid != null)
+                {
+                    OwningGrid.OnColumnElementStyleChanged(this);
+                }
+            }
+        }
+    }*/
+
+
+    #endregion
+}
---
+++ src/Avalonia.Controls/Utils/TreeHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Input;
+using Avalonia.VisualTree;
+
+namespace Avalonia.Controls
+{
+    internal static class TreeHelper
+    {
+        /// <summary>
+        /// Walks the visual tree to determine if a particular child is contained within a parent DependencyObject.
+        /// </summary>
+        /// <param name="element">Parent DependencyObject</param>
+        /// <param name="child">Child DependencyObject</param>
+        /// <returns>True if the parent element contains the child</returns>
+        internal static bool ContainsChild(this IVisual element, IVisual child)
+        {
+            if (element != null)
+            {
+                while (child != null)
+                {
+                    if (child == element)
+                    {
+                        return true;
+                    }
+
+                    // Walk up the visual tree.  If we hit the root, try using the framework element's
+                    // parent.  We do this because Popups behave differently with respect to the visual tree,
+                    // and it could have a parent even if the VisualTreeHelper doesn't find it.
+                    IVisual parent = child.GetVisualParent();
+                    if (parent == null)
+                    {
+                        if (child is IControl childElement)
+                        {
+                            parent = childElement.Parent;
+                        }
+                    }
+                    child = parent;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Walks the visual tree to determine if the currently focused element is contained within
+        /// a parent DependencyObject.  The FocusManager's GetFocusedElement method is used to determine
+        /// the currently focused element, which is updated synchronously.
+        /// </summary>
+        /// <param name="element">Parent DependencyObject</param>
+        /// <returns>True if the currently focused element is within the visual tree of the parent</returns>
+        internal static bool ContainsFocusedElement(this IVisual element)
+        {
+            return (element == null) ? false : element.ContainsChild(FocusManager.Instance.Current);
+        }
+    }
+}
