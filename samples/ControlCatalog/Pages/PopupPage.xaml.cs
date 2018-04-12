a0b1ad62b36e3806d1bb7da3280814dd956f0181
ahopper <alan@samsararesearch.com>
2018-04-12 11:30:53 +0100

    Fix #1484

---
+++ samples/ControlCatalog/Pages/PopupPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ControlCatalog.Pages
+{
+    public class PopupPage : UserControl
+    {
+        public PopupPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
