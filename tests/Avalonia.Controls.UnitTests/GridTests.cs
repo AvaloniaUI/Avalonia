431407437bca1340780614fca1d7c95aebb6bb29
walterlv <xblero@gmail.com>
2018-04-28 13:40:37 +0800

    Add unit test for grid layout: 1. Pixel row column 1. Start row column 1. Mix pixel star row column

--- tests/Avalonia.Controls.UnitTests/GridTests.cs
+++ tests/Avalonia.Controls.UnitTests/GridTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) The Avalonia Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using Avalonia.Controls;
 using Xunit;
 
@@ -63,6 +65,42 @@ namespace Avalonia.Controls.UnitTests
             Assert.Equal(new Rect(52, 0, 100, 25), target.Children[0].Bounds);
             Assert.Equal(new Rect(0, 25, 150, 25), target.Children[1].Bounds);
             Assert.Equal(new Rect(154, 25, 50, 25), target.Children[2].Bounds);
+        }
+
+        [Fact]
+        public void Layout_PixelRowColumn_BoundsCorrect()
+        {
+            // Arrange & Action
+            var rowGrid = GridMock.New(new RowDefinitions("100,200,300"));
+            var columnGrid = GridMock.New(new ColumnDefinitions("50,100,150"));
+
+            // Assert
+            GridAssert.ChildrenHeight(rowGrid, 100, 200, 300);
+            GridAssert.ChildrenWidth(columnGrid, 50, 100, 150);
+        }
+
+        [Fact]
+        public void Layout_StarRowColumn_BoundsCorrect()
+        {
+            // Arrange & Action
+            var rowGrid = GridMock.New(new RowDefinitions("1*,2*,3*"), arrange: 600);
+            var columnGrid = GridMock.New(new ColumnDefinitions("*,*,2*"), arrange: 600);
+
+            // Assert
+            GridAssert.ChildrenHeight(rowGrid, 100, 200, 300);
+            GridAssert.ChildrenWidth(columnGrid, 150, 150, 300);
+        }
+
+        [Fact]
+        public void Layout_MixPixelStarRowColumn_BoundsCorrect()
+        {
+            // Arrange & Action
+            var rowGrid = GridMock.New(new RowDefinitions("1*,2*,150"), arrange: 600);
+            var columnGrid = GridMock.New(new ColumnDefinitions("1*,2*,150"), arrange: 600);
+
+            // Assert
+            GridAssert.ChildrenHeight(rowGrid, 150, 300, 150);
+            GridAssert.ChildrenWidth(columnGrid, 150, 300, 150);
         }
     }
 }
