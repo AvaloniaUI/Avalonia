f3cae14fece97d423224dd2bbc4ddb44171df3ad
Dan Walmsley <dan@walms.co.uk>
2018-05-02 20:35:17 +0100

    add repro for TreeViewSelected item issue.

--- samples/ControlCatalog/Pages/TreeViewPage.xaml.cs
+++ samples/ControlCatalog/Pages/TreeViewPage.xaml.cs
@@ -3,15 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using ReactiveUI;
 
 namespace ControlCatalog.Pages
 {
+    public class TestItemViewModel
+    {
+        public string Name { get; set; }
+    }
+
+    public class TestItemViewModel2
+    {
+        public string Name { get; set; }
+    }
+
+    public class TestViewModel
+    {
+        public TestViewModel()
+        {
+            Items = new List<object>
+            {
+                new TestItemViewModel{ Name = "Item1"},
+                new TestItemViewModel2{ Name="Items2"}
+            };
+        }
+
+        public List<object> Items { get; set; }
+
+        private TestItemViewModel _selectedItem;
+
+        public TestItemViewModel SelectedItem
+        {
+            get { return _selectedItem; }
+            set {  _selectedItem = value; }
+        }
+    }
+
     public class TreeViewPage : UserControl
     {
         public TreeViewPage()
         {
             this.InitializeComponent();
-            DataContext = new Node().Children;
+            DataContext = new TestViewModel(); //new Node().Children;
         }
 
         private void InitializeComponent()
