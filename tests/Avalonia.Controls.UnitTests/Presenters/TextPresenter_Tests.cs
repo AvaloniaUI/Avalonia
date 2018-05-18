4b5ce0cf406b1c91ef7a7bfa0e04567a47173a8c
Unknown <dan@walms.co.uk>
2018-05-18 11:10:24 +0100

    add failing unit test.

---
+++ tests/Avalonia.Controls.UnitTests/Presenters/TextPresenter_Tests.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls.Presenters;
+using Xunit;
+
+namespace Avalonia.Controls.UnitTests.Presenters
+{
+    public class TextPresenter_Tests
+    {
+        [Fact]
+        public void TextPresenter_can_contain_null_with_password_char_set()
+        {
+            var target = new TextPresenter
+            {
+                PasswordChar = '*'
+            };
+
+            Assert.NotNull(target.FormattedText);
+        }
+
+        [Fact]
+        public void TextPresenter_can_contain_null_without_password_char_set()
+        {
+            var target = new TextPresenter();
+
+            Assert.NotNull(target.FormattedText);
+        }
+    }
+}
