61d76e91eb1be2601e29404b050e68a089905974
Nelson Carrillo <ncarrillo1@outlook.com>
2018-05-29 22:00:28 -0400

    Add Avalonia.OpenGL project with platform neutral interfaces

---
+++ src/OpenGL/Avalonia.OpenGL/GlRequest.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) The Avalonia Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+namespace Avalonia.OpenGL
+{
+    public class GlRequest
+    {
+        /// <summary>
+        /// OpenGL API to use
+        /// </summary>
+        public GlApi Api { get; set; }
+
+        /// <summary>
+        /// OpenGL version to use
+        /// </summary>
+        public GlVersion Version { get; set; }
+
+        /// <summary>
+        /// Leave this unset if the GLVersion used is Latest
+        /// </summary>
+        public int GlMajor { get; set; }
+
+        /// <summary>
+        /// Leave this unset if the GlVersion used is Latest
+        /// </summary>
+        public int GlMinor { get; set; }
+    }
+
+    public enum GlApi
+    {
+        /// <summary>
+        /// Tries GL and if that fails tries GLES
+        /// </summary>
+        Auto,
+
+        /// <summary>
+        /// Tries GL
+        /// </summary>
+        Gl,
+
+        /// <summary>
+        /// Tries GLES
+        /// </summary>
+        Gles
+    }
+
+    public enum GlVersion
+    {
+        /// <summary>
+        /// Use the lates GL version 
+        /// </summary>
+        Latest,
+
+        /// <summary>
+        /// Use a specific GL version
+        /// </summary>
+        Specific
+    }
+}
---
+++ src/OpenGL/Avalonia.OpenGL/IGlContext.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) The Avalonia Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System;
+
+namespace Avalonia.OpenGL
+{
+    /// <summary>
+    /// GL Context
+    /// </summary>
+    public interface IGlContext : IDisposable
+    {
+        /// <summary>
+        /// Sets the GL Surface for a Context
+        /// </summary>
+        IGlSurface Surface { get; }
+
+        /// <summary>
+        /// Swaps buffers
+        /// </summary>
+        void SwapBuffers();
+
+        /// <summary>
+        /// Makes this GL Context current
+        /// </summary>
+        void MakeCurrent();
+
+        IntPtr GetProcAddress(string functionName);
+
+        /// <returns>True if this Context is the current one. False otherwise.</returns>
+        bool IsCurrentContext();
+
+        /// <summary>
+        /// Recreates the current surface
+        /// </summary>
+        void RecreateSurface();
+    }
+}
