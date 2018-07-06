95ac57d648b0d48c7e20c4da43ac10e72e6f345a
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-07-06 14:27:15 -0500

    Add some SingleOrQueue documentation.

--- src/Avalonia.Base/Utilities/SingleOrQueue.cs
+++ src/Avalonia.Base/Utilities/SingleOrQueue.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace Avalonia.Utilities
 {
+    /// <summary>
+    /// FIFO Queue optimized for holding zero or one items.
+    /// </summary>
+    /// <typeparam name="T">The type of items held in the queue.</typeparam>
     public class SingleOrQueue<T>
     {
         private T _head;
@@ -11,6 +15,9 @@ namespace Avalonia.Utilities
 
         private Queue<T> Tail => _tail ?? (_tail = new Queue<T>());
 
+        /// <summary>
+        /// True if this queue has at some point had more than one element.
+        /// </summary>
         public bool HasTail => _tail != null;
 
         public bool Empty { get; private set; } = true;
