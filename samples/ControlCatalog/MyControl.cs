defcb6e32c665818dc11219b882efa1dc8c07722
Dan Walmsley <dan@walms.co.uk>
2018-07-20 11:43:01 +0100

    repro - attached property not showing in the dev tools.

---
+++ samples/ControlCatalog/MyControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Styling;
+
+namespace ControlCatalog
+{
+    public class MyControl : Grid, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(Grid);
+
+        /// <summary>
+        /// Defines the Proportion attached property.
+        /// </summary>
+        public static readonly AttachedProperty<double> ProportionProperty =
+            AvaloniaProperty.RegisterAttached<MyControl, IControl, double>("Proportion", double.NaN);
+
+        /// <summary>
+        /// Gets the value of the Proportion attached property on the specified control.
+        /// </summary>
+        /// <param name="control">The control.</param>
+        /// <returns>The Proportion attached property.</returns>
+        public static double GetProportion(IControl control)
+        {
+            return control.GetValue(ProportionProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of the Proportion attached property on the specified control.
+        /// </summary>
+        /// <param name="control">The control.</param>
+        /// <param name="value">The value of the Dock property.</param>
+        public static void SetProportion(IControl control, double value)
+        {
+            control.SetValue(ProportionProperty, value);
+        }
+    }
+}
