1a97b5b45baede8c33f582a14316cc7d41ad14d6
Dan Walmsley <dan@walms.co.uk>
2018-07-26 21:15:21 +0100

    implement close window after 5 seconds.

--- samples/ControlCatalog/Pages/ButtonPage.xaml.cs
+++ samples/ControlCatalog/Pages/ButtonPage.xaml.cs
@@ -1,5 +1,8 @@
+using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 
 namespace ControlCatalog.Pages
 {
@@ -8,11 +11,28 @@ namespace ControlCatalog.Pages
         public ButtonPage()
         {
             this.InitializeComponent();
+            DataContext = this;
+
+            Dispatcher.UIThread.Post(async () =>
+            {
+                await Task.Delay(5000);
+                Close();
+            });
+            
+
+            this.FindControl<Button>("TestButton").Click += (sender, e)=>{
+                Close();
+            };
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public void Close ()
+        {
+            Application.Current.MainWindow.Close();
         }
     }
 }
