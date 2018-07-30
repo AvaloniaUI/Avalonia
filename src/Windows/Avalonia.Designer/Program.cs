a1e3edb5257d7a0954e0bd4af6c5f9818af38fc1
Kasper Birkelund Larsen <kasperbirkelund@gmail.com>
2018-07-30 22:08:55 +0200

    test method with s7

--- src/Windows/Avalonia.Designer/Program.cs
+++ src/Windows/Avalonia.Designer/Program.cs
@@ -51,7 +51,7 @@ namespace Avalonia.Designer
             {
             }
 
-            public void Test(string s1, string s2, string s3, string s4, string s5, string s6)
+            public void Test(string s1, string s2, string s3, string s4, string s5, string s6, int s7)
             {
                 //nothing here
             }
