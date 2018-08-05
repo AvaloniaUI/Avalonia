abd9b0ef98b8cfeb18da6642f833c6a18fb36ba1
Nelson Carrillo <ncarrillo1@outlook.com>
2018-08-05 18:01:54 -0400

    Fix threading issue with winit

--- src/Avalonia.Windowing/WindowingPlatform.cs
+++ src/Avalonia.Windowing/WindowingPlatform.cs
@@ -34,6 +34,7 @@ namespace Avalonia.Windowing
         internal static WindowingPlatform Instance { get; private set; }
         private readonly EventsLoop _eventsLoop;
         private readonly Dictionary<WindowId, WindowImpl> _windows;
+        private int _uiThreadId;
 
         public WindowingPlatform()
         {
@@ -46,6 +47,7 @@ namespace Avalonia.Windowing
             _eventsLoop.OnShouldExitEventLoop += _eventsLoop_OnShouldExitEventLoop;
             _eventsLoop.OnCloseRequested += _eventsLoop_OnCloseRequested;
             _eventsLoop.OnFocused += _eventsLoop_OnFocused;
+            _uiThreadId = Thread.CurrentThread.ManagedThreadId;
             _windows = new Dictionary<WindowId, WindowImpl>();
         }
 
@@ -125,6 +127,7 @@ namespace Avalonia.Windowing
                 _signaled = false;
             }
 
+            System.Diagnostics.Debug.WriteLine(string.Format("TID {0}", Thread.CurrentThread.ManagedThreadId));
             Signaled?.Invoke(null);
         }
 
@@ -152,7 +155,7 @@ namespace Avalonia.Windowing
                 .Bind<IPlatformThreadingInterface>().ToConstant(this);
         }
 
-        public bool CurrentThreadIsLoopThread => true;
+        public bool CurrentThreadIsLoopThread => _uiThreadId == Thread.CurrentThread.ManagedThreadId;
         public event Action<DispatcherPriority?> Signaled;
 
         public IEmbeddableWindowImpl CreateEmbeddableWindow()
