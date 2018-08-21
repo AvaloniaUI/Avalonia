c3a60a54f2f8f43559a967621c5a5c6a2a375134
Siegfried Pammer <siegfriedpammer@gmail.com>
2018-08-21 15:30:34 +0200

    Fix #1782: Implement GroupName for RadioButton

--- src/Avalonia.Controls/RadioButton.cs
+++ src/Avalonia.Controls/RadioButton.cs
@@ -2,17 +2,41 @@
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Avalonia.Controls.Primitives;
+using Avalonia.Data;
 using Avalonia.VisualTree;
 
 namespace Avalonia.Controls
 {
     public class RadioButton : ToggleButton
     {
+        public static readonly DirectProperty<RadioButton, string> GroupNameProperty =
+            AvaloniaProperty.RegisterDirect<RadioButton, string>(
+                nameof(GroupName),
+                o => o.GroupName,
+                (o, v) => o.GroupName = v);
+
+        private string _groupName;
+
+        [ThreadStatic]
+        private static readonly Dictionary<string, List<WeakReference<RadioButton>>> s_registeredGroups;
+
+        static RadioButton()
+        {
+            s_registeredGroups = new Dictionary<string, List<WeakReference<RadioButton>>>();
+        }
+
         public RadioButton()
         {
             this.GetObservable(IsCheckedProperty).Subscribe(IsCheckedChanged);
+        }
+
+        public string GroupName
+        {
+            get { return _groupName; }
+            set { SetGroupName(value); }
         }
 
         protected override void Toggle()
@@ -23,21 +47,86 @@ namespace Avalonia.Controls
             }
         }
 
+        private void SetGroupName(string newGroupName)
+        {
+            string oldGroupName = GroupName;
+            if (newGroupName != oldGroupName)
+            {
+                lock (s_registeredGroups)
+                {
+                    if (!string.IsNullOrEmpty(oldGroupName) && s_registeredGroups.TryGetValue(oldGroupName, out var group))
+                    {
+                        int i = 0;
+                        while (i < group.Count)
+                        {
+                            if (!group[i].TryGetTarget(out var button) || button == this)
+                            {
+                                group.RemoveAt(i);
+                                continue;
+                            }
+                            i++;
+                        }
+                        if (group.Count == 0)
+                        {
+                            s_registeredGroups.Remove(oldGroupName);
+                        }
+                    }
+                    if (!string.IsNullOrEmpty(newGroupName))
+                    {
+                        if (!s_registeredGroups.TryGetValue(newGroupName, out group))
+                        {
+                            group = new List<WeakReference<RadioButton>>();
+                            s_registeredGroups.Add(newGroupName, group);
+                        }
+                        group.Add(new WeakReference<RadioButton>(this));
+                    }
+                }
+                _groupName = newGroupName;
+            }
+        }
+
         private void IsCheckedChanged(bool? value)
         {
-            var parent = this.GetVisualParent();
-
-            if (value.GetValueOrDefault() && parent != null)
+            string groupName = GroupName;
+            if (string.IsNullOrEmpty(groupName))
             {
-                var siblings = parent
-                    .GetVisualChildren()
-                    .OfType<RadioButton>()
-                    .Where(x => x != this);
+                var parent = this.GetVisualParent();
 
-                foreach (var sibling in siblings)
+                if (value.GetValueOrDefault() && parent != null)
                 {
-                    if (sibling.IsChecked.GetValueOrDefault())
-                        sibling.IsChecked = false;
+                    var siblings = parent
+                        .GetVisualChildren()
+                        .OfType<RadioButton>()
+                        .Where(x => x != this);
+
+                    foreach (var sibling in siblings)
+                    {
+                        if (sibling.IsChecked.GetValueOrDefault())
+                            sibling.IsChecked = false;
+                    }
+                }
+            }
+            else
+            {
+                lock (s_registeredGroups)
+                {
+                    if (value.GetValueOrDefault() && s_registeredGroups.TryGetValue(groupName, out var group))
+                    {
+                        int i = 0;
+                        while (i < group.Count)
+                        {
+                            if (!group[i].TryGetTarget(out var sibling))
+                            {
+                                group.RemoveAt(i);
+                            }
+                            else
+                            {
+                                if (sibling != this && sibling.IsChecked.GetValueOrDefault())
+                                    sibling.IsChecked = false;
+                                i++;
+                            }
+                        }
+                    }
                 }
             }
         }
