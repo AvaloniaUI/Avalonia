4f11ecdd38a6dc7bd50a342b22537f6970de7e95
Steven Kirk <grokys@users.noreply.github.com>
2018-09-12 13:07:32 +0200

    Added an example of dynamic menus to the ControlCatalog.
    Creates a menu using `DataTemplates` on the MenuPage.

--- samples/ControlCatalog/Pages/MenuPage.xaml.cs
+++ samples/ControlCatalog/Pages/MenuPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
@@ -8,11 +9,38 @@ namespace ControlCatalog.Pages
         public MenuPage()
         {
             this.InitializeComponent();
+            DataContext = new[]
+            {
+                new MenuItemViewModel
+                {
+                    Header = "_File",
+                    Items =
+                    {
+                        new MenuItemViewModel { Header = "_Open..." },
+                        new MenuItemViewModel { Header = "Save" },
+                    }
+                },
+                new MenuItemViewModel
+                {
+                    Header = "_Edit",
+                    Items =
+                    {
+                        new MenuItemViewModel { Header = "_Copy" },
+                        new MenuItemViewModel { Header = "_Paste" },
+                    }
+                }
+            };
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
+    }
+
+    public class MenuItemViewModel
+    {
+        public string Header { get; set; }
+        public IList<MenuItemViewModel> Items { get; } = new List<MenuItemViewModel>();
     }
 }
