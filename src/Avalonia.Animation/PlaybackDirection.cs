a462d0563c75e8c4c461e57847d7b4ad55e94157
Jumar Macato <hikari.netto23@gmail.com>
2018-09-14 10:50:18 +0800

    Add missing license headers on Avalonia.Animations.

--- src/Avalonia.Animation/PlaybackDirection.cs
+++ src/Avalonia.Animation/PlaybackDirection.cs
@@ -1,4 +1,7 @@
-﻿namespace Avalonia.Animation
+﻿// Copyright (c) The Avalonia Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+namespace Avalonia.Animation
 {
     /// <summary>
     /// Determines the playback direction of an animation.
