0410d121c2eadd41e75cd09d147c227d267e287f
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-09-20 21:42:55 -0500

    Add windowing subsystem hookup for reflection UsePlatformDetect.

---
+++ src/Avalonia.Native/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using Avalonia.MonoMac;
+using Avalonia.Platform;
+
+[assembly: ExportWindowingSubsystem(OperatingSystemType.OSX, 1, "MonoMac", typeof(MonoMacPlatform), nameof(MonoMacPlatform.Initialize))]
