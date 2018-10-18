6c729fa79eca0949dfcfc799293ee0f98a135e3a
Nikita Tsukanov <keks9n@gmail.com>
2018-10-18 12:34:35 +0300

    Line endings and csproj integration

--- src/Avalonia.Native/AvaloniaNativePlatform.cs
+++ src/Avalonia.Native/AvaloniaNativePlatform.cs
@@ -1,6 +1,5 @@
-﻿// Copyright (c) The Avalonia Project. All rights reserved.
+// Copyright (c) The Avalonia Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
-
 using System;
 using System.Diagnostics.Contracts;
 using System.Runtime.InteropServices;
--- src/Avalonia.Native/AvaloniaNativePlatformExtensions.cs
+++ src/Avalonia.Native/AvaloniaNativePlatformExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) The Avalonia Project. All rights reserved.
+// Copyright (c) The Avalonia Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
 using System;
