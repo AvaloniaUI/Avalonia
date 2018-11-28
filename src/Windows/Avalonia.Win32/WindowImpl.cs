743e61cfde37e608820e1d6c7e0671a5e22e781e
Dan Walmsley <dan@walms.co.uk>
2018-11-28 16:57:49 +0000

    Revert "Set win32 WindowStyle flags individually."
    This reverts commit c4cc3514a8c35f36230f8902bbddbf7bffaf624f.

--- src/Windows/Avalonia.Win32/WindowImpl.cs
+++ src/Windows/Avalonia.Win32/WindowImpl.cs
@@ -265,18 +265,13 @@ namespace Avalonia.Win32
                 return;
             }
 
-            var style = (UnmanagedMethods.WindowStyles)UnmanagedMethods.GetWindowLong(_hwnd, (int)UnmanagedMethods.WindowLongParam.GWL_STYLE);
+            var style = (UnmanagedMethods.WindowStyles)UnmanagedMethods.GetWindowLong(_hwnd, (int)UnmanagedMethods.WindowLongParam.GWL_STYLE);            
 
-            if (value)
-            {
-                style |= (WindowStyles.WS_CAPTION & WindowStyles.WS_MINIMIZEBOX & WindowStyles.WS_MAXIMIZEBOX & WindowStyles.WS_SYSMENU & WindowStyles.WS_SIZEFRAME);
-            }
-            else
-            {
-                style ^= (WindowStyles.WS_CAPTION & WindowStyles.WS_MINIMIZEBOX & WindowStyles.WS_MAXIMIZEBOX & WindowStyles.WS_SYSMENU & WindowStyles.WS_SIZEFRAME);
-            }
-            
-            UnmanagedMethods.GetWindowRect(_hwnd, out var windowRect);
+            style |= UnmanagedMethods.WindowStyles.WS_OVERLAPPEDWINDOW;
+
+            UnmanagedMethods.RECT windowRect;
+
+            UnmanagedMethods.GetWindowRect(_hwnd, out windowRect);
 
             Rect newRect;
             var oldThickness = BorderThickness;
@@ -441,7 +436,7 @@ namespace Avalonia.Win32
                 0,
                 atom,
                 null,
-                (int)(WindowStyles.WS_CAPTION & WindowStyles.WS_MINIMIZEBOX & WindowStyles.WS_MAXIMIZEBOX & WindowStyles.WS_SYSMENU & WindowStyles.WS_SIZEFRAME),
+                (int)UnmanagedMethods.WindowStyles.WS_OVERLAPPEDWINDOW,
                 UnmanagedMethods.CW_USEDEFAULT,
                 UnmanagedMethods.CW_USEDEFAULT,
                 UnmanagedMethods.CW_USEDEFAULT,
