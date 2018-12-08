ed90143e738f1181df2fcee1a1e2e164c6b596ef
Jumar Macato <hikari.netto23@gmail.com>
2018-12-08 16:44:51 +0800

    Add `Point` animator.

---
+++ src/Avalonia.Visuals/Animation/Animators/PointAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia.Logging;
+using Avalonia.Media;
+
+namespace Avalonia.Animation.Animators
+{
+    /// <summary>
+    /// Animator that handles <see cref="Point"/> properties.
+    /// </summary>
+    public class PointAnimator : Animator<Point>
+    {
+        public override Point Interpolate(double progress, Point oldValue, Point newValue)
+        {
+            var deltaX = newValue.X - oldValue.Y;
+            var deltaY = newValue.X - oldValue.Y;
+
+            var nX = progress * deltaX + oldValue.X;
+            var nY = progress * deltaY + oldValue.Y;
+            return new Point(nX, nY);
+        }
+    }
+}
