4c4e72ab862fd29889059918d038c9db5bf3ca9b
mstr2 <oxmyx@gmx.de>
2019-01-15 18:24:21 +0100

    Moved GL/EGL errors to separate enumeration

--- src/Avalonia.OpenGL/OpenGlException.cs
+++ src/Avalonia.OpenGL/OpenGlException.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Reflection;
 
 namespace Avalonia.OpenGL
 {
@@ -18,33 +17,26 @@ namespace Avalonia.OpenGL
 
         public static OpenGlException GetFormattedException(string funcName, EglInterface egl)
         {
-            return GetFormattedException(typeof(EglConsts).GetFields(), funcName, 0x3000, 0x301F, egl.GetError());
+            return GetFormattedException(typeof(EglErrors), funcName, egl.GetError());
         }
 
         public static OpenGlException GetFormattedException(string funcName, GlInterface gl)
         {
-            return GetFormattedException(typeof(GlConsts).GetFields(), funcName, 0x0500, 0x0505, gl.GetError());
+            return GetFormattedException(typeof(GlErrors), funcName, gl.GetError());
         }
 
-        private static OpenGlException GetFormattedException(
-            FieldInfo[] fields, string funcName, int minValue, int maxValue, int errorCode)
+        private static OpenGlException GetFormattedException(Type consts, string funcName, int errorCode)
         {
-            foreach (var field in fields)
+            try
             {
-                int value = (int)field.GetValue(null);
-                if (value < minValue || value > maxValue)
-                {
-                    continue;
-                }
-
-                if (value == errorCode)
-                {
-                    return new OpenGlException(
-                        $"{funcName} failed with error {field.Name} (0x{errorCode.ToString("X")})", errorCode);
-                }
+                string errorName = Enum.GetName(consts, errorCode);
+                return new OpenGlException(
+                    $"{funcName} failed with error {errorName} (0x{errorCode.ToString("X")})", errorCode);
             }
-
-            return new OpenGlException($"{funcName} failed with error 0x{errorCode.ToString("X")}", errorCode);
+            catch (ArgumentException)
+            {
+                return new OpenGlException($"{funcName} failed with error 0x{errorCode.ToString("X")}", errorCode);
+            }
         }
     }
 }
