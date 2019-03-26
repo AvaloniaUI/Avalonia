18f2e4e6be6eba17d0b2432555a09f733eb07a43
Steven Kirk <grokys@gmail.com>
2019-03-27 00:34:02 +0100

    Re-added an obsolete DropDown control.
    To provide backwards compatibility for the moment.

---
+++ src/Avalonia.Controls/DropDown.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Avalonia.Logging;
+using Avalonia.Styling;
+
+namespace Avalonia.Controls
+{
+    [Obsolete("Use ComboBox")]
+    public class DropDown : ComboBox, IStyleable
+    {
+        public DropDown()
+        {
+            Logger.Warning(LogArea.Control, this, "DropDown is deprecated: Use ComboBox");
+        }
+
+        Type IStyleable.StyleKey => typeof(ComboBox);
+    }
+
+    [Obsolete("Use ComboBoxItem")]
+    public class DropDownItem : ComboBoxItem, IStyleable
+    {
+        public DropDownItem()
+        {
+            Logger.Warning(LogArea.Control, this, "DropDownItem is deprecated: Use ComboBoxItem");
+        }
+
+        Type IStyleable.StyleKey => typeof(ComboBoxItem);
+    }
+}
