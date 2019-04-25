351f5d10f01eb1ae8d1d5a07aa6edf416089b218
Dan Walmsley <dan@walms.co.uk>
2019-04-25 21:33:41 +0100

    AdornerDecorator passes on OnAttachedToLogicalTree to the AdornerLayer

--- src/Avalonia.Controls/Primitives/AdornerDecorator.cs
+++ src/Avalonia.Controls/Primitives/AdornerDecorator.cs
@@ -1,6 +1,8 @@
 // Copyright (c) The Avalonia Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
+using Avalonia.LogicalTree;
+
 namespace Avalonia.Controls.Primitives
 {
     public class AdornerDecorator : Decorator
@@ -11,6 +13,13 @@ namespace Avalonia.Controls.Primitives
             ((ISetLogicalParent)AdornerLayer).SetParent(this);
             AdornerLayer.ZIndex = int.MaxValue;
             VisualChildren.Add(AdornerLayer);
+        }
+
+        protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToLogicalTree(e);
+
+            ((ILogical)AdornerLayer).NotifyAttachedToLogicalTree(e);
         }
 
         public AdornerLayer AdornerLayer
