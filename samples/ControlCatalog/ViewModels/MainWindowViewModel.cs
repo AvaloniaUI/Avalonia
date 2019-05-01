f09228cc483d015bfc747d0efd22b6bb889dc7e6
Dan Walmsley <dan@walms.co.uk>
2019-05-01 20:34:36 +0100

    simplify the usage of notification manager inside main window.

--- samples/ControlCatalog/ViewModels/MainWindowViewModel.cs
+++ samples/ControlCatalog/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,19 +24,19 @@ namespace ControlCatalog.ViewModels
                 await Task.Delay(5000);
 
 
-                Application.Current.MainWindow.LocalNotificationManager.Show(new NotificationViewModel { Title = "Warning", Message = "Please save your work before closing." });
+                NotificationManager.Show(new NotificationViewModel { Title = "Warning", Message = "Please save your work before closing." });
 
                 await Task.Delay(1500);
-                Application.Current.MainWindow.LocalNotificationManager.Show(new NotificationContent { Message = "Test2", Type = NotificationType.Error });
+                NotificationManager.Show(new NotificationContent { Message = "Test2", Type = NotificationType.Error });
 
                 await Task.Delay(2000);
-                Application.Current.MainWindow.LocalNotificationManager.Show(new NotificationContent { Message = "Test3", Type = NotificationType.Warning });
+                NotificationManager.Show(new NotificationContent { Message = "Test3", Type = NotificationType.Warning });
 
                 await Task.Delay(2500);
-                Application.Current.MainWindow.LocalNotificationManager.Show(new NotificationContent { Message = "Test4", Type = NotificationType.Success });
+                NotificationManager.Show(new NotificationContent { Message = "Test4", Type = NotificationType.Success });
 
                 await Task.Delay(500);
-                Application.Current.MainWindow.LocalNotificationManager.Show("Test5");
+                NotificationManager.Show("Test5");
 
             });
         }
