b4f7746c2a6ba9ed60bd83afc85b9cab8ff6cdc0
Steven Kirk <grokys@gmail.com>
2019-05-07 07:05:11 +0200

    Add AppBuilder log extensions that take an area.

--- src/Avalonia.Logging.Serilog/SerilogExtensions.cs
+++ src/Avalonia.Logging.Serilog/SerilogExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
 using Serilog;
+using Serilog.Configuration;
+using Serilog.Filters;
 using SerilogLevel = Serilog.Events.LogEventLevel;
 
 namespace Avalonia.Logging.Serilog
@@ -9,6 +12,8 @@ namespace Avalonia.Logging.Serilog
     /// </summary>
     public static class SerilogExtensions
     {
+        private const string DefaultTemplate = "[{Area}] {Message} ({SourceType} #{SourceHash})";
+
         /// <summary>
         /// Logs Avalonia events to the <see cref="System.Diagnostics.Debug"/> sink.
         /// </summary>
@@ -23,7 +28,31 @@ namespace Avalonia.Logging.Serilog
         {
             SerilogLogger.Initialize(new LoggerConfiguration()
                 .MinimumLevel.Is((SerilogLevel)level)
-                .WriteTo.Debug(outputTemplate: "{Area}: {Message}")
+                .Enrich.FromLogContext()
+                .WriteTo.Debug(outputTemplate: DefaultTemplate)
+                .CreateLogger());
+            return builder;
+        }
+
+        /// <summary>
+        /// Logs Avalonia events to the <see cref="System.Diagnostics.Debug"/> sink.
+        /// </summary>
+        /// <typeparam name="T">The application class type.</typeparam>
+        /// <param name="builder">The app builder instance.</param>
+        /// <param name="area">The area to log. Valid values are listed in <see cref="LogArea"/>.</param>
+        /// <param name="level">The minimum level to log.</param>
+        /// <returns>The app builder instance.</returns>
+        public static T LogToDebug<T>(
+            this T builder,
+            string area,
+            LogEventLevel level = LogEventLevel.Warning)
+                where T : AppBuilderBase<T>, new()
+        {
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Is((SerilogLevel)level)
+                .Filter.ByIncludingOnly(Matching.WithProperty("Area", area))
+                .Enrich.FromLogContext()
+                .WriteTo.Debug(outputTemplate: DefaultTemplate)
                 .CreateLogger());
             return builder;
         }
@@ -42,7 +71,31 @@ namespace Avalonia.Logging.Serilog
         {
             SerilogLogger.Initialize(new LoggerConfiguration()
                 .MinimumLevel.Is((SerilogLevel)level)
-                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .Enrich.FromLogContext()
+                .WriteTo.Trace(outputTemplate: DefaultTemplate)
+                .CreateLogger());
+            return builder;
+        }
+
+        /// <summary>
+        /// Logs Avalonia events to the <see cref="System.Diagnostics.Trace"/> sink.
+        /// </summary>
+        /// <typeparam name="T">The application class type.</typeparam>
+        /// <param name="builder">The app builder instance.</param>
+        /// <param name="area">The area to log. Valid values are listed in <see cref="LogArea"/>.</param>
+        /// <param name="level">The minimum level to log.</param>
+        /// <returns>The app builder instance.</returns>
+        public static T LogToTrace<T>(
+            this T builder,
+            string area,
+            LogEventLevel level = LogEventLevel.Warning)
+                where T : AppBuilderBase<T>, new()
+        {
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Is((SerilogLevel)level)
+                .Filter.ByIncludingOnly(Matching.WithProperty("Area", area))
+                .Enrich.FromLogContext()
+                .WriteTo.Trace(outputTemplate: DefaultTemplate)
                 .CreateLogger());
             return builder;
         }
