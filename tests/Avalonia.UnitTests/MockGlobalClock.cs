4a386d1b84e879bec43f89c084b2ed073c4f4484
Steven Kirk <grokys@gmail.com>
2019-05-14 14:36:59 +0200

    Added skipped failing leak test for transitions.
    It's leaking on master too, so address this separately.

---
+++ tests/Avalonia.UnitTests/MockGlobalClock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Avalonia.Animation;
+
+namespace Avalonia.UnitTests
+{
+    public class MockGlobalClock : ClockBase, IGlobalClock
+    {
+        public new void Pulse(TimeSpan systemTime) => base.Pulse(systemTime);
+    }
+}
