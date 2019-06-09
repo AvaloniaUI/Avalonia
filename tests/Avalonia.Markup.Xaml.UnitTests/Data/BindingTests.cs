433c70a6c826af2bf65fad745a4ff160502deab5
José Pedro <jpedrolmail@gmail.com>
2019-06-09 21:26:08 +0100

    Added BindingOperations.DoNothing. Added tests.

--- tests/Avalonia.Markup.Xaml.UnitTests/Data/BindingTests.cs
+++ tests/Avalonia.Markup.Xaml.UnitTests/Data/BindingTests.cs
@@ -1,20 +1,10 @@
 // Copyright (c) The Avalonia Project. All rights reserved.
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Linq;
 using Avalonia.Controls;
 using Avalonia.Data;
-using Avalonia.Markup.Data;
-using Moq;
-using Xunit;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using Avalonia.UnitTests;
-using Avalonia.Data.Converters;
-using Avalonia.Data.Core;
+using Xunit;
 
 namespace Avalonia.Markup.Xaml.UnitTests.Data
 {
@@ -34,10 +24,16 @@ namespace Avalonia.Markup.Xaml.UnitTests.Data
                 var window = (Window)loader.Load(xaml);
                 var textBlock = window.FindControl<TextBlock>("textBlock");
 
-                window.DataContext = "foo";
                 window.ApplyTemplate();
 
+                window.DataContext = "foo";
                 Assert.Equal("foo", textBlock.Text);
+
+                window.DataContext = BindingOperations.DoNothing;
+                Assert.Equal("foo", textBlock.Text);
+
+                window.DataContext = "bar";
+                Assert.Equal("bar", textBlock.Text);
             }
         }
     }
