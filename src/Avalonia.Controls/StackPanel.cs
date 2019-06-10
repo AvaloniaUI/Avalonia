8304eaab8502ac0e288b7c31bd40e721df7f8a34
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-06-10 21:26:14 +0200

    Fix

--- src/Avalonia.Controls/StackPanel.cs
+++ src/Avalonia.Controls/StackPanel.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
 using System;
-using Avalonia.Input
+using Avalonia.Input;
 
 namespace Avalonia.Controls
 {
