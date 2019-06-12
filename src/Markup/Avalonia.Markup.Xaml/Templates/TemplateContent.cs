76f9c8fdc5831c4571147cf098b082741ea49f44
Nikita Tsukanov <keks9n@gmail.com>
2019-06-12 10:22:38 +0300

    Removed legacy XAML loader

--- src/Markup/Avalonia.Markup.Xaml/Templates/TemplateContent.cs
+++ src/Markup/Avalonia.Markup.Xaml/Templates/TemplateContent.cs
@@ -7,42 +7,16 @@ using System.Collections.Generic;
 
 namespace Avalonia.Markup.Xaml.Templates
 {
-    using Portable.Xaml;
-
-    public class TemplateContent
+    
+    public static class TemplateContent
     {
-        public TemplateContent(IEnumerable<NamespaceDeclaration> namespaces, XamlReader reader,
-            IAmbientProvider ambientProvider)
-        {
-            ParentAmbientProvider = ambientProvider;
-            List = new XamlNodeList(reader.SchemaContext);
-
-            //we need to rpeserve all namespace and prefixes to writer
-            //otherwise they are lost. a bug in Portable.xaml or by design ??
-            foreach (var ns in namespaces)
-            {
-                List.Writer.WriteNamespace(ns);
-            }
-
-            XamlServices.Transform(reader, List.Writer);
-        }
-
-        public XamlNodeList List { get; }
-
-        private IAmbientProvider ParentAmbientProvider { get; }
-
-        public IControl Load()
-        {
-            return (IControl)AvaloniaXamlLoader.LoadFromReader(List.GetReader(), parentAmbientProvider: ParentAmbientProvider);
-        }
-
         public static IControl Load(object templateContent)
         {
             if (templateContent is Func<IServiceProvider, object> direct)
             {
                 return (IControl)direct(null);
             }
-            return ((TemplateContent)templateContent).Load();
+            throw new ArgumentException(nameof(templateContent));
         }
     }
 }
