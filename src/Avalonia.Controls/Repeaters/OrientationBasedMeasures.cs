e8a118e11c1fc6cfd031c547af4967199c1f8e76
Steven Kirk <grokys@gmail.com>
2019-06-25 13:58:04 +0200

    Added headers.

--- src/Avalonia.Controls/Repeaters/OrientationBasedMeasures.cs
+++ src/Avalonia.Controls/Repeaters/OrientationBasedMeasures.cs
@@ -1,4 +1,9 @@
-﻿namespace Avalonia.Controls.Repeaters
+﻿// This source file is adapted from the WinUI project.
+// (https://github.com/microsoft/microsoft-ui-xaml)
+//
+// Licensed to The Avalonia Project under MIT License, courtesy of The .NET Foundation.
+
+namespace Avalonia.Controls.Repeaters
 {
     internal class OrientationBasedMeasures
     {
--- src/Avalonia.Controls/Repeaters/ViewportManager.cs
+++ src/Avalonia.Controls/Repeaters/ViewportManager.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// This source file is adapted from the WinUI project.
+// (https://github.com/microsoft/microsoft-ui-xaml)
+//
+// Licensed to The Avalonia Project under MIT License, courtesy of The .NET Foundation.
+
+using System;
 using System.Collections.Generic;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
