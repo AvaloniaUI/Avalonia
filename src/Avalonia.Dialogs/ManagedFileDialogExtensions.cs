f5c85674591f4e89a6a29da9e433b680b517006f
Dan Walmsley <dan@walms.co.uk>
2019-07-27 10:58:27 +0100

    allow use of custom window for hosting file dialogs

--- src/Avalonia.Dialogs/ManagedFileDialogExtensions.cs
+++ src/Avalonia.Dialogs/ManagedFileDialogExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Avalonia;
@@ -10,46 +11,55 @@ namespace Avalonia.Dialogs
 {
 	public static class ManagedFileDialogExtensions
 	{
-		class ManagedSystemDialogImpl : ISystemDialogImpl
-		{
-			async Task<string[]> Show(SystemDialog d, IWindowImpl parent)
-			{
-				var model = new ManagedFileChooserViewModel((FileSystemDialog)d);
+        class CustomWindowManagedSystemDialogImpl<T> : ISystemDialogImpl where T : Window, new()
+        {
+            async Task<string[]> Show(SystemDialog d, IWindowImpl parent)
+            {
+                var model = new ManagedFileChooserViewModel((FileSystemDialog)d);
 
-				var dialog = new ManagedFileDialog
-				{
-					DataContext = model
-				};
+                var dialog = new T
+                {
+                    Content = new ManagedFileChooser(),
+                    DataContext = model
+                };
 
-				string[] result = null;
-				model.CompleteRequested += items =>
-				{
-					result = items;
-					dialog.Close();
-				};
-				model.CancelRequested += dialog.Close;
+                string[] result = null;
+                model.CompleteRequested += items =>
+                {
+                    result = items;
+                    dialog.Close();
+                };
+                model.CancelRequested += dialog.Close;
 
-				await dialog.ShowDialog<object>(parent);
-				return result;
-			}
+                await dialog.ShowDialog<object>(parent);
+                return result;
+            }
 
-			public async Task<string[]> ShowFileDialogAsync(FileDialog dialog, IWindowImpl parent)
-			{
-				return await Show(dialog, parent);
-			}
+            public async Task<string[]> ShowFileDialogAsync(FileDialog dialog, IWindowImpl parent)
+            {
+                return await Show(dialog, parent);
+            }
 
-			public async Task<string> ShowFolderDialogAsync(OpenFolderDialog dialog, IWindowImpl parent)
-			{
-				return (await Show(dialog, parent))?.FirstOrDefault();
-			}
-		}
+            public async Task<string> ShowFolderDialogAsync(OpenFolderDialog dialog, IWindowImpl parent)
+            {
+                return (await Show(dialog, parent))?.FirstOrDefault();
+            }
+        }
 
-		public static TAppBuilder UseManagedSystemDialogs<TAppBuilder>(this TAppBuilder builder)
+        public static TAppBuilder UseManagedSystemDialogs<TAppBuilder>(this TAppBuilder builder)
 			where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
 		{
 			builder.AfterSetup(_ =>
-				AvaloniaLocator.CurrentMutable.Bind<ISystemDialogImpl>().ToSingleton<ManagedSystemDialogImpl>());
+				AvaloniaLocator.CurrentMutable.Bind<ISystemDialogImpl>().ToSingleton<CustomWindowManagedSystemDialogImpl<Window>>());
 			return builder;
 		}
-	}
+
+        public static TAppBuilder UseManagedSystemDialogs<TAppBuilder, TWindow>(this TAppBuilder builder)
+            where TAppBuilder : AppBuilderBase<TAppBuilder>, new() where TWindow : Window, new()
+        {
+            builder.AfterSetup(_ =>
+                AvaloniaLocator.CurrentMutable.Bind<ISystemDialogImpl>().ToSingleton<CustomWindowManagedSystemDialogImpl<TWindow>>());
+            return builder;
+        }
+    }
 }
