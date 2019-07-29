63601c018e087f6524bc05c65f691230c28601ec
Jumar Macato <hikari.netto23@gmail.com>
2019-07-29 13:46:49 +0800

    Uhh at least it works.. albeit super slow..

--- samples/RenderDemo/Pages/AnimationsPage.xaml.cs
+++ samples/RenderDemo/Pages/AnimationsPage.xaml.cs
@@ -1,43 +1,170 @@
+using System.Collections.Generic;
 using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Shapes;
 using Avalonia.Data;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using ReactiveUI;
 using RenderDemo.ViewModels;
+using System;
+using System.Linq;
+using Avalonia.Controls.Presenters;
+using Avalonia.Markup.Xaml.Templates;
+using Avalonia.Controls.Templates;
+using System.Linq.Expressions;
 
 namespace RenderDemo.Pages
 {
-    public class AnimationsPage : UserControl
-    {
-        public AnimationsPage()
-        {
-            InitializeComponent();
-            this.DataContext = new AnimationsPageViewModel();
-        }
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+	public class AnimationsPage : UserControl
+	{
+		private ContentPresenter _headerSection;
 
-        private void ToggleClock(object sender, RoutedEventArgs args)
-        {
-            var button = sender as Button;
-            var clock = button.Clock;
+		public AnimationsPage()
+		{
+			InitializeComponent();
+			var vm = new AnimationsPageViewModel();
+			this.DataContext = vm;
+			this._headerSection = this.FindControl<ContentPresenter>("HeaderSection");
 
-            if (clock.PlayState == PlayState.Run)
-            {
-                clock.PlayState = PlayState.Pause;
-            }
-            else if (clock.PlayState == PlayState.Pause)
-            {
-                clock.PlayState = PlayState.Run;
-            }
-        }
-    }
+			vm.WhenAnyValue(x => x.DataHeaderDescriptors)
+			  .Where(x => x != null)
+			  .Do(GenerateHeaders)
+			  .Do(ActivateItemTemplateFunc)
+			  .Subscribe();
+
+		}
+
+		private void ActivateItemTemplateFunc(XDataGridHeaderDescriptors[] obj)
+		{
+			this.FindControl<ItemsRepeater>("repeater").ItemTemplate
+				 = new FuncDataTemplate<object>((rowData, _) =>
+				 {
+					 return GenerateCellDataTemplate(rowData, obj);
+				 });
+		}
+
+		private IControl GenerateCellDataTemplate(object rowData, XDataGridHeaderDescriptors[] obj)
+		{
+			var rowRoot = new Grid
+			{
+				DataContext = rowData
+			};
+
+			var rowCellList = new List<IControl>();
+
+			for (int i = 0; i < obj.Length; i++)
+			{
+				var headerDesc = obj[i];
+				var colDefHeaderCell = new ColumnDefinition(GridLength.Parse("*"));
+				var headerColWidthBinding = HeaderColWidths[i];
+
+				headerColWidthBinding.WhenAnyValue(x => x.Width)
+									 .Do(x => colDefHeaderCell.Width = x)
+									 .Subscribe();
+
+				var colDefGridSplitter = new ColumnDefinition(GridLength.Parse("Auto"));
+
+				rowRoot.ColumnDefinitions.AddRange(new[] { colDefHeaderCell, colDefGridSplitter });
+
+				var rowCell = new Grid();
+				var boundCellContent = new TextBlock();
+				var newBind = new Binding(headerDesc.PropertyName);
+				boundCellContent.Bind(TextBlock.TextProperty, newBind);
+
+				rowCell.Children.Add(boundCellContent);
+
+				Grid.SetColumn(rowCell, headerDesc.ColumnDefinitionIndex);
+
+				rowCellList.Add(rowCell);
+			}
+
+
+			headerRoot.WhenAnyValue(x => x.Width)
+			.Subscribe(x => rowRoot.Width = x);
+
+			rowRoot.Children.AddRange(rowCellList);
+
+			return rowRoot;
+		}
+
+		public class BoundColumnWidth : ReactiveObject
+		{
+			GridLength _width;
+			public GridLength Width
+			{
+				get => _width;
+				set => this.RaiseAndSetIfChanged(ref _width, value);
+			}
+		}
+
+		public Dictionary<int, BoundColumnWidth> HeaderColWidths
+		  = new Dictionary<int, BoundColumnWidth>();
+		private DockPanel dock;
+		private Grid headerRoot;
+
+		private void GenerateHeaders(XDataGridHeaderDescriptors[] headerDesc)
+		{
+			var colCount = headerDesc.Length;
+
+			this.headerRoot = new Grid();
+			var k = 0;
+			this.dock = this.FindControl<DockPanel>("dock");
+			dock.WhenAnyValue(x => x.Bounds)
+                .DistinctUntilChanged()
+                .Take(20)
+                .ObserveOn(RxApp.MainThreadScheduler)
+				.Subscribe(x => headerRoot.Width = x.Width);
+
+			headerRoot.HorizontalAlignment = HorizontalAlignment.Left;
+
+			for (int i = 0; i < colCount; i++)
+			{
+				var curDesc = headerDesc[i];
+
+				var colDefHeaderCell = new ColumnDefinition(GridLength.Parse("*"));
+				HeaderColWidths.Add(curDesc.Index, new BoundColumnWidth());
+
+				colDefHeaderCell.WhenAnyValue(x => x.Width)
+								.Do(x => HeaderColWidths[curDesc.Index].Width = x)
+								.Subscribe();
+
+				var colDefGridSplitter = new ColumnDefinition(GridLength.Parse("Auto"));
+
+				headerRoot.ColumnDefinitions.AddRange(new[] { colDefHeaderCell, colDefGridSplitter });
+
+				// Can be replaced with a DataHeaderCell class then
+				// adding the binding as DataContext.
+				var headerCell = new Grid();
+				headerCell.Children.Add(new TextBlock() { Text = curDesc.HeaderText });
+				curDesc.ColumnDefinitionIndex = k;
+				Grid.SetColumn(headerCell, k);
+				Grid.SetColumnSpan(headerCell, 1);
+				k++;
+
+				var gridSplitter = new GridSplitter();
+				Grid.SetColumn(gridSplitter, k);
+				Grid.SetColumnSpan(gridSplitter, 1);
+				k++;
+
+				headerRoot.Children.AddRange(new IControl[] { headerCell, gridSplitter });
+			}
+
+			headerRoot.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Parse("*")));
+
+			_headerSection.Content = headerRoot;
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
 }
--- samples/RenderDemo/ViewModels/AnimationsPageViewModel.cs
+++ samples/RenderDemo/ViewModels/AnimationsPageViewModel.cs
@@ -1,26 +1,136 @@
 ﻿using System;
 using ReactiveUI;
 using Avalonia.Animation;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Avalonia.Collections;
+using System.Reactive.Linq;
+using Avalonia.Markup.Xaml.Templates;
 
 namespace RenderDemo.ViewModels
 {
-    public class AnimationsPageViewModel : ReactiveObject
-    {
-        private bool _isPlaying = true;
 
-        private string _playStateText = "Pause animations on this page";
+	public class XDataGridHeaderDescriptors
+	{
+		public int Index { get; set; }
+		public string HeaderText { get; set; }
+		public string PropertyName { get; set; }
+		public string SharedSizeGroup => PropertyName + Index;
+		internal int ColumnDefinitionIndex { get; set; }
+	}
 
-        public void TogglePlayState()
-        {
-            PlayStateText = _isPlaying
-                ? "Resume animations on this page" : "Pause animations on this page";
-            _isPlaying = !_isPlaying;
-        }
+	public class SampleData
+	{
+		[DisplayName("Index")]
+		public int Index { get; set; }
 
-        public string PlayStateText
-        {
-            get { return _playStateText; }
-            set { this.RaiseAndSetIfChanged(ref _playStateText, value); }
-        }
-    }
+		[DisplayName("Some Text")]
+		public string SomeText { get; set; }
+
+
+		[DisplayName("Random Number 1")]
+		public string RndNum1 { get; set; }
+
+		[DisplayName("Random Number 2")]
+		public string RndNum2 { get; set; }
+
+		[DisplayName("Random Number 3")]
+		public string RndNum3 { get; set; }
+	}
+
+	public class AnimationsPageViewModel : ReactiveObject
+	{
+		private static Random random = new Random();
+		public static string RandomString(int length)
+		{
+			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			return new string(Enumerable.Repeat(chars, length)
+			  .Select(s => s[random.Next(s.Length)]).ToArray());
+		}
+
+		public AnimationsPageViewModel()
+		{
+
+			this.WhenAnyValue(x => x.DataType)
+				.Where(x => x != null)
+				.Subscribe(AutoGenerateHeaders);
+
+			var k = new AvaloniaList<object>();
+			var r = new Random();
+
+			for (int i = 0; i < 100_000; i++)
+			{
+				var l = new SampleData()
+				{
+					Index = i,
+					SomeText = RandomString(10),
+					RndNum1 = r.NextDouble().ToString(),
+					RndNum2 = r.NextDouble().ToString(),
+					RndNum3 = r.NextDouble().ToString(),
+				};
+
+				k.Add(l);
+			}
+
+			DataType = typeof(SampleData);
+			DataRows = k;
+		}
+
+		private void AutoGenerateHeaders(Type obj)
+		{
+			int i = 0;
+			var xdghList = new List<XDataGridHeaderDescriptors>();
+
+			foreach (var property in obj.GetProperties())
+			{
+				var q = property.GetCustomAttributes(typeof(DisplayNameAttribute), false);
+
+				if (!q.Any())
+					continue;
+
+				var attrib = (DisplayNameAttribute)q.Single();
+				var dName = attrib.DisplayName;
+
+				var xdgh = new XDataGridHeaderDescriptors()
+				{
+					Index = i,
+					HeaderText = dName,
+					PropertyName = property.Name,
+				};
+
+				i++;
+
+				xdghList.Add(xdgh);
+			}
+
+			DataHeaderDescriptors = xdghList.ToArray();
+		}
+
+		Type dataType;
+		public Type DataType
+		{
+			get => dataType;
+			set => this.RaiseAndSetIfChanged(ref dataType, value);
+		}
+
+
+		
+
+
+		XDataGridHeaderDescriptors[] dataHeaderDescriptors;
+		public XDataGridHeaderDescriptors[] DataHeaderDescriptors
+		{
+			get => dataHeaderDescriptors;
+			set => this.RaiseAndSetIfChanged(ref dataHeaderDescriptors, value);
+		}
+
+		IAvaloniaList<object> dataRows;
+
+		public IAvaloniaList<object> DataRows
+		{
+			get => dataRows;
+			set => this.RaiseAndSetIfChanged(ref dataRows, value);
+		}
+	}
 }
