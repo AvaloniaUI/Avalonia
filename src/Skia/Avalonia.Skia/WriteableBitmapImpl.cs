a78ee488fdc02cf4da5a613ca2ed938a8c982375
Benedikt Schroeder <benedikt.schroeder@outlook.de>
2019-08-23 12:10:51 +0200

    Revert SKImage usage

--- src/Skia/Avalonia.Skia/WriteableBitmapImpl.cs
+++ src/Skia/Avalonia.Skia/WriteableBitmapImpl.cs
@@ -17,14 +17,12 @@ namespace Avalonia.Skia
     /// </summary>
     internal class WriteableBitmapImpl : IWriteableBitmapImpl, IDrawableBitmapImpl
     {
-        private readonly SKImage _image;
+        private static readonly SKBitmapReleaseDelegate s_releaseDelegate = ReleaseProc;
         private readonly SKBitmap _bitmap;
         private readonly SKPixmap _pixmap;
-        private readonly IUnmanagedBlob _blob;
+        private readonly object _lock = new object();
 
         private SKSurface _surface;
-
-        internal readonly object _lock = new object();
 
         /// <summary>
         /// Create new writeable bitmap.
@@ -43,30 +41,21 @@ namespace Avalonia.Skia
 
             if (runtimePlatform != null)
             {
-                var nfo = new SKImageInfo(size.Width, size.Height, colorType, SKAlphaType.Premul);
-
-                _blob = runtimePlatform.AllocBlob(nfo.BytesSize);
-
-                _pixmap = new SKPixmap(nfo, _blob.Address, nfo.RowBytes);
-
                 _bitmap = new SKBitmap();
 
-                _bitmap.InstallPixels(_pixmap);
+                var nfo = new SKImageInfo(size.Width, size.Height, colorType, SKAlphaType.Premul);
+                var blob = runtimePlatform.AllocBlob(nfo.BytesSize);
 
-                _bitmap.SetImmutable();
-
-                _image = SKImage.FromBitmap(_bitmap);
+                _bitmap.InstallPixels(nfo, blob.Address, nfo.RowBytes, s_releaseDelegate, blob);
             }
             else
             {
                 _bitmap = new SKBitmap(size.Width, size.Height, colorType, SKAlphaType.Premul);
-
-                _pixmap = _bitmap.PeekPixels();
-
-                _image = SKImage.FromBitmap(_bitmap);
             }
 
-            _pixmap.Erase(SKColor.Empty);
+            _bitmap.Erase(SKColor.Empty);
+
+            _pixmap = _bitmap.PeekPixels();
         }
 
         public Vector Dpi { get; }
@@ -80,21 +69,19 @@ namespace Avalonia.Skia
         public void Draw(DrawingContextImpl context, SKRect sourceRect, SKRect destRect, SKPaint paint)
         {
             lock (_lock)
-                context.Canvas.DrawImage(_image, sourceRect, destRect, paint);
+                context.Canvas.DrawBitmap(_bitmap, sourceRect, destRect, paint);
         }
 
         /// <inheritdoc />
         public void Dispose()
         {
-            _surface?.Dispose();
-            _image.Dispose();
             _bitmap.Dispose();
-            _pixmap.Dispose();
-            _blob.Dispose();
         }
 
         public IDrawingContextImpl CreateDrawingContext(IVisualBrushRenderer visualBrushRenderer)
         {
+            Monitor.Enter(_lock);
+
             if (_surface == null)
             {
                 _surface = SKSurface.Create(_pixmap);
@@ -107,7 +94,11 @@ namespace Avalonia.Skia
                 VisualBrushRenderer = visualBrushRenderer
             };
 
-            return new DrawingContextImpl(createInfo, Disposable.Create(() => _bitmap.NotifyPixelsChanged()));
+            return new DrawingContextImpl(createInfo, Disposable.Create(() =>
+            {
+                _bitmap.NotifyPixelsChanged();
+                Monitor.Exit(_lock);
+            }));
         }
 
         /// <inheritdoc />
@@ -129,7 +120,7 @@ namespace Avalonia.Skia
         }
 
         /// <inheritdoc />
-        public ILockedFramebuffer Lock() => new BitmapFramebuffer(this, _bitmap);
+        public ILockedFramebuffer Lock() => new BitmapFramebuffer(this, _bitmap, _pixmap);
 
         /// <summary>
         /// Get snapshot as image.
@@ -140,57 +131,67 @@ namespace Avalonia.Skia
             lock (_lock)
                 return SKImage.FromPixelCopy(_pixmap);
         }
-    }
-
-    /// <summary>
-    /// Framebuffer for Pixmap.
-    /// </summary>
-    public class BitmapFramebuffer : ILockedFramebuffer
-    {
-        private WriteableBitmapImpl _parent;
-        private SKBitmap _bitmap;
 
         /// <summary>
-        /// Create framebuffer from given bitmap.
+        /// Release given unmanaged blob.
         /// </summary>
-        /// <param name="parent">Parent bitmap impl.</param>
-        /// <param name="bitmap">Bitmap</param>
-        internal BitmapFramebuffer(WriteableBitmapImpl parent, SKBitmap bitmap)
+        /// <param name="address">Blob address.</param>
+        /// <param name="ctx">Blob.</param>
+        private static void ReleaseProc(IntPtr address, object ctx)
         {
-            _parent = parent;
-            _bitmap = bitmap;
-            var pixmap = bitmap.PeekPixels();
-            Address = pixmap.GetPixels();
-            Size = new PixelSize(pixmap.Width, pixmap.Height);
-            RowBytes = pixmap.RowBytes;
-            Dpi = SkiaPlatform.DefaultDpi;
-            Format = pixmap.ColorType.ToPixelFormat();
-            Monitor.Enter(parent._lock);
+            ((IUnmanagedBlob)ctx).Dispose();
         }
 
-        /// <inheritdoc />
-        public void Dispose()
+        /// <summary>
+        /// Framebuffer for bitmap.
+        /// </summary>
+        private class BitmapFramebuffer : ILockedFramebuffer
         {
-            _bitmap.NotifyPixelsChanged();
-            _parent.Version++;
-            Monitor.Exit(_parent._lock);
-            _bitmap = null;
-            _parent = null;
+            private WriteableBitmapImpl _parent;
+            private SKBitmap _bitmap;
+
+            /// <summary>
+            /// Create framebuffer from given bitmap.
+            /// </summary>
+            /// <param name="parent">Parent bitmap impl.</param>
+            /// <param name="bitmap">Bitmap</param>
+            /// <param name="pixmap">Pixmap</param>
+            internal BitmapFramebuffer(WriteableBitmapImpl parent, SKBitmap bitmap, SKPixmap pixmap)
+            {
+                _parent = parent;
+                _bitmap = bitmap;
+                Address = pixmap.GetPixels();
+                Size = new PixelSize(pixmap.Width, pixmap.Height);
+                RowBytes = pixmap.RowBytes;
+                Dpi = SkiaPlatform.DefaultDpi;
+                Format = pixmap.ColorType.ToPixelFormat();
+                Monitor.Enter(parent._lock);
+            }
+
+            /// <inheritdoc />
+            public void Dispose()
+            {
+                _bitmap.NotifyPixelsChanged();
+                _parent.Version++;
+                Monitor.Exit(_parent._lock);
+                _bitmap = null;
+                _parent = null;
+            }
+
+            /// <inheritdoc />
+            public IntPtr Address { get; }
+
+            /// <inheritdoc />
+            public PixelSize Size { get; }
+
+            /// <inheritdoc />
+            public int RowBytes { get; }
+
+            /// <inheritdoc />
+            public Vector Dpi { get; }
+
+            /// <inheritdoc />
+            public PixelFormat Format { get; }
         }
-
-        /// <inheritdoc />
-        public IntPtr Address { get; }
-
-        /// <inheritdoc />
-        public PixelSize Size { get; }
-
-        /// <inheritdoc />
-        public int RowBytes { get; }
-
-        /// <inheritdoc />
-        public Vector Dpi { get; }
-
-        /// <inheritdoc />
-        public PixelFormat Format { get; }
     }
 }
