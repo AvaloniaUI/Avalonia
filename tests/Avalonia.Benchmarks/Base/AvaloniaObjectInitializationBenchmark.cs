addc1ddce2e4a2200953277517f54b92f941b3d0
Dariusz Komosinski <darek.komosinski@gmail.com>
2019-08-30 01:13:19 +0200

    Avoid initializing properties if there is no observer. Optimize access.

---
+++ tests/Avalonia.Benchmarks/Base/AvaloniaObjectInitializationBenchmark.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+using BenchmarkDotNet.Attributes;
+
+namespace Avalonia.Benchmarks.Base
+{
+    [MemoryDiagnoser]
+    public class AvaloniaObjectInitializationBenchmark
+    {
+        [Benchmark(OperationsPerInvoke = 1000)]
+        public Button InitializeButton()
+        {
+            return new Button();
+        }
+    }
+}
