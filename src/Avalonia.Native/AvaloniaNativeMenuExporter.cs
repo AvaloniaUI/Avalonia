8520eb32bc164b6ad648da7daeebd6c213597df0
Dan Walmsley <dan@walms.co.uk>
2019-09-22 11:07:43 +0100

    add scafolding for native osx menu

---
+++ src/Avalonia.Native/AvaloniaNativeMenuExporter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Controls;
+using Avalonia.Controls.Platform;
+
+namespace Avalonia.Native
+{
+    class AvaloniaNativeMenuExporter : ITopLevelNativeMenuExporter
+    {
+        public bool IsNativeMenuExported => throw new NotImplementedException();
+
+        public event EventHandler OnIsNativeMenuExportedChanged;
+
+        public void SetNativeMenu(NativeMenu menu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPrependApplicationMenu(bool prepend)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
