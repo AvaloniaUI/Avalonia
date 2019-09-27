e3b4ba5a61dfb1265a703e4d05b2f7ae563f0aa0
Dan Walmsley <dan@walms.co.uk>
2019-09-27 18:57:09 +0100

    trigger ci

--- samples/ControlCatalog.Android/MainActivity.cs
+++ samples/ControlCatalog.Android/MainActivity.cs
@@ -20,7 +20,7 @@ namespace ControlCatalog.Android
         {
             if (Avalonia.Application.Current == null)           
             {
-                AppBuilder.Configure<App>() 
+                AppBuilder.Configure<App>()
                     .UseAndroid()
                     .SetupWithoutStarting();
                 Content = new MainView();
