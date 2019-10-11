e7b39c1672ecefa99fb5095a17ee56998e0b0681
Dan Walmsley <dan@walms.co.uk>
2019-10-11 19:34:21 +0100

    convert osx special key codes.

--- src/Avalonia.Native/AvaloniaNativeMenuExporter.cs
+++ src/Avalonia.Native/AvaloniaNativeMenuExporter.cs
@@ -5,12 +5,89 @@ using System.Linq;
 using System.Text;
 using Avalonia.Controls;
 using Avalonia.Controls.Platform;
+using Avalonia.Input;
 using Avalonia.Native.Interop;
 using Avalonia.Platform.Interop;
 using Avalonia.Threading;
 
 namespace Avalonia.Native
 {
+    enum OsxUnicodeSpecialKey
+    {
+        NSUpArrowFunctionKey = 0xF700,
+        NSDownArrowFunctionKey = 0xF701,
+        NSLeftArrowFunctionKey = 0xF702,
+        NSRightArrowFunctionKey = 0xF703,
+        NSF1FunctionKey = 0xF704,
+        NSF2FunctionKey = 0xF705,
+        NSF3FunctionKey = 0xF706,
+        NSF4FunctionKey = 0xF707,
+        NSF5FunctionKey = 0xF708,
+        NSF6FunctionKey = 0xF709,
+        NSF7FunctionKey = 0xF70A,
+        NSF8FunctionKey = 0xF70B,
+        NSF9FunctionKey = 0xF70C,
+        NSF10FunctionKey = 0xF70D,
+        NSF11FunctionKey = 0xF70E,
+        NSF12FunctionKey = 0xF70F,
+        NSF13FunctionKey = 0xF710,
+        NSF14FunctionKey = 0xF711,
+        NSF15FunctionKey = 0xF712,
+        NSF16FunctionKey = 0xF713,
+        NSF17FunctionKey = 0xF714,
+        NSF18FunctionKey = 0xF715,
+        NSF19FunctionKey = 0xF716,
+        NSF20FunctionKey = 0xF717,
+        NSF21FunctionKey = 0xF718,
+        NSF22FunctionKey = 0xF719,
+        NSF23FunctionKey = 0xF71A,
+        NSF24FunctionKey = 0xF71B,
+        NSF25FunctionKey = 0xF71C,
+        NSF26FunctionKey = 0xF71D,
+        NSF27FunctionKey = 0xF71E,
+        NSF28FunctionKey = 0xF71F,
+        NSF29FunctionKey = 0xF720,
+        NSF30FunctionKey = 0xF721,
+        NSF31FunctionKey = 0xF722,
+        NSF32FunctionKey = 0xF723,
+        NSF33FunctionKey = 0xF724,
+        NSF34FunctionKey = 0xF725,
+        NSF35FunctionKey = 0xF726,
+        NSInsertFunctionKey = 0xF727,
+        NSDeleteFunctionKey = 0xF728,
+        NSHomeFunctionKey = 0xF729,
+        NSBeginFunctionKey = 0xF72A,
+        NSEndFunctionKey = 0xF72B,
+        NSPageUpFunctionKey = 0xF72C,
+        NSPageDownFunctionKey = 0xF72D,
+        NSPrintScreenFunctionKey = 0xF72E,
+        NSScrollLockFunctionKey = 0xF72F,
+        NSPauseFunctionKey = 0xF730,
+        NSSysReqFunctionKey = 0xF731,
+        NSBreakFunctionKey = 0xF732,
+        NSResetFunctionKey = 0xF733,
+        NSStopFunctionKey = 0xF734,
+        NSMenuFunctionKey = 0xF735,
+        NSUserFunctionKey = 0xF736,
+        NSSystemFunctionKey = 0xF737,
+        NSPrintFunctionKey = 0xF738,
+        NSClearLineFunctionKey = 0xF739,
+        NSClearDisplayFunctionKey = 0xF73A,
+        NSInsertLineFunctionKey = 0xF73B,
+        NSDeleteLineFunctionKey = 0xF73C,
+        NSInsertCharFunctionKey = 0xF73D,
+        NSDeleteCharFunctionKey = 0xF73E,
+        NSPrevFunctionKey = 0xF73F,
+        NSNextFunctionKey = 0xF740,
+        NSSelectFunctionKey = 0xF741,
+        NSExecuteFunctionKey = 0xF742,
+        NSUndoFunctionKey = 0xF743,
+        NSRedoFunctionKey = 0xF744,
+        NSFindFunctionKey = 0xF745,
+        NSHelpFunctionKey = 0xF746,
+        NSModeSwitchFunctionKey = 0xF747
+    }
+
     public class MenuActionCallback : CallbackBase, IAvnActionCallback
     {
         private Action _action;
@@ -49,6 +126,11 @@ namespace Avalonia.Native
         private bool _exported = false;
         private IAvnWindow _nativeWindow;
         private List<NativeMenuItem> _menuItems = new List<NativeMenuItem>();
+
+        private static Dictionary<Key, OsxUnicodeSpecialKey> osxKeys = new Dictionary<Key, OsxUnicodeSpecialKey>
+        {
+            { Key.F1, OsxUnicodeSpecialKey.NSF1FunctionKey },
+        };
 
         public AvaloniaNativeMenuExporter(IAvnWindow nativeWindow, IAvaloniaNativeFactory factory)
         {
@@ -147,6 +229,18 @@ namespace Avalonia.Native
             }
         }
 
+        private static string ConvertOSXSpecialKeyCodes(Key key)
+        {
+            if (osxKeys.ContainsKey(key))
+            {
+                return Encoding.UTF8.GetString(BitConverter.GetBytes((ushort)osxKeys[key]));
+            }
+            else
+            {
+                return key.ToString().ToLower();
+            }
+        }
+
         private void SetChildren(IAvnAppMenu menu, ICollection<NativeMenuItemBase> children)
         {
             foreach (var i in children)
@@ -164,7 +258,7 @@ namespace Avalonia.Native
 
                     if (item.Gesture != null)
                     {
-                        using (var buffer = new Utf8Buffer(item.Gesture.Key.ToString().ToLower()))
+                        using (var buffer = new Utf8Buffer(ConvertOSXSpecialKeyCodes(item.Gesture.Key)))
                         {
                             menuItem.SetGesture(buffer.DangerousGetHandle(), (AvnInputModifiers)item.Gesture.KeyModifiers);
                         }
