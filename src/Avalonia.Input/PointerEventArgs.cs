1b5f3b6cc8ea613af0521c1df080f4584ab0e5ea
Nikita Tsukanov <keks9n@gmail.com>
2019-10-12 10:31:18 +0300

    Renamed GetPointerPoint to GetCurrentPoint to match UWP

--- src/Avalonia.Input/PointerEventArgs.cs
+++ src/Avalonia.Input/PointerEventArgs.cs
@@ -88,9 +88,20 @@ namespace Avalonia.Input
             return _rootVisualPosition * _rootVisual.TransformToVisual(relativeTo) ?? default;
         }
 
-        public PointerPoint GetPointerPoint(IVisual relativeTo)
+        [Obsolete("Use GetCurrentPoint")]
+        public PointerPoint GetPointerPoint(IVisual relativeTo) => GetCurrentPoint(relativeTo);
+        
+        /// <summary>
+        /// Returns the PointerPoint associated with the current event
+        /// </summary>
+        /// <param name="relativeTo">The visual which coordinate system to use. Pass null for toplevel coordinate system</param>
+        /// <returns></returns>
+        public PointerPoint GetCurrentPoint(IVisual relativeTo)
             => new PointerPoint(Pointer, GetPosition(relativeTo), _properties);
 
+        /// <summary>
+        /// Returns the current pointer point properties
+        /// </summary>
         protected PointerPointProperties Properties => _properties;
     }
     
