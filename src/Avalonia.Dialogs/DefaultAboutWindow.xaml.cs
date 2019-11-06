48234ad3ab8aea1c8ff5801ff20c4ea565f2e615
Jumar Macato <hikari.netto23@gmail.com>
2019-11-06 20:21:15 +0800

    Add a Avalonia about window default implementation.

---
+++ src/Avalonia.Dialogs/DefaultAboutWindow.xaml.cs
@@ -0,0 +1,16 @@
+// Copyright (c) The Avalonia Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using Avalonia.Controls; 
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.Dialogs
+{
+    public class DefaultAboutWindow : Window
+    {
+        public DefaultAboutWindow()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
