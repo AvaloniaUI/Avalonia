d7cf88a9730831f710eced66dd45d7106d69c71b
Benedikt Schroeder <benedikt.schroeder@outlook.de>
2019-11-07 09:58:35 +0100

    Add unit test

---
+++ tests/Avalonia.UnitTests/MockFontManagerImpl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Media;
+using Avalonia.Platform;
+
+namespace Avalonia.UnitTests
+{
+    public class MockFontManagerImpl : IFontManagerImpl
+    {
+        public string DefaultFontFamilyName => "Default";
+
+        public IEnumerable<string> GetInstalledFontFamilyNames(bool checkForUpdates = false) =>
+            new[] { DefaultFontFamilyName };
+
+        public Typeface GetTypeface(FontFamily fontFamily, FontWeight fontWeight, FontStyle fontStyle)
+        {
+            return new Typeface(fontFamily, fontWeight, fontStyle);
+        }
+
+        public Typeface MatchCharacter(int codepoint, FontWeight fontWeight = default, FontStyle fontStyle = default,
+            FontFamily fontFamily = null, CultureInfo culture = null)
+        {
+            return null;
+        }
+    }
+}
