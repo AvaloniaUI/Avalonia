138ea74b88f64e2977482583b9e4042d68567b24
Dan Walmsley <dan@walms.co.uk>
2019-11-22 19:32:09 +0000

    Add code to show desired syntax.

--- samples/ControlCatalog/App.xaml.cs
+++ samples/ControlCatalog/App.xaml.cs
@@ -1,17 +1,31 @@
 using System;
+using System.Reactive;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using ReactiveUI;
 
 namespace ControlCatalog
 {
     public class App : Application
     {
+        public App()
+        {
+            DataContext = this;
+
+            AboutCommand = ReactiveCommand.Create(() =>
+            {
+
+            });
+        }
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
         }
+
+        public ReactiveCommand<Unit, Unit> AboutCommand { get; }
 
         public override void OnFrameworkInitializationCompleted()
         {
