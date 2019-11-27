e61933c206857c829cde9c148b6b278e5bcce314
Dan Walmsley <dan@walms.co.uk>
2019-11-27 19:59:45 +0000

    [OSX] fix the osx menu on catalina.

--- native/Avalonia.Native/src/OSX/app.mm
+++ native/Avalonia.Native/src/OSX/app.mm
@@ -1,16 +1,25 @@
 #include "common.h"
 @interface AvnAppDelegate : NSObject<NSApplicationDelegate>
 @end
+
 extern NSApplicationActivationPolicy AvnDesiredActivationPolicy = NSApplicationActivationPolicyRegular;
 @implementation AvnAppDelegate
 - (void)applicationWillFinishLaunching:(NSNotification *)notification
 {
-    [[NSApplication sharedApplication] setActivationPolicy: AvnDesiredActivationPolicy];
+    if([[NSApplication sharedApplication] activationPolicy] != AvnDesiredActivationPolicy)
+    {
+        for (NSRunningApplication * app in [NSRunningApplication runningApplicationsWithBundleIdentifier:@"com.apple.dock"]) {
+            [app activateWithOptions:NSApplicationActivateIgnoringOtherApps];
+            break;
+        }
+        
+        [[NSApplication sharedApplication] setActivationPolicy: AvnDesiredActivationPolicy];
+    }
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-    [NSApp activateIgnoringOtherApps:true];
+    [[NSRunningApplication currentApplication] activateWithOptions:NSApplicationActivateIgnoringOtherApps];
 }
 
 @end
@@ -20,5 +29,4 @@ extern void InitializeAvnApp()
     NSApplication* app = [NSApplication sharedApplication];
     id delegate = [AvnAppDelegate new];
     [app setDelegate:delegate];
-    
 }
