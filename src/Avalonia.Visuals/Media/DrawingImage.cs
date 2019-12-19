6bd9ca8fa369b6713403b9078eb8cc1497436653
Steven Kirk <grokys@gmail.com>
2019-12-19 17:35:59 +0100

    Initial implementation of DrawingImage.

---
+++ src/Avalonia.Visuals/Media/DrawingImage.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Platform;
+using Avalonia.Visuals.Media.Imaging;
+
+namespace Avalonia.Media
+{
+    public class DrawingImage : AvaloniaObject, IImage
+    {
+        public static readonly StyledProperty<Drawing> DrawingProperty =
+            AvaloniaProperty.Register<DrawingImage, Drawing>(nameof(Drawing));
+
+        public Drawing Drawing
+        {
+            get => GetValue(DrawingProperty);
+            set => SetValue(DrawingProperty, value);
+        }
+
+        public Size Size => Drawing?.GetBounds().Size ?? default;
+
+        public void Draw(
+            DrawingContext context,
+            double opacity,
+            Rect sourceRect,
+            Rect destRect,
+            BitmapInterpolationMode bitmapInterpolationMode)
+        {
+            Drawing?.Draw(context);
+        }
+    }
+}
