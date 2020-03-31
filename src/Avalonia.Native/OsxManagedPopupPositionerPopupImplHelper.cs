5397efc4f0cebf8c6ebc4e24d5912a805ce3479c
Steven Kirk <grokys@gmail.com>
2020-03-31 23:40:40 +0200

    Remove some left-over copyright headers.

--- src/Avalonia.Native/OsxManagedPopupPositionerPopupImplHelper.cs
+++ src/Avalonia.Native/OsxManagedPopupPositionerPopupImplHelper.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) The Avalonia Project. All rights reserved.
-// Licensed under the MIT license. See licence.md file in the project root for full license information.
-using Avalonia.Controls.Primitives.PopupPositioning;
+﻿using Avalonia.Controls.Primitives.PopupPositioning;
 using Avalonia.Platform;
 
 namespace Avalonia.Native
