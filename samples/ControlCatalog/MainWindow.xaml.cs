8b07ba1121d3c9f2d13e9f28a0eb3e0b45c33cfa
Dan Walmsley <dan@walms.co.uk>
2020-04-09 15:29:00 -0300

    whitespace

--- samples/ControlCatalog/MainWindow.xaml.cs
+++ samples/ControlCatalog/MainWindow.xaml.cs
@@ -28,7 +28,6 @@ namespace ControlCatalog
 
             DataContext = new MainWindowViewModel(_notificationArea);
             _recentMenu = ((NativeMenu.GetMenu(this).Items[0] as NativeMenuItem).Menu.Items[2] as NativeMenuItem).Menu;
-
             var mainMenu = this.FindControl<Menu>("MainMenu");
             mainMenu.AttachedToVisualTree += MenuAttached;
         }
