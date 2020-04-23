fb1da608f88989ad9eaf00c97f5318cb005dbd4b
Andrey Kunchev <donandren@gmail.com>
2020-04-23 14:45:38 +0300

    add failing tests for point parsing

--- tests/Avalonia.Markup.Xaml.UnitTests/Converters/PointsListTypeConverterTests.cs
+++ tests/Avalonia.Markup.Xaml.UnitTests/Converters/PointsListTypeConverterTests.cs
@@ -12,6 +12,22 @@ namespace Avalonia.Markup.Xaml.UnitTests.Converters
         [InlineData("1 2 3 4")]
         [InlineData("1 2,3 4")]
         [InlineData("1,2,3,4")]
+        public void TypeConverter_Should_Parse(string input)
+        {
+            var conv = new PointsListTypeConverter();
+
+            var points = (IList<Point>)conv.ConvertFrom(input);
+
+            Assert.Equal(2, points.Count);
+            Assert.Equal(new Point(1, 2), points[0]);
+            Assert.Equal(new Point(3, 4), points[1]);
+        }
+
+        [Theory]
+        [InlineData("1,2 3,4")]
+        [InlineData("1 2 3 4")]
+        [InlineData("1 2,3 4")]
+        [InlineData("1,2,3,4")]
         public void Should_Parse_Points_in_Xaml(string input)
         {
             var xaml = $"<Polygon xmlns='https://github.com/avaloniaui' Points='{input}' />";
