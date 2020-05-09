ac7e4af68f2db33ea60dc0951253be36899c68a1
mameolan <mameolan26@yahoo.de>
2020-05-09 19:14:52 +0200

    integrate groupbox add sample page

---
+++ samples/ControlCatalog/Pages/GroupBoxPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ControlCatalog.Pages
+{
+    public class GroupBoxPage : UserControl
+    {
+        public GroupBoxPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ src/Avalonia.Controls/GroupBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Avalonia.Controls.Primitives;
+
+namespace Avalonia.Controls
+{
+    /// <summary>
+    /// GroupBox Control class
+    /// </summary>
+    public class GroupBox: HeaderedContentControl
+    {
+        /// <summary>
+        /// style key of this control
+        /// </summary>
+        public Type StyleKey => typeof(GroupBox);
+
+    }
+}
