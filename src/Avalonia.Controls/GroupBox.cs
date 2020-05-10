9624269e2da92e06b0d81c191559550abb083a6a
mameolan <mameolan26@yahoo.de>
2020-05-10 07:46:19 +0200

    override some metadata: Focusable: false TabNavigation: None

--- src/Avalonia.Controls/GroupBox.cs
+++ src/Avalonia.Controls/GroupBox.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Avalonia.Controls.Primitives;
+using Avalonia.Input;
 
 namespace Avalonia.Controls
 {
@@ -13,5 +14,16 @@ namespace Avalonia.Controls
         /// </summary>
         public Type StyleKey => typeof(GroupBox);
 
+        /// <summary>
+        /// override some metadata:
+        /// Focusable: false
+        /// TabNavigation: None
+        /// </summary>
+        static GroupBox()
+        {
+            FocusableProperty.OverrideMetadata<GroupBox>(new StyledPropertyMetadata<bool>(false));
+            KeyboardNavigation.TabNavigationProperty.OverrideMetadata<GroupBox>
+                (new StyledPropertyMetadata<KeyboardNavigationMode>(KeyboardNavigationMode.None));
+        }
     }
 }
