4e0777145805f4e85b43f5fee17f0267f097c73a
Dan Walmsley <dan@walms.co.uk>
2020-05-11 15:52:55 -0300

    add to render demo

---
+++ samples/RenderDemo/Controls/LineBoundsDemoControl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Threading;
+
+namespace RenderDemo.Controls
+{
+    public class LineBoundsDemoControl : Control
+    {
+        private double angle = Math.PI / 8;
+
+        public static double CalculateOppSide(double angle, double hyp)
+        {
+            return Math.Sin(angle) * hyp;
+        }
+
+        public static double CalculateAdjSide(double angle, double hyp)
+        {
+            return Math.Cos(angle) * hyp;
+        }
+
+        public override void Render(DrawingContext drawingContext)
+        {
+            var lineLength = Math.Sqrt((100 * 100) + (100 * 100));
+
+            var diffX = CalculateAdjSide(angle, lineLength);
+            var diffY = CalculateOppSide(angle, lineLength);
+
+
+            var p1 = new Point(200, 200);
+            var p2 = new Point(p1.X + diffX, p1.Y + diffY);
+
+            var pen = new Pen(Brushes.Green, 20, lineCap: PenLineCap.Square);
+            var boundPen = new Pen(Brushes.Black);
+
+            drawingContext.DrawLine(pen, p1, p2);
+
+            drawingContext.DrawRectangle(boundPen, LineBoundsHelper.CalculateBounds(p1, p2, pen));
+
+            angle += Math.PI / 360;
+            Dispatcher.UIThread.Post(InvalidateVisual, DispatcherPriority.Background);
+        }
+    }
+}
---
+++ samples/RenderDemo/Pages/LineBoundsDemo.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RenderDemo.Pages
+{
+    public class LineBoundsDemo : UserControl
+    {
+        public LineBoundsDemo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
