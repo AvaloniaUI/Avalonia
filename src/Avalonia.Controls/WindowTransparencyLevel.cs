5b3d5db1d6c21338583843983f58d65a66cd99c6
Dan Walmsley <dan@walms.co.uk>
2020-05-19 10:30:12 -0300

    Add WindowTransparencyLevel enum.

---
+++ src/Avalonia.Controls/WindowTransparencyLevel.cs
@@ -0,0 +1,20 @@
+﻿namespace Avalonia.Controls
+{
+    public enum WindowTransparencyLevel
+    {
+        /// <summary>
+        /// The window background is Black where nothing is drawn in the window.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The window background is Transparent where nothing is drawn in the window.
+        /// </summary>
+        Transparent,
+
+        /// <summary>
+        /// The window background is a blur-behind where nothing is drawn in the window.
+        /// </summary>
+        Blur
+    }
+}
