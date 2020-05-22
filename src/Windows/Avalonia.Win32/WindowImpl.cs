3dbe59062a58070a7ba62535cf62030edcbc5285
Dan Walmsley <dan@walms.co.uk>
2020-05-22 08:53:34 -0300

    fix win32 generating borders in blur / transparent mode.

--- src/Windows/Avalonia.Win32/WindowImpl.cs
+++ src/Windows/Avalonia.Win32/WindowImpl.cs
@@ -259,7 +259,7 @@ namespace Avalonia.Win32
                     break;
             }
 
-            accent.AccentFlags = 2 | (int)(AccentFlags.DrawLeftBorder | AccentFlags.DrawBottomBorder | AccentFlags.DrawRightBorder | AccentFlags.DrawTopBorder);
+            accent.AccentFlags = 2;
             accent.GradientColor = 0x00FFFFFF;
 
             var accentPtr = Marshal.AllocHGlobal(accentStructSize);
