4bd7d4d7a5b74a1ffbff289c3416bbfd86df0020
Dan Walmsley <dan@walms.co.uk>
2020-05-29 13:42:56 -0300

    tidy pressed mixin.

--- src/Avalonia.Controls/Mixins/PressedMixin.cs
+++ src/Avalonia.Controls/Mixins/PressedMixin.cs
@@ -10,6 +10,17 @@ namespace Avalonia.Controls.Mixins
     /// </summary>
     public static class PressedMixin
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PressedMixin"/> class.
+        /// </summary>
+        /// <typeparam name="TControl">The control type.</typeparam>
+        public static void Attach<TControl>() where TControl : Control
+        {
+            InputElement.PointerPressedEvent.AddClassHandler<TControl>((x, e) => HandlePointerPressed(x, e), RoutingStrategies.Tunnel);
+            InputElement.PointerReleasedEvent.AddClassHandler<TControl>((x, e) => HandlePointerReleased(x), RoutingStrategies.Tunnel);
+            InputElement.PointerCaptureLostEvent.AddClassHandler<TControl>((x, e) => HandlePointerReleased(x), RoutingStrategies.Tunnel);
+        }
+
         private static void HandlePointerPressed<TControl>(TControl sender, PointerPressedEventArgs e) where TControl : Control
         {
             if (e.GetCurrentPoint(sender).Properties.IsLeftButtonPressed)
@@ -21,13 +32,6 @@ namespace Avalonia.Controls.Mixins
         private static void HandlePointerReleased<TControl>(TControl sender) where TControl : Control
         {
             ((IPseudoClasses)sender.Classes).Set(":pressed", false);
-        }
-
-        public static void Attach<TControl>() where TControl : Control
-        {
-            InputElement.PointerPressedEvent.AddClassHandler<TControl>((x, e) => HandlePointerPressed(x, e), RoutingStrategies.Tunnel, true);
-            InputElement.PointerReleasedEvent.AddClassHandler<TControl>((x, e) => HandlePointerReleased(x), RoutingStrategies.Tunnel, true);
-            InputElement.PointerCaptureLostEvent.AddClassHandler<TControl>((x, e) => HandlePointerReleased(x), RoutingStrategies.Tunnel, true);
         }
     }
 }
