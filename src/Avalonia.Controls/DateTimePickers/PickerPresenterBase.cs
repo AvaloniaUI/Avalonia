54a4f45d87d5fa168592cbfcc0847706705b622a
amwx <armccormickwx@gmail.com>
2020-06-22 22:23:53 -0500

    Revert and prep for new impl

--- src/Avalonia.Controls/DateTimePickers/PickerPresenterBase.cs
+++ src/Avalonia.Controls/DateTimePickers/PickerPresenterBase.cs
@@ -10,49 +10,6 @@ namespace Avalonia.Controls.Primitives
     /// </summary>
     public abstract class PickerPresenterBase : TemplatedControl
     {
-        /// <summary>
-        /// Raised when the AcceptButton is clicked on the Picker and the selected value is changed
-        /// </summary>
-        protected virtual void OnConfirmed()
-        {
-        }
-
-        /// <summary>
-        /// Gets whether the Accept/Dismiss buttons are shown in the picker
-        /// </summary>
-        /// <returns></returns>
-        protected virtual bool ShouldShowConfirmationButtons()
-        {
-            return true;
-        }
-
-        /// <summary>
-        /// Raised when the Popup opens
-        /// </summary>
-        public event EventHandler Opened;
-
-        /// <summary>
-        /// Raised when the Popup closes
-        /// </summary>
-        public event EventHandler Closed;
-
-        protected virtual void OnOpened()
-        {
-            Opened?.Invoke(this, EventArgs.Empty);
-        }
-
-        protected virtual void OnClosed()
-        {
-            Closed?.Invoke(this, EventArgs.Empty);
-        }
-
-        /// <summary>
-        /// Shows the popup for the PickerPresenter at the specified control.
-        /// </summary>
-        /// <param name="target"></param>
-        public abstract void ShowAt(Control target);
-
-
-        protected Popup _hostPopup;
+        
     }
 }
