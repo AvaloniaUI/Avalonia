e99a38cd02cab2203835e125b0c8296d7a471774
Benedikt Schroeder <benedikt.schroeder@outlook.de>
2020-06-30 13:54:29 +0200

    Remove redundant classes

--- src/Avalonia.Visuals/Media/TextFormatting/TextHidden.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Avalonia.Media.TextFormatting
-{
-    /// <summary>
-    /// Specialized text run used to mark a range of hidden characters
-    /// </summary>
-    public class TextHidden : TextRun
-    {
-    }
-}
