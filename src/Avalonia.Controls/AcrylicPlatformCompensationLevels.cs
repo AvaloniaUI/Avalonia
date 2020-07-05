e867a3575a67f5292fc72c2aea8edeacaf3963a1
Dan Walmsley <dan@walms.co.uk>
2020-07-05 11:51:25 -0300

    add AcrylicPlatformCompensationLevels.

---
+++ src/Avalonia.Controls/AcrylicPlatformCompensationLevels.cs
@@ -0,0 +1,16 @@
+﻿namespace Avalonia.Controls
+{
+    /// <summary>
+    /// Defines compensation levels for the platform depending on the transparency level.
+    /// It controls the base opacity level of the 'tracing paper' layer that compensates
+    /// for low blur radius.
+    /// </summary>
+    public struct AcrylicPlatformCompensationLevels
+    {
+        public double TransparentLevel { get; }
+
+        public double BlurLevel { get; }
+
+        public double AcrylicBlurLevel { get; }
+    }
+}
