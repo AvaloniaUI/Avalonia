45d543775e3a4068079fbd5b5d49f06ae7569730
Dan Walmsley <dan@walms.co.uk>
2020-07-07 15:31:01 -0300

    cleanup

--- src/Avalonia.Controls/RelativePanel.AttachedProperties.cs
+++ src/Avalonia.Controls/RelativePanel.AttachedProperties.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
-using Avalonia.Layout;
+﻿using Avalonia.Layout;
 
 namespace Avalonia.Controls
 {
