1ab2a4380c92a074eb905724de85f0cf78bc7060
Giuseppe Lippolis <workgroupengineering@outlook.com>
2020-08-20 11:57:08 +0200

    fixes warning CS0618: 'WriteableBitmap.WriteableBitmap(PixelSize, Vector, PixelFormat?)' is obsolete.

--- src/Avalonia.Visuals/Media/Imaging/WriteableBitmap.cs
+++ src/Avalonia.Visuals/Media/Imaging/WriteableBitmap.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Avalonia.Platform;
+﻿using Avalonia.Platform;
 
 namespace Avalonia.Media.Imaging
 {
@@ -8,18 +7,6 @@ namespace Avalonia.Media.Imaging
     /// </summary>
     public class WriteableBitmap : Bitmap
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="WriteableBitmap"/> class.
-        /// </summary>
-        /// <param name="size">The size of the bitmap in device pixels.</param>
-        /// <param name="dpi">The DPI of the bitmap.</param>
-        /// <param name="format">The pixel format (optional).</param>
-        /// <returns>An <see cref="IWriteableBitmapImpl"/>.</returns>
-        [Obsolete("Use overload taking an AlphaFormat.")]
-        public WriteableBitmap(PixelSize size, Vector dpi, PixelFormat? format = null)
-            : base(CreatePlatformImpl(size, dpi, format, null))
-        {
-        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="WriteableBitmap"/> class.
@@ -29,7 +16,7 @@ namespace Avalonia.Media.Imaging
         /// <param name="format">The pixel format (optional).</param>
         /// <param name="alphaFormat">The alpha format (optional).</param>
         /// <returns>An <see cref="IWriteableBitmapImpl"/>.</returns>
-        public WriteableBitmap(PixelSize size, Vector dpi, PixelFormat format, AlphaFormat alphaFormat) 
+        public WriteableBitmap(PixelSize size, Vector dpi, PixelFormat? format = null, AlphaFormat? alphaFormat = null) 
             : base(CreatePlatformImpl(size, dpi, format, alphaFormat))
         {
         }
