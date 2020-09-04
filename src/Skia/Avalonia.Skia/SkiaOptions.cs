6ed1669803d62f7a02f75d89b0e0ad66b8d5d983
Dan Walmsley <dan@walms.co.uk>
2020-09-04 15:49:04 +0100

    set a sensible cache size for skia.. based on what flutter uses.

--- src/Skia/Avalonia.Skia/SkiaOptions.cs
+++ src/Skia/Avalonia.Skia/SkiaOptions.cs
@@ -16,6 +16,6 @@ namespace Avalonia
         /// <summary>
         /// The maximum number of bytes for video memory to store textures and resources.
         /// </summary>
-        public long? MaxGpuResourceSizeBytes { get; set; }
+        public long? MaxGpuResourceSizeBytes { get; set; } = 1024 * 600 * 4 * 12; // ~28mb 12x 1024 x 600 textures. 
     }
 }
