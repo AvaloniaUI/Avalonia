35df5619e1bfa124c2c89530adf842fad88bc104
Giuseppe Lippolis <workgroupengineering@outlook.com>
2020-09-07 10:10:37 +0200

    Revert "fixes warning CS0618: 'WriteableBitmap.WriteableBitmap(PixelSize, Vector, PixelFormat?)' is obsolete."

--- src/Avalonia.Visuals/Media/Imaging/WriteableBitmap.cs
+++ src/Avalonia.Visuals/Media/Imaging/WriteableBitmap.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Platform;
+﻿using System;
+using Avalonia.Platform;
 
 namespace Avalonia.Media.Imaging
 {
@@ -7,6 +8,18 @@ namespace Avalonia.Media.Imaging
     /// </summary>
     public class WriteableBitmap : Bitmap
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WriteableBitmap"/> class.
+        /// </summary>
+        /// <param name="size">The size of the bitmap in device pixels.</param>
+        /// <param name="dpi">The DPI of the bitmap.</param>
+        /// <param name="format">The pixel format (optional).</param>
+        /// <returns>An <see cref="IWriteableBitmapImpl"/>.</returns>
+        [Obsolete("Use overload taking an AlphaFormat.")]
+        public WriteableBitmap(PixelSize size, Vector dpi, PixelFormat? format = null)
+            : base(CreatePlatformImpl(size, dpi, format, null))
+        {
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="WriteableBitmap"/> class.
@@ -16,10 +29,11 @@ namespace Avalonia.Media.Imaging
         /// <param name="format">The pixel format (optional).</param>
         /// <param name="alphaFormat">The alpha format (optional).</param>
         /// <returns>An <see cref="IWriteableBitmapImpl"/>.</returns>
-        public WriteableBitmap(PixelSize size, Vector dpi, PixelFormat? format = null, AlphaFormat? alphaFormat = null) 
+        public WriteableBitmap(PixelSize size, Vector dpi, PixelFormat format, AlphaFormat alphaFormat) 
             : base(CreatePlatformImpl(size, dpi, format, alphaFormat))
         {
         }
+
 
         public ILockedFramebuffer Lock() => ((IWriteableBitmapImpl) PlatformImpl.Item).Lock();
 
