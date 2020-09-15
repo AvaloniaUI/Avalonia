a6d83cd32a8e373b7b00f58b18b2947f135da3fa
Dan Walmsley <dan@walms.co.uk>
2020-09-15 06:12:47 -0700

    support any gles version with angle using angle options.

--- src/Avalonia.OpenGL/AngleOptions.cs
+++ src/Avalonia.OpenGL/AngleOptions.cs
@@ -10,6 +10,14 @@ namespace Avalonia.OpenGL
 			DirectX11
         }
 
+        public IList<GlVersion> GlProfiles { get; set; } = new List<GlVersion>
+        {
+            new GlVersion(GlProfileType.OpenGLES, 3, 2),
+            new GlVersion(GlProfileType.OpenGLES, 3, 1),
+            new GlVersion(GlProfileType.OpenGLES, 3, 0),
+            new GlVersion(GlProfileType.OpenGLES, 2, 0)
+        };
+
         public IList<PlatformApi> AllowedPlatformApis { get; set; } = null;
     }
 }
