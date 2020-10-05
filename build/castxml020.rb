cdaf0dc9b9adbe0a47e0b745d77ba2ff9eb34d5b
Nikita Tsukanov <keks9n@gmail.com>
2020-10-05 20:41:54 +0300

    Brew

--- build/castxml020.rb
+++ build/castxml020.rb
@@ -5,16 +5,8 @@ class Castxml020 < Formula
   sha256 "626c395d0d3c777b5a1582cdfc4d33d142acfb12204ebe251535209126705ec1"
   head "https://github.com/CastXML/castxml.git"
 
-  bottle do
-    cellar :any
-    sha256 "5ec79b331bd18ac2d619c2acb01c42ccfabac62898f5e83971d9594fea1e91ed" => :catalina
-    sha256 "3a87a080247a21ab0f05db2dafb826664bb88426563507bb6a00d9c465d41e62" => :mojave
-    sha256 "295056ef0feae25c6f00c1e7e669f7f017bc3242c4cbdb0d6c95b34568b31655" => :high_sierra
-    sha256 "aaf5927a5f3dfcdc3c88a936a2aa6964ff8f304c48a0690087e6350ef75b0206" => :sierra
-  end
-
   depends_on "cmake" => :build
-  depends_on "llvm"
+  depends_on "llvm@9"
 
   def install
     mkdir "build" do
