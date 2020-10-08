f4617593959f893318aa95e6cb5b77fc5577a6db
Andrey Kunchev <donandren@gmail.com>
2020-10-08 18:14:37 +0300

    tmp: test wrapping issue:4824, 4830 and issue:4831 textblock don;t update

--- samples/ControlCatalog/Pages/TextBlockPage.xaml.cs
+++ samples/ControlCatalog/Pages/TextBlockPage.xaml.cs
@@ -1,5 +1,8 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 
 namespace ControlCatalog.Pages
 {
@@ -8,6 +11,24 @@ namespace ControlCatalog.Pages
         public TextBlockPage()
         {
             this.InitializeComponent();
+
+            var txtChanging = this.Get<TextBlock>("txtChanging");
+            var tpChanging = this.Get<TextPresenter>("tpChanging");
+
+            void update()
+            {
+                var time = DateTime.Now.TimeOfDay.ToString();
+                txtChanging.Text = $"TextBlock: {time}";
+                tpChanging.Text = $"TextPresenter: {time}";
+            }
+            var btn = this.Get<Button>("btn");
+            btn.Click += (s, e) => update();
+            //DispatcherTimer.Run(() =>
+            //{
+            //    update();
+            //    return true;
+            //}, TimeSpan.FromSeconds(1));
+
         }
 
         private void InitializeComponent()
