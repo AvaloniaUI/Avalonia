74066d09464a0eeb2d9a101e2706011c6991cb27
Andrey Kunchev <donandren@gmail.com>
2020-10-12 02:19:09 +0300

    tmp: simulate bug with listbox 4855

--- samples/VirtualizationDemo/MainWindow.xaml.cs
+++ samples/VirtualizationDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using Avalonia;
+using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using VirtualizationDemo.ViewModels;
@@ -12,6 +13,15 @@ namespace VirtualizationDemo
             this.InitializeComponent();
             this.AttachDevTools();
             DataContext = new MainWindowViewModel();
+            //bug in listbox
+            var items = new AvaloniaList<string>() { "Item 1" };
+            var lst = this.Get<ListBox>("lst");
+            lst.Items = items;
+            this.Get<Button>("btnAdd").Click += (s, e) =>
+            {
+                items.Add($"Item {items.Count + 1}");
+                lst.SelectedIndex = items.Count - 1;
+            };
         }
 
         private void InitializeComponent()
