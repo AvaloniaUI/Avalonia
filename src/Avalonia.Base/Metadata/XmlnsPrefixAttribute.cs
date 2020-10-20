1aa50361bb1a27821a407a26df8f4216cdad00c4
bmello4688 <brian.mello@outlook.com>
2020-10-20 08:55:17 -0400

    add XmlnsPrefix which is in WPF

---
+++ src/Avalonia.Base/Metadata/XmlnsPrefixAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Avalonia.Metadata
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public sealed class XmlnsPrefixAttribute : Attribute
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="xmlNamespace">XML namespce</param>
+        /// <param name="prefix">recommended prefix</param>
+        public XmlnsPrefixAttribute(string xmlNamespace, string prefix)
+        {
+            XmlNamespace = xmlNamespace ?? throw new ArgumentNullException(nameof(xmlNamespace));
+
+            Prefix = prefix ?? throw new ArgumentNullException(nameof(prefix));
+        }
+
+        /// <summary>
+        /// XML Namespace
+        /// </summary>
+        public string XmlNamespace { get; }
+
+        /// <summary>
+        /// New Xml Namespace
+        /// </summary>
+        public string Prefix { get; }
+    }
+}
