583a2a011a2535bd2b4ba64ab5001765e635b991
Giuseppe Lippolis <workgroupengineering@outlook.com>
2020-11-06 18:18:53 +0100

    Add DevToolsOptions

---
+++ src/Avalonia.Diagnostics/Diagnostics/DevToolsOptions.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Input;
+
+namespace Avalonia.Diagnostics
+{
+    /// <summary>
+    /// DevTool instance settings
+    /// </summary>
+    public class DevToolsOptions
+    {
+        /// <summary>
+        /// The key gesture to open DevTools.
+        /// </summary>
+        public KeyGesture Gesture { get; set; } = new KeyGesture(Key.F12);
+
+        /// <summary>
+        /// Indicates whether DevTools should stay on top of the inspected control if possible.
+        /// </summary>
+        public bool OnTop { get; set; } = true;
+
+        /// <summary>
+        /// The size of DevTools window. Default value is 1024x512.
+        /// </summary>
+        public Size Size { get; set; } = new Size(1024, 512);
+    }
+}
