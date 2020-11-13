4563c6cd241b2de78b7c88d9883f05c0f308d023
Andrey Kunchev <donandren@gmail.com>
2020-11-13 18:16:20 +0200

    Compiled path support for cast

---
+++ src/Markup/Avalonia.Markup.Xaml/MarkupExtensions/CompiledBindings/StrongTypeCastNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Avalonia.Data.Core;
+
+namespace Avalonia.Markup.Xaml.MarkupExtensions.CompiledBindings
+{
+    public class StrongTypeCastNode : TypeCastNode
+    {
+        private Func<object, object> _cast;
+
+        public StrongTypeCastNode(Type type, Func<object, object> cast) : base(type)
+        {
+            _cast = cast;
+        }
+
+        protected override object Cast(object value)
+            => _cast(value);
+    }
+}
