cc06a60cf36105c6f456c017a715c44c9bce4ec6
Rustam Sayfutdinov <rstm.sf@gmail.com>
2020-12-06 11:35:10 +0300

    Add test for ParseQueryString

---
+++ tests/Avalonia.Base.UnitTests/Utilities/UriExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Avalonia.Utilities;
+using Xunit;
+
+namespace Avalonia.Base.UnitTests.Utilities
+{
+    public class UriExtensionsTests
+    {
+        [Fact]
+        public void Values_For_Query_Uri_Parsed()
+        {
+            const string key = "assembly";
+            const string value = "Avalonia.Themes.Default";
+
+            var uri = new Uri("resm:Avalonia.Themes.Default.Accents.BaseLight.xaml?"+ $"{key}={value}");
+            var parsed = uri.ParseQueryString();
+            
+            Assert.True(parsed.Count == 1);
+            Assert.Equal(key, parsed.Keys.First());
+            Assert.Equal(value, parsed.Values.First());
+        }
+    }
+}
