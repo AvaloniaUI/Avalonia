cd975b0fa1fa357d20ffa2c152e10c8ec42a43fa
Giuseppe Lippolis <workgroupengineering@outlook.com>
2021-01-20 15:59:43 +0100

    Android mark obsolete Invalidate methods overload

--- src/Android/Avalonia.Android/Platform/SkiaPlatform/InvalidationAwareSurfaceView.cs
+++ src/Android/Avalonia.Android/Platform/SkiaPlatform/InvalidationAwareSurfaceView.cs
@@ -43,11 +43,13 @@ namespace Avalonia.Android
             }
         }
 
+        [Obsolete]
         public override void Invalidate(global::Android.Graphics.Rect dirty)
         {
             Invalidate();
         }
 
+        [Obsolete]
         public override void Invalidate(int l, int t, int r, int b)
         {
             Invalidate();
