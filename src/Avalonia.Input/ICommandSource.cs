01507eb20a16e5274ab98dba821ed8e71ae33ccd
Giuseppe Lippolis <workgroupengineering@outlook.com>
2021-01-20 15:29:14 +0100

    fixes MenuItem warning CS8766 Nullability of reference types in return type of 'ICommand? MenuItem.Command.get' doesn't match implicitly implemented member 'ICommand ICommandSource.Command.get' (possibly because of nullability attributes).

--- src/Avalonia.Input/ICommandSource.cs
+++ src/Avalonia.Input/ICommandSource.cs
@@ -1,5 +1,5 @@
 ﻿using System.Windows.Input;
-
+#nullable enable
 namespace Avalonia.Input
 {
     ///<summary>
@@ -12,13 +12,13 @@ namespace Avalonia.Input
         /// Classes that implement this interface should enable or disable based on the command's CanExecute return value.
         /// The property may be implemented as read-write if desired.
         /// </summary>
-        ICommand Command { get; }
+        ICommand? Command { get; }
 
         /// <summary>
         /// The parameter that will be passed to the command when executing the command.
         /// The property may be implemented as read-write if desired.
         /// </summary>
-        object CommandParameter { get; }
+        object? CommandParameter { get; }
 
 
         /// <summary>
