65792cf4b037ec3f48d8e8c54de775608a519db8
artyom <worldbeater-dev@yandex.ru>
2021-04-02 02:21:08 +0300

    feature: Disable ReactiveUI platform registrations

--- src/Avalonia.ReactiveUI/AppBuilderExtensions.cs
+++ src/Avalonia.ReactiveUI/AppBuilderExtensions.cs
@@ -17,6 +17,8 @@ namespace Avalonia.ReactiveUI
         {
             return builder.AfterPlatformServicesSetup(_ =>
             {
+                PlatformRegistrationManager.SetRegistrationNamespaces(RegistrationNamespace.Avalonia);
+                Locator.CurrentMutable.InitializeReactiveUI();
                 RxApp.MainThreadScheduler = AvaloniaScheduler.Instance;
                 Locator.CurrentMutable.RegisterConstant(new AvaloniaActivationForViewFetcher(), typeof(IActivationForViewFetcher));
                 Locator.CurrentMutable.RegisterConstant(new AutoDataTemplateBindingHook(), typeof(IPropertyBindingHook));
