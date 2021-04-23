4e1b40e611d356c7910fcbe5e0262088f40f43a9
Steven Kirk <grokys@gmail.com>
2021-04-23 17:08:04 +0200

    Merge core libraries.
    Everything below `Avalonia.Controls` into `Avalonia.Base`.

--- src/Avalonia.Input/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-using Avalonia.Metadata;
-
-[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Input")]
-[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Input.GestureRecognizers")]
