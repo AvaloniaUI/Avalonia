c1966b7c748cd8e4ffcbd41a237a39c80170f953
Jumar Macato <hikari.netto23@gmail.com>
2021-05-21 23:52:22 +0800

    test for gpu surface canvas like api

--- samples/RenderDemo/Pages/CustomSkiaPage.cs
+++ samples/RenderDemo/Pages/CustomSkiaPage.cs
@@ -1,118 +1,117 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
+using Avalonia.Media.Immutable;
 using Avalonia.Platform;
 using Avalonia.Rendering.SceneGraph;
 using Avalonia.Skia;
 using Avalonia.Threading;
+using Avalonia.Utilities;
+using Avalonia.Visuals.Media.Imaging;
 using SkiaSharp;
 
 namespace RenderDemo.Pages
 {
+    class CustomDrawOp : ICustomDrawOperation
+    {
+        private readonly FormattedText _noSkia;
+        private readonly Rect _bounds;
+
+        public CustomDrawOp(Rect bounds)
+        {
+            _bounds = bounds;
+        }
+
+        public void Dispose()
+        {
+            // No-op
+        }
+
+        public Rect Bounds => _bounds;
+
+        public bool HitTest(Point p) => false;
+        public bool Equals(ICustomDrawOperation other) => false;
+
+        static Stopwatch St = Stopwatch.StartNew();
+
+        private Stack<TargetSurface> _activeSurfaces = new Stack<TargetSurface>();
+
+        readonly struct TargetSurface
+        {
+            public IDrawingContextLayerImpl Layer { get; }
+            public IDrawingContextImpl Context { get; }
+            public Size DipSize { get; }
+
+            public TargetSurface(IDrawingContextLayerImpl newLayer, IDrawingContextImpl layerContext, Size dipSize)
+            {
+                Layer = newLayer;
+                Context = layerContext;
+                DipSize = dipSize;
+            }
+        }
+
+        private void Save(IDrawingContextImpl mainCtx, Size size)
+        {
+            var newLayer = mainCtx.CreateLayer(size);
+            _activeSurfaces.Push(new TargetSurface(newLayer, newLayer.CreateDrawingContext(null), size));
+        }
+
+        private IDrawingContextImpl GetCurrentLayerCtx() => _activeSurfaces.Peek().Context;
+
+        private void Restore(IDrawingContextImpl mainCtx, BitmapBlendingMode blendingMode)
+        {
+            var poppedLayer = _activeSurfaces.Pop();
+
+
+            mainCtx.DrawBitmap(RefCountable.CreateUnownedNotClonable(poppedLayer.Layer),
+                1,
+                new Rect(poppedLayer.DipSize),
+                new Rect(poppedLayer.DipSize));
+            poppedLayer.Context.Dispose();
+
+            poppedLayer.Layer.Dispose();
+        }
+
+        public void Render(IDrawingContextImpl context)
+        {
+            Save(context, _bounds.Size);
+            
+            var cur = GetCurrentLayerCtx();
+            
+            cur.Transform *= Matrix.CreateTranslation(-25, -25);
+            cur.DrawRectangle(new ImmutableSolidColorBrush(new Color(127, 127, 127, 127)), null,
+                new RoundedRect(new Rect(0, 0, 250, 250)));
+
+            Restore(context, BitmapBlendingMode.Xor);
+            
+            
+            Save(context, _bounds.Size);
+            
+            var cur2 = GetCurrentLayerCtx();
+            
+            cur2.Transform *= Matrix.CreateTranslation(0, 0);
+            cur2.DrawRectangle(new ImmutableSolidColorBrush(new Color(127, 127, 127, 127)), null,
+                new RoundedRect(new Rect(0, 0, 250, 250)));
+
+            Restore(context, BitmapBlendingMode.Xor);
+ 
+        }
+    }
+
+
     public class CustomSkiaPage : Control
     {
         public CustomSkiaPage()
         {
             ClipToBounds = true;
         }
-        
-        class CustomDrawOp : ICustomDrawOperation
-        {
-            private readonly FormattedText _noSkia;
 
-            public CustomDrawOp(Rect bounds, FormattedText noSkia)
-            {
-                _noSkia = noSkia;
-                Bounds = bounds;
-            }
-            
-            public void Dispose()
-            {
-                // No-op
-            }
-
-            public Rect Bounds { get; }
-            public bool HitTest(Point p) => false;
-            public bool Equals(ICustomDrawOperation other) => false;
-            static Stopwatch St = Stopwatch.StartNew();
-            public void Render(IDrawingContextImpl context)
-            {
-                var canvas = (context as ISkiaDrawingContextImpl)?.SkCanvas;
-                if (canvas == null)
-                    context.DrawText(Brushes.Black, new Point(), _noSkia.PlatformImpl);
-                else
-                {
-                    canvas.Save();
-                    // create the first shader
-                    var colors = new SKColor[] {
-                        new SKColor(0, 255, 255),
-                        new SKColor(255, 0, 255),
-                        new SKColor(255, 255, 0),
-                        new SKColor(0, 255, 255)
-                    };
-
-                    var sx = Animate(100, 2, 10);
-                    var sy = Animate(1000, 5, 15);
-                    var lightPosition = new SKPoint(
-                        (float)(Bounds.Width / 2 + Math.Cos(St.Elapsed.TotalSeconds) * Bounds.Width / 4),
-                        (float)(Bounds.Height / 2 + Math.Sin(St.Elapsed.TotalSeconds) * Bounds.Height / 4));
-                    using (var sweep =
-                        SKShader.CreateSweepGradient(new SKPoint((int)Bounds.Width / 2, (int)Bounds.Height / 2), colors,
-                            null)) 
-                    using(var turbulence = SKShader.CreatePerlinNoiseFractalNoise(0.05f, 0.05f, 4, 0))
-                    using(var shader = SKShader.CreateCompose(sweep, turbulence, SKBlendMode.SrcATop))
-                    using(var blur = SKImageFilter.CreateBlur(Animate(100, 2, 10), Animate(100, 5, 15)))
-                    using (var paint = new SKPaint
-                    {
-                        Shader = shader,
-                        ImageFilter = blur
-                    })
-                        canvas.DrawPaint(paint);
-                    
-                    using (var pseudoLight = SKShader.CreateRadialGradient(
-                        lightPosition,
-                        (float) (Bounds.Width/3),
-                        new [] { 
-                            new SKColor(255, 200, 200, 100), 
-                            SKColors.Transparent,
-                            new SKColor(40,40,40, 220), 
-                            new SKColor(20,20,20, (byte)Animate(100, 200,220)) },
-                        new float[] { 0.3f, 0.3f, 0.8f, 1 },
-                        SKShaderTileMode.Clamp))
-                    using (var paint = new SKPaint
-                    {
-                        Shader = pseudoLight
-                    })
-                        canvas.DrawPaint(paint);
-                    canvas.Restore();
-                }
-            }    
-            static int Animate(int d, int from, int to)
-            {
-                var ms = (int)(St.ElapsedMilliseconds / d);
-                var diff = to - from;
-                var range = diff * 2;
-                var v = ms % range;
-                if (v > diff)
-                    v = range - v;
-                var rv = v + from;
-                if (rv < from || rv > to)
-                    throw new Exception("WTF");
-                return rv;
-            }
-        }
-
-
-        
         public override void Render(DrawingContext context)
         {
-            var noSkia = new FormattedText()
-            {
-                Text = "Current rendering API is not Skia"
-            };
-            context.Custom(new CustomDrawOp(new Rect(0, 0, Bounds.Width, Bounds.Height), noSkia));
+            context.Custom(new CustomDrawOp(new Rect(0, 0, Bounds.Width, Bounds.Height)));
             Dispatcher.UIThread.InvokeAsync(InvalidateVisual, DispatcherPriority.Background);
         }
     }
