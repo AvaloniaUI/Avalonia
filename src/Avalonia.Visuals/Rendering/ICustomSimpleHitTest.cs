ee18daa08cfdbd9e8eab12b9b78f64bab053e0da
Steven Kirk <grokys@gmail.com>
2021-05-31 18:22:05 +0200

    Revert back to `TransformedBounds`.

--- src/Avalonia.Visuals/Rendering/ICustomSimpleHitTest.cs
+++ src/Avalonia.Visuals/Rendering/ICustomSimpleHitTest.cs
@@ -25,7 +25,7 @@ namespace Avalonia.Rendering
     public static class CustomSimpleHitTestExtensions
     {
         public static bool HitTestCustom(this IVisual visual, Point point)
-            => (visual as ICustomSimpleHitTest)?.HitTest(point) ?? visual.Bounds.Contains(point);
+            => (visual as ICustomSimpleHitTest)?.HitTest(point) ?? visual.TransformedBounds?.Contains(point) == true;
 
         public static bool HitTestCustom(this IEnumerable<IVisual> children, Point point)
             => children.Any(ctrl => ctrl.HitTestCustom(point));
