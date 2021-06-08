c48a516ec6e30c6f2e6ebe52e0e6c06c62157c8a
Dariusz Komosinski <darek.komosinski@gmail.com>
2021-06-08 23:33:55 +0200

    Unify brush transition. Handle brush type mismatch in a nicer way.

--- src/Avalonia.Visuals/Animation/Transitions/BrushTransition.cs
+++ src/Avalonia.Visuals/Animation/Transitions/BrushTransition.cs
@@ -1,29 +1,67 @@
 ﻿using System;
 using Avalonia.Animation.Animators;
+using Avalonia.Animation.Easings;
 using Avalonia.Media;
+
+#nullable enable
 
 namespace Avalonia.Animation
 {
     /// <summary>
     /// Transition class that handles <see cref="AvaloniaProperty"/> with <see cref="IBrush"/> type.
-    /// Only values of <see cref="ISolidColorBrush"/> will correctly transition.
+    /// Only values of <see cref="ISolidColorBrush"/> will transition correctly at the moment.
     /// </summary>
-    public class ISolidColorBrushTransition : Transition<IBrush>
+    public class BrushTransition : Transition<IBrush?>
     {
         private static readonly ISolidColorBrushAnimator s_animator = new ISolidColorBrushAnimator();
 
-        public override IObservable<IBrush> DoTransition(IObservable<double> progress, IBrush oldValue, IBrush newValue)
+        public override IObservable<IBrush?> DoTransition(IObservable<double> progress, IBrush? oldValue, IBrush? newValue)
         {
-            var oldSolidBrush = AsImmutable(oldValue);
-            var newSolidBrush = AsImmutable(newValue);
+            var oldSolidColorBrush = TryGetSolidColorBrush(oldValue);
+            var newSolidColorBrush = TryGetSolidColorBrush(newValue);
 
-            return new AnimatorTransitionObservable<ISolidColorBrush, ISolidColorBrushAnimator>(
-                s_animator, progress, Easing, oldSolidBrush, newSolidBrush);
+            if (oldSolidColorBrush != null && newSolidColorBrush != null)
+            {
+                EnsureImmutable(ref oldSolidColorBrush);
+                EnsureImmutable(ref newSolidColorBrush);
+
+                return new AnimatorTransitionObservable<ISolidColorBrush, ISolidColorBrushAnimator>(
+                    s_animator, progress, Easing, oldSolidColorBrush, newSolidColorBrush);
+            }
+
+            return new IncompatibleTransitionObservable(progress, Easing, oldValue, newValue);
         }
 
-        private static ISolidColorBrush AsImmutable(IBrush brush)
+        private static void EnsureImmutable(ref ISolidColorBrush brush)
         {
-            return (ISolidColorBrush)(brush as ISolidColorBrush)?.ToImmutable();
+            brush = (ISolidColorBrush)brush.ToImmutable();
+        }
+
+        private static ISolidColorBrush? TryGetSolidColorBrush(IBrush? brush)
+        {
+            if (brush is null)
+            {
+                return Brushes.Transparent;
+            }
+
+            return brush as ISolidColorBrush;
+        }
+
+        private class IncompatibleTransitionObservable : TransitionObservableBase<IBrush?>
+        {
+            private readonly IBrush? _from;
+            private readonly IBrush? _to;
+
+            public IncompatibleTransitionObservable(IObservable<double> progress, Easing easing, IBrush? from, IBrush? to) : base(progress, easing)
+            {
+                _from = @from;
+                _to = to;
+            }
+
+            protected override IBrush? ProduceValue(double progress)
+            {
+                return progress < 0.5 ? _from : _to;
+            }
         }
     }
 }
