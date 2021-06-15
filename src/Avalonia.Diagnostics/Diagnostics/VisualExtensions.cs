f00a10124424986815eb7de876634176e2bc1c60
Giuseppe Lippolis <workgroupengineering@outlook.com>
2021-06-15 10:55:42 +0200

    fixes: screenshot Clip

--- src/Avalonia.Diagnostics/Diagnostics/VisualExtensions.cs
+++ src/Avalonia.Diagnostics/Diagnostics/VisualExtensions.cs
@@ -17,43 +17,45 @@ namespace Avalonia.Diagnostics
         /// <param name="dpi">(optional) dpi quality default 96</param>
         public static void RenderTo(this IControl source, Stream destination, double dpi = 96)
         {
-            var rect = source.Bounds;
+            if (source.TransformedBounds == null)
+            {
+                return;
+            }
+            var rect = source.TransformedBounds.Value.Clip;
+            var top = rect.TopLeft;
             var pixelSize = new PixelSize((int)rect.Width, (int)rect.Height);
             var dpiVector = new Vector(dpi, dpi);
 
             // get Visual root
-            var root = (source.VisualRoot 
+            var root = (source.VisualRoot
                 ?? source.GetVisualRoot())
                 as IControl ?? source;
 
-         
+
             IDisposable? clipSetter = default;
             IDisposable? clipToBoundsSetter = default;
             IDisposable? renderTransformOriginSetter = default;
             IDisposable? renderTransformSetter = default;
-
+            // Save current TransformerBounds
+            var currentTransformerBounds = source.TransformedBounds.Value;
             try
-            {
-                // Translate coordinate clinet to root
-                var top = source.TranslatePoint(new Point(0,0), root)!;
-                var bottomRight = source.TranslatePoint(rect.BottomRight, root)!;
-
+            {                
                 // Set clip region
-                var clipRegion = new Media.RectangleGeometry(new Rect(top.Value, bottomRight.Value));
+                var clipRegion = new Media.RectangleGeometry(rect);
                 clipToBoundsSetter = root.SetValue(Visual.ClipToBoundsProperty, true, BindingPriority.Animation);
-                clipSetter = root.SetValue(Visual.ClipProperty,clipRegion, BindingPriority.Animation);
+                clipSetter = root.SetValue(Visual.ClipProperty, clipRegion, BindingPriority.Animation);
 
                 // Translate origin
                 renderTransformOriginSetter = root.SetValue(Visual.RenderTransformOriginProperty,
-                    new RelativePoint(top.Value, RelativeUnit.Absolute),
+                    new RelativePoint(top, RelativeUnit.Absolute),
                     BindingPriority.Animation);
 
                 renderTransformSetter = root.SetValue(Visual.RenderTransformProperty,
-                    new Media.TranslateTransform(-top.Value.X, -top.Value.Y),
+                    new Media.TranslateTransform(-top.X, -top.Y),
                     BindingPriority.Animation);
 
                 using (var bitmap = new RenderTargetBitmap(pixelSize, dpiVector))
-                {
+                {                   
                     bitmap.Render(root);
                     bitmap.Save(destination);
                 }
@@ -61,11 +63,18 @@ namespace Avalonia.Diagnostics
             }
             finally
             {
-                // Restore current vaules
+                // Restore values before trasformation
+                renderTransformSetter?.Dispose();
+                renderTransformOriginSetter?.Dispose();
                 clipSetter?.Dispose();
                 clipToBoundsSetter?.Dispose();
-                renderTransformOriginSetter?.Dispose();
-                renderTransformSetter?.Dispose();
+                /* Restore TransformerBounds
+                 * this is workaraund for issue. When i call bitmap.Render(root), 
+                 * ImmediateRenderer set TransformedBounds.Clip to empty rect
+                 * (see https://github.com/AvaloniaUI/Avalonia/blob/151a7a010d477436b37dde0eb89deb1f0df42c7d/src/Avalonia.Visuals/Rendering/ImmediateRenderer.cs#L303-L308).
+                 * If you remake a screenshot of same control, the clip bounds of screenshot is not correct.                
+                 */
+                source.TransformedBounds = currentTransformerBounds;
             }
         }
     }
