146a2205f5fff5423f7bd61e18818d4d8e71a4ea
David Waterman <davidw@icselectronics.co.uk>
2021-06-18 11:45:06 +0100

    Add pixel format parameter to Linux frame buffer platform setup. Currently setup always changes the frame buffer pixel format to RGBA which results in the screen being cleared if the default pixel format is not RGBA  (as on a Raspberry PI) . This clears any splash screen and leaves the screen blank for a period which is not a good UX. Now the frame buffer setup can select the correct pixel format or use null to leave the pixel format unchanged.

--- src/Linux/Avalonia.LinuxFramebuffer/LinuxFramebufferPlatform.cs
+++ src/Linux/Avalonia.LinuxFramebuffer/LinuxFramebufferPlatform.cs
@@ -112,9 +112,9 @@ namespace Avalonia.LinuxFramebuffer
 
 public static class LinuxFramebufferPlatformExtensions
 {
-    public static int StartLinuxFbDev<T>(this T builder, string[] args, string fbdev = null, double scaling = 1)
+    public static int StartLinuxFbDev<T>(this T builder, string[] args, PixelFormat? format = null, string fbdev = null, double scaling = 1)
         where T : AppBuilderBase<T>, new() =>
-        StartLinuxDirect(builder, args, new FbdevOutput(fbdev) {Scaling = scaling});
+        StartLinuxDirect(builder, args, new FbdevOutput(format: format, fileName: fbdev) { Scaling = scaling });
 
     public static int StartLinuxDrm<T>(this T builder, string[] args, string card = null, double scaling = 1)
         where T : AppBuilderBase<T>, new() => StartLinuxDirect(builder, args, new DrmOutput(card) {Scaling = scaling});
