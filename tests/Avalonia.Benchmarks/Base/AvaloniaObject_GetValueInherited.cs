f3748f769bbba3590c57f9da5ca0aa9fdfb741bb
Steven Kirk <grokys@gmail.com>
2021-06-30 10:30:20 +0200

    Added additional benchmarks.

---
+++ tests/Avalonia.Benchmarks/Base/AvaloniaObject_GetValueInherited.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Avalonia.Controls;
+using BenchmarkDotNet.Attributes;
+
+#nullable enable
+
+namespace Avalonia.Benchmarks.Base
+{
+    [MemoryDiagnoser]
+    public class AvaloniaObject_GetValueInherited
+    {
+        private TestClass _root = null!;
+        private TestClass _target = null!;
+
+        public AvaloniaObject_GetValueInherited()
+        {
+            RuntimeHelpers.RunClassConstructor(typeof(TestClass).TypeHandle);
+        }
+
+        [Params(1, 2, 10, 50, 100, 200)]
+        public int Depth { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _root = new();
+            _root.SetValue(TestClass.StringProperty, "foo");
+            _root.SetValue(TestClass.Struct1Property, new(1));
+            _root.SetValue(TestClass.Struct2Property, new(1));
+            _root.SetValue(TestClass.Struct3Property, new(1));
+            _root.SetValue(TestClass.Struct4Property, new(1));
+            _root.SetValue(TestClass.Struct5Property, new(1));
+            _root.SetValue(TestClass.Struct6Property, new(1));
+            _root.SetValue(TestClass.Struct7Property, new(1));
+            _root.SetValue(TestClass.Struct8Property, new(1));
+
+            var parent = _root;
+
+            for (var i = 0; i < Depth; ++i)
+            {
+                var c = new TestClass();
+                ((ISetLogicalParent)c).SetParent(parent);
+                parent = c;
+            }
+
+            _target = parent;
+        }
+
+        [Benchmark]
+        public int GetInheritedValues()
+        {
+            var target = _target;
+            var result = 0;
+
+            for (var i = 0; i < 100; ++i)
+            {
+                result += target.GetValue(TestClass.StringProperty)?.Length ?? 0;
+                result += target.GetValue(TestClass.Struct1Property).Int1;
+                result += target.GetValue(TestClass.Struct2Property).Int1;
+                result += target.GetValue(TestClass.Struct3Property).Int1;
+                result += target.GetValue(TestClass.Struct4Property).Int1;
+                result += target.GetValue(TestClass.Struct5Property).Int1;
+                result += target.GetValue(TestClass.Struct6Property).Int1;
+                result += target.GetValue(TestClass.Struct7Property).Int1;
+                result += target.GetValue(TestClass.Struct8Property).Int1;
+            }
+
+            return result;
+        }
+
+        private class TestClass : Control
+        {
+            public static readonly StyledProperty<string> StringProperty =
+                AvaloniaProperty.Register<TestClass, string>("String", inherits: true);
+            public static readonly StyledProperty<Struct1> Struct1Property =
+                AvaloniaProperty.Register<TestClass, Struct1>("Struct1", inherits: true);
+            public static readonly StyledProperty<Struct2> Struct2Property =
+                AvaloniaProperty.Register<TestClass, Struct2>("Struct2", inherits: true);
+            public static readonly StyledProperty<Struct3> Struct3Property =
+                AvaloniaProperty.Register<TestClass, Struct3>("Struct3", inherits: true);
+            public static readonly StyledProperty<Struct4> Struct4Property =
+                AvaloniaProperty.Register<TestClass, Struct4>("Struct4", inherits: true);
+            public static readonly StyledProperty<Struct5> Struct5Property =
+                AvaloniaProperty.Register<TestClass, Struct5>("Struct5", inherits: true);
+            public static readonly StyledProperty<Struct6> Struct6Property =
+                AvaloniaProperty.Register<TestClass, Struct6>("Struct6", inherits: true);
+            public static readonly StyledProperty<Struct7> Struct7Property =
+                AvaloniaProperty.Register<TestClass, Struct7>("Struct7", inherits: true);
+            public static readonly StyledProperty<Struct8> Struct8Property =
+                AvaloniaProperty.Register<TestClass, Struct8>("Struct8", inherits: true);
+        }
+    }
+}
---
+++ tests/Avalonia.Benchmarks/TestTypes.cs
@@ -0,0 +1,260 @@
+﻿using System;
+
+namespace Avalonia.Benchmarks
+{
+    internal struct Struct1 : IEquatable<Struct1>
+    {
+        public int Int1;
+
+        public Struct1(int i)
+        {
+            Int1 = i;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Struct1 @struct && Equals(@struct);
+        }
+
+        public bool Equals(Struct1 other)
+        {
+            return Int1 == other.Int1;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Int1);
+        }
+    }
+
+    internal struct Struct2 : IEquatable<Struct2>
+    {
+        public int Int1;
+        public int Int2;
+
+        public Struct2(int i)
+        {
+            Int1 = Int2 = i;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Struct2 @struct && Equals(@struct);
+        }
+
+        public bool Equals(Struct2 other)
+        {
+            return Int1 == other.Int1 &&
+                   Int2 == other.Int2;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Int1, Int2);
+        }
+    }
+
+    internal struct Struct3 : IEquatable<Struct3>
+    {
+        public int Int1;
+        public int Int2;
+        public int Int3;
+
+        public Struct3(int i)
+        {
+            Int1 = Int2 = Int3 = i;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Struct3 @struct && Equals(@struct);
+        }
+
+        public bool Equals(Struct3 other)
+        {
+            return Int1 == other.Int1 &&
+                   Int2 == other.Int2 &&
+                   Int3 == other.Int3;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Int1, Int2, Int3);
+        }
+    }
+
+    internal struct Struct4 : IEquatable<Struct4>
+    {
+        public int Int1;
+        public int Int2;
+        public int Int3;
+        public int Int4;
+
+        public Struct4(int i)
+        {
+            Int1 = Int2 = Int3 = Int4 = i;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Struct4 @struct && Equals(@struct);
+        }
+
+        public bool Equals(Struct4 other)
+        {
+            return Int1 == other.Int1 &&
+                   Int2 == other.Int2 &&
+                   Int3 == other.Int3 &&
+                   Int4 == other.Int4;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Int1, Int2, Int3, Int4);
+        }
+    }
+
+    internal struct Struct5 : IEquatable<Struct5>
+    {
+        public int Int1;
+        public int Int2;
+        public int Int3;
+        public int Int4;
+        public int Int5;
+
+        public Struct5(int i)
+        {
+            Int1 = Int2 = Int3 = Int4 = Int5 = i;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Struct5 @struct && Equals(@struct);
+        }
+
+        public bool Equals(Struct5 other)
+        {
+            return Int1 == other.Int1 &&
+                   Int2 == other.Int2 &&
+                   Int3 == other.Int3 &&
+                   Int4 == other.Int4 &&
+                   Int5 == other.Int5;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Int1, Int2, Int3, Int4, Int5);
+        }
+    }
+
+    internal struct Struct6 : IEquatable<Struct6>
+    {
+        public int Int1;
+        public int Int2;
+        public int Int3;
+        public int Int4;
+        public int Int5;
+        public int Int6;
+
+        public Struct6(int i)
+        {
+            Int1 = Int2 = Int3 = Int4 = Int5 = Int6 = i;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Struct6 @struct && Equals(@struct);
+        }
+
+        public bool Equals(Struct6 other)
+        {
+            return Int1 == other.Int1 &&
+                   Int2 == other.Int2 &&
+                   Int3 == other.Int3 &&
+                   Int4 == other.Int4 &&
+                   Int5 == other.Int5 &&
+                   Int6 == other.Int6;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Int1, Int2, Int3, Int4, Int5, Int6);
+        }
+    }
+
+    internal struct Struct7 : IEquatable<Struct7>
+    {
+        public int Int1;
+        public int Int2;
+        public int Int3;
+        public int Int4;
+        public int Int5;
+        public int Int6;
+        public int Int7;
+
+        public Struct7(int i)
+        {
+            Int1 = Int2 = Int3 = Int4 = Int5 = Int6 = Int7 = i;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Struct7 @struct && Equals(@struct);
+        }
+
+        public bool Equals(Struct7 other)
+        {
+            return Int1 == other.Int1 &&
+                   Int2 == other.Int2 &&
+                   Int3 == other.Int3 &&
+                   Int4 == other.Int4 &&
+                   Int5 == other.Int5 &&
+                   Int6 == other.Int6 &&
+                   Int7 == other.Int7;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Int1, Int2, Int3, Int4, Int5, Int6, Int7);
+        }
+    }
+
+    internal struct Struct8 : IEquatable<Struct8>
+    {
+        public int Int1;
+        public int Int2;
+        public int Int3;
+        public int Int4;
+        public int Int5;
+        public int Int6;
+        public int Int7;
+        public int Int8;
+
+        public Struct8(int i)
+        {
+            Int1 = Int2 = Int3 = Int4 = Int5 = Int6 = Int7 = Int8 = i;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Struct8 @struct && Equals(@struct);
+        }
+
+        public bool Equals(Struct8 other)
+        {
+            return Int1 == other.Int1 &&
+                   Int2 == other.Int2 &&
+                   Int3 == other.Int3 &&
+                   Int4 == other.Int4 &&
+                   Int5 == other.Int5 &&
+                   Int6 == other.Int6 &&
+                   Int7 == other.Int7 &&
+                   Int8 == other.Int8;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Int1, Int2, Int3, Int4, Int5, Int6, Int7, Int8);
+        }
+    }
+}
