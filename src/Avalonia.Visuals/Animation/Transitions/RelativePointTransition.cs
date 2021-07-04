9f7a5de29edfced86e19a72d1fe015e01bc81fb5
Max Katz <maxkatz6@outlook.com>
2021-07-04 03:51:28 -0400

    Add RelativePointAnimator

---
+++ src/Avalonia.Visuals/Animation/Transitions/RelativePointTransition.cs
@@ -0,0 +1,11 @@
+using Avalonia.Animation.Animators;
+
+namespace Avalonia.Animation
+{
+    /// <summary>
+    /// Transition class that handles <see cref="AvaloniaProperty"/> with <see cref="RelativePoint"/> type.
+    /// </summary>  
+    public class RelativePointTransition : AnimatorDrivenTransition<RelativePoint, RelativePointAnimator>
+    {
+    }
+}
