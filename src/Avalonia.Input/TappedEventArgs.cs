3d7729b02e304d0d8362f486a5f781710f4183c9
GMIKE <roman.soloweow333@ya.ru>
2021-07-29 16:50:51 +0300

    Properties of PointerEventArgs in TappedEventArgs

--- src/Avalonia.Input/TappedEventArgs.cs
+++ src/Avalonia.Input/TappedEventArgs.cs
@@ -6,7 +6,12 @@ namespace Avalonia.Input
     public class TappedEventArgs : RoutedEventArgs
     {
         private readonly PointerEventArgs lastPointerEventArgs;
-
+        public IPointer Pointer => lastPointerEventArgs.Pointer;
+        public InputModifiers InputModifiers => lastPointerEventArgs.InputModifiers;
+        public KeyModifiers KeyModifiers => lastPointerEventArgs.KeyModifiers;
+        public ulong Timestamp => lastPointerEventArgs.Timestamp;
+        public IPointerDevice Device => lastPointerEventArgs.Device;
+        
         public TappedEventArgs(RoutedEvent routedEvent, PointerEventArgs lastPointerEventArgs)
             : base(routedEvent)
         {
