8bfe9f081f4788420b761dae7d8f68a21fd700ca
Nathan Garside <ngarside@users.noreply.github.com>
2021-08-07 10:21:50 +0100

    Update Window.cs

--- src/Avalonia.Controls/Window.cs
+++ src/Avalonia.Controls/Window.cs
@@ -856,10 +856,9 @@ namespace Avalonia.Controls
         {
             var scaling = owner?.DesktopScaling ?? PlatformImpl?.DesktopScaling ?? 1;
 
-            // TODO: We really need non-client size here.
             var rect = new PixelRect(
                 PixelPoint.Origin,
-                PixelSize.FromSize(ClientSize, scaling));
+                PixelSize.FromSize(FrameSize ?? ClientSize, scaling));
             var screen = Screens.ScreenFromPoint(owner?.Position ?? Position);
             var margin = Margin * scaling;
 
@@ -874,10 +873,9 @@ namespace Avalonia.Controls
                 case WindowStartupLocation.CenterOwner:
                     if (owner != null)
                     {
-                        // TODO: We really need non-client size here.
                         var ownerRect = new PixelRect(
                             owner.Position,
-                            PixelSize.FromSize(owner.ClientSize, scaling));
+                            PixelSize.FromSize(owner.FrameSize ?? owner.ClientSize, scaling));
                         Position = ownerRect.CenterRect(rect).Position;
                     }
                     break;
