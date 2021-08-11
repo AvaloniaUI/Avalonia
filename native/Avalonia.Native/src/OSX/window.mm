346015d804e777a1ea2f32f2252841a9135845ae
Steven Kirk <grokys@gmail.com>
2021-08-11 21:03:52 +0200

    Call WindowStateChanged when setting WindowState via code.
    Fixes #6399

--- native/Avalonia.Native/src/OSX/window.mm
+++ native/Avalonia.Native/src/OSX/window.mm
@@ -1204,6 +1204,7 @@ private:
                 }
                 
                 _actualWindowState = _lastWindowState;
+                WindowEvents->WindowStateChanged(_actualWindowState);
             }
             
             
