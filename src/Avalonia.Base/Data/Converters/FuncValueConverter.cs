0c20e122ba877db3ad702ce235858aef6ec590be
Yoh Deadfall <yoh.deadfall@hotmail.com>
2021-09-02 13:36:47 +0300

    Code deduplication

--- src/Avalonia.Base/Data/Converters/FuncValueConverter.cs
+++ src/Avalonia.Base/Data/Converters/FuncValueConverter.cs
@@ -26,7 +26,7 @@ namespace Avalonia.Data.Converters
         /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is TIn || (value == null && TypeUtilities.AcceptsNull<TIn>()))
+            if (TypeUtilities.CanCast<TIn>(value))
             {
                 return _convert((TIn)value);
             }
