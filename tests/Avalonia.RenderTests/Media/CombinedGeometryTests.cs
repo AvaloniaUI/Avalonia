79f601886e6ba7a124140d3c1e24a17f896fc1e4
Steven Kirk <grokys@gmail.com>
2021-09-12 19:31:07 +0200

    Added tests for transforms on CombinedGeometry children.

--- tests/Avalonia.RenderTests/Media/CombinedGeometryTests.cs
+++ tests/Avalonia.RenderTests/Media/CombinedGeometryTests.cs
@@ -52,5 +52,38 @@ namespace Avalonia.Direct2D1.RenderTests.Media
             await RenderToFile(target, testName);
             CompareImages(testName);
         }
+
+        [Fact]
+        public async Task Geometry1_Transform()
+        {
+            var target = new Border
+            {
+                Width = 200,
+                Height = 200,
+                Background = Brushes.White,
+                Child = new Path
+                {
+                    Data = new CombinedGeometry
+                    {
+                        Geometry1 = new RectangleGeometry(new Rect(25, 25, 100, 100))
+                        {
+                            Transform = new RotateTransform(45, 75, 75)
+                        },
+                        Geometry2 = new EllipseGeometry
+                        {
+                            Center = new Point(125, 125),
+                            RadiusX = 50,
+                            RadiusY = 50,
+                        }
+                    },
+                    Fill = Brushes.Blue,
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 1,
+                }
+            };
+
+            await RenderToFile(target);
+            CompareImages();
+        }
     }
 }
