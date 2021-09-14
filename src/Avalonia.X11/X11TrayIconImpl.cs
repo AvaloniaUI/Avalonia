e34f3ab947ab1776d84d3678691bfcebebb1e297
Dan Walmsley <dan@walms.co.uk>
2021-09-14 11:10:22 +0100

    add a tray icon stub to x11 platform.

---
+++ src/Avalonia.X11/X11TrayIconImpl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Avalonia.Controls.Platform;
+using Avalonia.Platform;
+
+namespace Avalonia.X11
+{
+    class X11TrayIconImpl : ITrayIconImpl
+    {
+        public INativeMenuExporter MenuExporter => null;
+
+        public Action OnClicked { get; set; }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void SetIcon(IWindowIconImpl icon)
+        {
+            
+        }
+
+        public void SetIsVisible(bool visible)
+        {
+            
+        }
+
+        public void SetToolTipText(string text)
+        {
+            
+        }
+    }
+}
