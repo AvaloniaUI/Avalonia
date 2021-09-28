23dcab47d4461d54bed91eec470c30f74e062843
Steven Kirk <grokys@gmail.com>
2021-09-28 12:41:11 +0200

    Allow customizing the framebuffer FPS.

---
+++ src/Linux/Avalonia.LinuxFramebuffer/LinuxFramebufferPlatformOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Avalonia.LinuxFramebuffer
+{
+    /// <summary>
+    /// Platform-specific options which apply to the Linux framebuffer.
+    /// </summary>
+    public class LinuxFramebufferPlatformOptions
+    {
+        /// <summary>
+        /// Gets or sets the number of frames per second at which the renderer should run.
+        /// Default 60.
+        /// </summary>
+        public int Fps { get; set; } = 60;
+    }
+}
