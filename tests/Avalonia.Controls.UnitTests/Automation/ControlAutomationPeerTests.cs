a5c28de7976cb7a460592fbfce444b3092fc0c56
Steven Kirk <grokys@gmail.com>
2021-10-03 21:17:59 +0200

    Reinstate ControlAutomationPeerTests.

--- tests/Avalonia.Controls.UnitTests/Automation/ControlAutomationPeerTests.cs
+++ tests/Avalonia.Controls.UnitTests/Automation/ControlAutomationPeerTests.cs
@@ -1,253 +1,229 @@
-﻿////using System.Linq;
-////using Avalonia.Automation.Peers;
-////using Avalonia.Automation.Platform;
-////using Avalonia.Automation.Provider;
-////using Avalonia.Controls.Presenters;
-////using Avalonia.Controls.Primitives;
-////using Avalonia.Controls.Templates;
-////using Avalonia.Platform;
-////using Avalonia.UnitTests;
-////using Avalonia.VisualTree;
-////using Moq;
-////using Xunit;
+﻿using System;
+using System.Linq;
+using Avalonia.Automation.Peers;
+using Avalonia.Automation.Provider;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Templates;
+using Avalonia.Platform;
+using Avalonia.UnitTests;
+using Avalonia.VisualTree;
+using Xunit;
 
-////namespace Avalonia.Controls.UnitTests.Automation
-////{
-////    public class ControlAutomationPeerTests
-////    {
-////        private static Mock<IAutomationNodeFactory> _factory;
+#nullable enable
 
-////        public ControlAutomationPeerTests()
-////        {
-////            _factory = new Mock<IAutomationNodeFactory>();
-////            _factory.Setup(x => x.CreateNode(It.IsAny<AutomationPeer>()))
-////                .Returns(() => Mock.Of<IAutomationNode>(x => x.Factory == _factory));
-////        }
+namespace Avalonia.Controls.UnitTests.Automation
+{
+    public class ControlAutomationPeerTests
+    {
+        public class Children
+        {
+            [Fact]
+            public void Creates_Children_For_Controls_In_Visual_Tree()
+            {
+                var panel = new Panel
+                {
+                    Children =
+                    {
+                        new Border(),
+                        new Border(),
+                    },
+                };
 
-////        public class Children
-////        {
-////            [Fact]
-////            public void Creates_Children_For_Controls_In_Visual_Tree()
-////            {
-////                var panel = new Panel
-////                {
-////                    Children =
-////                    {
-////                        new Border(),
-////                        new Border(),
-////                    },
-////                };
+                var target = CreatePeer(panel);
 
-////                var factory = CreateFactory();
-////                var target = CreatePeer(factory, panel);
+                Assert.Equal(
+                    panel.GetVisualChildren(),
+                    target.GetChildren().Cast<ControlAutomationPeer>().Select(x => x.Owner));
+            }
 
-////                Assert.Equal(
-////                    panel.GetVisualChildren(),
-////                    target.GetChildren().Cast<ControlAutomationPeer>().Select(x => x.Owner));
-////            }
+            [Fact]
+            public void Creates_Children_when_Controls_Attached_To_Visual_Tree()
+            {
+                var contentControl = new ContentControl
+                {
+                    Template = new FuncControlTemplate<ContentControl>((o, ns) =>
+                        new ContentPresenter
+                        {
+                            Name = "PART_ContentPresenter",
+                            [!ContentPresenter.ContentProperty] = o[!ContentControl.ContentProperty],
+                        }),
+                    Content = new Border(),
+                };
 
-////            [Fact]
-////            public void Creates_Children_when_Controls_Attached_To_Visual_Tree()
-////            {
-////                var contentControl = new ContentControl
-////                {
-////                    Template = new FuncControlTemplate<ContentControl>((o, ns) =>
-////                        new ContentPresenter
-////                        {
-////                            Name = "PART_ContentPresenter",
-////                            [!ContentPresenter.ContentProperty] = o[!ContentControl.ContentProperty],
-////                        }),
-////                    Content = new Border(),
-////                };
+                var target = CreatePeer(contentControl);
 
-////                var factory = CreateFactory();
-////                var target = CreatePeer(factory, contentControl);
+                Assert.Empty(target.GetChildren());
 
-////                Assert.Empty(target.GetChildren());
+                contentControl.Measure(Size.Infinity);
 
-////                contentControl.Measure(Size.Infinity);
+                Assert.Equal(1, target.GetChildren().Count);
+            }
 
-////                Assert.Equal(1, target.GetChildren().Count);
-////            }
+            [Fact]
+            public void Updates_Children_When_VisualChildren_Added()
+            {
+                var panel = new Panel
+                {
+                    Children =
+                    {
+                        new Border(),
+                        new Border(),
+                    },
+                };
 
-////            [Fact]
-////            public void Updates_Children_When_VisualChildren_Added()
-////            {
-////                var panel = new Panel
-////                {
-////                    Children =
-////                    {
-////                        new Border(),
-////                        new Border(),
-////                    },
-////                };
+                var target = CreatePeer(panel);
+                var children = target.GetChildren();
 
-////                var factory = CreateFactory();
-////                var target = CreatePeer(factory, panel);
-////                var children = target.GetChildren();
+                Assert.Equal(2, children.Count);
 
-////                Assert.Equal(2, children.Count);
+                panel.Children.Add(new Decorator());
 
-////                panel.Children.Add(new Decorator());
+                children = target.GetChildren();
+                Assert.Equal(3, children.Count);
+            }
 
-////                children = target.GetChildren();
-////                Assert.Equal(3, children.Count);
-////            }
+            [Fact]
+            public void Updates_Children_When_VisualChildren_Removed()
+            {
+                var panel = new Panel
+                {
+                    Children =
+                    {
+                        new Border(),
+                        new Border(),
+                    },
+                };
 
-////            [Fact]
-////            public void Updates_Children_When_VisualChildren_Removed()
-////            {
-////                var panel = new Panel
-////                {
-////                    Children =
-////                    {
-////                        new Border(),
-////                        new Border(),
-////                    },
-////                };
+                var target = CreatePeer(panel);
+                var children = target.GetChildren();
 
-////                var factory = CreateFactory();
-////                var target = CreatePeer(factory, panel);
-////                var children = target.GetChildren();
+                Assert.Equal(2, children.Count);
 
-////                Assert.Equal(2, children.Count);
+                panel.Children.RemoveAt(1);
 
-////                panel.Children.RemoveAt(1);
+                children = target.GetChildren();
+                Assert.Equal(1, children.Count);
+            }
 
-////                children = target.GetChildren();
-////                Assert.Equal(1, children.Count);
-////            }
+            [Fact]
+            public void Updates_Children_When_Visibility_Changes()
+            {
+                var panel = new Panel
+                {
+                    Children =
+                    {
+                        new Border(),
+                        new Border(),
+                    },
+                };
 
-////            [Fact]
-////            public void Updates_Children_When_Visibility_Changes()
-////            {
-////                var panel = new Panel
-////                {
-////                    Children =
-////                    {
-////                        new Border(),
-////                        new Border(),
-////                    },
-////                };
+                var target = CreatePeer(panel);
+                var children = target.GetChildren();
 
-////                var factory = CreateFactory();
-////                var target = CreatePeer(factory, panel);
-////                var children = target.GetChildren();
+                Assert.Equal(2, children.Count);
 
-////                Assert.Equal(2, children.Count);
+                panel.Children[1].IsVisible = false;
+                children = target.GetChildren();
+                Assert.Equal(1, children.Count);
 
-////                panel.Children[1].IsVisible = false;
-////                children = target.GetChildren();
-////                Assert.Equal(1, children.Count);
+                panel.Children[1].IsVisible = true;
+                children = target.GetChildren();
+                Assert.Equal(2, children.Count);
+            }
+        }
 
-////                panel.Children[1].IsVisible = true;
-////                children = target.GetChildren();
-////                Assert.Equal(2, children.Count);
-////            }
-////        }
+        public class Parent
+        {
+            [Fact]
+            public void Connects_Peer_To_Tree_When_GetParent_Called()
+            {
+                var border = new Border();
+                var tree = new Decorator
+                {
+                    Child = new Decorator
+                    {
+                        Child = border,
+                    }
+                };
 
-////        public class Parent
-////        {
-////            [Fact]
-////            public void Connects_Peer_To_Tree_When_GetParent_Called()
-////            {
-////                var border = new Border();
-////                var tree = new Decorator
-////                {
-////                    Child = new Decorator
-////                    {
-////                        Child = border,
-////                    }
-////                };
+                // We're accessing Border directly without going via its ancestors. Because the tree
+                // is built lazily, ensure that calling GetParent causes the ancestor tree to be built.
+                var target = CreatePeer(border);
 
-////                var factory = CreateFactory();
+                var parentPeer = Assert.IsAssignableFrom<ControlAutomationPeer>(target.GetParent());
+                Assert.Same(border.GetVisualParent(), parentPeer.Owner);
+            }
 
-////                // We're accessing Border directly without going via its ancestors. Because the tree
-////                // is built lazily, ensure that calling GetParent causes the ancestor tree to be built.
-////                var target = CreatePeer(factory, border);
+            [Fact]
+            public void Parent_Updated_When_Moved_To_Separate_Visual_Tree()
+            {
+                var border = new Border();
+                var root1 = new Decorator { Child = border };
+                var root2 = new Decorator();
+                var target = CreatePeer(border);
 
-////                var parentPeer = Assert.IsAssignableFrom<ControlAutomationPeer>(target.GetParent());
-////                Assert.Same(border.GetVisualParent(), parentPeer.Owner);
-////            }
+                var parentPeer = Assert.IsAssignableFrom<ControlAutomationPeer>(target.GetParent());
+                Assert.Same(root1, parentPeer.Owner);
 
-////            [Fact]
-////            public void Parent_Updated_When_Moved_To_Separate_Visual_Tree()
-////            {
-////                var border = new Border();
-////                var root1 = new Decorator { Child = border };
-////                var root2 = new Decorator();
-////                var factory = CreateFactory();
-////                var target = CreatePeer(factory, border);
+                root1.Child = null;
 
-////                var parentPeer = Assert.IsAssignableFrom<ControlAutomationPeer>(target.GetParent());
-////                Assert.Same(root1, parentPeer.Owner);
+                Assert.Null(target.GetParent());
 
-////                root1.Child = null;
+                root2.Child = border;
 
-////                Assert.Null(target.GetParent());
+                parentPeer = Assert.IsAssignableFrom<ControlAutomationPeer>(target.GetParent());
+                Assert.Same(root2, parentPeer.Owner);
+            }
+        }
 
-////                root2.Child = border;
+        private static AutomationPeer CreatePeer(Control control)
+        {
+            return ControlAutomationPeer.CreatePeerForElement(control);
+        }
 
-////                parentPeer = Assert.IsAssignableFrom<ControlAutomationPeer>(target.GetParent());
-////                Assert.Same(root2, parentPeer.Owner);
-////            }
-////        }
+        private class TestControl : Control
+        {
+            protected override AutomationPeer OnCreateAutomationPeer()
+            {
+                return new TestAutomationPeer(this);
+            }
+        }
 
-////        private static IAutomationNodeFactory CreateFactory()
-////        {
-////            var factory = new Mock<IAutomationNodeFactory>();
-////            factory.Setup(x => x.CreateNode(It.IsAny<AutomationPeer>()))
-////                .Returns(() => Mock.Of<IAutomationNode>(x => x.Factory == factory.Object));
-////            return factory.Object;
-////        }
+        private class AutomationTestRoot : TestRoot
+        {
+            protected override AutomationPeer OnCreateAutomationPeer()
+            {
+                return new TestRootAutomationPeer(this);
+            }
+        }
 
-////        private static AutomationPeer CreatePeer(IAutomationNodeFactory factory, Control control)
-////        {
-////            return ControlAutomationPeer.GetOrCreatePeer(factory, control);
-////        }
+        private class TestAutomationPeer : ControlAutomationPeer
+        {
+            public TestAutomationPeer( Control owner)
+                : base(owner)
+            {
+            }
+        }
 
-////        private class TestControl : Control
-////        {
-////            protected override AutomationPeer OnCreateAutomationPeer(IAutomationNodeFactory factory)
-////            {
-////                return new TestAutomationPeer(factory, this);
-////            }
-////        }
+        private class TestRootAutomationPeer : ControlAutomationPeer, IRootProvider
+        {
+            public TestRootAutomationPeer(Control owner)
+                : base(owner)
+            {
+            }
 
-////        private class AutomationTestRoot : TestRoot
-////        {
-////            protected override AutomationPeer OnCreateAutomationPeer(IAutomationNodeFactory factory)
-////            {
-////                return new TestRootAutomationPeer(factory, this);
-////            }
-////        }
+            public ITopLevelImpl PlatformImpl => throw new System.NotImplementedException();
+            public event EventHandler? FocusChanged;
 
-////        private class TestAutomationPeer : ControlAutomationPeer
-////        {
-////            public TestAutomationPeer(IAutomationNodeFactory factory, Control owner)
-////                : base(factory, owner) 
-////            {
-////            }
-////        }
+            public AutomationPeer GetFocus()
+            {
+                throw new System.NotImplementedException();
+            }
 
-////        private class TestRootAutomationPeer : ControlAutomationPeer, IRootProvider
-////        {
-////            public TestRootAutomationPeer(IAutomationNodeFactory factory, Control owner)
-////                : base(factory, owner)
-////            {
-////            }
-
-////            public ITopLevelImpl PlatformImpl => throw new System.NotImplementedException();
-
-////            public AutomationPeer GetFocus()
-////            {
-////                throw new System.NotImplementedException();
-////            }
-
-////            public AutomationPeer GetPeerFromPoint(Point p)
-////            {
-////                throw new System.NotImplementedException();
-////            }
-////        }
-////    }
-////}
+            public AutomationPeer GetPeerFromPoint(Point p)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
