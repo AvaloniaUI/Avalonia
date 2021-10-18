e078aa253daf625df49a5f4c494c9fb33ab3e50a
Nikita Tsukanov <keks9n@gmail.com>
2021-10-18 23:51:24 +0300

    Download SDK separately (unix)

---
+++ get-sdk.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SDK_DIR=$SCRIPT_DIR/sdk
+
+DOTNET_INSTALL_FILE="$SDK_DIR/dotnet-install.sh"
+DOTNET_INSTALL_URL="https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.sh"
+
+DOTNET_VERSION=5.0.302
+
+mkdir -p "$SDK_DIR"
+
+if [ ! -f "$DOTNET_INSTALL_FILE" ]; then
+	curl -Lsfo "$DOTNET_INSTALL_FILE" "$DOTNET_INSTALL_URL"
+	chmod +x "$DOTNET_INSTALL_FILE"
+fi
+
+"$DOTNET_INSTALL_FILE" --install-dir "$SDK_DIR" --version 5.0.302
+"$DOTNET_INSTALL_FILE" --install-dir "$SDK_DIR" --version 3.1.20 --runtime dotnet
+"$DOTNET_INSTALL_FILE" --install-dir "$SDK_DIR" --version 3.1.20 --runtime aspnetcore
+
+
