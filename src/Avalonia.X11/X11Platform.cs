1ae2e360b4e4bacaf5054dad24c5548bac5b1857
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2021-11-09 00:09:41 +0800

    Implement IPlatformLifetimeEvents for X11/Linux

--- src/Avalonia.X11/X11Platform.cs
+++ src/Avalonia.X11/X11Platform.cs
@@ -80,7 +80,8 @@ namespace Avalonia.X11
                 .Bind<IPlatformSettings>().ToConstant(new PlatformSettingsStub())
                 .Bind<IPlatformIconLoader>().ToConstant(new X11IconLoader(Info))
                 .Bind<ISystemDialogImpl>().ToConstant(new GtkSystemDialog())
-                .Bind<IMountedVolumeInfoProvider>().ToConstant(new LinuxMountedVolumeInfoProvider());
+                .Bind<IMountedVolumeInfoProvider>().ToConstant(new LinuxMountedVolumeInfoProvider())
+                .Bind<IPlatformLifetimeEventsImpl>().ToConstant(new X11PlatformLifetimeEvents());
             
             X11Screens = Avalonia.X11.X11Screens.Init(this);
             Screens = new X11Screens(X11Screens);
