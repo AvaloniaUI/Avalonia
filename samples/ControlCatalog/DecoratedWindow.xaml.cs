ce984a9d6f82ea3f294734ad3cb5526fb3081612
Giuseppe Lippolis <workgroupengineering@outlook.com>
2021-11-20 10:09:13 +0100

    fixes(ControlCatalog): remove AttachDevTools from DecoratedWindow

--- samples/ControlCatalog/DecoratedWindow.xaml.cs
+++ samples/ControlCatalog/DecoratedWindow.xaml.cs
@@ -11,7 +11,6 @@ namespace ControlCatalog
         public DecoratedWindow()
         {
             this.InitializeComponent();
-            this.AttachDevTools();
         }
 
         void SetupSide(string name, StandardCursorType cursor, WindowEdge edge)
