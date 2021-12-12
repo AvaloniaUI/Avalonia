28a2a2fc8eeacf5f68a7bb40526b76f3a407a2e9
Sergey Mikolaytis <blasterloremaster@gmail.com>
2021-12-12 04:53:24 +0300

    Windows IME (#7007)
    * win32 ime wip
    
    * ime window starts tracking the cursor, but coords are wrong
    
    * fix win32 ime cursor coord
    
    * win32-ime lang-specific behaviors
    
    * track language id in WindowImpl
    
    * lowercase dllimport
    
    * create initial ime on window creation
    
    * InputMethodManager: connect to client even if im is absent at the moment
    
    * proposal: IKeyboardDevice.NotifyInputMethodUpdated
    
    * finalizing
    
    * ime: allow client to request active state change
    
    * remove backward incompatible ActiveState.
    
    * InputMethodManager: NotifyInputMethodUpdated: filter the window of current focused element
    
    * [IME] [Windows] ability to enable/disable IME for any InputElement
    
    * [IME] [Windows] Refactor Imm32InputMethod - create a single one for dispatcher. Also change a method of enabling/disabling IME to work like in WPF.
    
    * [IME] [Windows] Fix IME after dialog show not working - active window context is not applied.
    
    * [IME] [Windows] fix intermediate input position
    
    * [IME] [Windows] PreEdit font size is applied
    
    * [IME] [Windows] Make MoveImeWindow code to be exact like in chrome - fix a lot of possible issues. Added comments. Minor Refactoring
    
    * [IME] [Windows] Refactor caret management, improve deactivation, remove comments
    
    * [IME] [Windows] Remove redundant api changes (request from @kekekeks)
    
    * Fix .sln and ApiCompatBesaline.txt redundant changes.
    
    * [Windows] [IME] move IsInputMethodEnabled subscription to InputMethodManager, Move check for IsInputMethodEnabled before TextInputMethodClientRequestedEvent query
    
    * [IME] [Windows] remove redundant SetActive(false) call, because it's called in Client setter
    
    * remove redundant change
    
    Co-authored-by: Yatao Li <yatli@microsoft.com>
    Co-authored-by: Max Katz <maxkatz6@outlook.com>

--- src/Avalonia.Input/TextInput/InputMethodManager.cs
+++ src/Avalonia.Input/TextInput/InputMethodManager.cs
@@ -8,9 +8,14 @@ namespace Avalonia.Input.TextInput
         private ITextInputMethodImpl? _im;
         private IInputElement? _focusedElement;
         private ITextInputMethodClient? _client;
+        private IDisposable? _subscribeDisposable;
         private readonly TransformTrackingHelper _transformTracker = new TransformTrackingHelper();
 
-        public TextInputMethodManager() => _transformTracker.MatrixChanged += UpdateCursorRect;
+        public TextInputMethodManager()
+        {
+            _transformTracker.MatrixChanged += UpdateCursorRect;
+            InputMethod.IsInputMethodEnabledProperty.Changed.Subscribe(OnIsInputMethodEnabledChanged);
+        }
 
         private ITextInputMethodClient? Client
         {
@@ -40,6 +45,7 @@ namespace Avalonia.Input.TextInput
                     _im?.SetOptions(optionsQuery);
                     _transformTracker?.SetVisual(_client?.TextViewVisual);
                     UpdateCursorRect();
+                    
                     _im?.SetActive(true);
                 }
                 else
@@ -50,6 +56,14 @@ namespace Avalonia.Input.TextInput
             }
         }
 
+        private void OnIsInputMethodEnabledChanged(AvaloniaPropertyChangedEventArgs<bool> obj)
+        {
+            if (ReferenceEquals(obj.Sender, _focusedElement))
+            {
+                TryFindAndApplyClient();
+            }
+        }
+
         private void OnTextViewVisualChanged(object sender, EventArgs e) 
             => _transformTracker.SetVisual(_client?.TextViewVisual);
 
@@ -57,6 +71,7 @@ namespace Avalonia.Input.TextInput
         {
             if (_im == null || _client == null || _focusedElement?.VisualRoot == null)
                 return;
+
             var transform = _focusedElement.TransformToVisual(_focusedElement.VisualRoot);
             if (transform == null)
                 _im.SetCursorRect(default);
@@ -75,17 +90,23 @@ namespace Avalonia.Input.TextInput
             if(_focusedElement == element)
                 return;
             _focusedElement = element;
-            
+
             var inputMethod = (element?.VisualRoot as ITextInputMethodRoot)?.InputMethod;
-            if(_im != inputMethod)
+            if (_im != inputMethod)
                 _im?.SetActive(false);
 
             _im = inputMethod;
-            
-            if (_focusedElement == null || _im == null)
+
+            TryFindAndApplyClient();
+        }
+
+        private void TryFindAndApplyClient()
+        {
+            if (_focusedElement is not InputElement focused ||
+                _im == null ||
+                !InputMethod.GetIsInputMethodEnabled(focused))
             {
                 Client = null;
-                _im?.SetActive(false);
                 return;
             }
 
@@ -93,7 +114,7 @@ namespace Avalonia.Input.TextInput
             {
                 RoutedEvent = InputElement.TextInputMethodClientRequestedEvent
             };
-            
+
             _focusedElement.RaiseEvent(clientQuery);
             Client = clientQuery.Client;
         }
