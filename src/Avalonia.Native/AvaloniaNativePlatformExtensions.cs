18115363f8445e3ea9894a098497ddd48ab0f745
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2021-12-17 10:45:35 +0800

    Update src/Avalonia.Native/AvaloniaNativePlatformExtensions.cs
    Co-authored-by: Max Katz <maxkatz6@outlook.com>

--- src/Avalonia.Native/AvaloniaNativePlatformExtensions.cs
+++ src/Avalonia.Native/AvaloniaNativePlatformExtensions.cs
@@ -75,7 +75,7 @@ namespace Avalonia
         public bool DisableDefaultApplicationMenuItems { get; set; }
         
         /// <summary>
-        /// 
+        /// Gets or sets a value indicating whether the native macOS menu bar will be enabled for the application.
         /// </summary>
         public bool DisableNativeMenus { get; set; }
     }
