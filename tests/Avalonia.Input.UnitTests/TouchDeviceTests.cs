f190c53c1dc64fe0f5870b7f2764e050ce1c416d
Takoooooo <dan.pan2012@yandex.ru>
2021-12-17 17:23:22 +0200

    initial testing infrastructure

---
+++ tests/Avalonia.Input.UnitTests/TouchDeviceTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Avalonia.Input.Raw;
+using Avalonia.UnitTests;
+using Xunit;
+
+namespace Avalonia.Input.UnitTests
+{
+    public class TouchDeviceTests
+    {
+        [Fact]
+        public void Tapped_Event_Is_Fired_With_Touch()
+        {
+            using (UnitTestApplication.Start(
+                new TestServices(inputManager: new InputManager())))
+            {
+                var inputManager = InputManager.Instance;
+                var root = new TestRoot();
+                var touchDevice = new TouchDevice();
+
+                var isTapped = false;
+                var executedTimes = 0;
+                root.Tapped += (a, e) =>
+                {
+                    isTapped = true;
+                    executedTimes++;
+                };
+                TapOnce(inputManager, touchDevice, root);
+                Assert.True(isTapped);
+                Assert.Equal(1, executedTimes);
+            }
+        }
+        private static void TapOnce(IInputManager inputManager, TouchDevice device, IInputRoot root)
+        {
+            inputManager.ProcessInput(new RawTouchEventArgs(device, 0,
+                                               root,
+                                               RawPointerEventType.TouchBegin,
+                                               new Point(0, 0),
+                                               RawInputModifiers.None,
+                                               1));
+            inputManager.ProcessInput(new RawTouchEventArgs(device, 0,
+                                                root,
+                                                RawPointerEventType.TouchEnd,
+                                                new Point(0, 0),
+                                                RawInputModifiers.None,
+                                                1));
+        }
+    }
+}
