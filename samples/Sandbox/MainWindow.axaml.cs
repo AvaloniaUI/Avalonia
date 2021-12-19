7f20078e4f3226ab98899ecd27355104eef6dcb7
abdesol <abdesoltak@gmail.com>
2021-12-19 08:52:31 +0300

    removed focus fix and added test in sandbox

--- samples/Sandbox/MainWindow.axaml.cs
+++ samples/Sandbox/MainWindow.axaml.cs
@@ -5,17 +5,40 @@ using Avalonia.Win32.WinRT.Composition;
 
 namespace Sandbox
 {
-    public class MainWindow : Window
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+#if DEBUG
             this.AttachDevTools();
+#endif
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            var button = new Button
+            {
+                Content = "Tabulate to this button"
+            };
+
+            Content = button;
+            button.GotFocus += (_, _) =>
+            {
+                button.IsEnabled = false;
+                button.Content =
+                    $"Now this button is disabled ({nameof(IsEnabled)}:{button.IsEnabled},{nameof(IsEffectivelyEnabled)}:{button.IsEffectivelyEnabled}), but you can still press Enter";
+                button.KeyDown += (_, _) =>
+                {
+                    button.Content = "button got fired by KeyDown event";
+                };
+            };
+            button.Click += (_, _) =>
+            {
+                button.Content = "It just has been clicked.";
+            };
         }
     }
 }
