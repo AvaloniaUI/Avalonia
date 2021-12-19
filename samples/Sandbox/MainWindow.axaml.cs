845ff07f1fc8d0f31836f73133487a8fb0ce245a
abdesol <abdesoltak@gmail.com>
2021-12-19 09:55:59 +0300

    Button_Invokes_Doesnt_Execute_When_Button_Disabled test added

--- samples/Sandbox/MainWindow.axaml.cs
+++ samples/Sandbox/MainWindow.axaml.cs
@@ -5,40 +5,17 @@ using Avalonia.Win32.WinRT.Composition;
 
 namespace Sandbox
 {
-    public partial class MainWindow : Window
+    public class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
-#if DEBUG
+            this.InitializeComponent();
             this.AttachDevTools();
-#endif
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-
-            var button = new Button
-            {
-                Content = "Tabulate to this button"
-            };
-
-            Content = button;
-            button.GotFocus += (_, _) =>
-            {
-                button.IsEnabled = false;
-                button.Content =
-                    $"Now this button is disabled ({nameof(IsEnabled)}:{button.IsEnabled},{nameof(IsEffectivelyEnabled)}:{button.IsEffectivelyEnabled}), but you can still press Enter";
-                button.KeyDown += (_, _) =>
-                {
-                    button.Content = "button got fired by KeyDown event";
-                };
-            };
-            button.Click += (_, _) =>
-            {
-                button.Content = "It just has been clicked.";
-            };
         }
     }
 }
