fd2635aae43500ca898ba6fe562192091ef68f78
robloo <robloo@users.noreply.github.com>
2022-02-27 23:56:21 -0500

    Readonly struct

--- src/Avalonia.Visuals/Media/HsvColor.cs
+++ src/Avalonia.Visuals/Media/HsvColor.cs
@@ -11,7 +11,7 @@ namespace Avalonia.Media
     /// <summary>
     /// Defines a color using the hue/saturation/value (HSV) model.
     /// </summary>
-    public struct HsvColor : IEquatable<HsvColor>
+    public readonly struct HsvColor : IEquatable<HsvColor>
     {
         /// <summary>
         /// Represents a null <see cref="HsvColor"/> with zero set for all channels.
