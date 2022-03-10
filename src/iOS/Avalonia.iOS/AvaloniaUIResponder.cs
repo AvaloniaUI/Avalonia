d717d10de802da0df60f715c381d0786de67d5f3
Dan Walmsley <dan@walms.co.uk>
2022-03-10 00:31:21 +0000

    use ime api to hide and show osk.

--- src/iOS/Avalonia.iOS/AvaloniaUIResponder.cs
+++ src/iOS/Avalonia.iOS/AvaloniaUIResponder.cs
@@ -1,6 +1,56 @@
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+using Avalonia.Input.TextInput;
+
 namespace Avalonia.iOS;
 
-public class AvaloniaUIResponder
+[Adopts("UIKeyInput")]
+public partial class AvaloniaView : ITextInputMethodImpl
 {
-    
+    public override bool CanResignFirstResponder => true;
+    public override bool CanBecomeFirstResponder => true;
+    public override bool CanBecomeFocused => true;
+
+    [Export("hasText")] public bool HasText => false;
+
+    [Export("insertText:")]
+    public void InsertText(string text)
+    {
+        
+    }
+
+    [Export("deleteBackward")]
+    public void DeleteBackward()
+    {
+    }
+
+    void ITextInputMethodImpl.SetActive(bool active)
+    {
+        if (active)
+        {
+            var isFr = IsFirstResponder;
+            var next = NextResponder;
+            var result = BecomeFirstResponder();
+        }
+        else
+        {
+            ResignFirstResponder();
+        }
+    }
+
+    void ITextInputMethodImpl.SetCursorRect(Rect rect)
+    {
+        
+    }
+
+    void ITextInputMethodImpl.SetOptions(TextInputOptionsQueryEventArgs options)
+    {
+        
+    }
+
+    void ITextInputMethodImpl.Reset()
+    {
+        
+    }
 }
