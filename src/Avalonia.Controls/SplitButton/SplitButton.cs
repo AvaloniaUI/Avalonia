14e4aec081e619ec9b94cb99b8eea191507b84e7
robloo <robloo@users.noreply.github.com>
2022-03-19 17:06:43 -0400

    Sync SplitButton with Button changes

--- src/Avalonia.Controls/SplitButton/SplitButton.cs
+++ src/Avalonia.Controls/SplitButton/SplitButton.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows.Input;
 using Avalonia.Controls.Metadata;
-using Avalonia.Controls.Mixins;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
@@ -314,16 +313,9 @@ namespace Avalonia.Controls
                 }
 
                 // Must unregister events here while a reference to the old flyout still exists
-                if (oldFlyout != null)
-                {
-                    UnregisterFlyoutEvents(oldFlyout);
-                }
+                UnregisterFlyoutEvents(oldFlyout);
 
-                if (newFlyout != null)
-                {
-                    RegisterFlyoutEvents(newFlyout);
-                }
-
+                RegisterFlyoutEvents(newFlyout);
                 UpdatePseudoClasses();
             }
 
@@ -415,6 +407,22 @@ namespace Avalonia.Controls
             }
         }
 
+        /// <summary>
+        /// Invoked when the split button's flyout is opened.
+        /// </summary>
+        protected virtual void OnFlyoutOpened()
+        {
+            // Available for derived types
+        }
+
+        /// <summary>
+        /// Invoked when the split button's flyout is closed.
+        /// </summary>
+        protected virtual void OnFlyoutClosed()
+        {
+            // Available for derived types
+        }
+
         ////////////////////////////////////////////////////////////////////////
         // Event Handling
         ////////////////////////////////////////////////////////////////////////
@@ -464,6 +472,8 @@ namespace Avalonia.Controls
             {
                 _isFlyoutOpen = true;
                 UpdatePseudoClasses();
+
+                OnFlyoutOpened();
             }
         }
 
@@ -479,6 +489,8 @@ namespace Avalonia.Controls
             {
                 _isFlyoutOpen = false;
                 UpdatePseudoClasses();
+
+                OnFlyoutClosed();
             }
         }
     }
