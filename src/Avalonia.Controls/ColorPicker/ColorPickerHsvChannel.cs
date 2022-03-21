e1e6789f7824c6a21f4d8c7288cf2cffe672ee00
robloo <robloo@users.noreply.github.com>
2022-03-20 23:05:29 -0400

    Add ColorPicker base enums

---
+++ src/Avalonia.Controls/ColorPicker/ColorPickerHsvChannel.cs
@@ -0,0 +1,33 @@
+﻿// This source file is adapted from the WinUI project.
+// (https://github.com/microsoft/microsoft-ui-xaml)
+//
+// Licensed to The Avalonia Project under the MIT License.
+
+namespace Avalonia.Controls
+{
+    /// <summary>
+    /// Defines a specific HSV color model channel.
+    /// </summary>
+    public enum ColorPickerHsvChannel
+    {
+        /// <summary>
+        /// The Hue channel.
+        /// </summary>
+        Hue,
+
+        /// <summary>
+        /// The Saturation channel.
+        /// </summary>
+        Saturation,
+
+        /// <summary>
+        /// The Value channel.
+        /// </summary>
+        Value,
+
+        /// <summary>
+        /// The Alpha channel.
+        /// </summary>
+        Alpha
+    };
+}
---
+++ src/Avalonia.Controls/ColorPicker/ColorSpectrumChannels.cs
@@ -0,0 +1,46 @@
+﻿// This source file is adapted from the WinUI project.
+// (https://github.com/microsoft/microsoft-ui-xaml)
+//
+// Licensed to The Avalonia Project under the MIT License.
+
+using Avalonia.Controls.Primitives;
+
+namespace Avalonia.Controls
+{
+    /// <summary>
+    /// Defines the two HSV color channels displayed by a <see cref="ColorSpectrum"/>.
+    /// Order of the color channels is important.
+    /// </summary>
+    public enum ColorSpectrumChannels
+    {
+        /// <summary>
+        /// The Hue and Value channels.
+        /// </summary>
+        HueValue,
+
+        /// <summary>
+        /// The Value and Hue channels.
+        /// </summary>
+        ValueHue,
+
+        /// <summary>
+        /// The Hue and Saturation channels.
+        /// </summary>
+        HueSaturation,
+
+        /// <summary>
+        /// The Saturation and Hue channels.
+        /// </summary>
+        SaturationHue,
+
+        /// <summary>
+        /// The Saturation and Value channels.
+        /// </summary>
+        SaturationValue,
+
+        /// <summary>
+        /// The Value and Saturation channels.
+        /// </summary>
+        ValueSaturation,
+    };
+}
