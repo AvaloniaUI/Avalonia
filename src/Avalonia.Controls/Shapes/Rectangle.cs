1747e733836aa6b0a61dea6c10d148a3a6d6dae2
robloo <robloo@users.noreply.github.com>
2022-04-08 12:30:30 -0400

    Add CornerRadius property to Rectangle

--- src/Avalonia.Controls/Shapes/Rectangle.cs
+++ src/Avalonia.Controls/Shapes/Rectangle.cs
@@ -10,6 +10,12 @@ namespace Avalonia.Controls.Shapes
         private const double PiOver2 = 1.57079633; // 90 deg to rad
 
         /// <summary>
+        /// Defines the <see cref="CornerRadius"/> property.
+        /// </summary>
+        public static readonly StyledProperty<CornerRadius> CornerRadiusProperty =
+            AvaloniaProperty.Register<Rectangle, CornerRadius>(nameof(CornerRadius));
+
+        /// <summary>
         /// Defines the <see cref="RadiusX"/> property.
         /// </summary>
         public static readonly StyledProperty<double> RadiusXProperty =
@@ -25,15 +31,36 @@ namespace Avalonia.Controls.Shapes
         {
             AffectsGeometry<Rectangle>(
                 BoundsProperty,
+                CornerRadiusProperty,
                 RadiusXProperty,
                 RadiusYProperty,
                 StrokeThicknessProperty);
         }
 
         /// <summary>
+        /// Gets or sets the radii used to round the corners of the rectangle.
+        /// Radii may be either circular or elliptical and can change for each corner.
+        /// </summary>
+        /// <remarks>
+        /// This property should be used in place of <see cref="RadiusX"/> and <see cref="RadiusY"/>
+        /// in new code. It will always take precedence over the other two which exist for backwards
+        /// compatibility.
+        /// </remarks>
+        public CornerRadius CornerRadius
+        {
+            get => GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        /// <summary>
         /// Gets or sets the radius on the X-axis used to round the corners of the rectangle.
         /// Corner radii are represented by an ellipse so this is 1/2 the X-axis width of the ellipse.
         /// </summary>
+        /// <remarks>
+        /// This property exists for compatibility with other XAML frameworks and will be overridden
+        /// by setting <see cref="CornerRadius"/>. Please use <see cref="CornerRadius"/> instead for
+        /// new code.
+        /// </remarks>
         public double RadiusX
         {
             get => GetValue(RadiusXProperty);
@@ -44,6 +71,11 @@ namespace Avalonia.Controls.Shapes
         /// Gets or sets the radius on the Y-axis used to round the corners of the rectangle.
         /// Corner radii are represented by an ellipse so this is 1/2 the Y-axis height of the ellipse.
         /// </summary>
+        /// <remarks>
+        /// This property exists for compatibility with other XAML frameworks and will be overridden
+        /// by setting <see cref="CornerRadius"/>. Please use <see cref="CornerRadius"/> instead for
+        /// new code.
+        /// </remarks>
         public double RadiusY
         {
             get => GetValue(RadiusYProperty);
@@ -56,10 +88,20 @@ namespace Avalonia.Controls.Shapes
             // TODO: If RectangleGeometry ever supports RadiusX/Y like in WPF,
             // this code can be removed/combined with that implementation
 
-            double x = RadiusX;
-            double y = RadiusY;
+            CornerRadius radii;
 
-            if (x == 0 && y == 0)
+            // Determine which corner radius properties to use
+            if (IsSet(CornerRadiusProperty) &&
+                CornerRadius.IsEmpty == false)
+            {
+                radii = CornerRadius;
+            }
+            else
+            {
+                radii = new CornerRadius(new EllipticalRadius(RadiusX, RadiusY));
+            }
+
+            if (radii.IsEmpty)
             {
                 // Optimization when there are no corner radii
                 var rect = new Rect(Bounds.Size).Deflate(StrokeThickness / 2);
@@ -69,7 +111,6 @@ namespace Avalonia.Controls.Shapes
             {
                 var rect = new Rect(Bounds.Size).Deflate(StrokeThickness / 2);
                 var geometry = new StreamGeometry();
-                var arcSize = new Size(x, y);
 
                 using (StreamGeometryContext context = geometry.Open())
                 {
@@ -90,39 +131,40 @@ namespace Avalonia.Controls.Shapes
                     // - Lines 2,4 follow the deflated rectangle bounds minus RadiusY
                     // - All corners are constructed using elliptical arcs 
 
+                    context.BeginFigure(new Point(rect.Left + radii.TopLeftComponents.RadiusX, rect.Top), true);
+
                     // Line 1 + Corner 1
-                    context.BeginFigure(new Point(rect.Left + x, rect.Top), true);
-                    context.LineTo(new Point(rect.Right - x, rect.Top));
+                    context.LineTo(new Point(rect.Right - radii.TopRightComponents.RadiusX, rect.Top));
                     context.ArcTo(
-                        new Point(rect.Right, rect.Top + y),
-                        arcSize,
+                        new Point(rect.Right, rect.Top + radii.TopRightComponents.RadiusY),
+                        size: radii.TopRightComponents,
                         rotationAngle: PiOver2,
                         isLargeArc: false,
                         SweepDirection.Clockwise);
 
                     // Line 2 + Corner 2
-                    context.LineTo(new Point(rect.Right, rect.Bottom - y));
+                    context.LineTo(new Point(rect.Right, rect.Bottom - radii.BottomRightComponents.RadiusY));
                     context.ArcTo(
-                        new Point(rect.Right - x, rect.Bottom),
-                        arcSize,
+                        new Point(rect.Right - radii.BottomRightComponents.RadiusX, rect.Bottom),
+                        size: radii.BottomRightComponents,
                         rotationAngle: PiOver2,
                         isLargeArc: false,
                         SweepDirection.Clockwise);
 
                     // Line 3 + Corner 3
-                    context.LineTo(new Point(rect.Left + x, rect.Bottom));
+                    context.LineTo(new Point(rect.Left + radii.BottomLeftComponents.RadiusX, rect.Bottom));
                     context.ArcTo(
-                        new Point(rect.Left, rect.Bottom - y),
-                        arcSize,
+                        new Point(rect.Left, rect.Bottom - radii.BottomLeftComponents.RadiusY),
+                        size: radii.BottomLeftComponents,
                         rotationAngle: PiOver2,
                         isLargeArc: false,
                         SweepDirection.Clockwise);
 
                     // Line 4 + Corner 4
-                    context.LineTo(new Point(rect.Left, rect.Top + y));
+                    context.LineTo(new Point(rect.Left, rect.Top + radii.TopLeftComponents.RadiusY));
                     context.ArcTo(
-                        new Point(rect.Left + x, rect.Top),
-                        arcSize,
+                        new Point(rect.Left + radii.TopLeftComponents.RadiusX, rect.Top),
+                        size: radii.TopLeftComponents,
                         rotationAngle: PiOver2,
                         isLargeArc: false,
                         SweepDirection.Clockwise);
