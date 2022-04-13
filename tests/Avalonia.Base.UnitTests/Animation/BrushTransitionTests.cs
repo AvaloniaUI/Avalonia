e130cb84d5cfac7856f565cdad64bab18f86b3ee
Steven Kirk <grokys@gmail.com>
2022-04-13 22:38:11 +0200

    Fix merge errors.

--- tests/Avalonia.Base.UnitTests/Animation/BrushTransitionTests.cs
+++ tests/Avalonia.Base.UnitTests/Animation/BrushTransitionTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Avalonia.Base.UnitTests.Animation;
 using Avalonia.Controls;
 using Avalonia.Media;
 using Xunit;
--- tests/Avalonia.Base.UnitTests/Media/TextFormatting/BiDiClassTestDataGenerator.cs
+++ tests/Avalonia.Base.UnitTests/Media/TextFormatting/BiDiClassTestDataGenerator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using Avalonia.Base.UnitTests.Media.TextFormatting;
 
 namespace Avalonia.Visuals.UnitTests.Media.TextFormatting
 {
