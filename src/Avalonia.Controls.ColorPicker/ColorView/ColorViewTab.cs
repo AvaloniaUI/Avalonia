0c7be285301cba8ea39a1282f4f132955ff2cda1
robloo <robloo@users.noreply.github.com>
2022-05-20 00:01:55 -0400

    Add ColorViewTab enum

---
+++ src/Avalonia.Controls.ColorPicker/ColorView/ColorViewTab.cs
@@ -0,0 +1,23 @@
+﻿namespace Avalonia.Controls
+{
+    /// <summary>
+    /// Defines a specific tab (subview) within the <see cref="ColorView"/>.
+    /// </summary>
+    public enum ColorViewTab
+    {
+        /// <summary>
+        /// The components view with sliders and numeric input boxes.
+        /// </summary>
+        Components,
+
+        /// <summary>
+        /// The color palette view with a grid of colors and shades.
+        /// </summary>
+        Palette,
+
+        /// <summary>
+        /// The color spectrum view with a box/ring spectrum and sliders.
+        /// </summary>
+        Spectrum,
+    }
+}
