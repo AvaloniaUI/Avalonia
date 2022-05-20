fa5a47b4426d32a00b01233540e25342f2013646
robloo <robloo@users.noreply.github.com>
2022-05-20 00:01:44 -0400

    Add initial ColorView.Properties

---
+++ src/Avalonia.Controls.ColorPicker/ColorView/ColorView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace Avalonia.Controls
+{
+    /// <summary>
+    /// Presents a color for user editing using a spectrum, palette and component sliders.
+    /// </summary>
+    public partial class ColorView : TemplatedControl
+    {
+        private ObservableCollection<Color> _customPaletteColors = new ObservableCollection<Color>();
+
+
+    }
+}
