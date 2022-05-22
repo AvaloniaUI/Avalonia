e461fc3ff60e79e6c7c9ec3801dd7a81e5670f67
robloo <robloo@users.noreply.github.com>
2022-05-22 12:09:28 -0400

    Add incomplete ColorView control template

--- src/Avalonia.Controls.ColorPicker/ColorView/ColorView.cs
+++ src/Avalonia.Controls.ColorPicker/ColorView/ColorView.cs
@@ -14,8 +14,94 @@ namespace Avalonia.Controls
     /// </summary>
     public partial class ColorView : TemplatedControl
     {
+        /// <summary>
+        /// Event for when the selected color changes within the slider.
+        /// </summary>
+        public event EventHandler<ColorChangedEventArgs>? ColorChanged;
+
+        private bool disableUpdates = false;
+
         private ObservableCollection<Color> _customPaletteColors = new ObservableCollection<Color>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorView"/> class.
+        /// </summary>
+        public ColorView() : base()
+        {
+        }
 
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            this.CustomPalette = new FluentColorPalette();
+
+            base.OnApplyTemplate(e);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+        {
+            if (disableUpdates)
+            {
+                base.OnPropertyChanged(change);
+                return;
+            }
+
+            // Always keep the two color properties in sync
+            if (change.Property == ColorProperty)
+            {
+                disableUpdates = true;
+
+                HsvColor = Color.ToHsv();
+
+                OnColorChanged(new ColorChangedEventArgs(
+                    change.GetOldValue<Color>(),
+                    change.GetNewValue<Color>()));
+
+                disableUpdates = false;
+            }
+            else if (change.Property == HsvColorProperty)
+            {
+                disableUpdates = true;
+
+                Color = HsvColor.ToRgb();
+
+                OnColorChanged(new ColorChangedEventArgs(
+                    change.GetOldValue<HsvColor>().ToRgb(),
+                    change.GetNewValue<HsvColor>().ToRgb()));
+
+                disableUpdates = false;
+            }
+            else if (change.Property == CustomPaletteProperty)
+            {
+                IColorPalette? palette = CustomPalette;
+
+                // Any custom palette change must be automatically synced with the
+                // bound properties controlling the palette grid
+                if (palette != null)
+                {
+                    CustomPaletteColumnCount = palette.ColorCount;
+                    CustomPaletteColors.Clear();
+
+                    for (int shadeIndex = 0; shadeIndex < palette.ShadeCount; shadeIndex++)
+                    {
+                        for (int colorIndex = 0; colorIndex < palette.ColorCount; colorIndex++)
+                        {
+                            CustomPaletteColors.Add(palette.GetColor(colorIndex, shadeIndex));
+                        }
+                    }
+                }
+            }
+
+            base.OnPropertyChanged(change);
+        }
+
+        /// <summary>
+        /// Called before the <see cref="ColorChanged"/> event occurs.
+        /// </summary>
+        /// <param name="e">The <see cref="ColorChangedEventArgs"/> defining old/new colors.</param>
+        protected virtual void OnColorChanged(ColorChangedEventArgs e)
+        {
+            ColorChanged?.Invoke(this, e);
+        }
     }
 }
--- src/Avalonia.Controls.ColorPicker/ColorView/ColorViewTab.cs
+++ src/Avalonia.Controls.ColorPicker/ColorView/ColorViewTab.cs
@@ -6,17 +6,17 @@
     public enum ColorViewTab
     {
         /// <summary>
-        /// The components view with sliders and numeric input boxes.
+        /// The components subview with sliders and numeric input boxes.
         /// </summary>
         Components,
 
         /// <summary>
-        /// The color palette view with a grid of colors and shades.
+        /// The color palette subview with a grid of selectable colors.
         /// </summary>
         Palette,
 
         /// <summary>
-        /// The color spectrum view with a box/ring spectrum and sliders.
+        /// The color spectrum subview with a box/ring spectrum and sliders.
         /// </summary>
         Spectrum,
     }
