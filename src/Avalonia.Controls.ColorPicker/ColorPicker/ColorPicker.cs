5cd0ea68053394bb85f7db96c65e7203448dd1cf
robloo <robloo@users.noreply.github.com>
2022-05-30 20:39:17 -0400

    Complete initial ColorPicker

--- src/Avalonia.Controls.ColorPicker/ColorPicker/ColorPicker.cs
+++ src/Avalonia.Controls.ColorPicker/ColorPicker/ColorPicker.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace Avalonia.Controls
 {
     /// <summary>
-    /// 
+    /// Presents a color for user editing using a spectrum, palette and component sliders within a drop down.
+    /// Editing is available when the drop down flyout is opened; otherwise, only the preview color is shown.
     /// </summary>
     public class ColorPicker : ColorView
     {
