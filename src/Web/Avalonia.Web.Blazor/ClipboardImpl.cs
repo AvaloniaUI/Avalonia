9cc69eec227ed01bb14c15c9e665b2fd02ba19eb
Oxc3 <61174136+Oxc3@users.noreply.github.com>
2022-06-01 14:27:38 -0700

    added ClipboardImpl with hooks to navigator.clipboard.readtext and naviagot.clipboard.writeText so Avalonia web can TextBox copy-paste works

---
+++ src/Web/Avalonia.Web.Blazor/ClipboardImpl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Input;
+using Avalonia.Input.Platform;
+using Microsoft.JSInterop;
+
+namespace Avalonia.Web.Blazor
+{
+    internal class ClipboardImpl : IClipboard
+    {
+        public async Task<string> GetTextAsync()
+        {
+            return await AvaloniaLocator.Current.GetRequiredService<IJSInProcessRuntime>().
+                InvokeAsync<string>("navigator.clipboard.readText");
+        }
+
+        public async Task SetTextAsync(string text)
+        {
+            await AvaloniaLocator.Current.GetRequiredService<IJSInProcessRuntime>().
+                InvokeAsync<string>("navigator.clipboard.writeText",text);
+        }
+
+        public Task ClearAsync() => Task.CompletedTask;
+
+        public Task SetDataObjectAsync(IDataObject data) => Task.CompletedTask;
+
+        public Task<string[]> GetFormatsAsync() => Task.FromResult(Array.Empty<string>());
+
+        public Task<object> GetDataAsync(string format) => Task.FromResult<object>(new());
+    }
+}
