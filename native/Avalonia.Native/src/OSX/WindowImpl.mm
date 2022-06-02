603f5433b81278a015082cad18a84b32b57c38f2
Dan Walmsley <dan@walms.co.uk>
2022-06-02 23:03:31 +0100

    osx: fix crash when modal dialog is opened over fullscreen parent.

--- native/Avalonia.Native/src/OSX/WindowImpl.mm
+++ native/Avalonia.Native/src/OSX/WindowImpl.mm
@@ -563,6 +563,11 @@ bool WindowImpl::IsDialog() {
 
 NSWindowStyleMask WindowImpl::GetStyle() {
     unsigned long s = NSWindowStyleMaskBorderless;
+    
+    if(_actualWindowState == FullScreen)
+    {
+        s |= NSWindowStyleMaskFullScreen;
+    }
 
     switch (_decorations) {
         case SystemDecorationsNone:
