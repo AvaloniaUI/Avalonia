6f9eb2fb79bded5d9ac506bd17cb4871f248c8ca
amwx <40413319+amwx@users.noreply.github.com>
2022-06-24 19:03:08 -0400

    Implement IFocusScope on EmbeddableControlRoot

--- src/Avalonia.Controls/Embedding/EmbeddableControlRoot.cs
+++ src/Avalonia.Controls/Embedding/EmbeddableControlRoot.cs
@@ -1,25 +1,46 @@
 ﻿using System;
 using System.ComponentModel;
 using Avalonia.Controls.Platform;
+using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Platform;
 using Avalonia.Styling;
+using Avalonia.VisualTree;
 using JetBrains.Annotations;
 
 namespace Avalonia.Controls.Embedding
 {
     public class EmbeddableControlRoot : TopLevel, IStyleable, IFocusScope, IDisposable
     {
+        private FocusManager _focusManager;
+        private OverlayLayer? _overlayLayer;
+
         public EmbeddableControlRoot(ITopLevelImpl impl) : base(impl)
         {
-            
+            _focusManager = new FocusManager(this);
         }
 
         public EmbeddableControlRoot() : base(PlatformManager.CreateEmbeddableWindow())
         {
+            _focusManager = new FocusManager(this);
         }
 
         protected bool EnforceClientSize { get; set; } = true;
+
+        FocusManager IFocusScope.FocusManager => _focusManager;
+
+        IOverlayHost? IFocusScope.OverlayHost
+        {
+            get
+            {
+                if (_overlayLayer == null)
+                {
+                    _overlayLayer = OverlayLayer.GetOverlayLayer(this);
+                }
+
+                return _overlayLayer;
+            }
+        }
 
         public void Prepare()
         {
