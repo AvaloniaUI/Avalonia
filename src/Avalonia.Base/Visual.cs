1251ede74af0a51d0e173003c4906740f8e00e1d
Nikita Tsukanov <keks9n@gmail.com>
2022-06-26 17:36:44 +0300

    Release CompositionVisual reference when Visual is detached

--- src/Avalonia.Base/Visual.cs
+++ src/Avalonia.Base/Visual.cs
@@ -495,6 +495,7 @@ namespace Avalonia
 
             DisableTransitions();
             OnDetachedFromVisualTree(e);
+            CompositionVisual = null;
             DetachedFromVisualTree?.Invoke(this, e);
             e.Root?.Renderer?.AddDirty(this);
 
