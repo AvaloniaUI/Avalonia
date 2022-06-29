41bcc1e22159c90995b3a69632d8559f6fe58f11
Max Katz <maxkatz6@outlook.com>
2022-06-28 22:13:13 -0400

    Apply target's TemplatedParent to a Flyout and Tooltip, when it's opened

--- src/Avalonia.Controls/Primitives/Popup.cs
+++ src/Avalonia.Controls/Primitives/Popup.cs
@@ -860,22 +860,7 @@ namespace Avalonia.Controls.Primitives
         {
             if (control != null)
             {
-                var templatedParent = TemplatedParent;
-
-                if (control.TemplatedParent == null)
-                {
-                    control.SetValue(TemplatedParentProperty, templatedParent);
-                }
-
-                control.ApplyTemplate();
-
-                if (!(control is IPresenter) && control.TemplatedParent == templatedParent)
-                {
-                    foreach (IControl child in control.VisualChildren)
-                    {
-                        SetTemplatedParentAndApplyChildTemplates(child);
-                    }
-                }
+                TemplatedControl.ApplyTemplatedParent(control, TemplatedParent);
             }
         }
 
