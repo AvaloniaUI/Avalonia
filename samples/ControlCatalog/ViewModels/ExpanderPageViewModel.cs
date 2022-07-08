4f3fafc9dfa6835ed1707715eabd0665dda9bb52
Steven Kirk <grokys@gmail.com>
2022-07-08 14:56:36 +0200

    Tweaked Expander ControlTheme.
    And added "Rounded" option to ControlCatalog for testing.

---
+++ samples/ControlCatalog/ViewModels/ExpanderPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using MiniMvvm;
+
+namespace ControlCatalog.ViewModels
+{
+    public class ExpanderPageViewModel : ViewModelBase
+    {
+        private CornerRadius _cornerRadius;
+        private bool _rounded;
+
+        public CornerRadius CornerRadius
+        {
+            get => _cornerRadius;
+            private set => RaiseAndSetIfChanged(ref _cornerRadius, value);
+        }
+
+        public bool Rounded
+        {
+            get => _rounded;
+            set
+            {
+                if (RaiseAndSetIfChanged(ref _rounded, value))
+                    CornerRadius = _rounded ? new CornerRadius(25) : default;
+            }
+        }
+    }
+}
