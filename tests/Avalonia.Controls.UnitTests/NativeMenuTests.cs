e711f75f053dfcdd3dc0ab8114c03056f88c1ccb
Takoooooo <tako0qq@gmail.com>
2022-07-15 17:11:43 +0300

    Test v1

--- tests/Avalonia.Controls.UnitTests/NativeMenuTests.cs
+++ tests/Avalonia.Controls.UnitTests/NativeMenuTests.cs
@@ -1,36 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Avalonia.Platform;
 using Avalonia.UnitTests;
+using Moq;
 using Xunit;
 
 namespace Avalonia.Controls.UnitTests
 {
     public class NativeMenuTests
     {
-        [Fact]
-        public void NativeMenuItems_Should_Get_Collected_By_GC_When_Not_In_Menu()
+        // Code below is in the separate method because .NET will hold last enumerated NativeMenuItem in a local variable which will prevent it from being GC-ed.
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        public static List<WeakReference<NativeMenuItem>> GetNativeMenuItemWeakReferences()
         {
-            using (UnitTestApplication.Start(TestServices.StyledWindow))
+            var weakReferences = new List<WeakReference<NativeMenuItem>>();
+            var windowImpl = new Mock<IWindowImpl>();
+            var windowingPlatform = new MockWindowingPlatform(() => windowImpl.Object);
+
+            using (UnitTestApplication.Start(new TestServices(windowingPlatform: windowingPlatform)))
             {
                 var window = new Window();
-                NativeMenu.SetMenu(window,new NativeMenu());
+                NativeMenu.SetMenu(window, new NativeMenu());
                 var nativeMenu = NativeMenu.GetMenu(window);
-                var topMenus = new string[] { "TopMenu1", "TopMenu2"};
-                var weakReferences = new List<WeakReference<NativeMenuItem>>();
+                var topMenus = new string[] { "TopMenu1", "TopMenu2" };
+
                 foreach (var topMenu in topMenus)
                 {
-                    var menuItem= new NativeMenuItem(topMenu);
+                    var menuItem = new NativeMenuItem(topMenu) { Menu = new NativeMenu() };
                     weakReferences.Add(new WeakReference<NativeMenuItem>(menuItem));
-                    var menu = new NativeMenu();
-                    menuItem.Menu = menu;
                     nativeMenu.Add(menuItem);
                 }
                 nativeMenu.Items.Clear();
-                GC.Collect();
+
             }
+            return weakReferences;
+        }
+
+        [Fact]
+        public void NativeMenuItems_Should_Get_Collected_By_GC_When_Not_In_Menu()
+        {
+            var weakReferences = GetNativeMenuItemWeakReferences();
+            GC.Collect();
+
+            Assert.False(weakReferences[0].TryGetTarget(out var _));
+            Assert.False(weakReferences[1].TryGetTarget(out var _));
+
         }
     }
 }
