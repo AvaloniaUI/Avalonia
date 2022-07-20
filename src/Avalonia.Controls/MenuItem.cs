961c693e343ee698227448da77823ec6d1dc2da2
Dan Walmsley <dan@walms.co.uk>
2022-07-20 11:37:11 +0100

    fix win32native to managed menu exporter.

--- src/Avalonia.Controls/MenuItem.cs
+++ src/Avalonia.Controls/MenuItem.cs
@@ -304,7 +304,7 @@ namespace Avalonia.Controls
         bool IMenuItem.IsPointerOverSubMenu => _popup?.IsPointerOverPopup ?? false;
 
         /// <inheritdoc/>
-        IMenuElement? IMenuItem.Parent => this.FindLogicalAncestorOfType<IMenuElement>();
+        IMenuElement? IMenuItem.Parent => Parent as IMenuElement;
 
         protected override bool IsEnabledCore => base.IsEnabledCore && _commandCanExecute;
 
