2a5760921761d819264d190a381a2314b09a0516
Evgeny Gorbovoy <e.gorbovoy@staffwerke.de>
2022-07-30 17:16:16 +0200

    + more `ContainsExclusive` for nodes

--- src/Avalonia.Base/Rendering/SceneGraph/GlyphRunNode.cs
+++ src/Avalonia.Base/Rendering/SceneGraph/GlyphRunNode.cs
@@ -73,6 +73,6 @@ namespace Avalonia.Rendering.SceneGraph
         }
 
         /// <inheritdoc/>
-        public override bool HitTest(Point p) => Bounds.Contains(p);
+        public override bool HitTest(Point p) => Bounds.ContainsExclusive(p);
     }
 }
