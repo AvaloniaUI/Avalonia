197f514ae7fbb94849ce048c28434de4f1f2aee0
Giuseppe Lippolis <workgroupengineering@outlook.com>
2022-08-02 10:47:11 +0200

    feat(X11): NullInputBackend

---
+++ src/Linux/Avalonia.LinuxFramebuffer/Input/NullInput/NullInputBackend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Avalonia.Input;
+using Avalonia.Input.Raw;
+
+namespace Avalonia.LinuxFramebuffer.Input.NullInput;
+
+internal class NullInputBackend : IInputBackend
+{
+
+    public void Initialize(IScreenInfoProvider screen, Action<RawInputEventArgs> onInput)
+    {
+    }
+
+    public void SetInputRoot(IInputRoot root)
+    {
+    }
+}
