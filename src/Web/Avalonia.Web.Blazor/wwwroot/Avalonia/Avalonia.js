5501010f2dab979366a3a24a22cfd514d399d360
Dan Walmsley <dan@walms.co.uk>
2022-09-07 15:17:04 +0100

    remove wwwroot

--- src/Web/Avalonia.Web.Blazor/wwwroot/Avalonia/Avalonia.js
+++
@@ -1,6 +0,0 @@
-export { DpiWatcher } from "./DpiWatcher";
-export { InputHelper } from "./InputHelper";
-export { NativeControlHost } from "./NativeControlHost";
-export { SizeWatcher } from "./SizeWatcher";
-export { SKHtmlCanvas } from "./SKHtmlCanvas";
-//# sourceMappingURL=Avalonia.js.map
