ff3ac1b0b4f9508a7a5348dfd6f5c29cbc12eff9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-19 20:50:59 +0200

    Move to Avalonia.Utilities

--- src/Avalonia.Base/Utilities/SpringSolver.cs
+++ src/Avalonia.Base/Utilities/SpringSolver.cs
@@ -24,9 +24,10 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 using System;
 
-namespace Avalonia.Animation;
+namespace Avalonia.Utilities;
 
 internal struct SpringSolver 
 {
