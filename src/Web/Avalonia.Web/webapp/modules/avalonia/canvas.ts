77f06aa5299ce17f9eeb6ece6b56a898bf280517
Dan Walmsley <dan@walms.co.uk>
2022-09-24 09:18:56 +0100

    remove test function.

--- src/Web/Avalonia.Web/webapp/modules/avalonia/canvas.ts
+++ src/Web/Avalonia.Web/webapp/modules/avalonia/canvas.ts
@@ -89,13 +89,6 @@ export class Canvas {
         }
     }
 
-
-    static Foo(canvas: HTMLCanvasElement) {
-        const ctx = canvas.getContext("2d")!;
-        ctx.fillStyle = "#FF0000";
-        ctx.fillRect(0, 0, 150, 75);
-    }
-
     static createWebGLContext(htmlCanvas: HTMLCanvasElement): WebGLRenderingContext | WebGL2RenderingContext {
         const contextAttributes = {
             alpha: 1,
