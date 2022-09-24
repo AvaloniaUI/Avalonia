722c05acc73e46604306c96c4efba5495b4c718d
Max Katz <maxkatz6@outlook.com>
2022-09-23 22:16:57 -0400

    Add initial stream interop

---
+++ src/Web/Avalonia.Web/Interop/StreamHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+using System.Threading.Tasks;
+
+namespace Avalonia.Web.Storage;
+
+/// <summary>
+/// Set of FileSystemWritableFileStream and Blob methods.
+/// </summary>
+internal static partial class StreamHelper
+{
+    [JSImport("StreamHelper.seek", "avalonia.ts")]
+    public static partial void Seek(JSObject stream, [JSMarshalAs<JSType.Number>] long position);
+
+    [JSImport("StreamHelper.truncate", "avalonia.ts")]
+    public static partial void Truncate(JSObject stream, [JSMarshalAs<JSType.Number>] long position);
+
+    [JSImport("StreamHelper.write", "avalonia.ts")]
+    public static partial void Write(JSObject stream, [JSMarshalAs<JSType.MemoryView>] Span<byte> data);
+
+    [JSImport("StreamHelper.write", "avalonia.ts")]
+    public static partial Task WriteAsync(JSObject stream, [JSMarshalAs<JSType.MemoryView>] ArraySegment<byte> data);
+
+    [JSImport("StreamHelper.close", "avalonia.ts")]
+    public static partial void Close(JSObject stream);
+
+    [JSImport("StreamHelper.close", "avalonia.ts")]
+    public static partial Task CloseAsync(JSObject stream);
+
+    [JSImport("StreamHelper.size", "avalonia.ts")]
+    [return: JSMarshalAs<JSType.Number>]
+    public static partial long Size(JSObject stream);
+
+    [JSImport("StreamHelper.byteLength", "avalonia.ts")]
+    [return: JSMarshalAs<JSType.Number>]
+    public static partial long ByteLength(JSObject stream);
+
+    [JSImport("StreamHelper.sliceToArray", "avalonia.ts")]
+    [return: JSMarshalAs<JSType.MemoryView>]
+    public static partial Span<byte> Slice(JSObject stream, [JSMarshalAs<JSType.Number>] long offset, int count);
+
+    public static async Task<ArraySegment<byte>> SliceAsync(JSObject stream, long offset, int count)
+    {
+        using var buffer = await SliceToBufferAsync(stream, offset, count);
+        return BufferToArray(buffer);
+    }
+
+    [JSImport("StreamHelper.slice", "avalonia.ts")]
+    [return: JSMarshalAs<JSType.Promise<JSType.Object>>]
+    private static partial Task<JSObject> SliceToBufferAsync(JSObject stream, [JSMarshalAs<JSType.Number>] long offset, int count);
+
+    [JSImport("StreamHelper.toArray", "avalonia.ts")]
+    [return: JSMarshalAs<JSType.MemoryView>]
+    private static partial ArraySegment<byte> BufferToArray(JSObject stream);
+}
---
+++ src/Web/Avalonia.Web/Storage/WriteableStream.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.JavaScript;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Avalonia.Web.Storage;
+
+// Loose wrapper implementaion of a stream on top of FileAPI FileSystemWritableFileStream
+internal sealed class JSWriteableStream : Stream
+{
+    private JSObject? _jSReference;
+
+    // Unfortunatelly we can't read current length/position, so we need to keep it C#-side only.
+    private long _length, _position;
+
+    internal JSWriteableStream(JSObject jSReference, long initialLength)
+    {
+        _jSReference = jSReference;
+        _length = initialLength;
+    }
+
+    private JSObject JSReference => _jSReference ?? throw new ObjectDisposedException(nameof(JSWriteableStream));
+
+    public override bool CanRead => false;
+
+    public override bool CanSeek => true;
+
+    public override bool CanWrite => true;
+
+    public override long Length => _length;
+
+    public override long Position
+    {
+        get => _position;
+        set => Seek(_position, SeekOrigin.Begin);
+    }
+
+    public override void Flush()
+    {
+        // no-op
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        var position = origin switch
+        {
+            SeekOrigin.Current => _position + offset,
+            SeekOrigin.End => _length + offset,
+            _ => offset
+        };
+        StreamHelper.Seek(JSReference, position);
+        return position;
+    }
+
+    public override void SetLength(long value)
+    {
+        _length = value;
+
+        // See https://docs.w3cub.com/dom/filesystemwritablefilestream/truncate
+        // If the offset is smaller than the size, it remains unchanged. If the offset is larger than size, the offset is set to that size
+        if (_position > _length)
+        {
+            _position = _length;
+        }
+
+        StreamHelper.Truncate(JSReference, value);
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        StreamHelper.Write(JSReference, buffer.AsSpan(offset, count));
+    }
+
+    public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    {
+        if (offset != 0 || count != buffer.Length)
+        {
+            // TODO, we need to pass prepared buffer to the JS
+            // Can't use ArrayPool as it can return bigger array than requested
+            // Can't use Span/Memory, as it's not supported by JS interop yet.
+            // Alternatively we can pass original buffer and offset+count, so it can be trimmed on the JS side (but is it more efficient tho?)
+            buffer = buffer.AsMemory(offset, count).ToArray();
+        }
+        return WriteAsyncInternal(buffer, cancellationToken);
+    }
+
+    public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+    {
+        return new ValueTask(WriteAsyncInternal(buffer.ToArray(), cancellationToken));
+    }
+
+    private Task WriteAsyncInternal(byte[] buffer, CancellationToken _)
+    {
+        _position += buffer.Length;
+
+        return StreamHelper.WriteAsync(JSReference, buffer);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (_jSReference is { } jsReference)
+        {
+            _jSReference = null;
+            try
+            {
+                StreamHelper.Close(JSReference);
+            }
+            finally
+            {
+                jsReference.Dispose();
+            }
+        }
+    }
+
+    public override async ValueTask DisposeAsync()
+    {
+        if (_jSReference is { } jsReference)
+        {
+            _jSReference = null;
+            try
+            {
+                await StreamHelper.CloseAsync(JSReference);
+            }
+            finally
+            {
+                jsReference.Dispose();
+            }
+        }
+    }
+}
