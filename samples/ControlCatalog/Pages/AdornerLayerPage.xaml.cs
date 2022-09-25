4b31690b12c55368181c1726d2975386936f29d0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-25 13:30:16 +0200

    Add add / remove adorner buttons for testing

--- samples/ControlCatalog/Pages/AdornerLayerPage.xaml.cs
+++ samples/ControlCatalog/Pages/AdornerLayerPage.xaml.cs
@@ -1,11 +1,14 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace ControlCatalog.Pages
 {
     public class AdornerLayerPage : UserControl
     {
+        private Control? _adorner;
+
         public AdornerLayerPage()
         {
             this.InitializeComponent();
@@ -14,6 +17,32 @@ namespace ControlCatalog.Pages
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void RemoveAdorner_OnClick(object? sender, RoutedEventArgs e)
+        {
+            var adornerButton = this.FindControl<Button>("AdornerButton");
+            if (adornerButton is { })
+            {
+                var adorner = AdornerLayer.GetAdorner(adornerButton);
+                if (adorner is { })
+                {
+                    _adorner = adorner;
+                }
+                AdornerLayer.SetAdorner(adornerButton, null);
+            }
+        }
+
+        private void AddAdorner_OnClick(object? sender, RoutedEventArgs e)
+        {
+            var adornerButton = this.FindControl<Button>("AdornerButton");
+            if (adornerButton is { })
+            {
+                if (_adorner is { })
+                {
+                    AdornerLayer.SetAdorner(adornerButton, _adorner);
+                }
+            }
         }
     }
 }
