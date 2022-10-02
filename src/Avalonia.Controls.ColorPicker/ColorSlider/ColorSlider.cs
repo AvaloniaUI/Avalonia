99624a288471786d4c645d5ee8b29b3a178643ea
robloo <robloo@users.noreply.github.com>
2022-10-02 12:02:45 -0400

    Revert "Track background bitmap size and invalidate accordingly in ColorSlider"
    This reverts commit 88e9a7a25b60c47ffa5d128a6a3654d082f6cbec.

--- src/Avalonia.Controls.ColorPicker/ColorSlider/ColorSlider.cs
+++ src/Avalonia.Controls.ColorPicker/ColorSlider/ColorSlider.cs
@@ -33,9 +33,6 @@ namespace Avalonia.Controls.Primitives
         protected bool ignorePropertyChanged = false;
 
         private WriteableBitmap? _backgroundBitmap;
-        private int _backgroundBitmapPixelCount;
-        private int _backgroundBitmapPixelWidth;
-        private int _backgroundBitmapPixelHeight;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ColorSlider"/> class.
@@ -136,21 +133,15 @@ namespace Avalonia.Controls.Primitives
 
                 if (bitmap != null)
                 {
-                    if (_backgroundBitmap != null &&
-                        _backgroundBitmapPixelCount == bitmap.Length &&
-                        _backgroundBitmapPixelWidth == pixelWidth &&
-                        _backgroundBitmapPixelHeight == pixelHeight)
+                    if (_backgroundBitmap != null)
                     {
                         // Re-use the existing WriteableBitmap
+                        // This assumes the height, width and byte counts are the same and must be set to null
+                        // elsewhere if that assumption is ever not true.
                         ColorPickerHelpers.UpdateBitmapFromPixelData(_backgroundBitmap, bitmap);
                     }
                     else
                     {
-                        _backgroundBitmap?.Dispose();
-                        _backgroundBitmapPixelCount = bitmap.Length;
-                        _backgroundBitmapPixelWidth = pixelWidth;
-                        _backgroundBitmapPixelHeight = pixelHeight;
-
                         _backgroundBitmap = ColorPickerHelpers.CreateBitmapFromPixelData(bitmap, pixelWidth, pixelHeight);
                     }
 
@@ -447,9 +438,6 @@ namespace Avalonia.Controls.Primitives
                 // This means the existing bitmap must be released to be recreated correctly in UpdateBackground().
                 _backgroundBitmap?.Dispose();
                 _backgroundBitmap = null;
-                _backgroundBitmapPixelCount = 0;
-                _backgroundBitmapPixelWidth = 0;
-                _backgroundBitmapPixelHeight = 0;
 
                 UpdateBackground();
             }
