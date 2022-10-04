4da0ae8dab4f844a9d7a4ddd6748c55aa6cee09e
Arhell <arhell333@gmail.com>
2022-10-05 01:10:26 +0300

    update test folder links

--- tests/Avalonia.Base.UnitTests/VisualTree/MockRenderInterface.cs
+++ tests/Avalonia.Base.UnitTests/VisualTree/MockRenderInterface.cs
@@ -235,7 +235,7 @@ namespace Avalonia.Base.UnitTests.VisualTree
 
                 public bool FillContains(Point point)
                 {
-                    // Use the algorithm from http://www.blackpawn.com/texts/pointinpoly/default.html
+                    // Use the algorithm from https://www.blackpawn.com/texts/pointinpoly/default.html
                     // to determine if the point is in the geometry (since it will always be convex in this situation)
                     for (int i = 0; i < points.Count; i++)
                     {
