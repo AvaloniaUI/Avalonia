1a994036bfe005e1106e5c4e76d0427244eb3ade
Dan Walmsley <dan@walms.co.uk>
2022-10-06 16:23:43 +0100

    fix avalonia.web package.

--- src/Web/Avalonia.Web.Sample/Program.cs
+++ src/Web/Avalonia.Web.Sample/Program.cs
@@ -7,15 +7,11 @@ internal partial class Program
 {
     private static void Main(string[] args)
     {
-        Emscripten.Log(EM_LOG.INFO, "Call from Main");
-
         BuildAvaloniaApp()
             .AfterSetup(_ =>
             {
                 ControlCatalog.Pages.EmbedSample.Implementation = new EmbedSampleWeb();
             }).SetupBrowserApp("out");
-
-        BuildAvaloniaApp().SetupBrowserApp("out");
     }
 
     public static AppBuilder BuildAvaloniaApp()
