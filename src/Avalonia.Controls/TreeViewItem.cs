65d39603fac9f963324158ab8d7c420565c5785b
dif-sam <41672086+dif-sam@users.noreply.github.com>
2022-10-13 16:55:55 +0400

    Forgotten namespase added to usings

--- src/Avalonia.Controls/TreeViewItem.cs
+++ src/Avalonia.Controls/TreeViewItem.cs
@@ -6,6 +6,7 @@ using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
 using Avalonia.Input;
 using Avalonia.LogicalTree;
+using Avalonia.Threading;
 
 namespace Avalonia.Controls
 {
