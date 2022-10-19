aa09803e58a04ffa85c082b9a2705c729ca100f7
Dan Walmsley <dan@walms.co.uk>
2022-10-19 15:10:00 +0100

    add a platform info page to control catalog that tells user where avalonia thinks its running,

---
+++ samples/ControlCatalog/Pages/PlatformInfoPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using ControlCatalog.ViewModels;
+
+namespace ControlCatalog.Pages
+{
+    public class PlatformInfoPage : UserControl
+    {
+        public PlatformInfoPage()
+        {
+            this.InitializeComponent();
+            DataContext = new PlatformInformationViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
