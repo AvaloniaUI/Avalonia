00f576a610c9ded7d2b5a988bf6c96aa0445f397
rabbitism <popmessiah@hotmail.com>
2022-11-09 01:58:28 +0800

    feat: fix styled property syntax error. add documentation.

--- src/Avalonia.Controls/HyperlinkButton.cs
+++ src/Avalonia.Controls/HyperlinkButton.cs
@@ -15,32 +15,50 @@ namespace Avalonia.Controls
         public const string pcVisited = ":visited";
 
         Type IStyleable.StyleKey => typeof(HyperlinkButton);
-
+        /// <summary>
+        /// Defines the <see cref="NavigateUri"/> property.
+        /// </summary>
         public static readonly DirectProperty<HyperlinkButton, Uri?> NavigateUriProperty =
             AvaloniaProperty.RegisterDirect<HyperlinkButton, Uri?>(nameof(NavigateUri), x => x.NavigateUri, (x, v) => x.NavigateUri = v);
 
         private Uri? _navigateUri;
+        /// <summary>
+        /// Gets or sets the Uri to navigate to when the button is clicked.
+        /// </summary>
         public Uri? NavigateUri
         {
             get => _navigateUri;
             set => SetAndRaise(NavigateUriProperty, ref _navigateUri, value);
         }
-
+        
+        /// <summary>
+        /// Defines the <see cref="IsVisited"/> property.
+        /// </summary>
         public static readonly StyledProperty<bool> IsVisitedProperty =
             AvaloniaProperty.Register<HyperlinkButton, bool>(nameof(IsVisited));
+        /// <summary>
+        /// Gets or sets if the <see cref="NavigateUri"/> is visited for this button.
+        /// </summary>
         public bool IsVisited
         {
             get => GetValue(IsVisitedProperty);
             set => SetValue(IsVisitedProperty, value);
         }
 
+        /// <summary>
+        /// Defines the <see cref="IsVisitedSetOnClick"/> property.
+        /// </summary>
         public static readonly StyledProperty<bool> IsVisitedSetOnClickProperty =
             AvaloniaProperty.Register<HyperlinkButton, bool>(nameof(IsVisitedSetOnClick), true);
-        private bool _isVisitedSetOnClick;
+        /// <summary>
+        /// <para>Gets or Sets whether <see cref="IsVisited"/> property should be determined when button is clicked. </para>
+        /// <para>If this property is set to true, then <see cref="IsVisited"/> is automatically set to true when button is clicked. </para>
+        /// <para>If this property is set to false, then <see cref="IsVisited"/> takes assigned value. </para>
+        /// </summary>
         public bool IsVisitedSetOnClick
         {
-            get => _isVisitedSetOnClick;
-            set => SetAndRaise(IsVisitedSetOnClickProperty, ref _isVisitedSetOnClick, value);
+            get => GetValue(IsVisitedSetOnClickProperty);
+            set => SetValue(IsVisitedSetOnClickProperty, value);
         }
 
         static HyperlinkButton()
