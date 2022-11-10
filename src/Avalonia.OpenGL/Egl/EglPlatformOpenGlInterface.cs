ae7a532d068633e4043351f625a7500afb959298
Emmanuel Hansen <emmausssss@gmail.com>
2022-11-10 14:19:23 +0000

    do not create a surface if the window provided is invalid

--- src/Avalonia.OpenGL/Egl/EglPlatformOpenGlInterface.cs
+++ src/Avalonia.OpenGL/Egl/EglPlatformOpenGlInterface.cs
@@ -50,6 +50,8 @@ namespace Avalonia.OpenGL.Egl
         {
             using (PrimaryContext.MakeCurrent())
             {
+                if(window == IntPtr.Zero)
+                    throw new OpenGlException($"Window {window} is invalid.");
                 var s = Display.EglInterface.CreateWindowSurface(Display.Handle, Display.Config, window,
                     new[] { EGL_NONE, EGL_NONE });
                 if (s == IntPtr.Zero)
