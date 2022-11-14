dc0c7e756aaf95bd5efd0d2a0e00648544751b96
Max Katz <maxkatz6@outlook.com>
2022-11-13 23:42:46 -0500

    Respect AvaloniaUseCompiledBindingsByDefault in previewer

--- src/Markup/Avalonia.Markup.Xaml.Loader/AvaloniaRuntimeXamlLoader.cs
+++ src/Markup/Avalonia.Markup.Xaml.Loader/AvaloniaRuntimeXamlLoader.cs
@@ -88,34 +88,4 @@ namespace Avalonia.Markup.Xaml
             => (T)Parse(xaml, localAssembly);
             
     }
-    
-    public class RuntimeXamlLoaderConfiguration
-    {
-        /// <summary>
-        /// The URI of the XAML being loaded.
-        /// </summary>
-        public Uri? BaseUri { get; set; }
-
-        /// <summary>
-        /// Default assembly for clr-namespace:.
-        /// </summary>
-        public Assembly LocalAssembly { get; set; }
-            
-        /// <summary>
-        /// The optional instance into which the XAML should be loaded.
-        /// </summary>
-        public object? RootInstance { get; set; }
-            
-        /// <summary>
-        /// Defines is CompiledBinding should be used by default.
-        /// Default is 'false'.
-        /// </summary>
-        public bool UseCompiledBindingsByDefault { get; set; } = false;
-
-        /// <summary>
-        /// Indicates whether the XAML is being loaded in design mode.
-        /// Default is 'false'.
-        /// </summary>
-        public bool DesignMode { get; set; } = false;
-    }
 }
