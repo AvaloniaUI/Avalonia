fdb32b7fde67a153134802e2fdb9dfe3ff09f019
Dan Walmsley <dan@avaloniaui.net>
2022-11-19 14:47:02 +0000

    Merge pull request #9469 from AvaloniaUI/fixes/window-border-for-opaque-windows
    [OSX] Use non-opaque window background for non-transparent windows
    # Conflicts:
    #	src/Avalonia.Native/avn.idl

--- native/Avalonia.Native/src/OSX/AvnWindow.mm
+++ native/Avalonia.Native/src/OSX/AvnWindow.mm
@@ -170,9 +170,7 @@
     _closed = false;
     _isEnabled = true;
 
-    [self backingScaleFactor];
     [self setOpaque:NO];
-    [self setBackgroundColor: [NSColor clearColor]];
 
     _isExtended = false;
     _isTransitioningToFullScreen = false;
