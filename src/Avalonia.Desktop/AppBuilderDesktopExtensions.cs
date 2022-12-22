ab64b3347005ad62c980e0b209d9721828288641
Max Katz <maxkatz6@outlook.com>
2022-12-22 16:33:45 -0500

    Remove IRuntimePlatform.OperatingSystem API and replace with trimmable OperatingSystem API

--- src/Avalonia.Desktop/AppBuilderDesktopExtensions.cs
+++ src/Avalonia.Desktop/AppBuilderDesktopExtensions.cs
@@ -1,5 +1,6 @@
+using Avalonia.Compatibility;
 using Avalonia.Controls;
-using Avalonia.Platform;
+using Avalonia.Logging;
 
 namespace Avalonia
 {
@@ -8,8 +9,6 @@ namespace Avalonia
         public static TAppBuilder UsePlatformDetect<TAppBuilder>(this TAppBuilder builder)
             where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
         {
-            var os = builder.RuntimePlatform.GetRuntimeInfo().OperatingSystem;
-
             // We don't have the ability to load every assembly right now, so we are
             // stuck with manual configuration  here
             // Helpers are extracted to separate methods to take the advantage of the fact
@@ -17,21 +16,27 @@ namespace Avalonia
             // Additionally, by having a hard reference to each assembly,
             // we verify that the assemblies are in the final .deps.json file
             //  so .NET Core knows where to load the assemblies from,.
-            if (os == OperatingSystemType.WinNT)
+            if (OperatingSystemEx.IsWindows())
             {
                 LoadWin32(builder);
                 LoadSkia(builder);
             }
-            else if(os==OperatingSystemType.OSX)
+            else if(OperatingSystemEx.IsMacOS())
             {
                 LoadAvaloniaNative(builder);
                 LoadSkia(builder);
             }
-            else
+            else if (OperatingSystemEx.IsLinux())
             {
                 LoadX11(builder);
                 LoadSkia(builder);
             }
+            else
+            {
+                Logger.TryGet(LogEventLevel.Warning, LogArea.Platform)?.Log(builder,
+                    "Avalonia.Desktop package was referenced on non-desktop platform or it isn't supported");
+            }
+
             return builder;
         }
 
