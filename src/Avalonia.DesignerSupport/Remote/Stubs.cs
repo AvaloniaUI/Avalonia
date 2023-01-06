36f93ae7bc71fd2f54cc36b78706546e0d9ca501
Max Katz <maxkatz6@outlook.com>
2023-01-06 02:14:16 -0500

    Update tests and designer

--- src/Avalonia.DesignerSupport/Remote/Stubs.cs
+++ src/Avalonia.DesignerSupport/Remote/Stubs.cs
@@ -182,7 +182,9 @@ namespace Avalonia.DesignerSupport.Remote
         public bool IsClientAreaExtendedToDecorations { get; }
 
         public bool NeedsManagedDecorations => false;
-        
+
+        public void SetFrameThemeVariant(PlatformThemeVariant themeVariant) { }
+
         public AcrylicPlatformCompensationLevels AcrylicCompensationLevels { get; } = new AcrylicPlatformCompensationLevels(1, 1, 1);
     }
 
