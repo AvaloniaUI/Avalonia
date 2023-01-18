151fe0031a31c34acbd50600bba03a916a5bf20b
Max Katz <maxkatz6@outlook.com>
2023-01-18 02:13:01 -0500

    Update control catalog and samples

---
+++ samples/ControlCatalog/Pages/ThemePage.axaml.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace ControlCatalog.Pages
+{
+    public class ThemePage : UserControl
+    {
+        public static ThemeVariant Pink { get; } = new("Pink", ThemeVariant.Light);
+        
+        public ThemePage()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+            var selector = this.FindControl<ComboBox>("Selector")!;
+            var themeVariantScope = this.FindControl<ThemeVariantScope>("ThemeVariantScope")!;
+
+            selector.Items = new[]
+            {
+                new ThemeVariant("Default"),
+                ThemeVariant.Dark,
+                ThemeVariant.Light,
+                Pink
+            };
+            selector.SelectedIndex = 0;
+
+            selector.SelectionChanged += (_, _) =>
+            {
+                if (selector.SelectedItem is ThemeVariant theme)
+                {
+                    themeVariantScope.RequestedThemeVariant = theme;
+                }
+            };
+        }
+    }
+}
