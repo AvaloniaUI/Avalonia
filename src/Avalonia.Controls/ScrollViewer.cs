78abd6d4b588a2913441114219969085c34ce01b
Emmanuel Hansen <emmausssss@gmail.com>
2023-01-24 20:03:02 +0000

    Add IsScrollInertiaEnabled attached property to scrollviewer

--- src/Avalonia.Controls/ScrollViewer.cs
+++ src/Avalonia.Controls/ScrollViewer.cs
@@ -194,6 +194,14 @@ namespace Avalonia.Controls
                 defaultValue: true);
 
         /// <summary>
+        /// Defines the <see cref="IsScrollInertiaEnabled"/> property.
+        /// </summary>
+        public static readonly AttachedProperty<bool> IsScrollInertiaEnabledProperty =
+            AvaloniaProperty.RegisterAttached<ScrollViewer, Control, bool>(
+                nameof(IsScrollInertiaEnabled),
+                defaultValue: true);
+
+        /// <summary>
         /// Defines the <see cref="ScrollChanged"/> event.
         /// </summary>
         public static readonly RoutedEvent<ScrollChangedEventArgs> ScrollChangedEvent =
@@ -442,6 +450,15 @@ namespace Avalonia.Controls
         {
             get => GetValue(IsScrollChainingEnabledProperty);
             set => SetValue(IsScrollChainingEnabledProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets whether scroll gestures should include inertia in their behavior and value.
+        /// </summary>
+        public bool IsScrollInertiaEnabled
+        {
+            get => GetValue(IsScrollInertiaEnabledProperty);
+            set => SetValue(IsScrollInertiaEnabledProperty, value);
         }
 
         /// <summary>
