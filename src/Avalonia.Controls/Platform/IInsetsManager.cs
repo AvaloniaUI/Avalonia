63b931253a92881198cc189f22124b2e698a89bb
Emmanuel Hansen <emmausssss@gmail.com>
2023-01-31 20:45:47 +0000

    add comment docs

--- src/Avalonia.Controls/Platform/IInsetsManager.cs
+++ src/Avalonia.Controls/Platform/IInsetsManager.cs
@@ -1,21 +1,40 @@
 ﻿using System;
+using Avalonia.Interactivity;
 
 namespace Avalonia.Controls.Platform
 {
     [Avalonia.Metadata.Unstable]
     public interface IInsetsManager
     {
+        /// <summary>
+        /// Gets or sets the theme for the system bars, if supported.
+        /// </summary>
         SystemBarTheme? SystemBarTheme { get; set; }
 
+        /// <summary>
+        /// Gets or sets whether the system bars are visible.
+        /// </summary>
         bool? IsSystemBarVisible { get; set; }
+
+        /// <summary>
+        /// Occurs when safe area for the current window changes.
+        /// </summary>
 
         event EventHandler<SafeAreaChangedArgs> SafeAreaChanged;
 
+
+        /// <summary>
+        /// Gets or sets whether the window draws edge to edge. behind any visibile system bars.
+        /// </summary>
         bool DisplayEdgeToEdge { get; set; }
 
+        /// <summary>
+        /// Gets the current safe area padding.
+        /// </summary>
+        /// <returns></returns>
         Thickness GetSafeAreaPadding();
 
-        public class SafeAreaChangedArgs : EventArgs
+        public class SafeAreaChangedArgs : RoutedEventArgs
         {
             public SafeAreaChangedArgs(Thickness safeArePadding)
             {
@@ -28,7 +47,14 @@ namespace Avalonia.Controls.Platform
 
     public enum SystemBarTheme
     {
+        /// <summary>
+        /// Light system bar theme, with light background and a dark foreground
+        /// </summary>
         Light,
+
+        /// <summary>
+        /// Bark system bar theme, with dark background and a light foreground
+        /// </summary>
         Dark
     }
 }
