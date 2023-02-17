0c6387a0fcc35d2134f4266e71d1622703c07b77
pr8x <luisvde@outlook.de>
2023-02-17 13:43:51 +0100

    Add support for geometry

--- src/Avalonia.Diagnostics/Diagnostics/Controls/PropertyValueEditorView.cs
+++ src/Avalonia.Diagnostics/Diagnostics/Controls/PropertyValueEditorView.cs
@@ -11,14 +11,16 @@ using Avalonia.Diagnostics.ViewModels;
 using Avalonia.Layout;
 using Avalonia.Media;
 using Avalonia.Reactive;
-using Image = Avalonia.Controls.Image;
 
 namespace Avalonia.Diagnostics.Controls
 {
     internal class PropertyValueEditorView : UserControl
     {
-        private static Geometry ImageGeometry = Geometry.Parse(
+        private static readonly Geometry ImageIcon = Geometry.Parse(
             "M12.25 6C8.79822 6 6 8.79822 6 12.25V35.75C6 37.1059 6.43174 38.3609 7.16525 39.3851L21.5252 25.0251C22.8921 23.6583 25.1081 23.6583 26.475 25.0251L40.8348 39.385C41.5683 38.3608 42 37.1058 42 35.75V12.25C42 8.79822 39.2018 6 35.75 6H12.25ZM34.5 17.5C34.5 19.7091 32.7091 21.5 30.5 21.5C28.2909 21.5 26.5 19.7091 26.5 17.5C26.5 15.2909 28.2909 13.5 30.5 13.5C32.7091 13.5 34.5 15.2909 34.5 17.5ZM39.0024 41.0881L24.7072 26.7929C24.3167 26.4024 23.6835 26.4024 23.293 26.7929L8.99769 41.0882C9.94516 41.6667 11.0587 42 12.25 42H35.75C36.9414 42 38.0549 41.6666 39.0024 41.0881Z");
+
+        private static readonly Geometry GeometryIcon = Geometry.Parse(
+            "M23.25 15.5H30.8529C29.8865 8.99258 24.2763 4 17.5 4C10.0442 4 4 10.0442 4 17.5C4 24.2763 8.99258 29.8865 15.5 30.8529V23.25C15.5 18.9698 18.9698 15.5 23.25 15.5ZM23.25 18C20.3505 18 18 20.3505 18 23.25V38.75C18 41.6495 20.3505 44 23.25 44H38.75C41.6495 44 44 41.6495 44 38.75V23.25C44 20.3505 41.6495 18 38.75 18H23.25Z");
 
         private readonly CompositeDisposable _cleanup = new();
         private PropertyViewModel? Property => (PropertyViewModel?)DataContext;
@@ -35,6 +37,12 @@ namespace Avalonia.Diagnostics.Controls
             base.OnDetachedFromVisualTree(e);
 
             _cleanup.Clear();
+        }
+
+        private static bool ImplementsInterface<TInterface>(Type type)
+        {
+            var interfaceType = typeof(TInterface);
+            return type == interfaceType || type.GetInterface(interfaceType.FullName!) != null;
         }
 
         private Control? UpdateControl()
@@ -63,8 +71,6 @@ namespace Avalonia.Diagnostics.Controls
                 return control;
             }
 
-            bool isObjectType = propertyType == typeof(object);
-
             if (propertyType == typeof(bool))
                 return CreateControl<CheckBox>(ToggleButton.IsCheckedProperty);
 
@@ -80,21 +86,27 @@ namespace Avalonia.Diagnostics.Controls
                         n.ParsingNumberStyle = NumberStyles.Integer;
                     });
 
-            if (propertyType == typeof(Color))
-                return CreateControl<ColorPicker>(ColorView.ColorProperty);
+            if (propertyType == typeof(Color)) return CreateControl<ColorPicker>(ColorView.ColorProperty);
 
-            if (!isObjectType && propertyType.IsAssignableFrom(typeof(IBrush)))
+            if (ImplementsInterface<IBrush>(propertyType))
                 return CreateControl<BrushEditor>(BrushEditor.BrushProperty);
 
-            if (!isObjectType && propertyType.IsAssignableFrom(typeof(IImage)))
+            var isImage = ImplementsInterface<IImage>(propertyType);
+            var isGeometry = propertyType == typeof(Geometry);
+
+            if (isImage || isGeometry)
             {
                 var valueObservable = Property.GetObservable(x => x.Value);
-
                 var tbl = new TextBlock { VerticalAlignment = VerticalAlignment.Center };
 
                 tbl.Bind(TextBlock.TextProperty,
                         valueObservable.Select(
-                            value => value is IImage image ? $"{image.Size.Width} x {image.Size.Height}" : "(null)"))
+                            value => value switch
+                            {
+                                IImage img => $"{img.Size.Width} x {img.Size.Height}",
+                                Geometry geom => $"{geom.Bounds.Width} x {geom.Bounds.Height}",
+                                _ => "(null)"
+                            }))
                     .DisposeWith(_cleanup);
 
                 var sp = new StackPanel
@@ -106,7 +118,7 @@ namespace Avalonia.Diagnostics.Controls
                     {
                         new Path
                         {
-                            Data = ImageGeometry,
+                            Data = isImage ? ImageIcon : GeometryIcon,
                             Fill = Brushes.Gray,
                             Width = 12,
                             Height = 12,
@@ -117,17 +129,37 @@ namespace Avalonia.Diagnostics.Controls
                     }
                 };
 
-                var previewImage = new Image
+                if (isImage)
                 {
-                    Width = 300,
-                    Height = 300
-                };
+                    var previewImage = new Image { Width = 300, Height = 300 };
 
-                previewImage
-                    .Bind(Image.SourceProperty, valueObservable)
-                    .DisposeWith(_cleanup);
+                    previewImage
+                        .Bind(Image.SourceProperty, valueObservable)
+                        .DisposeWith(_cleanup);
 
-                ToolTip.SetTip(sp, previewImage);
+                    ToolTip.SetTip(sp, previewImage);
+                }
+                else
+                {
+                    var previewShape = new Path
+                    {
+                        Stretch = Stretch.Uniform,
+                        Fill = Brushes.White,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center
+                    };
+
+                    previewShape
+                        .Bind(Path.DataProperty, valueObservable)
+                        .DisposeWith(_cleanup);
+
+                    ToolTip.SetTip(sp, new Border
+                    {
+                        Child = previewShape,
+                        Width = 300, 
+                        Height = 300
+                    });
+                }
 
                 return sp;
             }
@@ -147,7 +179,7 @@ namespace Avalonia.Diagnostics.Controls
                     t.Watermark = "(null)";
                 });
 
-            tb.IsEnabled &= !isObjectType &&
+            tb.IsEnabled &= propertyType != typeof(object) &&
                             StringConversionHelper.CanConvertFromString(propertyType);
 
             return tb;
@@ -157,17 +189,6 @@ namespace Avalonia.Diagnostics.Controls
         private class ValueConverter : IValueConverter
         {
             private bool _firstUpdate = true;
-
-            protected virtual object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-            {
-                return value;
-            }
-
-            protected virtual object? ConvertBack(object? value, Type targetType, object? parameter,
-                CultureInfo culture)
-            {
-                return value;
-            }
 
             object? IValueConverter.Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
             {
@@ -184,6 +205,17 @@ namespace Avalonia.Diagnostics.Controls
                 }
 
                 return ConvertBack(value, targetType, parameter, culture);
+            }
+
+            protected virtual object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            {
+                return value;
+            }
+
+            protected virtual object? ConvertBack(object? value, Type targetType, object? parameter,
+                CultureInfo culture)
+            {
+                return value;
             }
         }
 
