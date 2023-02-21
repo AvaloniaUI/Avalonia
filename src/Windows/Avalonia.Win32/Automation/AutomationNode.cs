dce799ba10c5fdb60d053927821e19e013146fe6
Daniil Pavliuchyk <tako0qq@gmail.com>
2023-02-21 17:10:50 +0200

    RadioButtonAutomationPeer WIP

--- src/Windows/Avalonia.Win32/Automation/AutomationNode.cs
+++ src/Windows/Avalonia.Win32/Automation/AutomationNode.cs
@@ -41,6 +41,8 @@ namespace Avalonia.Win32.Automation
             { SelectionPatternIdentifiers.CanSelectMultipleProperty, UiaPropertyId.SelectionCanSelectMultiple },
             { SelectionPatternIdentifiers.IsSelectionRequiredProperty, UiaPropertyId.SelectionIsSelectionRequired },
             { SelectionPatternIdentifiers.SelectionProperty, UiaPropertyId.SelectionSelection },
+            { SelectionItemPatternIdentifiers.IsSelectedProperty, UiaPropertyId.SelectionItemIsSelected },
+            { SelectionItemPatternIdentifiers.SelectionContainerProperty, UiaPropertyId.SelectionItemSelectionContainer }
         };
 
         private static ConditionalWeakTable<AutomationPeer, AutomationNode> s_nodes = new();
