7be869995e9aa8d4e2f631fed4abda039690f806
Steven Kirk <grokys@gmail.com>
2023-03-02 22:33:42 +0100

    WIP

--- tests/Avalonia.Controls.UnitTests/Primitives/SelectingItemsControlTests.cs
+++ tests/Avalonia.Controls.UnitTests/Primitives/SelectingItemsControlTests.cs
@@ -1908,9 +1908,10 @@ namespace Avalonia.Controls.UnitTests.Primitives
             {
                 [!ListBox.ItemsSourceProperty] = new Binding("Items"),
                 [!ListBox.SelectedItemsProperty] = new Binding("SelectedItems"),
-                DataContext = vm,
+                
             };
 
+            target.DataContext = vm;
             Prepare(target);
 
             Assert.Equal(1, target.SelectedIndex);
