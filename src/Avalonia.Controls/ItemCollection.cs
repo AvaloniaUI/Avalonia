b917cc5a2e6c4c76d2855d4f85894b6b4e1e3dd5
Steven Kirk <grokys@users.noreply.github.com>
2023-03-08 15:06:23 +0100

    Added some XML docs.

--- src/Avalonia.Controls/ItemCollection.cs
+++ src/Avalonia.Controls/ItemCollection.cs
@@ -6,6 +6,9 @@ using Avalonia.Collections;
 
 namespace Avalonia.Controls
 {
+    /// <summary>
+    /// Holds the list of items that constitute the content of an <see cref="ItemsControl"/>.
+    /// </summary>
     public class ItemCollection : ItemsSourceView, IList
     {
 // Suppress "Avoid zero-length array allocations": This is a sentinel value and must be unique.
@@ -30,11 +33,55 @@ namespace Avalonia.Controls
 
         internal event EventHandler? SourceChanged;
 
+        /// <summary>
+        /// Adds an item to the <see cref="ItemsControl"/>.
+        /// </summary>
+        /// <param name="value">The item to add to the collection.</param>
+        /// <returns>
+        /// The position into which the new element was inserted, or -1 to indicate that
+        /// the item was not inserted into the collection.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// The collection is in ItemsSource mode.
+        /// </exception>
         public int Add(object? value) => WritableSource.Add(value);
+
+        /// <summary>
+        /// Clears the collection and releases the references on all items currently in the
+        /// collection.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        /// The collection is in ItemsSource mode.
+        /// </exception>
         public void Clear() => WritableSource.Clear();
+
+        /// <summary>
+        /// Inserts an element into the collection at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index at which to insert the item.</param>
+        /// <param name="value">The item to insert.</param>
+        /// <exception cref="InvalidOperationException">
+        /// The collection is in ItemsSource mode.
+        /// </exception>
         public void Insert(int index, object? value) => WritableSource.Insert(index, value);
+
+        /// <summary>
+        /// Removes the item at the specified index of the collection or view.
+        /// </summary>
+        /// <param name="index">The zero-based index of the item to remove.</param>
+        /// <exception cref="InvalidOperationException">
+        /// The collection is in ItemsSource mode.
+        /// </exception>
         public void RemoveAt(int index) => WritableSource.RemoveAt(index);
 
+        /// <summary>
+        /// Removes the specified item reference from the collection or view.
+        /// </summary>
+        /// <param name="value">The object to remove.</param>
+        /// <returns>True if the item was removed; otherwise false.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// The collection is in ItemsSource mode.
+        /// </exception>
         public bool Remove(object? value)
         {
             var c = Count;
