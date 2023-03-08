16cb38f60d8296f4eb6f664a9b5eefe42a468f7e
Emmanuel Hansen <emmausssss@gmail.com>
2023-03-08 19:43:17 +0000

    add new IStorage api

--- src/Avalonia.Base/Platform/Storage/IStorageFolder.cs
+++ src/Avalonia.Base/Platform/Storage/IStorageFolder.cs
@@ -17,4 +17,18 @@ public interface IStorageFolder : IStorageItem
     /// When this method completes successfully, it returns a list of the files and folders in the current folder. Each item in the list is represented by an <see cref="IStorageItem"/> implementation object.
     /// </returns>
     IAsyncEnumerable<IStorageItem> GetItemsAsync();
+
+    /// <summary>
+    /// Creates a file with specified name as a child of the current storage folder
+    /// </summary>
+    /// <param name="name">The display name</param>
+    /// <returns>A new <see cref="IStorageFile"/> pointing to the moved file. If not null, the current storage item becomes invalid</returns>
+    Task<IStorageFile?> CreateFile(string name);
+
+    /// <summary>
+    /// Creates a folder with specified name as a child of the current storage folder
+    /// </summary>
+    /// <param name="name">The display name</param>
+    /// <returns>A new <see cref="IStorageFolder"/> pointing to the moved file. If not null, the current storage item becomes invalid</returns>
+    Task<IStorageFolder?> CreateFolder(string name);
 }
