791ea8beef1cd3aad574ff5d1f42299d57f17f34
Steven Kirk <grokys@users.noreply.github.com>
2023-03-09 16:13:40 +0100

    Added container lifecycle events.
    Seems we weren't calling `ClearItemContainer` for `ItemIsOwnContainer` items in `PanelContainerGenerator`. Fixed that too.

---
+++ src/Avalonia.Controls/ContainerClearingEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Avalonia.Controls
+{
+    /// <summary>
+    /// Provides data for the <see cref="ItemsControl.ContainerClearing"/> event.
+    /// </summary>
+    public class ContainerClearingEventArgs : EventArgs
+    {
+        public ContainerClearingEventArgs(Control container)
+        {
+            Container = container;
+        }
+
+        /// <summary>
+        /// Gets the prepared container.
+        /// </summary>
+        public Control Container { get; }
+    }
+}
