ed85e1b4492155b5ed739a7d85e6a94401370b1e
jankrib <jan.bjerke@gmail.com>
2023-04-02 14:58:01 +0200

    Make it accessible

---
+++ src/Avalonia.Controls/Platform/IOverlayWindowImpl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Avalonia.Platform
+{
+    public interface IOverlayWindowImpl
+    {
+        Action<string> FirstResponderChanged { get; set; }
+
+        Func<Point, bool> ShouldPassThrough { get; set; }
+    }
+}
