d203c1b3861d9d557c11d20d6f29d20d6b2a900e
Emmanuel Hansen <emmausssss@gmail.com>
2023-04-12 13:34:51 +0000

    add safe area demo

---
+++ samples/SafeAreaDemo/Views/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SafeAreaDemo.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
