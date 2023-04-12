b95feeb0aff5e68c066701bf7bea8b9d883f0786
Emmanuel Hansen <emmausssss@gmail.com>
2023-04-12 14:48:55 +0000

    fix nullable warnings

--- samples/SafeAreaDemo/ViewLocator.cs
+++ samples/SafeAreaDemo/ViewLocator.cs
@@ -7,7 +7,7 @@ namespace SafeAreaDemo
 {
     public class ViewLocator : IDataTemplate
     {
-        public Control Build(object data)
+        public Control? Build(object? data)
         {
             if (data is null)
                 return null;
