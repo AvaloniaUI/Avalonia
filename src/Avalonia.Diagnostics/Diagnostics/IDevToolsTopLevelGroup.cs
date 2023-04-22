2acb4b7589153f7e249ee2a45a2d6864e5c111fb
Max Katz <maxkatz6@outlook.com>
2023-04-21 21:55:15 -0400

    Override GetHashCode on types used in the dictionary

--- src/Avalonia.Diagnostics/Diagnostics/IDevToolsTopLevelGroup.cs
+++ src/Avalonia.Diagnostics/Diagnostics/IDevToolsTopLevelGroup.cs
@@ -12,20 +12,45 @@ internal interface IDevToolsTopLevelGroup
 
 internal class ClassicDesktopStyleApplicationLifetimeTopLevelGroup : IDevToolsTopLevelGroup
 {
+    private readonly IClassicDesktopStyleApplicationLifetime _lifetime;
+
     public ClassicDesktopStyleApplicationLifetimeTopLevelGroup(IClassicDesktopStyleApplicationLifetime lifetime)
     {
-        Items = lifetime?.Windows ?? throw new ArgumentNullException(nameof(lifetime));
+        _lifetime = lifetime ?? throw new ArgumentNullException(nameof(lifetime));
     }
-    
-    public IReadOnlyList<TopLevel> Items { get; }
+
+    public IReadOnlyList<TopLevel> Items => _lifetime.Windows;
+
+    public override int GetHashCode()
+    {
+        return _lifetime.GetHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is ClassicDesktopStyleApplicationLifetimeTopLevelGroup g && g._lifetime == _lifetime;
+    }
 }
 
 internal class SingleViewTopLevelGroup : IDevToolsTopLevelGroup
 {
+    private readonly TopLevel _topLevel;
+
     public SingleViewTopLevelGroup(TopLevel topLevel)
     {
+        _topLevel = topLevel;
         Items = new[] { topLevel ?? throw new ArgumentNullException(nameof(topLevel)) };
     }
     
     public IReadOnlyList<TopLevel> Items { get; }
+    
+    public override int GetHashCode()
+    {
+        return _topLevel.GetHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is SingleViewTopLevelGroup g && g._topLevel == _topLevel;
+    }
 }
