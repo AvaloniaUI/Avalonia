2da88e7cfcee9f7bbae8640d6fdd78c3271411de
Max Katz <maxkatz6@outlook.com>
2023-04-27 00:33:04 -0400

    Implement Avalonia.Headless.NUnit integration

---
+++ src/Headless/Avalonia.Headless.NUnit/AvaloniaTestCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Commands;
+
+namespace Avalonia.Headless.NUnit;
+
+internal class AvaloniaTestCommand : DelegatingTestCommand
+{
+    private readonly HeadlessUnitTestSession _session;
+
+    public AvaloniaTestCommand(HeadlessUnitTestSession session, TestCommand innerCommand)
+        : base(innerCommand)
+    {
+        _session = session;
+    }
+
+    public override TestResult Execute(TestExecutionContext context)
+    {
+        return _session.Dispatcher.InvokeAsync<Task<TestResult>>(async () =>
+        {
+            var testMethod = innerCommand.Test.Method;
+            var methodInfo = testMethod!.MethodInfo;
+
+            var result = methodInfo.Invoke(context.TestObject, innerCommand.Test.Arguments);
+            // Only Task, non generic ValueTask are supported in async context. No ValueTask<> nor F# tasks.
+            if (result is Task task)
+            {
+                await task;
+            }
+            else if (result is ValueTask valueTask)
+            {
+                await valueTask;
+            }
+
+            context.CurrentResult.SetResult(ResultState.Success);
+
+            if (context.CurrentResult.AssertionResults.Count > 0)
+                context.CurrentResult.RecordTestCompletion();
+
+            return context.CurrentResult;
+        }).GetTask().Unwrap().Result;
+    }
+}
---
+++ src/Headless/Avalonia.Headless.NUnit/ExecutionQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Avalonia.Threading;
+
+namespace Avalonia.Headless.NUnit;
+
+internal static class ExecutionQueue
+{
+    static bool _running;
+    static Queue<Func<Task>> _queue = new();
+    static async void TryExecuteNext()
+    {
+        if (_running || _queue.Count == 0) return;
+        try
+        {
+            _running = true;
+            await _queue.Dequeue()();
+        }
+        finally
+        {
+            _running = false;
+        }
+        TryExecuteNext();
+    }
+
+    static void ExecuteOnQueue(this Dispatcher dispatcher, Func<Task> cb)
+    {
+        dispatcher.Post(() =>
+        {
+            _queue.Enqueue(cb);
+            TryExecuteNext();
+        });
+    }
+}
