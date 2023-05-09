c490184870091a698cfb8f06851abd3baad60338
Steven Kirk <grokys@users.noreply.github.com>
2023-05-09 14:55:00 +0200

    Tried to clarify the behavior of page transitions.

--- src/Avalonia.Base/Animation/IPageTransition.cs
+++ src/Avalonia.Base/Animation/IPageTransition.cs
@@ -26,6 +26,13 @@ namespace Avalonia.Animation
         /// <returns>
         /// A <see cref="Task"/> that tracks the progress of the animation.
         /// </returns>
+        /// <remarks>
+        /// The <paramref name="from"/> and <paramref name="to"/> controls will be made visible
+        /// and <paramref name="from"/> transitioned to <paramref name="to"/>. At the end of the
+        /// animation (when the returned task completes), <paramref name="from"/> will be made
+        /// invisible but all other properties involved in the transition will have been left
+        /// unchanged.
+        /// </remarks>
         Task Start(Visual? from, Visual? to, bool forward, CancellationToken cancellationToken);
     }
 }
