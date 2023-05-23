7bf2b7668c583620466312ec4ae8f6d851c962df
Max Katz <maxkatz6@outlook.com>
2023-05-22 23:39:03 -0400

    Use Skia in windows interop test + remove old hacks for old wpf control

--- samples/interop/WindowsInteropTest/Program.cs
+++ samples/interop/WindowsInteropTest/Program.cs
@@ -16,12 +16,7 @@ namespace WindowsInteropTest
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
             AppBuilder.Configure<App>()
                 .UseWin32()
-                .UseDirect2D1()
-                .With(new Win32PlatformOptions
-                {
-                    UseWindowsUIComposition = false,
-                    ShouldRenderOnUIThread = true // necessary for WPF
-                })
+                .UseSkia()
                 .SetupWithoutStarting();
             System.Windows.Forms.Application.Run(new SelectorForm());
         }
