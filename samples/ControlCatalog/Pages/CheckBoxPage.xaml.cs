d24903957eb27ca89670c28e5f4d34b8e123e58f
Dan Walmsley <dan@walms.co.uk>
2023-05-23 20:40:25 +0100

    revert control catalog changes.

--- samples/ControlCatalog/Pages/CheckBoxPage.xaml.cs
+++ samples/ControlCatalog/Pages/CheckBoxPage.xaml.cs
@@ -1,32 +1,18 @@
 using Avalonia.Controls;
-using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace ControlCatalog.Pages
 {
-    public partial class CheckBoxPage : UserControl
+    public class CheckBoxPage : UserControl
     {
-        private TextBlock myTb;
-        private int count;
-
         public CheckBoxPage()
         {
             this.InitializeComponent();
-
-            myTb = this.FindControl<TextBlock>("myTb");
-
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        protected override void OnPointerMoved(PointerEventArgs e)
-        {
-            base.OnPointerMoved(e);
-
-            myTb.Text = e.GetPosition(this).ToString() + ", " + count++;
         }
     }
 }
