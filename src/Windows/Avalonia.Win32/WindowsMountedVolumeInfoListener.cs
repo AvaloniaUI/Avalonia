312e20c3bc9ad0cd32bc83d1c70cad9531be61bf
AlmightyJu <almightyju2@gmail.com>
2023-05-31 21:51:31 +0100

    Run windows files system polling in background thread

--- src/Windows/Avalonia.Win32/WindowsMountedVolumeInfoListener.cs
+++ src/Windows/Avalonia.Win32/WindowsMountedVolumeInfoListener.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Avalonia.Controls.Platform;
 using Avalonia.Logging;
 using Avalonia.Threading;
@@ -10,7 +12,7 @@ namespace Avalonia.Win32
 {
     internal class WindowsMountedVolumeInfoListener : IDisposable
     {
-        private readonly IDisposable _disposable;
+        private readonly CancellationTokenSource _pollCTS;
         private bool _beenDisposed;
         private readonly ObservableCollection<MountedVolumeInfo> _mountedDrives;
 
@@ -18,47 +20,57 @@ namespace Avalonia.Win32
         {
             _mountedDrives = mountedDrives;
 
-            _disposable = DispatcherTimer.Run(Poll, TimeSpan.FromSeconds(1));
+            _pollCTS = new();
 
-            Poll();
+            Task.Factory.StartNew(() => Poll(_pollCTS.Token), _pollCTS.Token, TaskCreationOptions.LongRunning, TaskScheduler.Current);
         }
 
-        private bool Poll()
+        private async Task Poll(CancellationToken cancellationToken)
         {
-            var allDrives = DriveInfo.GetDrives();
-
-            var mountVolInfos = allDrives
-                                .Where(p =>
-                                {
-                                    try
-                                    {
-                                        var ret = p.IsReady;
-                                        return ret;
-                                    }
-                                    catch (Exception e)
-                                    {
-                                        Logger.TryGet(LogEventLevel.Warning, LogArea.Control)?.Log(this, $"Error in Windows drive enumeration: {e.Message}");
-                                    }
-                                    return false;
-                                })
-                                .Select(p => new MountedVolumeInfo()
-                                {
-                                    VolumeLabel = string.IsNullOrEmpty(p.VolumeLabel.Trim()) ? p.RootDirectory.FullName
-                                                                                             : $"{p.VolumeLabel} ({p.Name})",
-                                    VolumePath = p.RootDirectory.FullName,
-                                    VolumeSizeBytes = (ulong)p.TotalSize
-                                })
-                                .ToArray();
-
-            if (_mountedDrives.SequenceEqual(mountVolInfos))
-                return true;
-            else
+            while(true)
             {
-                _mountedDrives.Clear();
+                if(cancellationToken.IsCancellationRequested)
+                    return;
 
-                foreach (var i in mountVolInfos)
-                    _mountedDrives.Add(i);
-                return true;
+                var allDrives = DriveInfo.GetDrives();
+
+                var mountVolInfos = allDrives
+                                    .Where(p =>
+                                    {
+                                        try
+                                        {
+                                            var ret = p.IsReady;
+                                            return ret;
+                                        }
+                                        catch (Exception e)
+                                        {
+                                            Logger.TryGet(LogEventLevel.Warning, LogArea.Control)?.Log(this, $"Error in Windows drive enumeration: {e.Message}");
+                                        }
+                                        return false;
+                                    })
+                                    .Select(p => new MountedVolumeInfo()
+                                    {
+                                        VolumeLabel = string.IsNullOrEmpty(p.VolumeLabel.Trim()) ? p.RootDirectory.FullName
+                                                                                                 : $"{p.VolumeLabel} ({p.Name})",
+                                        VolumePath = p.RootDirectory.FullName,
+                                        VolumeSizeBytes = (ulong)p.TotalSize
+                                    })
+                                    .ToArray();
+
+                await Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    if (_mountedDrives.SequenceEqual(mountVolInfos))
+                        return;
+                    else
+                    {
+                        _mountedDrives.Clear();
+
+                        foreach (var i in mountVolInfos)
+                            _mountedDrives.Add(i);
+                    }
+                });
+
+                await Task.Delay(1000, cancellationToken);
             }
         }
 
@@ -68,7 +80,8 @@ namespace Avalonia.Win32
             {
                 if (disposing)
                 {
-                    _disposable.Dispose();
+                    _pollCTS.Cancel();
+                    _pollCTS.Dispose();
                 }
                 _beenDisposed = true;
             }
