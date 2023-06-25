0042cf476dfb21456e18e30549ca2aaa5dbb1488
Tom Edwards <t.edwards@enscape3d.com>
2023-06-25 11:16:56 +0200

    Added BitmapResourceExtension and use it to swap between light/dark icons within Avalonia controls

---
+++ src/Markup/Avalonia.Markup.Xaml/MarkupExtensions/BitmapResourceExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace Avalonia.Markup.Xaml.MarkupExtensions;
+
+/// <summary>
+/// Reads a file or an asset and loads it as a <see cref="Bitmap"/>. Supports absolute file URIs plus all the URI schemes of <see cref="AssetLoader"/>.
+/// </summary>
+public class BitmapResourceExtension
+{
+    public BitmapResourceExtension() { }
+
+    /// <inheritdoc cref="BitmapResourceExtension"/>
+    public BitmapResourceExtension(Uri? source)
+    {
+        Source = source;
+    }
+
+    public Uri? Source { get; set; }
+
+    public Bitmap? ProvideValue(IServiceProvider serviceProvider) => Source switch
+    {
+        { IsAbsoluteUri: true, IsFile: true, LocalPath: { } localPath } => new Bitmap(localPath),
+        { } uri => new Bitmap(AssetLoader.Open(uri, serviceProvider.GetContextBaseUri())),
+        _ => null,
+    };
+}
