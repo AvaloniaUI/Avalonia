cb33529af4eeeb4cbbbb0fab71a0d7aab19bd0d0
Max Katz <maxkatz6@outlook.com>
2023-06-27 17:28:01 -0400

    Make IRuntimePlatform private API and remove some members that are always have single implementation

--- src/Avalonia.Base/Platform/IRuntimePlatform.cs
+++ src/Avalonia.Base/Platform/IRuntimePlatform.cs
@@ -3,24 +3,13 @@ using Avalonia.Metadata;
 
 namespace Avalonia.Platform
 {
-    [Unstable]
+    [PrivateApi]
     public interface IRuntimePlatform
     {
-        IDisposable StartSystemTimer(TimeSpan interval, Action tick);
         RuntimePlatformInfo GetRuntimeInfo();
-        IUnmanagedBlob AllocBlob(int size);
     }
 
-    [Unstable]
-    public interface IUnmanagedBlob : IDisposable
-    {
-        IntPtr Address { get; }
-        int Size { get; }
-        bool IsDisposed { get; }
-
-    }
-
-    [Unstable]
+    [PrivateApi]
     public record struct RuntimePlatformInfo
     {
         public FormFactorType FormFactor => IsDesktop ? FormFactorType.Desktop :
