eebcdb9b9c528f82f82585f44b372efe158e377b
Emmanuel Hansen <emmausssss@gmail.com>
2023-07-10 13:26:40 +0000

    add doc comments

--- src/Avalonia.Base/Styling/Queries.cs
+++ src/Avalonia.Base/Styling/Queries.cs
@@ -3,23 +3,55 @@ using System.Collections.Generic;
 
 namespace Avalonia.Styling
 {
+    /// <summary>
+    /// Extension methods for <see cref="Query"/>.
+    /// </summary>
     public static class Queries
     {
+        /// <summary>
+        /// Returns a query which matches the specified platform.
+        /// </summary>
+        /// <param name="previous">The previous query.</param>
+        /// <param name="argument">The platform string.</param>
+        /// <returns>The query.</returns>
         public static Query Platform(this Query? previous, string argument)
         {
             return new PlatformMediaQuery(previous, argument);
         }
 
+        /// <summary>
+        /// Returns a query which matches the specified orientation.
+        /// </summary>
+        /// <param name="previous">The previous query.</param>
+        /// <param name="argument">The orientation.</param>
+        /// <returns>The query.</returns>
         public static Query Orientation(this Query? previous, MediaOrientation argument)
         {
             return new OrientationMediaQuery(previous, argument);
         }
 
+
+        /// <summary>
+        /// Returns a query which matches the device width with a value.
+        /// </summary>
+        /// <param name="previous">The previous query.</param>
+        /// <param name="operator">The operator to match the device width</param>
+        /// <param name="value">The width to match</param>
+        /// <returns>The query.</returns>
         public static Query Width(this Query? previous, QueryComparisonOperator @operator, double value)
         {
             return new WidthMediaQuery(previous, @operator, value);
         }
 
+
+
+        /// <summary>
+        /// Returns a query which matches the device height with a value.
+        /// </summary>
+        /// <param name="previous">The previous query.</param>
+        /// <param name="operator">The operator to match the device height</param>
+        /// <param name="value">The height to match</param>
+        /// <returns>The query.</returns>
         public static Query Height(this Query? previous, QueryComparisonOperator @operator, double value)
         {
             return new HeightMediaQuery(previous, @operator, value);
