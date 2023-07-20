1c2ead55649d40829495d1764ad34c27931d1e9a
Chris Pulman <chris.pulman@yahoo.com>
2023-07-20 01:46:39 +0100

    Add DryIoc and Microsoft.Extensions.DependencyInjection

---
+++ src/Avalonia.ReactiveUI.Splat.DryLoc/AvaloniaMixins.cs
@@ -0,0 +1,43 @@
+﻿using DryIoc;
+using ReactiveUI;
+using Splat;
+using Splat.DryIoc;
+
+namespace Avalonia.ReactiveUI.Splat
+{
+    /// <summary>
+    /// Avalonia Mixins.
+    /// </summary>
+    public static class AvaloniaMixins
+    {
+        /// <summary>
+        /// Uses the splat with dry ioc.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="configure">The configure.</param>
+        /// <returns>An App Builder.</returns>
+        public static AppBuilder UseSplatWithDryIoc(this AppBuilder builder, Action<Container> configure)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.AfterPlatformServicesSetup(_ =>
+            {
+                if (Locator.CurrentMutable is null)
+                {
+                    return;
+                }
+
+                var container = new Container();
+                Locator.CurrentMutable.RegisterConstant(container, typeof(Container));
+                container.UseDryIocDependencyResolver();
+
+                RxApp.MainThreadScheduler = AvaloniaScheduler.Instance;
+
+                configure(container);
+            });
+        }
+    }
+}
---
+++ src/Avalonia.ReactiveUI.Splat.MicrosoftDependencyResolver/AvaloniaMixins.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ReactiveUI;
+using Splat;
+using Splat.Microsoft.Extensions.DependencyInjection;
+
+namespace Avalonia.ReactiveUI.Splat
+{
+    /// <summary>
+    /// Avalonia Mixins.
+    /// </summary>
+    public static class AvaloniaMixins
+    {
+        /// <summary>
+        /// Uses the splat with microsoft dependency resolver.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="configure">The configure.</param>
+        /// <param name="getServiceProvider">The get service provider.</param>
+        /// <returns>An App Builder.</returns>
+        public static AppBuilder UseSplatWithMicrosoftDependencyResolver(this AppBuilder builder, Action<IServiceCollection> configure, Action<IServiceProvider?>? getServiceProvider = null)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.AfterPlatformServicesSetup(_ =>
+            {
+                if (Locator.CurrentMutable is null)
+                {
+                    return;
+                }
+
+                IServiceCollection services = new ServiceCollection();
+                Locator.CurrentMutable.RegisterConstant(services, typeof(IServiceCollection));
+                services.UseMicrosoftDependencyResolver();
+
+                RxApp.MainThreadScheduler = AvaloniaScheduler.Instance;
+
+                configure(services);
+                if (getServiceProvider is null)
+                {
+                    return;
+                }
+
+                var serviceProvider = services.BuildServiceProvider();
+                serviceProvider.UseMicrosoftDependencyResolver();
+                getServiceProvider(serviceProvider);
+            });
+        }
+    }
+}
