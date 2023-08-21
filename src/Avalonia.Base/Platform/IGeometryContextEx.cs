4862a3eaafb60c63f3afcfe56216bb9e6bb0306e
Emmanuel Hansen <emmausssss@gmail.com>
2023-08-21 14:12:14 +0000

    move IGeometryContextEx

---
+++ src/Avalonia.Base/Platform/IGeometryContextEx.cs
@@ -0,0 +1,13 @@
+﻿namespace Avalonia.Platform
+{
+    public interface IGeometryContextEx : IGeometryContext
+    {
+        /// <summary>
+        /// Draws a line to the specified point.
+        /// </summary>
+        /// <param name="point">The destination point.</param>
+        /// <param name="isStroked">Whether the segment is stroked</param>
+        void LineTo(Point point, bool isStroked);
+    }
+
+}
