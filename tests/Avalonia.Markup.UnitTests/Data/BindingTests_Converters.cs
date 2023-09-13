afabe9e7c8b40f5a509b1c7b7a18dfd0ad1332b1
Steven Kirk <grokys@users.noreply.github.com>
2023-09-13 11:39:19 +0200

    Added ConverterCulture to bindings.
    Similar to WPF's `Binding.ConverterCulture` property.

--- tests/Avalonia.Markup.UnitTests/Data/BindingTests_Converters.cs
+++ tests/Avalonia.Markup.UnitTests/Data/BindingTests_Converters.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Globalization;
 using Avalonia.Controls;
 using Avalonia.Data;
 using Avalonia.Data.Converters;
 using Avalonia.Data.Core;
+using Moq;
 using Xunit;
 
 namespace Avalonia.Markup.UnitTests.Data
@@ -133,6 +135,60 @@ namespace Avalonia.Markup.UnitTests.Data
             textBlock.Bind(TextBlock.TextProperty, target);
 
             Assert.Equal("Hello True", textBlock.Text);
+        }
+
+        [Fact]
+        public void ConverterCulture_Should_Be_Passed_To_Converter_Convert()
+        {
+            var textBlock = new TextBlock
+            {
+                DataContext = new Class1(),
+            };
+
+            var culture = new CultureInfo("ar-SA");
+            var converter = new Mock<IValueConverter>();
+            var target = new Binding(nameof(Class1.Foo))
+            {
+                Converter = converter.Object,
+                ConverterCulture = culture,
+            };
+
+            textBlock.Bind(TextBlock.TextProperty, target);
+
+            converter.Verify(converter => converter.Convert(
+                "foo",
+                typeof(string),
+                null,
+                culture), 
+                Times.Once);
+        }
+
+        [Fact]
+        public void ConverterCulture_Should_Be_Passed_To_Converter_ConvertBack()
+        {
+            var textBlock = new TextBlock
+            {
+                DataContext = new Class1(),
+            };
+
+            var culture = new CultureInfo("ar-SA");
+            var converter = new Mock<IValueConverter>();
+            var target = new Binding(nameof(Class1.Foo))
+            {
+                Converter = converter.Object,
+                ConverterCulture = culture,
+                Mode = BindingMode.TwoWay,
+            };
+
+            textBlock.Bind(TextBlock.TextProperty, target);
+            textBlock.Text = "bar";
+
+            converter.Verify(converter => converter.ConvertBack(
+                "bar",
+                typeof(string),
+                null,
+                culture),
+                Times.Once);
         }
 
         private class Class1
