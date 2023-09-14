8da329e205524b2e7a1cd907c56d647ee484742c
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-09-14 14:35:26 +0200

    feat: Benckmark

---
+++ tests/Avalonia.Benchmarks/Text/TextPresenterAllocation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Avalonia.UnitTests;
+using BenchmarkDotNet.Attributes;
+
+namespace Avalonia.Benchmarks.Text;
+
+[MemoryDiagnoser]
+public class TextPresenterAllocation : IDisposable
+{
+    private readonly IDisposable _app;
+    private readonly Controls.Presenters.TextPresenter _presenter;
+
+    public TextPresenterAllocation()
+    {
+        _app = UnitTestApplication.Start(TestServices.StyledWindow);
+
+        var root = new TestRoot(true, null)
+        {
+            Renderer = new NullRenderer()
+        };
+
+        _presenter = new()
+        {
+            Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789&?%$@",
+        };
+
+        root.Child = _presenter;
+
+        root.LayoutManager.ExecuteInitialLayoutPass();
+    }
+
+    [Benchmark]
+    public async Task CarretBlink()
+    {
+        _presenter.ShowCaret();
+        await Task.Delay(10000);
+    }
+
+    public void Dispose()
+    {
+        _app.Dispose();
+    }
+}
