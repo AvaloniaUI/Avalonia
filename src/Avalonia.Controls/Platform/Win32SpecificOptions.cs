86d33a3416bee070c10be9c554dd5d1290b9deb0
Emmanuel Hansen <emmausssss@gmail.com>
2023-09-25 08:20:55 +0000

    replace Set callbacks with Add/Remove callbacks

--- src/Avalonia.Controls/Platform/Win32SpecificOptions.cs
+++ src/Avalonia.Controls/Platform/Win32SpecificOptions.cs
@@ -16,28 +16,54 @@ namespace Avalonia.Controls.Platform
         public delegate IntPtr CustomWndProcHookCallback(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam, ref bool handled);
 
         /// <summary>
-        /// Sets a callback to set the window's style.
+        /// Adds a callback to set the window's style.
         /// </summary>
         /// <param name="topLevel">The window implementation</param>
         /// <param name="callback">The callback</param>
-        public static void SetWindowStylesCallback(TopLevel topLevel, CustomWindowStylesCallback? callback)
+        public static void AddWindowStylesCallback(TopLevel topLevel, CustomWindowStylesCallback? callback)
         {
             if (topLevel.PlatformImpl is IWin32OptionsTopLevelImpl toplevelImpl)
             {
-                toplevelImpl.WindowStylesCallback = callback;
+                toplevelImpl.WindowStylesCallback += callback;
             }
         }
 
         /// <summary>
-        /// Sets a custom callback for the window's WndProc
+        /// Removes a callback to set the window's style.
         /// </summary>
-        /// <param name="topLevel">The window</param>
-        /// <param name="callback"></param>
-        public static void SetWndProcHookCallback(TopLevel topLevel, CustomWndProcHookCallback? callback)
+        /// <param name="topLevel">The window implementation</param>
+        /// <param name="callback">The callback</param>
+        public static void RemoveWindowStylesCallback(TopLevel topLevel, CustomWindowStylesCallback? callback)
         {
             if (topLevel.PlatformImpl is IWin32OptionsTopLevelImpl toplevelImpl)
             {
-                toplevelImpl.WndProcHookCallback = callback;
+                toplevelImpl.WindowStylesCallback -= callback;
+            }
+        }
+
+        /// <summary>
+        /// Adds a custom callback for the window's WndProc
+        /// </summary>
+        /// <param name="topLevel">The window</param>
+        /// <param name="callback"></param>
+        public static void AddWndProcHookCallback(TopLevel topLevel, CustomWndProcHookCallback? callback)
+        {
+            if (topLevel.PlatformImpl is IWin32OptionsTopLevelImpl toplevelImpl)
+            {
+                toplevelImpl.WndProcHookCallback += callback;
+            }
+        }
+
+        /// <summary>
+        /// Removes a custom callback for the window's WndProc
+        /// </summary>
+        /// <param name="topLevel">The window</param>
+        /// <param name="callback"></param>
+        public static void RemoveWndProcHookCallback(TopLevel topLevel, CustomWndProcHookCallback? callback)
+        {
+            if (topLevel.PlatformImpl is IWin32OptionsTopLevelImpl toplevelImpl)
+            {
+                toplevelImpl.WndProcHookCallback -= callback;
             }
         }
     }
