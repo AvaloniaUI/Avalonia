0e5cefef81f376e7531cbaf9e7485d42e0d9945d
rabbitism <dongbin@irihi.tech>
2023-09-30 17:09:06 +0800

    feat: add Clear function to Date and Time pickers.

--- src/Avalonia.Controls/CalendarDatePicker/CalendarDatePicker.cs
+++ src/Avalonia.Controls/CalendarDatePicker/CalendarDatePicker.cs
@@ -918,5 +918,13 @@ namespace Avalonia.Controls
         {
             return !string.IsNullOrWhiteSpace(formatString);
         }
+
+        /// <summary>
+        /// Clear SelectedDate
+        /// </summary>
+        public void Clear()
+        {
+            SetCurrentValue(SelectedDateProperty, null);
+        }
     }
 }
--- src/Avalonia.Controls/DateTimePickers/TimePicker.cs
+++ src/Avalonia.Controls/DateTimePickers/TimePicker.cs
@@ -283,5 +283,13 @@ namespace Avalonia.Controls
             _popup!.Close();
             SetCurrentValue(SelectedTimeProperty, _presenter!.Time);
         }
+
+        /// <summary>
+        /// Clear SelectedTime.
+        /// </summary>
+        public void Clear()
+        {
+            SetCurrentValue(SelectedTimeProperty, null);
+        }
     }
 }
