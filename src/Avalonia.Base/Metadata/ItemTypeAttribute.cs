5d33ec0243d9a60085783882dc9cd9b73237271a
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-10-01 19:23:32 +0200

    feat: Create ItemTypeAttribure

---
+++ src/Avalonia.Base/Metadata/ItemTypeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Avalonia.Metadata;
+
+/// <summary>
+/// Allows AvaloniaVS to identify the base element type of ItemsControl and derivatives
+/// </summary>
+[AttributeUsage(AttributeTargets.Class,
+    AllowMultiple = false,
+    Inherited = true)]
+public sealed class ItemTypeAttribute: DesignerAttribute
+{
+    readonly Type _baseType;
+    public ItemTypeAttribute(Type baseType)
+    {
+        _baseType = baseType;
+    }
+
+    public Type BaseType => _baseType;
+}
