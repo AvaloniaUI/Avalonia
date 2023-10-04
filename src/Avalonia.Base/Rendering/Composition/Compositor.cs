8d748f2288f0902214ec9a2fe2fb0215368a0119
Julien Lebosquain <julien@lebosquain.net>
2023-10-05 00:45:23 +0200

    Obsolete CubicBezierEasing (#13137)
    #Conflicts:
    #	src/Avalonia.Base/Animation/Easings/CubicBezierEasing.cs

--- src/Avalonia.Base/Rendering/Composition/Compositor.cs
+++ src/Avalonia.Base/Rendering/Composition/Compositor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Avalonia.Animation;
 using Avalonia.Animation.Easings;
 using Avalonia.Media;
 using Avalonia.Metadata;
@@ -85,7 +86,7 @@ namespace Avalonia.Rendering.Composition
             _server = new ServerCompositor(loop, gpu, _batchObjectPool, _batchMemoryPool);
             _triggerCommitRequested = () => scheduler.CommitRequested(this);
 
-            DefaultEasing = new CubicBezierEasing(new Point(0.25f, 0.1f), new Point(0.25f, 1f));
+            DefaultEasing = new SplineEasing(new KeySpline(0.25, 0.1, 0.25, 1.0));
         }
 
         /// <summary>
