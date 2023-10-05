25834d787a8c1d3a880962f4e812730cf99355bb
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-10-05 17:07:34 +0200

    test: Check ProgressBar Classes does not contain :indeterminate when IsVisible is false

---
+++ tests/Avalonia.Controls.UnitTests/ProgressBarTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Avalonia.Controls.UnitTests;
+
+public class ProgressBarTests
+{
+    [Fact]
+    public void Indeterminate_Animation_Is_Not_Running_When_IsVisible_false()
+    {
+        var progressBar = new ProgressBar()
+        {
+            IsIndeterminate = true,
+        };
+
+        Assert.Contains(":indeterminate", progressBar.Classes);
+
+        progressBar.IsVisible = false;
+
+        Assert.DoesNotContain(":indeterminate", progressBar.Classes);
+    }
+}
