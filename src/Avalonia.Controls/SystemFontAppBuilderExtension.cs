36ec244b2c374648448f0928278af84cd3480d81
Benedikt Stebner <Gillibald@users.noreply.github.com>
2023-10-10 10:32:18 +0200

    Introduce AppBuilder extension to customize system fonts

---
+++ src/Avalonia.Controls/SystemFontAppBuilderExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Avalonia.Media;
+using Avalonia.Media.Fonts;
+
+namespace Avalonia
+{
+    public static class SystemFontAppBuilderExtension
+    {
+        public static AppBuilder WithCustomSystemFonts(this AppBuilder appBuilder, IReadOnlyList<FontFamily> customFontFamilies)
+        {
+            return appBuilder.ConfigureFonts(fontManager =>
+            {
+                if(fontManager.SystemFonts is SystemFontCollection systemFontCollection)
+                {
+                    systemFontCollection.AddCustomFontFamilies(customFontFamilies);
+                }
+            });
+        }
+    }
+}
