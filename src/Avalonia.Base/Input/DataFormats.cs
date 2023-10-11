f597a7a3a378c3fe3bd4af2f84da9426240c3579
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2023-10-11 11:32:43 +0800

    Update src/Avalonia.Base/Input/DataFormats.cs

--- src/Avalonia.Base/Input/DataFormats.cs
+++ src/Avalonia.Base/Input/DataFormats.cs
@@ -22,7 +22,7 @@ namespace Avalonia.Input
         /// <remarks>
         /// This data format is supported only on desktop platforms.
         /// </remarks>
-        [Unstable("Use DataFormats.Files, this format is supported only on desktop platforms. And it will be removed in 12.0"), EditorBrowsable(EditorBrowsableState.Never)]
+        [Unstable("Use DataFormats.Files, this format is supported only on desktop platforms. And it will be removed in 12.0."), EditorBrowsable(EditorBrowsableState.Never)]
         public static readonly string FileNames = nameof(FileNames);
     }
 }
