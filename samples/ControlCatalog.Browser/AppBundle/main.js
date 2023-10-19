ebab7af2db2d5ba8b6274a78b851332ccd90e096
Max Katz <maxkatz6@outlook.com>
2023-10-19 02:49:28 -0700

    Minor fixes

--- samples/ControlCatalog.Browser/AppBundle/main.js
+++ samples/ControlCatalog.Browser/AppBundle/main.js
@@ -1,8 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// import { dotnet } from './dotnet.js' // NET 7
-import { dotnet } from './_framework/dotnet.js' // NET 8+
+import { dotnet } from './dotnet.js' // NET 7
+//import { dotnet } from './_framework/dotnet.js' // NET 8+
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
