2aba0b12efee0fa186aa42e9d9bf2e71316488dc
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-10-23 19:02:25 +0200

    feat(DevTools): Pin properties

--- src/Avalonia.Diagnostics/Diagnostics/ViewModels/PropertyViewModel.cs
+++ src/Avalonia.Diagnostics/Diagnostics/ViewModels/PropertyViewModel.cs
@@ -1,27 +1,29 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Globalization;
-using System.Reflection;
 
-namespace Avalonia.Diagnostics.ViewModels
+namespace Avalonia.Diagnostics.ViewModels;
+
+internal abstract class PropertyViewModel : ViewModelBase
 {
-    internal abstract class PropertyViewModel : ViewModelBase
-    {
-        public abstract object Key { get; }
-        public abstract string Name { get; }
-        public abstract string Group { get; }
-        public abstract Type AssignedType { get; }
-        public abstract Type? DeclaringType { get; }
-        public abstract object? Value { get; set; }
-        public abstract string Priority { get; }
-        public abstract bool? IsAttached { get; }
-        public abstract void Update();
-        public abstract Type PropertyType { get; }
+    private bool _isPinned;
 
-        public string Type => PropertyType == AssignedType ?
-            PropertyType.GetTypeName() :
-            $"{PropertyType.GetTypeName()} {{{AssignedType.GetTypeName()}}}";
+    public abstract object Key { get; }
+    public abstract string Name { get; }
+    public abstract string Group { get; }
+    public abstract Type AssignedType { get; }
+    public abstract Type? DeclaringType { get; }
+    public abstract object? Value { get; set; }
+    public abstract string Priority { get; }
+    public abstract bool? IsAttached { get; }
+    public abstract void Update();
+    public abstract Type PropertyType { get; }
 
-        public abstract bool IsReadonly { get; }
-    }
+    public string Type => PropertyType == AssignedType ?
+        PropertyType.GetTypeName() :
+        $"{PropertyType.GetTypeName()} {{{AssignedType.GetTypeName()}}}";
+
+    public abstract bool IsReadonly { get; }
+
+    public bool IsPinned { get => _isPinned; set => RaiseAndSetIfChanged(ref _isPinned, value); }
+
+    public string FullName => $"{GetType().Name.Replace("PropertyViewModel","")}:{DeclaringType?.FullName}.{Name}";
 }
