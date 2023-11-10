ffaeabc119478e236de54179b5f884f7cf46dcbb
Tim <47110241+timunie@users.noreply.github.com>
2023-11-10 12:02:28 +0100

    Add `PlacementConstraintAdjustmentProperty` to PopupFlyoutBase
    This adds much more flexibility to control flyout positioning when there is not enough space to show the Flyout.

--- src/Avalonia.Controls/Flyouts/PopupFlyoutBase.cs
+++ src/Avalonia.Controls/Flyouts/PopupFlyoutBase.cs
@@ -47,6 +47,13 @@ namespace Avalonia.Controls.Primitives
         public static readonly StyledProperty<IInputElement?> OverlayInputPassThroughElementProperty =
             Popup.OverlayInputPassThroughElementProperty.AddOwner<PopupFlyoutBase>();
         
+        /// <summary>
+        /// Defines the <see cref="OverlayInputPassThroughElement"/> property
+        /// </summary>
+        public static readonly StyledProperty<PopupPositionerConstraintAdjustment> PlacementConstraintAdjustmentProperty =
+            Popup.PlacementConstraintAdjustmentProperty.AddOwner<PopupFlyoutBase>();
+        
+        
         private readonly Lazy<Popup> _popupLazy;
         private Rect? _enlargedPopupRect;
         private PixelRect? _enlargePopupRectScreenPixelRect;
@@ -119,6 +126,16 @@ namespace Avalonia.Controls.Primitives
             set => SetValue(OverlayInputPassThroughElementProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets a value describing how the popup position will be adjusted if the
+        /// unadjusted position would result in the popup being partly constrained.
+        /// </summary>
+        public PopupPositionerConstraintAdjustment PlacementConstraintAdjustment
+        {
+            get => GetValue(PlacementConstraintAdjustmentProperty);
+            set => SetValue(PlacementConstraintAdjustmentProperty, value);
+        }
+        
         IPopupHost? IPopupHostProvider.PopupHost => Popup?.Host;
 
         event Action<IPopupHost?>? IPopupHostProvider.PopupHostChanged
@@ -424,9 +441,7 @@ namespace Avalonia.Controls.Primitives
             else
             {
                 Popup.Placement = Placement;
-                Popup.PlacementConstraintAdjustment =
-                    PopupPositioning.PopupPositionerConstraintAdjustment.SlideX |
-                    PopupPositioning.PopupPositionerConstraintAdjustment.SlideY;
+                Popup.PlacementConstraintAdjustment = PlacementConstraintAdjustment;
             }
         }
 
