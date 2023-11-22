6f85a41f7fe08488ff597e9364eb424846301ce6
Max Katz <maxkatz6@outlook.com>
2023-11-21 18:58:12 -0800

    Implement ManagedFileChooserOverwritePrompt with new styles, instead of hardcoding prompt creation

---
+++ src/Avalonia.Dialogs/ManagedFileChooserOverwritePrompt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Avalonia.Dialogs;
+
+public class ManagedFileChooserOverwritePrompt : TemplatedControl
+{
+    internal event Action<bool>? Result;
+
+    private string _fileName = "";
+
+    public static readonly DirectProperty<ManagedFileChooserOverwritePrompt, string> FileNameProperty = AvaloniaProperty.RegisterDirect<ManagedFileChooserOverwritePrompt, string>(
+        "FileName", o => o.FileName, (o, v) => o.FileName = v);
+
+    public string FileName
+    {
+        get => _fileName;
+        set => SetAndRaise(FileNameProperty, ref _fileName, value);
+    }
+
+    public void Confirm()
+    {
+        Result?.Invoke(true);
+    }
+
+    public void Cancel()
+    {
+        Result?.Invoke(false);
+    }
+}
