b06a037fac6a71654c119e0968776fa9144f1b69
Max Katz <maxkatz6@outlook.com>
2023-11-23 01:23:22 -0800

    Fix iOS event

--- src/iOS/Avalonia.iOS/UIKitInputPane.cs
+++ src/iOS/Avalonia.iOS/UIKitInputPane.cs
@@ -16,7 +16,7 @@ internal sealed class UIKitInputPane : IInputPane
     {
         NSNotificationCenter
             .DefaultCenter
-            .AddObserver(UIKeyboard.DidShowNotification, KeyboardUpNotification);
+            .AddObserver(UIKeyboard.WillShowNotification, KeyboardUpNotification);
         NSNotificationCenter
             .DefaultCenter
             .AddObserver(UIKeyboard.WillHideNotification, KeyboardDownNotification);
@@ -44,10 +44,11 @@ internal sealed class UIKitInputPane : IInputPane
             : curve.HasFlag(UIViewAnimationOptions.CurveEaseOut) ? new QuadraticEaseOut()
             : curve.HasFlag(UIViewAnimationOptions.CurveEaseInOut) ? new QuadraticEaseInOut()
             : null;
-        
+
         var startRect = new Rect(startFrame.X, startFrame.Y, startFrame.Width, startFrame.Height);
         OccludedRect = new Rect(endFrame.X, endFrame.Y, endFrame.Width, endFrame.Height);
 
+        Debug.WriteLine($"iOS {State} {startFrame} {endFrame} {duration} {curve}");
         StateChanged?.Invoke(this, new InputPaneStateEventArgs(
             State, startRect, OccludedRect, TimeSpan.FromSeconds(duration), easing));
     }
