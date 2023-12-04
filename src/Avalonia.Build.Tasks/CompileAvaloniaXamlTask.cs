223cfaba9ae49a7252c1a73c8970bd9d61bd8915
Tom Edwards <t.edwards@enscape3d.com>
2023-12-04 21:06:40 +0100

    Avoid constantly recompiling Avalonia Xaml files by implementing incremental build checks - Should be a (or even the) fix for file permission errors during builds CompileAvaloniaXaml no longer overwrites the Compile output, but creates its own output files - This supports incremental build checks and is safer in general Removed unused executable features from Avalonia.Build.Tasks - This is instead of refactoring for the new ITaskItem properties Updated Desktop SLNF

--- src/Avalonia.Build.Tasks/CompileAvaloniaXamlTask.cs
+++ src/Avalonia.Build.Tasks/CompileAvaloniaXamlTask.cs
@@ -8,103 +8,57 @@ namespace Avalonia.Build.Tasks
 {
     public class CompileAvaloniaXamlTask: ITask
     {
+        public const string AvaloniaCompileOutputMetadataName = "AvaloniaCompileOutput";
+
         public bool Execute()
         {
             Enum.TryParse(ReportImportance, true, out MessageImportance outputImportance);
-            var writtenFilePaths = new List<string>();
+            
+            var outputPath = AssemblyFile.GetMetadata(AvaloniaCompileOutputMetadataName);
+            var refOutputPath = RefAssemblyFile?.GetMetadata(AvaloniaCompileOutputMetadataName);
 
-            OutputPath ??= AssemblyFile;
-            RefOutputPath ??= RefAssemblyFile;
-            var outputPdb = GetPdbPath(OutputPath);
-            var input = AssemblyFile;
-            var refInput = RefOutputPath;
-            var inputPdb = GetPdbPath(input);
-            // Make a copy and delete the original file to prevent MSBuild from thinking that everything is OK
-            if (OriginalCopyPath != null)
+            Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+            if (!string.IsNullOrEmpty(refOutputPath))
             {
-                var originalCopyPathRef = Path.ChangeExtension(OriginalCopyPath, ".ref.dll");
-
-                File.Copy(AssemblyFile, OriginalCopyPath, true);
-                writtenFilePaths.Add(OriginalCopyPath);
-                input = OriginalCopyPath;
-                File.Delete(AssemblyFile);
-
-                if (File.Exists(inputPdb))
-                {
-                    var copyPdb = GetPdbPath(OriginalCopyPath);
-                    File.Copy(inputPdb, copyPdb, true);
-                    writtenFilePaths.Add(copyPdb);
-                    File.Delete(inputPdb);
-                    inputPdb = copyPdb;
-                }
-                
-                if (!string.IsNullOrWhiteSpace(RefAssemblyFile) && File.Exists(RefAssemblyFile))
-                {
-                    // We also copy ref assembly just for case if needed later for testing.
-                    // But do not remove the original one, as MSBuild actually complains about it with multi-thread compiling.
-                    File.Copy(RefAssemblyFile, originalCopyPathRef, true);
-                    writtenFilePaths.Add(originalCopyPathRef);
-                    refInput = originalCopyPathRef;
-                }
+                Directory.CreateDirectory(Path.GetDirectoryName(refOutputPath));
             }
 
-            var msg = $"CompileAvaloniaXamlTask -> AssemblyFile:{AssemblyFile}, ProjectDirectory:{ProjectDirectory}, OutputPath:{OutputPath}";
+            var msg = $"CompileAvaloniaXamlTask -> AssemblyFile:{AssemblyFile}, ProjectDirectory:{ProjectDirectory}, OutputPath:{outputPath}";
             BuildEngine.LogMessage(msg, outputImportance < MessageImportance.Low ? MessageImportance.High : outputImportance);
 
             var res = XamlCompilerTaskExecutor.Compile(BuildEngine,
-                input, OutputPath,
-                refInput, RefOutputPath,
-                File.ReadAllLines(ReferencesFilePath).Where(l => !string.IsNullOrWhiteSpace(l)).ToArray(),
+                AssemblyFile.ItemSpec, outputPath,
+                RefAssemblyFile?.ItemSpec, refOutputPath,
+                References?.Select(i => i.ItemSpec).ToArray() ?? Array.Empty<string>(),
                 ProjectDirectory, VerifyIl, DefaultCompileBindings, outputImportance,
                 new XamlCompilerDiagnosticsFilter(AnalyzerConfigFiles),
                 (SignAssembly && !DelaySign) ? AssemblyOriginatorKeyFile : null,
                 SkipXamlCompilation, DebuggerLaunch, VerboseExceptions);
-            if (!res.Success)
+
+            if (res.Success && !res.WrittenFile)
             {
-                WrittenFilePaths = writtenFilePaths.ToArray();
-                return false;
+                // To simplify incremental build checks, copy the input files to the expected output locations even if the Xaml compiler didn't do anything.
+                File.Copy(AssemblyFile.ItemSpec, outputPath, overwrite: true);
+                File.Copy(Path.ChangeExtension(AssemblyFile.ItemSpec, ".pdb"), Path.ChangeExtension(outputPath, ".pdb"), overwrite: true);
+
+                if (!string.IsNullOrEmpty(refOutputPath))
+                {
+                    File.Copy(RefAssemblyFile.ItemSpec, refOutputPath, overwrite: true);
+                }
             }
 
-            if (!res.WrittenFile)
-            {
-                File.Copy(input, OutputPath, true);
-                if (File.Exists(inputPdb))
-                    File.Copy(inputPdb, outputPdb, true);
-            }
-            else if (!string.IsNullOrWhiteSpace(RefOutputPath) && File.Exists(RefOutputPath))
-                writtenFilePaths.Add(RefOutputPath);
-
-            writtenFilePaths.Add(OutputPath);
-            if (File.Exists(outputPdb))
-                writtenFilePaths.Add(outputPdb);
-
-            WrittenFilePaths = writtenFilePaths.ToArray();
-            return true;
-        }
-
-        string GetPdbPath(string p)
-        {
-            var d = Path.GetDirectoryName(p);
-            var f = Path.GetFileNameWithoutExtension(p);
-            var rv = f + ".pdb";
-            if (d != null)
-                rv = Path.Combine(d, rv);
-            return rv;
+            return res.Success;
         }
         
-        [Required]
-        public string AssemblyFile { get; set; }
-        [Required]
-        public string ReferencesFilePath { get; set; }
-        [Required]
-        public string OriginalCopyPath { get; set; }
         [Required]
         public string ProjectDirectory { get; set; }
         
-        public string RefAssemblyFile { get; set; }
-        public string RefOutputPath { get; set; }
-        
-        public string OutputPath { get; set; }
+        [Required]
+        public ITaskItem AssemblyFile { get; set; }
+
+        public ITaskItem? RefAssemblyFile { get; set; }
+
+        public ITaskItem[]? References { get; set; }
 
         public bool VerifyIl { get; set; }
 
@@ -126,8 +80,5 @@ namespace Avalonia.Build.Tasks
         public bool VerboseExceptions { get; set; }
         
         public ITaskItem[] AnalyzerConfigFiles { get; set; }
-        
-        [Output]
-        public string[] WrittenFilePaths { get; private set; } = Array.Empty<string>();
     }
 }
