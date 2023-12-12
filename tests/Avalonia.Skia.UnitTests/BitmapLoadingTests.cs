c3346b5a22135bcae73d1ed3c269627c190e4561
Tim <tim.uniewski@gmail.com>
2023-12-12 11:46:37 +0100

    Add failing test for #13239

---
+++ tests/Avalonia.Skia.UnitTests/BitmapLoadingTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Skia.RenderTests;
+using Avalonia.UnitTests;
+using Xunit;
+
+namespace Avalonia.Skia.UnitTests;
+
+public class BitmapLoadingTests : TestBase
+{
+    public BitmapLoadingTests()
+        :base("BitmapLoadingTests")
+    {
+    }
+    
+    [Theory]
+    [InlineData("1.jpg")]
+    public void DecodeToWidth_ShouldNotThrowAccessViolation(string imageName)
+    {
+        try
+        {
+                    using var fs = File.Open(GetImageFileName(imageName), FileMode.Open, FileAccess.Read,
+                        FileShare.Read);
+                    using var bmp = Bitmap.DecodeToWidth(fs, 50);
+        }
+        catch (AccessViolationException exception)
+        {
+            // Fail
+            Assert.True(false);
+        }
+    }
+
+
+    private static string GetImageFileName(string imageName)
+    {
+        var path = Directory.GetCurrentDirectory();
+
+        while (path.Length > 0 && Path.GetFileName(path) != "Avalonia.Skia.UnitTests")
+        {
+            path = Path.GetDirectoryName(path);
+        }
+
+        return Path.Combine(path, "Images", imageName);
+    }
+}
