24b6d2e014e2a9e45a00f3b5f712dacce2598230
Max Katz <maxkatz6@outlook.com>
2023-12-15 13:36:53 -0800

    Add TODO12

--- src/Avalonia.Base/Controls/IInternalScroller.cs
+++ src/Avalonia.Base/Controls/IInternalScroller.cs
@@ -2,6 +2,7 @@
 
 namespace Avalonia.Controls.Primitives;
 
+// TODO12: Integrate with existing IScrollable interface, breaking change
 internal interface IInternalScroller
 {
     bool CanHorizontallyScroll { get; }
