e5ee2b202eee45ec0af83c0176647a7fdaa50120
Max Katz <maxkatz6@outlook.com>
2023-12-26 02:11:46 -0800

    Convert MobileSandbox to be a single project

--- samples/MobileSandbox/Platforms/Android/MainActivity.cs
+++ samples/MobileSandbox/Platforms/Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Avalonia.Android;
 
 namespace MobileSandbox.Android
 {
-    [Activity(Label = "MobileSandbox.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    [Activity(Label = "MobileSandbox.Android", Theme = "@style/MyTheme.NoActionBar", MainLauncher = true, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
     public class MainActivity : AvaloniaMainActivity<App>
     {
     }
