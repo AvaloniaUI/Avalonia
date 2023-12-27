1006eb8e3493546cede6642a46195cd1834d20b1
robloo <robloo@users.noreply.github.com>
2023-12-27 18:02:58 -0500

    Use RectangleGeometry directly in the Rectangle shape
    Since RectangleGeometry now supports RadiusX/Y there is no longer a need to have a separate geometry creation path.

--- src/Avalonia.Controls/Shapes/Rectangle.cs
+++ src/Avalonia.Controls/Shapes/Rectangle.cs
@@ -7,8 +7,6 @@ namespace Avalonia.Controls.Shapes
     /// </summary>
     public class Rectangle : Shape
     {
-        private const double PiOver2 = 1.57079633; // 90 deg to rad
-
         /// <summary>
         /// Defines the <see cref="RadiusX"/> property.
         /// </summary>
@@ -30,20 +28,14 @@ namespace Avalonia.Controls.Shapes
                 StrokeThicknessProperty);
         }
 
-        /// <summary>
-        /// Gets or sets the radius on the X-axis used to round the corners of the rectangle.
-        /// Corner radii are represented by an ellipse so this is the X-axis width of the ellipse.
-        /// </summary>
+        /// <inheritdoc cref="RectangleGeometry.RadiusX"/>
         public double RadiusX
         {
             get => GetValue(RadiusXProperty);
             set => SetValue(RadiusXProperty, value);
         }
 
-        /// <summary>
-        /// Gets or sets the radius on the Y-axis used to round the corners of the rectangle.
-        /// Corner radii are represented by an ellipse so this is the Y-axis height of the ellipse.
-        /// </summary>
+        /// <inheritdoc cref="RectangleGeometry.RadiusY"/>
         public double RadiusY
         {
             get => GetValue(RadiusYProperty);
@@ -53,28 +45,9 @@ namespace Avalonia.Controls.Shapes
         /// <inheritdoc/>
         protected override Geometry CreateDefiningGeometry()
         {
-            // TODO: If RectangleGeometry ever supports RadiusX/Y like in WPF,
-            // this code can be removed/combined with that implementation
-
-            double x = RadiusX;
-            double y = RadiusY;
             var rect = new Rect(Bounds.Size).Deflate(StrokeThickness / 2);
 
-            if (x == 0 && y == 0)
-            {
-                // Optimization when there are no corner radii
-                return new RectangleGeometry(rect);
-            }
-            else
-            {
-                var geometry = new StreamGeometry();
-                using (StreamGeometryContext context = geometry.Open())
-                {
-                    GeometryBuilder.DrawRoundedCornersRectangle(context, rect, x, y);
-                }
-
-                return geometry;
-            }
+            return new RectangleGeometry(rect, RadiusX, RadiusY);
         }
 
         /// <inheritdoc/>
