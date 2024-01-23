92e1c653a5986fbd19252d214bd30bdd84d736fa
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2024-01-23 19:32:05 +0800

    Track IsEffectivelyVisible state (#13972)
    * add IsEffectivelyVisible test
    
    * test IEV update
    
    * update self and descendants
    
    * sync from parent's isvisible and own on attach/detach
    
    * update test
    
    * update from review
    
    * add comments
    
    * remove whitespace changes
    
    * Added some more IsEffectivelyVisible tests.
    
    One failing.
    
    * Rework UpdateIsEffectivelyVisible.
    
    - Pass the parent state to `UpdateIsEffectivelyVisible`
    - Remove some unneeded methods
    - Update `IsEffectivelyVisible` before calling visual tree attach/detach events.
    
    ---------
    
    Co-authored-by: Steven Kirk <grokys@users.noreply.github.com>

--- src/Avalonia.Base/Visual.cs
+++ src/Avalonia.Base/Visual.cs
@@ -195,23 +195,32 @@ namespace Avalonia
         /// <summary>
         /// Gets a value indicating whether this control and all its parents are visible.
         /// </summary>
-        public bool IsEffectivelyVisible
+        public bool IsEffectivelyVisible { get; private set; } = true;
+        
+        /// <summary>
+        /// Updates the <see cref="IsEffectivelyVisible"/> property based on the parent's
+        /// <see cref="IsEffectivelyVisible"/>.
+        /// </summary>
+        /// <param name="parentState">The effective visibility of the parent control.</param>
+        private void UpdateIsEffectivelyVisible(bool parentState)
         {
-            get
+            var isEffectivelyVisible = parentState && IsVisible;
+
+            if (IsEffectivelyVisible == isEffectivelyVisible)
+                return;
+
+            IsEffectivelyVisible = isEffectivelyVisible;
+
+            // PERF-SENSITIVE: This is called on entire hierarchy and using foreach or LINQ
+            // will cause extra allocations and overhead.
+            
+            var children = VisualChildren;
+
+            // ReSharper disable once ForCanBeConvertedToForeach
+            for (int i = 0; i < children.Count; ++i)
             {
-                Visual? node = this;
-
-                while (node != null)
-                {
-                    if (!node.IsVisible)
-                    {
-                        return false;
-                    }
-
-                    node = node.VisualParent;
-                }
-
-                return true;
+                var child = children[i];
+                child.UpdateIsEffectivelyVisible(isEffectivelyVisible);
             }
         }
 
@@ -453,7 +462,11 @@ namespace Avalonia
         {
             base.OnPropertyChanged(change);
 
-            if (change.Property == FlowDirectionProperty)
+            if (change.Property == IsVisibleProperty)
+            {
+                UpdateIsEffectivelyVisible(VisualParent?.IsEffectivelyVisible ?? true);
+            } 
+            else if (change.Property == FlowDirectionProperty)
             {
                 InvalidateMirrorTransform();
 
@@ -463,7 +476,7 @@ namespace Avalonia
                 }
             }
         }
-
+ 
         protected override void LogicalChildrenCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
         {
             base.LogicalChildrenCollectionChanged(sender, e);
@@ -492,14 +505,16 @@ namespace Avalonia
                 AttachToCompositor(compositingRenderer.Compositor);
             }
             InvalidateMirrorTransform();
+            UpdateIsEffectivelyVisible(_visualParent!.IsEffectivelyVisible);
             OnAttachedToVisualTree(e);
             AttachedToVisualTree?.Invoke(this, e);
             InvalidateVisual();
+            
             _visualRoot.Renderer.RecalculateChildren(_visualParent!);
-
-            if (ZIndex != 0 && VisualParent is Visual parent)
-                parent.HasNonUniformZIndexChildren = true;
-
+            
+            if (ZIndex != 0 && _visualParent is { })
+                _visualParent.HasNonUniformZIndexChildren = true;
+            
             var visualChildren = VisualChildren;
             var visualChildrenCount = visualChildren.Count;
 
@@ -529,6 +544,7 @@ namespace Avalonia
             }
 
             DisableTransitions();
+            UpdateIsEffectivelyVisible(true);
             OnDetachedFromVisualTree(e);
             DetachFromCompositor();
 
