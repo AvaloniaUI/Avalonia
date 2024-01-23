2cfbe95236330a7a112fed697024b7077e14f9c0
laolarou <luolinrui726@outlook.com>
2024-01-22 23:03:25 -0800

    Remove unnecessary SKImage wrappers

--- src/Skia/Avalonia.Skia/Helpers/ImageSavingHelper.cs
+++ src/Skia/Avalonia.Skia/Helpers/ImageSavingHelper.cs
@@ -24,10 +24,53 @@ namespace Avalonia.Skia.Helpers
             if (image == null) throw new ArgumentNullException(nameof(image));
             if (fileName == null) throw new ArgumentNullException(nameof(fileName));
 
-            using (var stream = File.Create(fileName))
-            {
-                SaveImage(image, stream, quality);
-            }
+            using var stream = File.Create(fileName);
+
+            SaveImage(image, stream, quality);
+        }
+
+        /// <summary>
+        /// Save Skia bitmap to a file.
+        /// </summary>
+        /// <param name="bitmap">Bitmap to save</param>
+        /// <param name="fileName">Target file.</param>
+        /// <param name="quality">
+        /// The optional quality for PNG compression. 
+        /// The quality value is interpreted from 0 - 100. If quality is null 
+        /// the encoder applies the default quality value.
+        /// </param>
+        public static void SaveImage(SKBitmap bitmap, string fileName, int? quality = null)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException(nameof(bitmap));
+            if (fileName == null)
+                throw new ArgumentNullException(nameof(fileName));
+
+            using var stream = File.Create(fileName);
+
+            SaveImage(bitmap, stream, quality);
+        }
+
+        /// <summary>
+        /// Save Skia bitmap to a stream.
+        /// </summary>
+        /// <param name="bitmap">Bitmap to save</param>
+        /// <param name="stream">The output stream to save the image.</param>
+        /// <param name="quality">
+        /// The optional quality for PNG compression. 
+        /// The quality value is interpreted from 0 - 100. If quality is null 
+        /// the encoder applies the default quality value.
+        /// </param>
+        public static void SaveImage(SKBitmap bitmap, Stream stream, int? quality = null)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException(nameof(bitmap));
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
+            using var data = bitmap.Encode(SKEncodedImageFormat.Png, quality ?? 100);
+
+            data.SaveTo(stream);
         }
 
         /// <summary>
@@ -45,20 +88,9 @@ namespace Avalonia.Skia.Helpers
             if (image == null) throw new ArgumentNullException(nameof(image));
             if (stream == null) throw new ArgumentNullException(nameof(stream));
 
-            if (quality == null)
-            {
-                using (var data = image.Encode())
-                {
-                    data.SaveTo(stream);
-                }
-            }
-            else
-            {
-                using (var data = image.Encode(SKEncodedImageFormat.Png, (int)quality))
-                {
-                    data.SaveTo(stream);
-                }
-            }
+            using var data = image.Encode(SKEncodedImageFormat.Png, quality ?? 100);
+
+            data.SaveTo(stream);
         }
 
         // This method is here mostly for debugging purposes
