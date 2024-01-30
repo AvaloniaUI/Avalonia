e00454cc3182ee7e9636297b1c0c4129f8a86e27
Benedikt Stebner <Gillibald@users.noreply.github.com>
2024-01-30 10:34:13 +0100

    Failing line

--- native/Avalonia.Native/src/OSX/app.mm
+++ native/Avalonia.Native/src/OSX/app.mm
@@ -45,48 +45,30 @@ ComPtr<IAvnApplicationEvents> _events;
 
 -(BOOL)applicationShouldHandleReopen:(NSApplication *)sender hasVisibleWindows:(BOOL)flag
 {
-    if([[NSApplication self] isRunning]){
-        _events->OnReopen();
-    }
+    _events->OnReopen();
     
     return YES;
 }
 
 - (void)applicationDidHide:(NSNotification *)notification
 {
-    if(![[NSApplication self] isRunning]){
-        return;
-    }
-    
     _events->OnHide();
 }
 
 - (void)applicationDidUnhide:(NSNotification *)notification
 {
-    if(![[NSApplication self] isRunning]){
-        return;
-    }
-    
     _events->OnUnhide();
 }
 
 - (void)application:(NSApplication *)sender openFiles:(NSArray<NSString *> *)filenames
 {
-    if(![[NSApplication self] isRunning]){
-        return;
-    }
-    
     auto array = CreateAvnStringArray(filenames);
     
-    _events->FilesOpened(array);
+    //_events->FilesOpened(array);
 }
 
 - (void)application:(NSApplication *)application openURLs:(NSArray<NSURL *> *)urls
 {
-    if(![[NSApplication self] isRunning]){
-        return;
-    }
-    
     auto array = CreateAvnStringArray(urls);
     
     _events->FilesOpened(array);
