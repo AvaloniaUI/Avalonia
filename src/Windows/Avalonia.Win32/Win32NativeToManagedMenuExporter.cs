ae127630ee5d0e92f6e6784ef24b268e3efdcca0
Max Katz <maxkatz6@outlook.com>
2024-02-02 21:04:14 -0800

    Remove NativeMenuBar styles, unify Win32.TrayIcon and NativeMenuBar items generation

--- src/Windows/Avalonia.Win32/Win32NativeToManagedMenuExporter.cs
+++ src/Windows/Avalonia.Win32/Win32NativeToManagedMenuExporter.cs
@@ -1,74 +1,16 @@
-﻿using Avalonia.Reactive;
-using Avalonia.Collections;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Controls.Platform;
-using Avalonia.Data;
 
-namespace Avalonia.Win32
+namespace Avalonia.Win32;
+
+internal class Win32NativeToManagedMenuExporter : INativeMenuExporter
 {
-    internal class Win32NativeToManagedMenuExporter : INativeMenuExporter
+    private NativeMenu? _nativeMenu;
+
+    public void SetNativeMenu(NativeMenu? nativeMenu)
     {
-        private NativeMenu? _nativeMenu;
-
-        public void SetNativeMenu(NativeMenu? nativeMenu)
-        {
-            _nativeMenu = nativeMenu;
-        }
-
-        private static AvaloniaList<object> Populate(NativeMenu nativeMenu)
-        {
-            var result = new AvaloniaList<object>();
-            
-            foreach (var menuItem in nativeMenu.Items)
-            {
-                if (menuItem is NativeMenuItemSeparator)
-                {
-                    result.Add(new Separator());
-                }
-                else if (menuItem is NativeMenuItem item)
-                {
-                    var newItem = new MenuItem
-                    {
-                        [!MenuItem.HeaderProperty] = item.GetObservable(NativeMenuItem.HeaderProperty).ToBinding(),
-                        [!MenuItem.IconProperty] = item.GetObservable(NativeMenuItem.IconProperty)
-                            .Select(i => i is {} bitmap ? new Image { Source = bitmap } : null).ToBinding(),
-                        [!MenuItem.IsEnabledProperty] = item.GetObservable(NativeMenuItem.IsEnabledProperty).ToBinding(),
-                        [!MenuItem.CommandProperty] = item.GetObservable(NativeMenuItem.CommandProperty).ToBinding(),
-                        [!MenuItem.CommandParameterProperty] = item.GetObservable(NativeMenuItem.CommandParameterProperty).ToBinding(),
-                        [!MenuItem.InputGestureProperty] = item.GetObservable(NativeMenuItem.GestureProperty).ToBinding(),
-                        [!MenuItem.ToggleTypeProperty] = item.GetObservable(NativeMenuItem.ToggleTypeProperty)
-                            // TODO12 remove NativeMenuItemToggleType
-                            .Select(v => (MenuItemToggleType)v).ToBinding()
-                    };
-
-                    BindingOperations.Apply(newItem, MenuItem.IsCheckedProperty, InstancedBinding.TwoWay(
-                        item.GetObservable(NativeMenuItem.IsCheckedProperty).Select(v => (object)v),
-                        new AnonymousObserver<object?>(v => item.SetValue(NativeMenuItem.IsCheckedProperty, v))), null);
-
-                    if (item.Menu != null)
-                    {
-                        newItem.ItemsSource = Populate(item.Menu);
-                    }
-                    else if (item.HasClickHandlers && item is INativeMenuItemExporterEventsImplBridge bridge)
-                    {
-                        newItem.Click += (_, _) => bridge.RaiseClicked();
-                    }
-
-                    result.Add(newItem);
-                }
-            }
-
-            return result;
-        }
-
-        public AvaloniaList<object>? GetMenu()
-        {
-            if (_nativeMenu != null)
-            {
-                return Populate(_nativeMenu);
-            }
-
-            return null;
-        }
+        _nativeMenu = nativeMenu;
     }
+
+    internal NativeMenu? GetNativeMenu() => _nativeMenu;
 }
