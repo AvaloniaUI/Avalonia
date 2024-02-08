AvaloniaUI/Avalonia

commit 567561e27212a2c39ef59448a98d4e3497dd647c

Use popupParent as X11 parent window (#14366)
Previously, the popupParent was ignored when present causing popups to
have the X root window as its parent.

This cause issues on gamescope (SteamOS session compositor) where that window would end up behind the
main window.

Signed-off-by: Mary Guillemard <mary@mary.zone>
Co-authored-by: Max Katz <maxkatz6@outlook.com>

src/Avalonia.X11/X11Window.cs
@@ -142,7 +142,9 @@ namespace Avalonia.X11
             defaultWidth = Math.Max(defaultWidth, 300);
             defaultHeight = Math.Max(defaultHeight, 200);
 
-            _handle = XCreateWindow(_x11.Display, _x11.RootWindow, 10, 10, defaultWidth, defaultHeight, 0,
+            var parentHandle = popupParent != null ? ((X11Window)popupParent)._handle : _x11.RootWindow;
+
+            _handle = XCreateWindow(_x11.Display, parentHandle, 10, 10, defaultWidth, defaultHeight, 0,
                 depth,
                 (int)CreateWindowArgs.InputOutput, 
                 visual,

commit 83dd325a303555d035b66c17aeb3e90e463f780f

Translate root window coordinates to window coordinates when setting X11Window.Position
567561e27212a2c39ef59448a98d4e3497dd647c caused the origin of window to not be (0, 0) for popups on X.

As a result, all popups were wrongly positioned.

This change Position to translate from root window coordinates (the display space) to parent window coordinates.

Signed-off-by: Mary Guillemard <mary@mary.zone>

src/Avalonia.X11/X11Window.cs
@@ -1100,10 +1100,12 @@ namespace Avalonia.X11
                     UpdateSizeHints(null);
                 }
 
+                XTranslateCoordinates(_x11.Display, _parentHandle, _x11.RootWindow, 0, 0, out var wx, out var wy, out _);
+
                 var changes = new XWindowChanges
                 {
-                    x = value.X,
-                    y = (int)value.Y
+                    x = value.X - wx,
+                    y = (int)value.Y - wy
                 };
 
                 XConfigureWindow(_x11.Display, _handle, ChangeWindowFlags.CWX | ChangeWindowFlags.CWY,