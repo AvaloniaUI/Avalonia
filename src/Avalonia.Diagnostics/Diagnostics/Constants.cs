9354a34a9d9fd4604a63201f8c0d8fb6eb6d3d33
Giuseppe Lippolis <workgroupengineering@outlook.com>
2024-02-13 17:43:24 +0100

    fix: typo

--- src/Avalonia.Diagnostics/Diagnostics/Constants.cs
+++ src/Avalonia.Diagnostics/Diagnostics/Constants.cs
@@ -1,9 +1,9 @@
 ﻿namespace Avalonia.Diagnostics;
 
-static internal class Constants
+internal static  class Constants
 {
     /// <summary>
-    /// DevTools Clipoard data format
+    /// DevTools Clipboard data format
     /// </summary>
     static public class DataFormats
     {
@@ -12,7 +12,7 @@ static internal class Constants
         /// </summary>
         public const string Avalonia_DevTools_Selector = nameof(Avalonia_DevTools_Selector);
         /// <summary>
-        /// Clipboard data format for selector. It is added for quicks IDE reqcongnize format
+        /// Clipboard data format for selector. It is added for quicks IDE recognize format
         /// </summary>
         public const string Avalonia_DevTools_SelectorFromTemplate = nameof(Avalonia_DevTools_SelectorFromTemplate);
     }
