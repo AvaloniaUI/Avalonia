3d726854dadd923b65751e57c9b781517fe1e94b
Max Katz <maxkatz6@outlook.com>
2024-02-14 02:29:38 -0800

    Add some docs

--- src/Avalonia.Base/Metadata/InheritDataTypeFromAttribute.cs
+++ src/Avalonia.Base/Metadata/InheritDataTypeFromAttribute.cs
@@ -8,6 +8,12 @@ public enum InheritDataTypeFromScopeKind
     ControlTemplate,
 }
 
+/// <summary>
+/// Instructs the compiler to resolve the data type using hints such as scope kind - Style or ControlTemplate. 
+/// </summary>
+/// <remarks>
+/// Currently used to configure markup extensions like TemplateBinding to properly parse AvaloniaProperty values, targeting specific scope data type.  
+/// </remarks>
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
 public sealed class InheritDataTypeFromAttribute : Attribute
 {
@@ -15,6 +21,6 @@ public sealed class InheritDataTypeFromAttribute : Attribute
     {
         ScopeKind = scopeKind;
     }
-    
+
     public InheritDataTypeFromScopeKind ScopeKind { get; }
 }
