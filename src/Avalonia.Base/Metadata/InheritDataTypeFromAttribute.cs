237e77459d9ea4ed30fce72aaf09f5637f6230c2
Max Katz <maxkatz6@outlook.com>
2024-02-14 02:24:17 -0800

    Add InheritDataTypeFromAttribute and use it in TemplateBinding

---
+++ src/Avalonia.Base/Metadata/InheritDataTypeFromAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Avalonia.Metadata;
+
+public enum InheritDataTypeFromScopeKind
+{
+    Style = 1,
+    ControlTemplate,
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+public sealed class InheritDataTypeFromAttribute : Attribute
+{
+    public InheritDataTypeFromAttribute(InheritDataTypeFromScopeKind scopeKind)
+    {
+        ScopeKind = scopeKind;
+    }
+    
+    public InheritDataTypeFromScopeKind ScopeKind { get; }
+}
