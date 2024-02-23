760c6749d0186026607b358e50bf1a3f0a8fbfb2
Benedikt Stebner <Gillibald@users.noreply.github.com>
2024-02-23 08:15:54 +0100

    Recreate TextLayout on measure to make sure constraint is updated (#14705)
    * Recreate TextLayout on measure to make sure constraint is updated
    
    * Add failing test

--- src/Avalonia.Controls/TextBlock.cs
+++ src/Avalonia.Controls/TextBlock.cs
@@ -677,7 +677,6 @@ namespace Avalonia.Controls
         {
             _textLayout?.Dispose();
             _textLayout = null;
-
             _textRuns = null;
 
             base.OnMeasureInvalidated();
@@ -689,6 +688,10 @@ namespace Avalonia.Controls
             var padding = LayoutHelper.RoundLayoutThickness(Padding, scale, scale);
 
             _constraint = availableSize.Deflate(padding);
+
+            //Reset TextLayout otherwise constraint might be outdated.
+            _textLayout?.Dispose();
+            _textLayout = null;
 
             var inlines = Inlines;
 
@@ -819,15 +822,19 @@ namespace Avalonia.Controls
             {
                 oldValue.LogicalChildren = null;
                 oldValue.InlineHost = null;
-                oldValue.Invalidated -= (s, e) => InvalidateMeasure();
+                oldValue.Invalidated -= Invalidated;
             }
 
             if (newValue is not null)
             {
                 newValue.LogicalChildren = LogicalChildren;
                 newValue.InlineHost = this;
-                newValue.Invalidated += (s, e) => InvalidateMeasure();
+                newValue.Invalidated += Invalidated;
             }
+
+            return;
+
+            void Invalidated(object? sender, EventArgs e) => InvalidateMeasure();
         }
 
         void IInlineHost.Invalidate()
