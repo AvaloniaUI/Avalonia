36dbaaff0cb414d8249403747d511e8d0d3d7057
Max Katz <maxkatz6@outlook.com>
2024-03-21 21:43:01 -0700

    Update browser sub-project from the templates

--- samples/MobileSandbox/Platforms/Browser/wwwroot/main.js
+++ samples/MobileSandbox/Platforms/Browser/wwwroot/main.js
@@ -1,7 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-import { dotnet } from './_framework/dotnet.js'
+﻿import { dotnet } from './_framework/dotnet.js'
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
@@ -13,4 +10,4 @@ const dotnetRuntime = await dotnet
 
 const config = dotnetRuntime.getConfig();
 
-await dotnetRuntime.runMain(config.mainAssemblyName, ["dotnet", "is", "great!"]);
+await dotnetRuntime.runMain(config.mainAssemblyName, [window.location.search]);
