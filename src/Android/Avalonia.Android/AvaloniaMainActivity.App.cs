27c05e6e88651e979857482bcd848e3c4f147614
Max Katz <maxkatz6@outlook.com>
2024-03-28 18:12:49 -0700

    Fix missing .UseAndroid()

--- src/Android/Avalonia.Android/AvaloniaMainActivity.App.cs
+++ src/Android/Avalonia.Android/AvaloniaMainActivity.App.cs
@@ -11,5 +11,5 @@ namespace Avalonia.Android;
 public class AvaloniaMainActivity<TApp> : AvaloniaMainActivity
     where TApp : Application, new()
 {
-    protected override AppBuilder CreateAppBuilder() => AppBuilder.Configure<TApp>();
+    protected override AppBuilder CreateAppBuilder() => AppBuilder.Configure<TApp>().UseAndroid();
 }
