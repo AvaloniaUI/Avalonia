b3d5940c84993d0987891e4fe271b9bf665ed0d9
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2024-03-30 08:49:30 +0100

    Add basic support for automation for DataGrid and DataGridRow (#15174)
    * Add basic support for automation for DataGrid and DataGridRow
    
    * Add DataGridCellAutomationPeer
    
    * Add DataGridColumnHeaderAutomationPeer
    
    * Add DataGridColumnHeadersPresenterAutomationPeer
    
    * Add DataGridDetailsPresenterAutomationPeer

---
+++ src/Avalonia.Controls.DataGrid/Automation/Peers/DataGridRowAutomationPeer.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Controls;
+
+namespace Avalonia.Automation.Peers
+{
+    public class DataGridRowAutomationPeer : ControlAutomationPeer
+    {
+        public DataGridRowAutomationPeer(DataGridRow owner)
+            : base(owner)
+        {
+        }
+
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.DataItem;
+        }
+
+        protected override bool IsContentElementCore() => true;
+        protected override bool IsControlElementCore() => true;
+    }
+}
