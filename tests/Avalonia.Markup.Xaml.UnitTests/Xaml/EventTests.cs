9bab952433fd9b00cfa85d84e374f4a1e8bbd79e
Giuseppe Lippolis <workgroupengineering@outlook.com>
2024-04-09 17:47:46 +0200

    test: Automatic RoutedEvent Handler Generation

--- tests/Avalonia.Markup.Xaml.UnitTests/Xaml/EventTests.cs
+++ tests/Avalonia.Markup.Xaml.UnitTests/Xaml/EventTests.cs
@@ -40,6 +40,23 @@ namespace Avalonia.Markup.Xaml.UnitTests.Xaml
         }
 
         [Fact]
+        public void Attached_Event_Is_Assigned_Generic()
+        {
+            var xaml = @"<Panel xmlns='https://github.com/avaloniaui'><Grid DoubleTapped='OnTapped'><Button Name='target'/></Grid></Panel>";
+            var host = new MyPanel();
+
+            AvaloniaRuntimeXamlLoader.Load(xaml, rootInstance: host);
+
+            var target = host.FindControl<Button>("target");
+
+            Assert.NotNull(target);
+
+            target.RaiseEvent(new TappedEventArgs(Gestures.DoubleTappedEvent, default));
+
+            Assert.True(host.WasTapped);
+        }
+
+        [Fact]
         public void Exception_Is_Thrown_If_Event_Not_Found()
         {
             var xaml = @"<Button xmlns='https://github.com/avaloniaui' Click='NotFound'/>";
@@ -48,11 +65,38 @@ namespace Avalonia.Markup.Xaml.UnitTests.Xaml
             XamlTestHelpers.AssertThrowsXamlException(() => AvaloniaRuntimeXamlLoader.Load(xaml, rootInstance: target));
         }
 
+
+
+        [Fact]
+        public void Attached_Event_Routed_Event_Handler()
+        {
+            var xaml = @"<Panel xmlns='https://github.com/avaloniaui' Button.Click='OnClick'><Button Name='target'/></Panel>";
+            var host = new MyPanel();
+
+            AvaloniaRuntimeXamlLoader.Load(xaml, rootInstance: host);
+
+            var target = host.FindControl<Button>("target");
+            target.RaiseEvent(new RoutedEventArgs
+            {
+                RoutedEvent = Button.ClickEvent,
+            });
+
+            Assert.True(host.WasClicked);
+        }
+
         public class MyButton : Button
         {
             public bool WasClicked { get; private set; }
             public bool WasTapped { get; private set; }
 
+            public void OnClick(object sender, RoutedEventArgs e) => WasClicked = true;
+            public void OnTapped(object sender, RoutedEventArgs e) => WasTapped = true;
+        }
+
+        public class MyPanel : Panel
+        {
+            public bool WasClicked { get; private set; }
+            public bool WasTapped { get; private set; }
             public void OnClick(object sender, RoutedEventArgs e) => WasClicked = true;
             public void OnTapped(object sender, RoutedEventArgs e) => WasTapped = true;
         }
