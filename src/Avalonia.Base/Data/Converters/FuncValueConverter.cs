dd27d61b9a27eda2a8280da16bb5d11bb8f1d5e8
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2024-04-12 00:31:52 +0200

    Add FuncValueConverter with parameter support (#15323)
    * Add FuncValueConverter with parameter support
    
    * Fix summary comment
    
    * Fix summary comment
    
    * Add Equal and NotEqual converters to a ObjectConverters class
    
    * Fix condition for Equal
    
    * Added ObjectConverters unit tests

--- src/Avalonia.Base/Data/Converters/FuncValueConverter.cs
+++ src/Avalonia.Base/Data/Converters/FuncValueConverter.cs
@@ -5,7 +5,7 @@ using Avalonia.Utilities;
 namespace Avalonia.Data.Converters
 {
     /// <summary>
-    /// A general purpose <see cref="IValueConverter"/> that uses a <see cref="Func{T1, TResult}"/>
+    /// A general purpose <see cref="IValueConverter"/> that uses a <see cref="Func{TIn, TResult}"/>
     /// to provide the converter logic.
     /// </summary>
     /// <typeparam name="TIn">The input type.</typeparam>
@@ -29,6 +29,46 @@ namespace Avalonia.Data.Converters
             if (TypeUtilities.CanCast<TIn>(value))
             {
                 return _convert((TIn?)value);
+            }
+            else
+            {
+                return AvaloniaProperty.UnsetValue;
+            }
+        }
+
+        /// <inheritdoc/>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// A general purpose <see cref="IValueConverter"/> that uses a <see cref="Func{TIn, TParam, TOut}"/>
+    /// to provide the converter logic.
+    /// </summary>
+    /// <typeparam name="TIn">The input type.</typeparam>
+    /// <typeparam name="TParam">The param type.</typeparam>
+    /// <typeparam name="TOut">The output type.</typeparam>
+    public class FuncValueConverter<TIn, TParam, TOut> : IValueConverter
+    {
+        private readonly Func<TIn?, TParam?, TOut> _convert;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FuncValueConverter{TIn, TParam, TOut}"/> class.
+        /// </summary>
+        /// <param name="convert">The convert function.</param>
+        public FuncValueConverter(Func<TIn?, TParam?, TOut> convert)
+        {
+            _convert = convert;
+        }
+
+        /// <inheritdoc/>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (TypeUtilities.CanCast<TIn>(value) && TypeUtilities.CanCast<TParam>(parameter))
+            {
+                return _convert((TIn?)value, (TParam?)parameter);
             }
             else
             {
