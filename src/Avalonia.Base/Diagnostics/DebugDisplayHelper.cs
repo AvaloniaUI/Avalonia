cfc0f158b3b96bcbaa250cdc9446251df87e944c
Julien Lebosquain <julien@lebosquain.net>
2024-04-12 04:26:07 +0200

    Improve debug display of controls and related error messages (#15219)
    * Improve debug display of controls and related error messages
    
    * Move DebugDisplayHelper
    
    ---------
    
    Co-authored-by: Max Katz <maxkatz6@outlook.com>

---
+++ src/Avalonia.Base/Diagnostics/DebugDisplayHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace Avalonia;
+
+internal static class DebugDisplayHelper
+{
+    public static void AppendOptionalValue(StringBuilder builder, string name, object? value, bool includeContent)
+    {
+        const int maxValueLength = 50;
+
+        if (value is null or string { Length: 0 })
+        {
+            return;
+        }
+
+        if (builder.Length > 0 && builder[builder.Length - 1] == ')')
+        {
+            --builder.Length;
+            builder.Append(", ");
+        }
+        else
+        {
+            builder.Append(" (");
+        }
+
+        builder.Append(name);
+        builder.Append(" = ");
+
+        if (value is AvaloniaObject avaloniaObject)
+        {
+            avaloniaObject.BuildDebugDisplay(builder, includeContent);
+        }
+        else
+        {
+            var stringValue = value.ToString();
+            if (stringValue?.Length > maxValueLength)
+            {
+                builder.Append(stringValue, 0, maxValueLength - 1);
+                builder.Append('…');
+            }
+            else
+            {
+                builder.Append(stringValue);
+            }
+        }
+
+        builder.Append(')');
+    }
+}
