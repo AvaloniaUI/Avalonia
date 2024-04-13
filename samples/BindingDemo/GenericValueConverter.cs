ca520a233264208ccb489cd16d3ed2d1bda38883
Steven Kirk <grokys@users.noreply.github.com>
2024-04-13 13:07:07 +0300

    Added failing test cases for #10856. (#15338)
    This issue only shows up when using the cecil backend for XamlX and so can't easily be tested with unit tests. The easiest way to test it is to add it to a sample project, `BindingDemo` is as good as anywhere I think.

---
+++ samples/BindingDemo/GenericValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+#nullable enable
+
+namespace BindingDemo;
+
+public class GenericValueConverter<T> : IValueConverter
+{
+    public GenericValueConverter()
+    {
+        
+    }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is T)
+        {
+            return $"{typeof(T).Name}: {value}";
+        }
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
