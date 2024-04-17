66e133b9569e4424c32d89c232e121ac7a62b3d6
Dameng <313880747@qq.com>
2024-04-17 10:23:33 +0800

    upgrade `Quamotion.RemoteViewing` to 1.1.211 to work with `RealVNC Viewer`

--- src/Headless/Avalonia.Headless.Vnc/HeadlessVncPlatformExtensions.cs
+++ src/Headless/Avalonia.Headless.Vnc/HeadlessVncPlatformExtensions.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Headless;
 using Avalonia.Headless.Vnc;
 using Avalonia.Platform;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
 using RemoteViewing.Vnc;
 using RemoteViewing.Vnc.Server;
 
@@ -16,35 +19,61 @@ namespace Avalonia
         public static int StartWithHeadlessVncPlatform(
             this AppBuilder builder,
             string host, int port,
-            string[] args, ShutdownMode shutdownMode = ShutdownMode.OnLastWindowClose)
+            string[] args,
+            ShutdownMode shutdownMode = ShutdownMode.OnLastWindowClose,
+            string? password = null,
+            ILogger? logger = null)
         {
+            logger = logger ??= NullLogger.Instance;
             var tcpServer = new TcpListener(host == null ? IPAddress.Loopback : IPAddress.Parse(host), port);
-            tcpServer.Start();    
+            tcpServer.Start();
             return builder
                 .UseHeadless(new AvaloniaHeadlessPlatformOptions
                 {
                     UseHeadlessDrawing = false,
                     FrameBufferFormat = PixelFormat.Bgra8888
                 })
-                .AfterApplicationSetup(_ =>
+                .AfterSetup(_ =>
                 {
-                    var lt = ((IClassicDesktopStyleApplicationLifetime)builder.Instance!.ApplicationLifetime!);
+                    var lt = ((IClassicDesktopStyleApplicationLifetime) builder.Instance!.ApplicationLifetime!);
                     lt.Startup += async delegate
                     {
                         while (true)
                         {
-                            var client = await tcpServer.AcceptTcpClientAsync();
-                            var options = new VncServerSessionOptions
+                            try
                             {
-                                AuthenticationMethod = AuthenticationMethod.None
-                            };
-                            var session = new VncServerSession();
-                            
-                            session.SetFramebufferSource(new HeadlessVncFramebufferSource(
-                                session, lt.MainWindow ?? throw new InvalidOperationException("MainWindow wasn't initialized")));
-                            session.Connect(client.GetStream(), options);
+                                var client = await tcpServer.AcceptTcpClientAsync();
+                                var options = new VncServerSessionOptions
+                                {
+                                    AuthenticationMethod = string.IsNullOrWhiteSpace(password)
+                                        ? AuthenticationMethod.None
+                                        : AuthenticationMethod.Password
+                                };
+                                var session = new VncServerSession(new VncPasswordChallenge(),
+                                    logger: logger);
+                                if (string.IsNullOrWhiteSpace(password) == false)
+                                {
+                                    session.PasswordProvided += (s, e) =>
+                                    {
+                                        e.Accept(password.ToCharArray());
+                                    };
+                                }
+
+                                session.SetFramebufferSource(new HeadlessVncFramebufferSource(
+                                    session,
+                                    lt.MainWindow ??
+                                    throw new InvalidOperationException("MainWindow wasn't initialized")));
+                                session.Connect(client.GetStream(), options);
+                            }
+                            catch (Exception e)
+                            {
+                                logger.LogError(e,"VNC Connection Exception");
+                            }
+                            finally
+                            {
+                                await Task.Delay(100);
+                            }
                         }
-                        
                     };
                 })
                 .StartWithClassicDesktopLifetime(args, shutdownMode);
