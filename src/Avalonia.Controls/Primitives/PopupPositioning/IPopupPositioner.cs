6389df9db222a364570ed7f3dc3e748d0b656442
Max Katz <maxkatz6@outlook.com>
2024-05-10 00:09:24 +0900

    Extract new types into separated files

--- src/Avalonia.Controls/Primitives/PopupPositioning/IPopupPositioner.cs
+++ src/Avalonia.Controls/Primitives/PopupPositioning/IPopupPositioner.cs
@@ -426,51 +426,6 @@ namespace Avalonia.Controls.Primitives.PopupPositioning
     }
 
     /// <summary>
-    /// Represents a method that provides custom positioning for a <see cref="Popup"/> control.
-    /// </summary>
-    /// <param name="popupSize">The <see cref="Size"/> of the <see cref="Popup"/> control.</param>
-    /// <param name="targetRect">The <see cref="Rect"/> of the <see cref="Popup.PlacementTarget"/>.</param>
-    /// <param name="offset">The <see cref="Point"/> computed from the <see cref="Popup.HorizontalOffset"/> and <see cref="Popup.VerticalOffset"/> property values.</param>
-    public delegate CustomPopupPlacement CustomPopupPlacementCallback(Size popupSize, Rect targetRect, Point offset);
-
-    /// <summary>
-    /// Defines custom placement parameters for a Popup control.
-    /// </summary>
-    public record struct CustomPopupPlacement
-    {
-        private PopupGravity _gravity;
-        private PopupAnchor _anchor;
-
-        /// <see cref="PopupPositionerParameters.Anchor"/>
-        public PopupAnchor Anchor
-        {
-            get => _anchor;
-            init
-            {
-                PopupPositioningEdgeHelper.ValidateEdge(value);
-                _anchor = value;
-            }
-        }
-
-        /// <see cref="PopupPositionerParameters.Gravity"/>
-        public PopupGravity Gravity
-        {
-            get => _gravity;
-            init
-            {
-                PopupPositioningEdgeHelper.ValidateGravity(value);
-                _gravity = value;
-            }
-        }
-
-        /// <see cref="PopupPositionerParameters.ConstraintAdjustment"/>
-        public PopupPositionerConstraintAdjustment ConstraintAdjustment { get; init; }
-
-        /// <see cref="PopupPositionerParameters.Offset"/>
-        public Point Offset { get; init; }
-    }
-
-    /// <summary>
     /// Positions an <see cref="IPopupHost"/>.
     /// </summary>
     /// <remarks>
