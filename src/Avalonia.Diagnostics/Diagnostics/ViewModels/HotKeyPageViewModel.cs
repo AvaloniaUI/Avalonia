345cc3232b2cc79ad4e05c39ff51a687c4d152e9
Steve Monaco <1782158+stevemonaco@users.noreply.github.com>
2024-05-12 20:34:44 -0400

    Add HotKeys Page to DevTools

---
+++ src/Avalonia.Diagnostics/Diagnostics/ViewModels/HotKeyPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Avalonia.Diagnostics.ViewModels;
+
+internal record HotKeyDescription(string Description, string LongDescription, string Gesture);
+internal class HotKeyPageViewModel : ViewModelBase
+{
+    public ObservableCollection<HotKeyDescription> HotKeys { get; } = new();
+
+    public HotKeyPageViewModel()
+    {
+        HotKeys = new()
+        {
+            new("Enable Snapshot Frames", "Pauses refreshing the Value Frames inspector for the selected Control", "Alt+S"),
+            new("Disable Snapshot Frames", "Resumes refreshing the Value Frames inspector for the selected Control", "Alt+D"),
+            new("Inspect Control Under Pointer", "Inspects the hovered Control in the Logical or Visual Tree Page", "Ctrl+Shift"),
+            new("Toggle Popup Freeze", "Prevents visible Popups from closing so they can be inspected", "Ctrl+Alt+F"),
+            new("Screenshot Selected Control", "Saves a Screenshot of the Selected Control in the Logical or Visual Tree Page", "F8")
+        };
+    }
+}
