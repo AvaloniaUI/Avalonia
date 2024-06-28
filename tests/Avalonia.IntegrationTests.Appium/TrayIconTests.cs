9f86e28f874ecc2d1da5fb237e7be3f211b904b9
Max Katz <maxkatz6@outlook.com>
2024-06-27 21:07:43 -0700

    Implement macOS tray icon tests

--- tests/Avalonia.IntegrationTests.Appium/TrayIconTests.cs
+++ tests/Avalonia.IntegrationTests.Appium/TrayIconTests.cs
@@ -8,29 +8,31 @@ using Xunit;
 
 namespace Avalonia.IntegrationTests.Appium;
 
-// TODO implement macOS tests.
 [Collection("Default")]
 public class TrayIconTests : IDisposable
 {
     private readonly AppiumDriver _session;
-    private readonly AppiumDriver _rootSession;
+    private readonly AppiumDriver? _rootSession;
     private const string TrayIconName = "IntegrationTestApp TrayIcon";
 
     public TrayIconTests(DefaultAppFixture fixture)
     {
         _session = fixture.Session;
+
         // "Root" is a special name for windows the desktop session, that has access to task bar.
-        _rootSession = fixture.CreateNestedSession("Root");
+        if (OperatingSystem.IsWindows())
+            _rootSession = fixture.CreateNestedSession("Root");
 
         var tabs = _session.FindElementByAccessibilityId("MainTabs");
         var tab = tabs.FindElementByName("Desktop");
         tab.Click();
     }
 
+    // Left click is only supported on Windows.
     [PlatformFact(TestPlatforms.Windows)]
     public void Should_Handle_Left_Click()
     {
-        var avaloinaTrayIconButton = GetTrayIconButton(_rootSession, TrayIconName);
+        var avaloinaTrayIconButton = TryGetTrayIconButton(_rootSession ?? _session, TrayIconName);
         Assert.NotNull(avaloinaTrayIconButton);
 
         avaloinaTrayIconButton.Click();
@@ -41,10 +43,10 @@ public class TrayIconTests : IDisposable
         Assert.True(checkBox.GetIsChecked());
     }
 
-    [PlatformFact(TestPlatforms.Windows)]
+    [Fact]
     public void Should_Handle_Context_Menu_Item_Click()
     {
-        var avaloinaTrayIconButton = GetTrayIconButton(_rootSession, TrayIconName);
+        var avaloinaTrayIconButton = TryGetTrayIconButton(_rootSession ?? _session, TrayIconName);
         Assert.NotNull(avaloinaTrayIconButton);
 
         var contextMenu = ShowAndGetTrayMenu(avaloinaTrayIconButton, TrayIconName);
@@ -59,65 +61,83 @@ public class TrayIconTests : IDisposable
         Assert.True(checkBox.GetIsChecked());
     }
 
-    [PlatformFact(TestPlatforms.Windows)]
+    [Fact]
     public void Can_Toggle_TrayIcon_Visibility()
     {
-        var avaloinaTrayIconButton = GetTrayIconButton(_rootSession, TrayIconName);
+        var avaloinaTrayIconButton = TryGetTrayIconButton(_rootSession ?? _session, TrayIconName);
         Assert.NotNull(avaloinaTrayIconButton);
 
         var toggleButton = _session.FindElementByAccessibilityId("ToggleTrayIconVisible");
         toggleButton.SendClick();
 
-        avaloinaTrayIconButton = GetTrayIconButton(_rootSession, TrayIconName);
+        avaloinaTrayIconButton = TryGetTrayIconButton(_rootSession ?? _session, TrayIconName);
         Assert.Null(avaloinaTrayIconButton);
 
         toggleButton.SendClick();
 
-        avaloinaTrayIconButton = GetTrayIconButton(_rootSession, TrayIconName);
+        avaloinaTrayIconButton = TryGetTrayIconButton(_rootSession ?? _session, TrayIconName);
         Assert.NotNull(avaloinaTrayIconButton);
     }
 
-    private static AppiumWebElement? GetTrayIconButton(AppiumDriver session, string trayIconName)
+    private static AppiumWebElement? TryGetTrayIconButton(AppiumDriver session, string trayIconName)
     {
-        var taskBar = session.FindElementByName("Taskbar");
-
-        if (TryToGetIcon(taskBar, trayIconName) is { } trayIcon)
+        if (OperatingSystem.IsWindows())
         {
-            return trayIcon;
+            var taskBar = session.FindElementsByName("Taskbar")
+                .FirstOrDefault() ?? throw new InvalidOperationException("Couldn't find Taskbar on current system.");
+
+            if (TryToGetIcon(taskBar, trayIconName) is { } trayIcon)
+            {
+                return trayIcon;
+            }
+            else
+            {
+                // Add a sleep here, as previous test might still run popup closing animation.
+                Thread.Sleep(1000);
+
+                // Try to open "Show hidden icons"
+                var trayIconsButton = taskBar.FindElementByAccessibilityId("SystemTrayIcon");
+                trayIconsButton.Click();
+
+                // Is it localizable? Or is it safe?
+                var trayIconsFlyout = session.FindElementByName("System tray overflow window.");
+                return TryToGetIcon(trayIconsFlyout, trayIconName);
+            }
+
+            static AppiumWebElement? TryToGetIcon(AppiumWebElement parent, string trayIconName) =>
+                parent.FindElementsByName(trayIconName).LastOrDefault()
+                // Some icons (including Avalonia) for some reason include leading whitespace in their name.
+                // Couldn't find any info on that, which is weird.
+                ?? parent.FindElementsByName(" " + trayIconName).LastOrDefault();
+        }
+        if (OperatingSystem.IsMacOS())
+        {
+            return session.FindElementsByXPath("//XCUIElementTypeStatusItem").FirstOrDefault();
+        }
+
+        throw new PlatformNotSupportedException();
+    }
+
+    private static AppiumWebElement ShowAndGetTrayMenu(AppiumWebElement trayIcon, string trayIconName)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            var session = (AppiumDriver)trayIcon.WrappedDriver;
+            new Actions(trayIcon.WrappedDriver).ContextClick(trayIcon).Perform();
+
+            Thread.Sleep(1000);
+
+            return session.FindElementByXPath($"//Window[@AutomationId='AvaloniaTrayPopupRoot_{trayIconName}']");
         }
         else
         {
-            // Add a sleep here, as previous test might still run popup closing animation.
-            Thread.Sleep(1000);
-            
-            // Try to open "Show hidden icons"
-            var trayIconsButton = taskBar.FindElementByAccessibilityId("SystemTrayIcon");
-            trayIconsButton.Click();
-
-            // Is it localizable? Or is it safe?
-            var trayIconsFlyout = session.FindElementByName("System tray overflow window.");
-            return TryToGetIcon(trayIconsFlyout, trayIconName);
+            trayIcon.Click();
+            return trayIcon.FindElementByXPath("//XCUIElementTypeStatusItem/XCUIElementTypeMenu");
         }
-
-        static AppiumWebElement? TryToGetIcon(AppiumWebElement parent, string trayIconName) =>
-            parent.FindElementsByName(trayIconName).LastOrDefault()
-            // Some icons (including Avalonia) for some reason include leading whitespace in their name.
-            // Couldn't find any info on that, which is weird.
-            ?? parent.FindElementsByName(" " + trayIconName).LastOrDefault();
-    }
-
-    private static AppiumWebElement? ShowAndGetTrayMenu(AppiumWebElement trayIcon, string trayIconName)
-    {
-        var session = (AppiumDriver)trayIcon.WrappedDriver;
-        new Actions(trayIcon.WrappedDriver).ContextClick(trayIcon).Perform();
-
-        Thread.Sleep(1000);
-
-        return session.FindElementByXPath($"//Window[@AutomationId='AvaloniaTrayPopupRoot_{trayIconName}']");
     }
 
     public void Dispose()
     {
-        _rootSession.Dispose();
+        _rootSession?.Dispose();
     }
 }
