fc045661d15392c5b879dda4179723dcda028ce7
Max Katz <maxkatz6@outlook.com>
2024-06-27 19:40:16 -0700

    [Windows] Add left click and menu item click e2e tests for TrayIcon

---
+++ tests/Avalonia.IntegrationTests.Appium/TrayIconTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Interactions;
+using Xunit;
+
+namespace Avalonia.IntegrationTests.Appium;
+
+// TODO implement macOS tests.
+[Collection("Default")]
+public class TrayIconTests : IDisposable
+{
+    private readonly AppiumDriver _session;
+    private readonly AppiumDriver _rootSession;
+    private const string TrayIconName = "IntegrationTestApp TrayIcon";
+
+    public TrayIconTests(DefaultAppFixture fixture)
+    {
+        _session = fixture.Session;
+        // "Root" is a special name for windows the desktop session, that has access to task bar.
+        _rootSession = fixture.CreateNestedSession("Root");
+
+        var tabs = _session.FindElementByAccessibilityId("MainTabs");
+        var tab = tabs.FindElementByName("Desktop");
+        tab.Click();
+    }
+
+    [PlatformFact(TestPlatforms.Windows)]
+    public void Should_Handle_Left_Click()
+    {
+        var avaloinaTrayIconButton = GetTrayIconButton(_rootSession, TrayIconName);
+        Assert.NotNull(avaloinaTrayIconButton);
+
+        avaloinaTrayIconButton.Click();
+
+        Thread.Sleep(1000);
+        
+        var checkBox = _session.FindElementByAccessibilityId("TrayIconClicked");
+        Assert.True(checkBox.GetIsChecked());
+    }
+
+    [PlatformFact(TestPlatforms.Windows)]
+    public void Should_Handle_Context_Menu_Item_Click()
+    {
+        var avaloinaTrayIconButton = GetTrayIconButton(_rootSession, TrayIconName);
+        Assert.NotNull(avaloinaTrayIconButton);
+
+        var contextMenu = ShowAndGetTrayMenu(avaloinaTrayIconButton, TrayIconName);
+        Assert.NotNull(contextMenu);
+
+        var menuItem = contextMenu.FindElementByName("Raise Menu Clicked");
+        menuItem.Click();
+        
+        Thread.Sleep(1000);
+
+        var checkBox = _session.FindElementByAccessibilityId("TrayIconMenuClicked");
+        Assert.True(checkBox.GetIsChecked());
+    }
+
+    private static AppiumWebElement? GetTrayIconButton(AppiumDriver session, string trayIconName)
+    {
+        var taskBar = session.FindElementByName("Taskbar");
+
+        if (TryToGetIcon(taskBar, trayIconName) is { } trayIcon)
+        {
+            return trayIcon;
+        }
+        else
+        {
+            // Try to open "Show hidden icons"
+            var trayIconsButton = taskBar.FindElementByAccessibilityId("SystemTrayIcon");
+            trayIconsButton.Click();
+
+            // Is it localizable? Or is it safe?
+            var trayIconsFlyout = session.FindElementByName("System tray overflow window.");
+            return TryToGetIcon(trayIconsFlyout, trayIconName);
+        }
+
+        static AppiumWebElement? TryToGetIcon(AppiumWebElement parent, string trayIconName) =>
+            parent.FindElementsByName(trayIconName).LastOrDefault()
+            // Some icons (including Avalonia) for some reason include leading whitespace in their name.
+            // Couldn't find any info on that, which is weird.
+            ?? parent.FindElementsByName(" " + trayIconName).LastOrDefault();
+    }
+
+    private static AppiumWebElement? ShowAndGetTrayMenu(AppiumWebElement trayIcon, string trayIconName)
+    {
+        var session = (AppiumDriver)trayIcon.WrappedDriver;
+        new Actions(trayIcon.WrappedDriver).ContextClick(trayIcon).Perform();
+
+        Thread.Sleep(1000);
+
+        return session.FindElementByXPath($"//Window[@AutomationId='AvaloniaTrayPopupRoot_{trayIconName}']");
+    }
+
+    public void Dispose()
+    {
+        _rootSession.Dispose();
+    }
+}
