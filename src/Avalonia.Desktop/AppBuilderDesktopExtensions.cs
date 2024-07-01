2ac6ed41772e0196179fd22e99713d24a1844fb3
Heku <heku.git@outlook.com>
2024-07-02 05:39:36 +0800

    Fix typos (#16183)

--- src/Avalonia.Desktop/AppBuilderDesktopExtensions.cs
+++ src/Avalonia.Desktop/AppBuilderDesktopExtensions.cs
@@ -9,12 +9,12 @@ namespace Avalonia
         public static AppBuilder UsePlatformDetect(this AppBuilder builder)
         {
             // We don't have the ability to load every assembly right now, so we are
-            // stuck with manual configuration  here
+            // stuck with manual configuration here
             // Helpers are extracted to separate methods to take the advantage of the fact
             // that CLR doesn't try to load dependencies before referencing method is jitted
             // Additionally, by having a hard reference to each assembly,
             // we verify that the assemblies are in the final .deps.json file
-            //  so .NET Core knows where to load the assemblies from,.
+            //  so .NET Core knows where to load the assemblies from.
             if (OperatingSystemEx.IsWindows())
             {
                 LoadWin32(builder);
