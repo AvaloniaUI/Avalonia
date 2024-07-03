913d9c9c998cb2decd8aecd4d6f3c57b68c8ec36
rabbitism <popmessiah@hotmail.com>
2024-07-03 16:00:21 +0800

    feat: add default ctor for compatibility check.

--- src/Avalonia.Base/Controls/ResourcesChangedEventArgs.cs
+++ src/Avalonia.Base/Controls/ResourcesChangedEventArgs.cs
@@ -10,18 +10,23 @@ namespace Avalonia.Controls
         /// <summary>
         /// Get the empty event args with <see cref="Key"/> as null.
         /// </summary>
-        public new static readonly ResourcesChangedEventArgs Empty = new ResourcesChangedEventArgs();
+        public new static readonly ResourcesChangedEventArgs Empty = new();
         
         /// <summary>
         /// Get the key of changed resource. Key is null if event is raised not for a particular resource change, such as attaching or owner changing.
         /// </summary>
-        public object? Key { get; } 
+        public object? Key { get; }
+
+        /// <summary>
+        /// Create an empty <see cref="ResourcesChangedEventArgs"/>.
+        /// </summary>
+        public ResourcesChangedEventArgs() { }
         
         /// <summary>
         /// Create an instance of <see cref="ResourcesChangedEventArgs"/> with the specified key.
         /// </summary>
         /// <param name="key">The key of changed resource. Key can be null if event is not raised for a particular resource. </param>
-        public ResourcesChangedEventArgs(object? key = null)
+        public ResourcesChangedEventArgs(object? key)
         {
             Key = key;
         }
