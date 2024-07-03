ea8eccb8a558e96f58b9abf368cfbeebcdee12ee
rabbitism <popmessiah@hotmail.com>
2024-07-03 11:06:35 +0800

    feat: add resource key to ResourcesChangedEventArgs

--- src/Avalonia.Base/Controls/ResourcesChangedEventArgs.cs
+++ src/Avalonia.Base/Controls/ResourcesChangedEventArgs.cs
@@ -2,8 +2,30 @@
 
 namespace Avalonia.Controls
 {
+    /// <summary>
+    /// Represents the event args for the ResourcesChanged event. 
+    /// </summary>
     public class ResourcesChangedEventArgs : EventArgs
     {
-        public static new readonly ResourcesChangedEventArgs Empty = new ResourcesChangedEventArgs();
+        /// <summary>
+        /// Get the empty event args with <see cref="Key"/> as null.
+        /// </summary>
+        public new static readonly ResourcesChangedEventArgs Empty = new ResourcesChangedEventArgs();
+        
+        /// <summary>
+        /// Get the key of changed resource. Key is null if event is raised not for a particular resource change, such as attaching or owner changing.
+        /// </summary>
+        public object? Key { get; } 
+        
+        /// <summary>
+        /// Create an instance of <see cref="ResourcesChangedEventArgs"/> with the specified key.
+        /// </summary>
+        /// <param name="key">The key of changed resource. Key can be null if event is not raised for a particular resource. </param>
+        public ResourcesChangedEventArgs(object? key = null)
+        {
+            Key = key;
+        }
+
+        
     }
 }
