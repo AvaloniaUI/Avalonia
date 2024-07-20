0e869730a389d2266f6ed2e30809adffcb9c1d28
Steven Kirk <grokys@users.noreply.github.com>
2024-07-20 23:15:49 +0200

    Fix context menu keyboard selection. (#16354)
    When opening a context menu and pressing the "arrow down" key, the first menu item was not selected. This issue was introduced by #11287. The fix is to make the `ContextMenu` focusable.
    
    Added an integration test to try to prevent regressions.
    
    Co-authored-by: Max Katz <maxkatz6@outlook.com>

--- tests/Avalonia.IntegrationTests.Appium/ElementExtensions.cs
+++ tests/Avalonia.IntegrationTests.Appium/ElementExtensions.cs
@@ -91,8 +91,7 @@ namespace Avalonia.IntegrationTests.Appium
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                var active = element.WrappedDriver.SwitchTo().ActiveElement() as AppiumWebElement;
-                return element.Id == active?.Id;
+                return element.GetAttribute("HasKeyboardFocus") == "True";
             }
             else
             {
