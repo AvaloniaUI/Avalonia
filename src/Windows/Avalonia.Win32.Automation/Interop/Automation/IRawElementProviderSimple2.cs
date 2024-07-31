e4cb94338329d141222154cc38b4e2cc0ff5a663
Max Katz <maxkatz6@outlook.com>
2024-07-30 21:17:07 -0700

    Fix IRawElementProviderSimple2 definition on legacy COM interop

--- src/Windows/Avalonia.Win32.Automation/Interop/Automation/IRawElementProviderSimple2.cs
+++ src/Windows/Avalonia.Win32.Automation/Interop/Automation/IRawElementProviderSimple2.cs
@@ -10,8 +10,18 @@ namespace Avalonia.Win32.Interop.Automation
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 #endif
     [Guid("a0a839a9-8da1-4a82-806a-8e0d44e79f56")]
-    internal unsafe partial interface IRawElementProviderSimple2 : IRawElementProviderSimple
+    internal partial interface IRawElementProviderSimple2 : IRawElementProviderSimple
     {
+#if !NET8_0_OR_GREATER
+        // Hack for the legacy COM interop
+        // See https://learn.microsoft.com/en-us/dotnet/standard/native-interop/comwrappers-source-generation#derived-interfaces
+        new ProviderOptions GetProviderOptions();
+        [return: MarshalAs(UnmanagedType.Interface)]
+        new object? GetPatternProvider(int patternId);
+        [return: MarshalAs(UnmanagedType.Struct)]
+        new object? GetPropertyValue(int propertyId);
+        new IRawElementProviderSimple? HostRawElementProvider();
+#endif
         void ShowContextMenu();
     }
 }
