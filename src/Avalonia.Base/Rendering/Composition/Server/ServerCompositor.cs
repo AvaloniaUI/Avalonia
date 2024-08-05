3e61cdf10ba29fcc9e252023ede091595da153dc
Nikita Tsukanov <keks9n@gmail.com>
2024-08-06 04:25:18 +0600

    Added Compositor.CreateCompositionVisualSnapshot API (#16599)
    * Added Compositor.CreateCompositionVisualSnapshot API
    
    * Hurr durr api compat in [Unstable] interface
    
    ---------
    
    Co-authored-by: Max Katz <maxkatz6@outlook.com>
    #Conflicts:
    #	api/Avalonia.Skia.nupkg.xml
    #	samples/ControlCatalog/Pages/OpenGlPage.xaml
    #	samples/ControlCatalog/Pages/OpenGlPage.xaml.cs

--- src/Avalonia.Base/Rendering/Composition/Server/ServerCompositor.cs
+++ src/Avalonia.Base/Rendering/Composition/Server/ServerCompositor.cs
@@ -23,6 +23,7 @@ namespace Avalonia.Rendering.Composition.Server
 
         private readonly Queue<CompositionBatch> _batches = new Queue<CompositionBatch>();
         private readonly Queue<Action> _receivedJobQueue = new();
+        private readonly Queue<Action> _receivedPostTargetJobQueue = new();
         public long LastBatchId { get; private set; }
         public Stopwatch Clock { get; } = Stopwatch.StartNew();
         public TimeSpan ServerNow { get; private set; }
@@ -38,6 +39,8 @@ namespace Avalonia.Rendering.Composition.Server
         internal static readonly object RenderThreadDisposeStartMarker = new();
         internal static readonly object RenderThreadJobsStartMarker = new();
         internal static readonly object RenderThreadJobsEndMarker = new();
+        internal static readonly object RenderThreadPostTargetJobsStartMarker = new();
+        internal static readonly object RenderThreadPostTargetJobsEndMarker = new();
         public CompositionOptions Options { get; }
 
         public ServerCompositor(IRenderLoop renderLoop, IPlatformGraphics? platformGraphics,
@@ -83,7 +86,12 @@ namespace Avalonia.Rendering.Composition.Server
                         var readObject = stream.ReadObject();
                         if (readObject == RenderThreadJobsStartMarker)
                         {
-                            ReadServerJobs(stream);
+                            ReadServerJobs(stream, _receivedJobQueue, RenderThreadJobsEndMarker);
+                            continue;
+                        }
+                        if (readObject == RenderThreadPostTargetJobsStartMarker)
+                        {
+                            ReadServerJobs(stream, _receivedPostTargetJobQueue, RenderThreadPostTargetJobsEndMarker);
                             continue;
                         }
 
@@ -111,11 +119,11 @@ namespace Avalonia.Rendering.Composition.Server
             }
         }
 
-        void ReadServerJobs(BatchStreamReader reader)
+        void ReadServerJobs(BatchStreamReader reader, Queue<Action> queue, object endMarker)
         {
             object? readObject;
-            while ((readObject = reader.ReadObject()) != RenderThreadJobsEndMarker)
-                _receivedJobQueue.Enqueue((Action)readObject!);
+            while ((readObject = reader.ReadObject()) != endMarker)
+                queue.Enqueue((Action)readObject!);
         }
 
         void ReadDisposeJobs(BatchStreamReader reader)
@@ -128,12 +136,12 @@ namespace Avalonia.Rendering.Composition.Server
             }
         }
 
-        void ExecuteServerJobs()
+        void ExecuteServerJobs(Queue<Action> queue)
         {
-            while(_receivedJobQueue.Count > 0)
+            while(queue.Count > 0)
                 try
                 {
-                    _receivedJobQueue.Dequeue()();
+                    queue.Dequeue()();
                 }
                 catch
                 {
@@ -224,9 +232,10 @@ namespace Avalonia.Rendering.Composition.Server
             try
             {
                 RenderInterface.EnsureValidBackendContext();
-                ExecuteServerJobs();
+                ExecuteServerJobs(_receivedJobQueue);
                 foreach (var t in _activeTargets)
                     t.Render();
+                ExecuteServerJobs(_receivedPostTargetJobQueue);
             }
             catch (Exception e) when(RT_OnContextLostExceptionFilterObserver(e) && catchExceptions)
             {
