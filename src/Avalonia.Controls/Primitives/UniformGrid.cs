9a2d0fc317c3c4cab8e29b87e6047d5a2884a813
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2024-08-07 01:24:39 +0800

    Port UniformGrid from WPF

--- src/Avalonia.Controls/Primitives/UniformGrid.cs
+++ src/Avalonia.Controls/Primitives/UniformGrid.cs
@@ -60,105 +60,151 @@ namespace Avalonia.Controls.Primitives
             set => SetValue(FirstColumnProperty, value);
         }
 
-        protected override Size MeasureOverride(Size availableSize)
+        /// <summary>
+        /// Compute the desired size of this UniformGrid by measuring all of the
+        /// children with a constraint equal to a cell's portion of the given
+        /// constraint (e.g. for a 2 x 4 grid, the child constraint would be
+        /// constraint.Width*0.5 x constraint.Height*0.25).  The maximum child
+        /// width and maximum child height are tracked, and then the desired size
+        /// is computed by multiplying these maximums by the row and column count
+        /// (e.g. for a 2 x 4 grid, the desired size for the UniformGrid would be
+        /// maxChildDesiredWidth*2 x maxChildDesiredHeight*4).
+        /// </summary>
+        /// <param name="constraint">Constraint</param>
+        /// <returns>Desired size</returns>
+        protected override Size MeasureOverride(Size constraint)
         {
-            UpdateRowsAndColumns();
+            UpdateComputedValues();
 
-            var maxWidth = 0d;
-            var maxHeight = 0d;
+            Size childConstraint = new Size(constraint.Width / _columns, constraint.Height / _rows);
+            double maxChildDesiredWidth = 0.0;
+            double maxChildDesiredHeight = 0.0;
 
-            var childAvailableSize = new Size(availableSize.Width / _columns, availableSize.Height / _rows);
-
-            foreach (var child in Children)
+            //  Measure each child, keeping track of maximum desired width and height.
+            for (int i = 0, count = Children.Count; i < count; ++i)
             {
-                child.Measure(childAvailableSize);
+                var child = Children[i];
 
-                if (child.DesiredSize.Width > maxWidth)
+                // Measure the child.
+                child.Measure(childConstraint);
+                Size childDesiredSize = child.DesiredSize;
+
+                if (maxChildDesiredWidth < childDesiredSize.Width)
                 {
-                    maxWidth = child.DesiredSize.Width;
+                    maxChildDesiredWidth = childDesiredSize.Width;
                 }
 
-                if (child.DesiredSize.Height > maxHeight)
+                if (maxChildDesiredHeight < childDesiredSize.Height)
                 {
-                    maxHeight = child.DesiredSize.Height;
+                    maxChildDesiredHeight = childDesiredSize.Height;
                 }
             }
 
-            return new Size(maxWidth * _columns, maxHeight * _rows);
+            return new Size((maxChildDesiredWidth * _columns),(maxChildDesiredHeight * _rows));
         }
 
-        protected override Size ArrangeOverride(Size finalSize)
+        /// <summary>
+        /// Arrange the children of this UniformGrid by distributing space evenly 
+        /// among all the children, making each child the size equal to a cell's
+        /// portion of the given arrangeSize (e.g. for a 2 x 4 grid, the child size
+        /// would be arrangeSize*0.5 x arrangeSize*0.25)
+        /// </summary>
+        /// <param name="arrangeSize">Arrange size</param>
+        protected override Size ArrangeOverride(Size arrangeSize)
         {
-            var x = FirstColumn;
-            var y = 0;
+            double childBoundsX = 0; 
+            double childBoundsY = 0;
+            double childBoundsWidth = arrangeSize.Width / _columns;
+            double childBoundsHeight = arrangeSize.Height / _rows;
+            
+            double xStep = childBoundsWidth;
+            double xBound = arrangeSize.Width - 1.0;
+            
+            childBoundsX += childBoundsWidth * FirstColumn;
 
-            var width = finalSize.Width / _columns;
-            var height = finalSize.Height / _rows;
-
-            foreach (var child in Children)
+            // Arrange and Position each child to the same cell size
+            for (int i = 0, count = Children.Count; i < count; ++i)
             {
-                if (!child.IsVisible)
+                var child = Children[i];
+
+                child.Arrange(new Rect(childBoundsX, childBoundsY, childBoundsWidth, childBoundsHeight));
+
+                // only advance to the next grid cell if the child was not collapsed
+                if (child.IsEffectivelyVisible)
                 {
-                    continue;
-                }
-
-                child.Arrange(new Rect(x * width, y * height, width, height));
-
-                x++;
-
-                if (x >= _columns)
-                {
-                    x = 0;
-                    y++;
+                    childBoundsX += xStep;
+                    if (childBoundsX >= xBound)
+                    {
+                        childBoundsY += childBoundsHeight;
+                        childBoundsX = 0;
+                    }
                 }
             }
 
-            return finalSize;
-        }
+            return arrangeSize;
+        } 
 
-        private void UpdateRowsAndColumns()
+        /// <summary>
+        /// If either Rows or Columns are set to 0, then dynamically compute these
+        /// values based on the actual number of non-collapsed children.
+        ///
+        /// In the case when both Rows and Columns are set to 0, then make Rows 
+        /// and Columns be equal, thus laying out in a square grid.
+        /// </summary>
+        private void UpdateComputedValues()
         {
-            _rows = Rows;
             _columns = Columns;
+            _rows = Rows;
 
-            if (FirstColumn >= Columns)
+            //parameter checking. 
+            if (FirstColumn >= _columns)
             {
                 SetCurrentValue(FirstColumnProperty, 0);
             }
 
-            var itemCount = FirstColumn;
-
-            foreach (var child in Children)
+            if ((_rows == 0) || (_columns == 0))
             {
-                if (child.IsVisible)
-                {
-                    itemCount++;
-                }
-            }
+                int nonCollapsedCount = 0;
 
-            if (_rows == 0)
-            {
-                if (_columns == 0)
+                // First compute the actual # of non-collapsed children to be laid out
+                for (int i = 0, count = Children.Count; i < count; ++i)
                 {
-                    _rows = _columns = (int)Math.Ceiling(Math.Sqrt(itemCount));
-                }
-                else
-                {
-                    _rows = Math.DivRem(itemCount, _columns, out int rem);
-
-                    if (rem != 0)
+                    var child = Children[i];
+                    if (child.IsEffectivelyVisible)
                     {
-                        _rows++;
+                        nonCollapsedCount++;
                     }
                 }
-            }
-            else if (_columns == 0)
-            {
-                _columns = Math.DivRem(itemCount, _rows, out int rem);
 
-                if (rem != 0)
+                // to ensure that we have at leat one row & column, make sure
+                // that nonCollapsedCount is at least 1
+                if (nonCollapsedCount == 0)
                 {
-                    _columns++;
+                    nonCollapsedCount = 1;
+                }
+
+                if (_rows == 0)
+                {
+                    if (_columns > 0)
+                    {
+                        // take FirstColumn into account, because it should really affect the result
+                        _rows = (nonCollapsedCount + FirstColumn + (_columns - 1)) / _columns;
+                    }
+                    else
+                    {
+                        // both rows and columns are unset -- lay out in a square
+                        _rows = (int)Math.Sqrt(nonCollapsedCount);
+                        if ((_rows * _rows) < nonCollapsedCount)
+                        {
+                            _rows++;
+                        }
+                        _columns = _rows;
+                    }
+                }
+                else if (_columns == 0)
+                {
+                    // guaranteed that _rows is not 0, because we're in the else clause of the check for _rows == 0
+                    _columns = (nonCollapsedCount + (_rows - 1)) / _rows;
                 }
             }
         }
