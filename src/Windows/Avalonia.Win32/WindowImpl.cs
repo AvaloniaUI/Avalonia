a847b8f92d46e96719f3283d52493f7c8676b3cc
Emmanuel Hansen <emmausssss@gmail.com>
2024-08-07 07:37:56 +0000

    remove forced position setting

--- src/Windows/Avalonia.Win32/WindowImpl.cs
+++ src/Windows/Avalonia.Win32/WindowImpl.cs
@@ -1451,8 +1451,6 @@ namespace Avalonia.Win32
 
             if (!_isFullScreenActive && ((oldProperties.Decorations != newProperties.Decorations) || forceChanges))
             {
-                var style = GetStyle();
-
                 var margin = newProperties.Decorations == SystemDecorations.BorderOnly ? 1 : 0;
 
                 var margins = new MARGINS
@@ -1467,10 +1465,9 @@ namespace Avalonia.Win32
 
                 if (_shown || forceChanges)
                 {
-                    GetWindowRect(_hwnd, out var rcWindow);
-
-                    SetWindowPos(_hwnd, IntPtr.Zero, rcWindow.left, rcWindow.top, 0 ,0,
-                        SetWindowPosFlags.SWP_NOZORDER | SetWindowPosFlags.SWP_NOACTIVATE | SetWindowPosFlags.SWP_NOSIZE |
+                    SetWindowPos(_hwnd, IntPtr.Zero, 0, 0, 0 ,0,
+                        SetWindowPosFlags.SWP_NOZORDER | SetWindowPosFlags.SWP_NOACTIVATE |
+                        SetWindowPosFlags.SWP_NOSIZE | SetWindowPosFlags.SWP_NOMOVE |
                         SetWindowPosFlags.SWP_FRAMECHANGED);
                 }
             }
