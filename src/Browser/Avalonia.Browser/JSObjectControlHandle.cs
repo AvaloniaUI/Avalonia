a607c6855aa8f3c99c738d1a7c8fd25933abbec6
Max Katz <maxkatz6@outlook.com>
2024-08-09 20:19:39 -0700

    Browser screen implementation, aditionally add JSObjectPlatformHandle

--- src/Browser/Avalonia.Browser/JSObjectControlHandle.cs
+++ src/Browser/Avalonia.Browser/JSObjectControlHandle.cs
@@ -1,28 +1,37 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.JavaScript;
 
 using Avalonia.Controls.Platform;
+using Avalonia.Platform;
 
 namespace Avalonia.Browser;
 
-public class JSObjectControlHandle : INativeControlHostDestroyableControlHandle
+public class JSObjectPlatformHandle : PlatformHandle
 {
     internal const string ElementReferenceDescriptor = "JSObject";
 
-    public JSObjectControlHandle(JSObject reference)
+    public JSObjectPlatformHandle(JSObject reference) : base(GetJsHandle(reference), ElementReferenceDescriptor)
     {
         Object = reference;
     }
 
     public JSObject Object { get; }
 
-    public IntPtr Handle => throw new NotSupportedException();
+    // https://github.com/dotnet/runtime/blob/v8.0.7/src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/JSObject.References.cs#L12
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "JSHandle")]
+    internal static extern ref nint GetJsHandle(JSObject @this);
+} 
 
-    public string? HandleDescriptor => ElementReferenceDescriptor;
+public class JSObjectControlHandle : JSObjectPlatformHandle, INativeControlHostDestroyableControlHandle
+{
+    public JSObjectControlHandle(JSObject reference) : base(reference)
+    {
+    }
 
     public void Destroy()
     {
-        if (Object is JSObject inProcess && !inProcess.IsDisposed)
+        if (Object is { } inProcess && !inProcess.IsDisposed)
         {
             inProcess.Dispose();
         }
