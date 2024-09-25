a35f4b8e7b27f7b3e018cef5ad35a4b9a5c70ed0
lindexi <lindexi_gd@163.com>
2024-09-25 17:47:50 +0800

    Try fix comment.

--- src/Avalonia.X11/LibC.cs
+++ src/Avalonia.X11/LibC.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Avalonia.X11;
 
@@ -12,15 +8,16 @@ internal static class LibC
     [DllImport("libc", SetLastError = true)]
     public static extern IntPtr shmat(int shmid, IntPtr shmaddr, int shmflg);
 
-    //    #define IPC_CREAT	01000		/* create key if key does not exist */
-    // #define IPC_PRIVATE	((key_t) 0)	/* private key */
-
+    /// <summary>
+    /// create key if key does not exist
+    /// </summary>
     public const int IPC_CREAT = 01000;
+
+    /// <summary>
+    /// private key
+    /// </summary>
     public const int IPC_PRIVATE = 0;
 
-    /*
-     * int shmget(key_t key, size_t size, int shmflg);
-     */
     [DllImport("libc", SetLastError = true)]
     public static extern int shmget(int key, IntPtr size, int shmflg);
 }
