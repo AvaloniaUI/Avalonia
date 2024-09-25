5be7d3b86b60a09a74023e2d816ef04b48f76b72
lindexi <lindexi_gd@163.com>
2024-09-25 17:30:10 +0800

    Begin define some of the type to X11 XShm
    https://github.com/AvaloniaUI/Avalonia/discussions/16690

---
+++ src/Avalonia.X11/XShmExtensions/X11ShmImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.X11.XShmExtensions;
+internal class X11ShmImage
+{
+}
+
+internal class X11ShmImageManager
+{
+
+}
+
+class DeferredDisplayEvents
+{
+
+}
+
+internal class X11ShmImageSwapchain
+{
+
+}
