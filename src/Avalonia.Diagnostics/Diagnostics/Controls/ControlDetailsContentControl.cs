6bb9c29a2d10c5c554e1ada9609498645436e118
pavelovcharov <1357165+pavelovcharov@users.noreply.github.com>
2024-11-06 22:13:56 +0300

    DevTools should not add visual elements to its logical tree

---
+++ src/Avalonia.Diagnostics/Diagnostics/Controls/ControlDetailsContentControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace Avalonia.Diagnostics.Controls
+{
+    internal class ControlDetailsContentControl : ContentControl
+    {
+        protected override Type StyleKeyOverride => typeof(ContentControl);
+
+        internal override bool AddContentToLogicalTree => false;
+    }
+}
