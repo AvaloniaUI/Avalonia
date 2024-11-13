415efe349ed4c9c62e53f96f64833dc168b7c821
Baochen Wang <wangbaochen@live.com>
2024-11-13 09:12:33 +0800

    fix automation name

--- src/Avalonia.Controls/Automation/Peers/UserControlAutomationPeer.cs
+++ src/Avalonia.Controls/Automation/Peers/UserControlAutomationPeer.cs
@@ -1,3 +1,4 @@
+using Avalonia.Automation;
 using Avalonia.Automation.Peers;
 
 namespace Avalonia.Controls.Automation.Peers;
@@ -10,4 +11,16 @@ public class UserControlAutomationPeer : ContentControlAutomationPeer
     }
     
     protected override AutomationControlType GetAutomationControlTypeCore() => AutomationControlType.Custom;
+
+    protected override string? GetNameCore()
+    {
+        var result = AutomationProperties.GetName(Owner);
+
+        if (string.IsNullOrWhiteSpace(result) && GetLabeledBy() is AutomationPeer labeledBy)
+        {
+            result = labeledBy.GetName();
+        }
+
+        return result;
+    }
 }
