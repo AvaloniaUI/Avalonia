d5719f279576b2dc5399ad037335b989ff433a0f
Max Katz <maxkatz6@outlook.com>
2024-12-24 02:44:34 -0800

    Impl DispatcherTaskScheduler

---
+++ src/Avalonia.Base/Threading/DispatcherTaskScheduler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Avalonia.Threading;
+
+internal class DispatcherTaskScheduler : TaskScheduler
+{
+    private static DispatcherTaskScheduler? s_uiThread;
+    public static DispatcherTaskScheduler UIThread => s_uiThread ??= new DispatcherTaskScheduler(Dispatcher.UIThread); 
+
+    private readonly Dispatcher _dispatcher;
+    private readonly SendOrPostCallback _postCallback;
+
+    public DispatcherTaskScheduler(Dispatcher dispatcher)
+    {
+        _dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
+        _postCallback = QueueTaskCallback;
+    }
+
+    protected override IEnumerable<Task>? GetScheduledTasks() => null;
+
+    protected override void QueueTask(Task task)
+    {
+        _dispatcher.Post(_postCallback, task);
+    }
+
+    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+    {
+        if (!_dispatcher.CheckAccess())
+            return false;
+
+        return TryExecuteTask(task);
+    }
+
+    private void QueueTaskCallback(object? state) => TryExecuteTask((Task)state!);
+}
