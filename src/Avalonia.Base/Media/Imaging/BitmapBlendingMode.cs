768eabbdab0ffe8f66674904f978bcd4fb566cb3
Leo Bottaro <github@leobottaro.com>
2025-01-07 03:12:10 -0300

    Add missing Bitmap Blend Modes supported by Skia

--- src/Avalonia.Base/Media/Imaging/BitmapBlendingMode.cs
+++ src/Avalonia.Base/Media/Imaging/BitmapBlendingMode.cs
@@ -54,6 +54,70 @@ namespace Avalonia.Media.Imaging
         /// <summary>
         /// Display the sum of the source image and destination image.
         /// </summary>
-        Plus
+        Plus,
+        /// <summary>
+        /// Multiplies all components (alpha and color).
+        /// </summary>
+        Modulate,
+        /// <summary>
+        /// Multiplies the complements of the backdrop and source color values, then complements the result.
+        /// </summary>
+        Screen,
+        /// <summary>
+        /// Multiplies or screens the colors, depending on the backdrop color value.
+        /// </summary>
+        Overlay,
+        /// <summary>
+        /// Selects the darker of the backdrop and source colors.
+        /// </summary>
+        Darken,
+        /// <summary>
+        /// Selects the lighter of the backdrop and source colors.
+        /// </summary>
+        Lighten,
+        /// <summary>
+        /// Darkens the backdrop color to reflect the source color.
+        /// </summary>
+        ColorDodge,
+        /// <summary>
+        /// Multiplies or screens the colors, depending on the source color value.
+        /// </summary>
+        ColorBurn,
+        /// <summary>
+        /// Darkens or lightens the colors, depending on the source color value.
+        /// </summary>
+        HardLight,
+        /// <summary>
+        /// Subtracts the darker of the two constituent colors from the lighter color.
+        /// </summary>
+        SoftLight,
+        /// <summary>
+        /// Produces an effect similar to that of the Difference mode but lower in contrast.
+        /// </summary>
+        Difference,
+        /// <summary>
+        /// The source color is multiplied by the destination color and replaces the destination
+        /// </summary>
+        Exclusion,
+        /// <summary>
+        /// Creates a color with the hue of the source color and the saturation and luminosity of the backdrop color.
+        /// </summary>
+        Multiply,
+        /// <summary>
+        /// Creates a color with the hue of the source color and the saturation and luminosity of the backdrop color.
+        /// </summary>
+        Hue,
+        /// <summary>
+        /// Creates a color with the saturation of the source color and the hue and luminosity of the backdrop color.
+        /// </summary>
+        Saturation,
+        /// <summary>
+        /// Creates a color with the hue and saturation of the source color and the luminosity of the backdrop color.
+        /// </summary>
+        Color,
+        /// <summary>
+        /// Creates a color with the luminosity of the source color and the hue and saturation of the backdrop color.
+        /// </summary>
+        Luminosity
     }
 }
