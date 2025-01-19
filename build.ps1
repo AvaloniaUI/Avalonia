3ac84418c42a42f7c9e9e1a1091ae935a96309c4
timunie <tim.uniewski@gmail.com>
2025-01-19 22:59:20 +0100

    WIP

--- build.ps1
+++ build.ps1
@@ -68,4 +68,4 @@ Write-Output "Microsoft (R) .NET SDK version $(& $env:DOTNET_EXE --version)"
 
 ExecSafe { & $env:DOTNET_EXE build $BuildProjectFile /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary --verbosity quiet }
 ExecSafe { & $env:DOTNET_EXE run --project $BuildProjectFile --no-build -- $BuildArguments }
-ExecSafe { & $env:DOTNET_EXE build $BuildDocumentationFile}
+# ExecSafe { & $env:DOTNET_EXE build $BuildDocumentationFile}
