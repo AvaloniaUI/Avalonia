f99ea3e70c8e59670d72f1f264df45c7d00b0ae2
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-01-21 10:05:26 -0600

    Implement iOS VoiceOver support

---
+++ src/iOS/Avalonia.iOS/AvaloniaView.Automation.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using UIKit;
+
+namespace Avalonia.iOS
+{
+    public partial class AvaloniaView : IUIAccessibilityContainer
+    {
+        private readonly AutomationPeerWrapper _accessWrapper;
+
+        public nint AccessibilityElementCount() => 
+            _accessWrapper.AccessibilityElementCount();
+
+        public NSObject? GetAccessibilityElementAt(nint index) => 
+            _accessWrapper.GetAccessibilityElementAt(index);
+
+        public nint GetIndexOfAccessibilityElement(NSObject element) => 
+            _accessWrapper.GetIndexOfAccessibilityElement(element);
+    }
+}
