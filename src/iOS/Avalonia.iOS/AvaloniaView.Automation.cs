7edd7ee98f7db103f881a06ef3dca0c06412be64
IsaMorphic <me@isamorphic.dev>
2025-01-21 15:13:20 -0600

    Some investigation (it is not working)

--- src/iOS/Avalonia.iOS/AvaloniaView.Automation.cs
+++ src/iOS/Avalonia.iOS/AvaloniaView.Automation.cs
@@ -6,13 +6,23 @@ namespace Avalonia.iOS
     public partial class AvaloniaView : IUIAccessibilityContainer
     {
         private readonly AutomationPeerWrapper _accessWrapper;
+        
+        [Export("accessibilityContainerType")]
+        public UIAccessibilityContainerType AccessibilityContainerType 
+        {
+            get => _accessWrapper.AccessibilityContainerType;
+            set => _accessWrapper.AccessibilityContainerType = value;
+        }
 
+        [Export("accessibilityElementCount")]
         public nint AccessibilityElementCount() => 
             _accessWrapper.AccessibilityElementCount();
 
+        [Export("accessibilityElementAtIndex:")]
         public NSObject? GetAccessibilityElementAt(nint index) => 
             _accessWrapper.GetAccessibilityElementAt(index);
 
+        [Export("indexOfAccessibilityElement:")]
         public nint GetIndexOfAccessibilityElement(NSObject element) => 
             _accessWrapper.GetIndexOfAccessibilityElement(element);
     }
