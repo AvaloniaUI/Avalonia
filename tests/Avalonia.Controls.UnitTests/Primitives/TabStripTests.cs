49efe97f1f6dad93d6bca22fb74089a8bae7c46a
Niek Deibus <nickdeibus@outlook.de>
2025-02-12 16:30:26 +0100

    Replaced failing selection test for TabStrip + removed unneded one

--- tests/Avalonia.Controls.UnitTests/Primitives/TabStripTests.cs
+++ tests/Avalonia.Controls.UnitTests/Primitives/TabStripTests.cs
@@ -1,10 +1,6 @@
-using System.Collections.ObjectModel;
-using System.Linq;
-using Moq;
 using Avalonia.Controls.Presenters;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
-using Avalonia.LogicalTree;
 using Xunit;
 
 namespace Avalonia.Controls.UnitTests.Primitives
@@ -12,7 +8,7 @@ namespace Avalonia.Controls.UnitTests.Primitives
     public class TabStripTests
     {
         [Fact]
-        public void First_Tab_Should_Be_Selected_By_Default()
+        public void First_Tab_Should_Not_Be_Selected_By_Default()
         {
             var target = new TabStrip
             {
@@ -32,8 +28,8 @@ namespace Avalonia.Controls.UnitTests.Primitives
 
             target.ApplyTemplate();
 
-            Assert.Equal(0, target.SelectedIndex);
-            Assert.Same(target.Items[0], target.SelectedItem);
+            Assert.Equal(-1, target.SelectedIndex);
+            Assert.Same(null, target.SelectedItem);
         }
 
         [Fact]
@@ -60,39 +56,6 @@ namespace Avalonia.Controls.UnitTests.Primitives
 
             Assert.Equal(1, target.SelectedIndex);
             Assert.Same(target.Items[1], target.SelectedItem);
-        }
-
-        [Fact]
-        public void Removing_Selected_Should_Select_First()
-        {
-            var target = new TabStrip
-            {
-                Template = new FuncControlTemplate<TabStrip>(CreateTabStripTemplate),
-                Items =
-                {
-                    new TabItem
-                    {
-                        Name = "first"
-                    },
-                    new TabItem
-                    {
-                        Name = "second"
-                    },
-                    new TabItem
-                    {
-                        Name = "3rd"
-                    },
-                }
-            };
-
-            target.ApplyTemplate();
-            target.SelectedItem = target.Items[1];
-            Assert.Same(target.Items[1], target.SelectedItem);
-            target.Items.RemoveAt(1);
-
-            Assert.Equal(0, target.SelectedIndex);
-            Assert.Same(target.Items[0], target.SelectedItem);
-            Assert.Same("first", ((TabItem)target.SelectedItem).Name);
         }
 
         private Control CreateTabStripTemplate(TabStrip parent, INameScope scope)
