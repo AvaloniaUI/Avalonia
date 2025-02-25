b1df81c1338a1267f91074a152a91f891a6b43fc
Max Katz <maxkatz6@outlook.com>
2025-02-25 02:22:23 -0800

    Add RaisingRoutedEvent activity

---
+++ src/Avalonia.Base/Diagnostics/Diagnostic.Activities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace Avalonia.Diagnostics;
+
+internal static partial class Diagnostic
+{
+    private static readonly ActivitySource s_diagnostic = new("Avalonia.Diagnostic.Source");
+}
