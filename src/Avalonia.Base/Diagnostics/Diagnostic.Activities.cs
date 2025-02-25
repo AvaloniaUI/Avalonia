4b97ee16b6d68d6bee756611ecfc333e6883421a
Max Katz <maxkatz6@outlook.com>
2025-02-25 02:22:57 -0800

    Add FindingResourceActivity activity

--- src/Avalonia.Base/Diagnostics/Diagnostic.Activities.cs
+++ src/Avalonia.Base/Diagnostics/Diagnostic.Activities.cs
@@ -8,4 +8,8 @@ namespace Avalonia.Diagnostics;
 internal static partial class Diagnostic
 {
     private static readonly ActivitySource s_diagnostic = new("Avalonia.Diagnostic.Source");
+
+    public static Activity? FindingResourceActivity() => s_diagnostic
+        .StartActivity("Avalonia.Controls.ResourceNode.FindResource");
+
 }
