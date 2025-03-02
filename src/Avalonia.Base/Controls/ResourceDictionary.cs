3e5d1fc538ad30b3fa4f25cca815be2d59e30d8b
Stefan Koell <stefan.koell@royalapps.com>
2025-03-02 12:42:04 +0100

    Implement AddOrUpdateRange method in ResourceDictionary

--- src/Avalonia.Base/Controls/ResourceDictionary.cs
+++ src/Avalonia.Base/Controls/ResourceDictionary.cs
@@ -13,6 +13,7 @@ namespace Avalonia.Controls
     /// </summary>
     public class ResourceDictionary : ResourceProvider, IResourceDictionary, IThemeVariantProvider
     {
+        private bool _suspendNotifications;
         private object? _lastDeferredItemKey;
         private Dictionary<object, object?>? _inner;
         private AvaloniaList<IResourceProvider>? _mergedDictionaries;
@@ -40,7 +41,7 @@ namespace Avalonia.Controls
             set
             {
                 Inner[key] = value;
-                Owner?.NotifyHostedResourcesChanged(ResourcesChangedEventArgs.Empty);
+                NotifyHostedResourcesChanged(Owner);
             }
         }
 
@@ -150,6 +151,21 @@ namespace Avalonia.Controls
         public void AddNotSharedDeferred(object key, IDeferredContent deferredContent)
             => Add(key, new NotSharedDeferredItem(deferredContent));
 
+        public void AddOrUpdateRange(IEnumerable<KeyValuePair<object, object?>> values)
+        {
+            _suspendNotifications = true;
+            try
+            {
+                foreach (var value in values)
+                    Inner[value.Key] = value.Value;
+            }
+            finally
+            {
+                _suspendNotifications = false;
+                NotifyHostedResourcesChanged(Owner);
+            }
+        }
+        
         public void Clear()
         {
             if (_inner?.Count > 0)
@@ -376,6 +392,13 @@ namespace Avalonia.Controls
             }
         }
 
+        private void NotifyHostedResourcesChanged(IResourceHost? owner)
+        {
+            if (_suspendNotifications)
+                return;
+            owner?.NotifyHostedResourcesChanged(ResourcesChangedEventArgs.Empty);            
+        }
+        
         private sealed class DeferredItem : IDeferredContent
         {
             private readonly Func<IServiceProvider?,object?> _factory;
