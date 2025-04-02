b09e0d56775a67e52b5b8eec68c6171f4991d28d
Julien Lebosquain <julien@lebosquain.net>
2025-04-02 21:28:06 +0200

    Implement TextSearch.TextBinding (#18405)
    * Implement TextSearch.TextBinding
    
    * Move AssignBinding to TextSearch.GetTextBinding

--- src/Avalonia.Controls/Presenters/ItemsPresenter.cs
+++ src/Avalonia.Controls/Presenters/ItemsPresenter.cs
@@ -197,44 +197,6 @@ namespace Avalonia.Controls.Presenters
             return Panel?.Children;
         }
         
-        internal static bool ControlMatchesTextSearch(Control control, string textSearchTerm)
-        {
-            if (control is AvaloniaObject ao && ao.IsSet(TextSearch.TextProperty))
-            {
-                var searchText = ao.GetValue(TextSearch.TextProperty);
-                
-                if (searchText?.StartsWith(textSearchTerm, StringComparison.OrdinalIgnoreCase) == true)
-                {
-                    return true;
-                }
-            }
-            return control is IContentControl cc && 
-                   cc.Content?.ToString()?.StartsWith(textSearchTerm, StringComparison.OrdinalIgnoreCase) == true;
-        }
-        
-        internal int GetIndexFromTextSearch(string textSearch)
-        {
-            if (Panel is VirtualizingPanel v)
-                return v.GetIndexFromTextSearch(textSearch);
-            return GetIndexFromTextSearch(ItemsControl?.Items, textSearch);
-        }
-
-        internal static int GetIndexFromTextSearch(IReadOnlyList<object?>? items, string textSearchTerm)
-        {
-            if (items is null)
-                return -1;
-            
-            for (var i = 0; i < items.Count; i++)
-            {
-                if (items[i] is Control c && ControlMatchesTextSearch(c, textSearchTerm)
-                    || items[i]?.ToString()?.StartsWith(textSearchTerm, StringComparison.OrdinalIgnoreCase) == true)
-                {
-                    return i;
-                }
-            }
-            return -1;
-        }
-
         internal int IndexFromContainer(Control container)
         {
             if (Panel is VirtualizingPanel v)
