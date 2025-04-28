35951cd0afa2842ab0109d9dcad9f03da8f7a7da
Simon Cropp <simon.cropp@gmail.com>
2025-04-28 14:41:11 +1000

    add ThemeVariantValues

---
+++ src/Headless/Avalonia.Headless.NUnit/ThemeVariantValuesAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using Avalonia.Styling;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace Avalonia.Headless.NUnit;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public sealed class ThemeVariantValuesAttribute : NUnitAttribute, IParameterDataSource
+{
+    static readonly object[] data = [ThemeVariant.Light, ThemeVariant.Dark];
+
+    IEnumerable IParameterDataSource.GetData(IParameterInfo parameter)
+    {
+        if (parameter.ParameterType == typeof(ThemeVariant))
+        {
+            return data;
+        }
+
+        throw new($"Parameter {parameter.ParameterInfo.Name} must be a ThemeVariant to use {nameof(ThemeVariantValuesAttribute)}.");
+    }
+}
---
+++ tests/Avalonia.Headless.NUnit.UnitTests/ThemeVariantAttributeTests.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Styling;
+
+namespace Avalonia.Headless.NUnit.UnitTests;
+
+public class ThemeVariantValuesAttributeTests
+{
+    [AvaloniaTest, Timeout(10000)]
+    public void ThemeVariantValuesTest([ThemeVariantValues] ThemeVariant theme)
+    {
+        Application.Current!.RequestedThemeVariant = theme;
+
+        var window = new Window();
+        window.Show();
+
+        Assert.AreEqual(theme, window.ActualThemeVariant);
+    }
+}
