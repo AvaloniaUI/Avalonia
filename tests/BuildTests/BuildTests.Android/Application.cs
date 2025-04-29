f02c20be6abf7a8c0cea0041bb4201ee4862af5f
Emmanuel Hansen <emmausssss@gmail.com>
2025-04-29 13:36:36 +0000

    fix tests

---
+++ tests/BuildTests/BuildTests.Android/Application.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Runtime;
+using Avalonia.Android;
+
+namespace BuildTests.Android
+{
+    [Application]
+    public class Application : AvaloniaAndroidApplication<App>
+    {
+        protected Application(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+    }
+}
