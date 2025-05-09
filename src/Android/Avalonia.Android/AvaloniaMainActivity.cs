306f37d518581b494bde765a8415c569310841b0
Emmanuel Hansen <emmausssss@gmail.com>
2025-05-09 08:30:01 -0400

    Move android app initialization to custom application class (#18756)
    * move android app initialization to custom application class
    
    * fix tests
    
    * update api suppression
    
    * address comments

--- src/Android/Avalonia.Android/AvaloniaMainActivity.cs
+++ src/Android/Avalonia.Android/AvaloniaMainActivity.cs
@@ -8,41 +8,18 @@ namespace Avalonia.Android;
 
 public class AvaloniaMainActivity : AvaloniaActivity
 {
-    private protected static SingleViewLifetime? Lifetime;
-
     private protected override void InitializeAvaloniaView(object? initialContent)
     {
-        // Android can run OnCreate + InitializeAvaloniaView multiple times per process lifetime.
-        // On each call we need to create new AvaloniaView, but we can't recreate Avalonia nor Avalonia controls.
-        // So, if lifetime was already created previously - recreate AvaloniaView.
-        // If not, initialize Avalonia, and create AvaloniaView inside of AfterSetup callback.
-        // We need this AfterSetup callback to match iOS/Browser behavior and ensure that view/toplevel is available in custom AfterSetup calls.
-        if (Lifetime is not null)
+        if (Application is IAndroidApplication application && application.Lifetime is { } lifetime)
         {
-            initialContent ??= Lifetime.MainView; 
+            initialContent ??= lifetime.MainView; 
 
             _view = new AvaloniaView(this) { Content = initialContent };
-            Lifetime.Activity = this;
+            lifetime.Activity = this;
         }
-        else
-        {
-            var builder = CreateAppBuilder();
-            builder = CustomizeAppBuilder(builder);
 
-            Lifetime = new SingleViewLifetime();
-            Lifetime.Activity = this;
- 
-            builder
-                .AfterApplicationSetup(_ =>
-                {
-                    _view = new AvaloniaView(this) { Content = initialContent };
-                })
-                .SetupWithLifetime(Lifetime);
-
-            // AfterPlatformServicesSetup should always be called. If it wasn't, we have an unusual problem.
-            if (_view is null)
-                throw new InvalidOperationException("Unknown error: AvaloniaView initialization has failed.");
-        }
+        if (_view is null)
+            throw new InvalidOperationException("Unknown error: AvaloniaView initialization has failed.");
 
         if (Avalonia.Application.Current?.TryGetFeature<IActivatableLifetime>()
             is AndroidActivatableLifetime activatableLifetime)
