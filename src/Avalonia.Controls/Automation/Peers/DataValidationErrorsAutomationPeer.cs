74e9b892e1f3aac0a088e1259cf78208de757fd5
Long Bui <long.bui@taitcommunications.com>
2025-06-18 16:11:07 +0700

    add missing file

---
+++ src/Avalonia.Controls/Automation/Peers/DataValidationErrorsAutomationPeer.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Linq;
+using Avalonia.Automation.Peers;
+
+namespace Avalonia.Controls.Automation.Peers
+{
+    public class DataValidationErrorsAutomationPeer(DataValidationErrors owner) : ContentControlAutomationPeer(owner)
+    {
+        protected override bool IsContentElementCore() => true;
+        protected override bool IsControlElementCore() => true;
+
+        protected override string? GetNameCore()
+        {
+            return owner.Owner?.Name;
+        }
+
+        protected override string? GetHelpTextCore()
+        {
+            if (owner.Owner is not null)
+            {
+                var errors = DataValidationErrors.GetErrors(owner.Owner);
+                var errorsStringList = errors?.OfType<string>().Select(x => x);
+                return errorsStringList != null ? string.Join("\n", errorsStringList.ToArray()) : string.Empty;
+            }
+
+            return string.Empty;
+        }
+
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.Custom;
+        }
+    }
+}
