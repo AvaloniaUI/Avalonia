e5b5cb2245330f596aea01422a3473e0dd9478a7
Steven Kirk <grokys@users.noreply.github.com>
2025-06-24 16:46:07 +0200

    Remove InstancedBinding from NativeMenuBarPresenter.

--- src/Avalonia.Controls/NativeMenuBarPresenter.cs
+++ src/Avalonia.Controls/NativeMenuBarPresenter.cs
@@ -25,6 +25,7 @@ internal class NativeMenuBarPresenter : Menu
                     nativeItem.GetObservable(NativeMenuItem.HeaderProperty).ToBinding(),
                 [!MenuItem.IconProperty] = nativeItem.GetObservable(NativeMenuItem.IconProperty)
                     .Select(i => i is { } bitmap ? new Image { Source = bitmap } : null).ToBinding(),
+                [!!MenuItem.IsCheckedProperty] = nativeItem[!!NativeMenuItem.IsCheckedProperty],
                 [!MenuItem.IsEnabledProperty] = nativeItem.GetObservable(NativeMenuItem.IsEnabledProperty).ToBinding(),
                 [!MenuItem.IsVisibleProperty] = nativeItem.GetObservable(NativeMenuItem.IsVisibleProperty).ToBinding(),
                 [!MenuItem.CommandProperty] = nativeItem.GetObservable(NativeMenuItem.CommandProperty).ToBinding(),
@@ -37,10 +38,6 @@ internal class NativeMenuBarPresenter : Menu
                 [!ToolTip.TipProperty] =
                     nativeItem.GetObservable(NativeMenuItem.ToolTipProperty).ToBinding(),
             };
-
-            BindingOperations.Apply(newItem, MenuItem.IsCheckedProperty, InstancedBinding.TwoWay(
-                nativeItem.GetObservable(NativeMenuItem.IsCheckedProperty).Select(v => (object)v),
-                new AnonymousObserver<object?>(v => nativeItem.SetValue(NativeMenuItem.IsCheckedProperty, v))));
 
             newItem.Click += MenuItemOnClick;
 
