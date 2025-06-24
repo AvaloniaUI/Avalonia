d2c2b1fc23814916c7b6ccbffd199c6e34515527
Steven Kirk <grokys@users.noreply.github.com>
2025-06-24 16:11:02 +0200

    Add XML docs to BindingBase.

--- src/Avalonia.Base/Data/BindingBase.cs
+++ src/Avalonia.Base/Data/BindingBase.cs
@@ -5,6 +5,22 @@ namespace Avalonia.Data;
 /// </summary>
 public abstract class BindingBase
 {
+    /// <summary>
+    /// Creates a <see cref="BindingExpressionBase"/> from a binding.
+    /// </summary>
+    /// <param name="target">The target of the binding.</param>
+    /// <param name="targetProperty">The target property of the binding.</param>
+    /// <param name="anchor">
+    /// If <paramref name="target"/> is not a control, provides an anchor object from which to
+    /// locate a data context or other controls.
+    /// </param>
+    /// <returns>
+    /// A newly instantiated <see cref="BindingExpressionBase"/>.
+    /// </returns>
+    /// <remarks>
+    /// This is a low-level method which returns a binding expression that is not yet connected to
+    /// a binding sink, and so is inactive.
+    /// </remarks>
     internal abstract BindingExpressionBase Instance(
         AvaloniaObject target,
         AvaloniaProperty? targetProperty,
