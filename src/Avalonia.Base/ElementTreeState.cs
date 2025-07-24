fe4a579aee9d1ec781dff2910a10beaa77ec3279
Emmanuel Hansen <emmausssss@gmail.com>
2025-07-24 13:44:20 +0000

    track keyboard focus with focus tree state

---
+++ src/Avalonia.Base/ElementTreeState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Avalonia
+{
+    internal class ElementTreeState
+    {
+        private Dictionary<Visual, Visual?> _visualParents = new Dictionary<Visual, Visual?>();
+
+        public void SetVisualParent(Visual visual, Visual? parent)
+        {
+            if (!_visualParents.ContainsKey(visual))
+                _visualParents[visual] = parent;
+        }
+
+        public Visual? GetVisualParent(Visual? visual)
+        {
+            if (visual != null && _visualParents.TryGetValue(visual, out var parent))
+                return parent;
+
+            return null;
+        }
+
+        public void RemoveVisualParent(Visual? visual)
+        {
+            if(visual != null && _visualParents.ContainsKey(visual))
+                _visualParents.Remove(visual);
+        }
+
+        public void Clear()
+        {
+            _visualParents.Clear();
+        }
+    }
+}
