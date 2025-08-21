3ee2009c43b61d69b19f7b5d085172f93c7c0109
Benedikt Stebner <Gillibald@users.noreply.github.com>
2025-08-21 12:25:50 +0200

    Remove using

--- tests/Avalonia.Skia.UnitTests/Media/TextFormatting/TextFormatterTests.cs
+++ tests/Avalonia.Skia.UnitTests/Media/TextFormatting/TextFormatterTests.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Avalonia.Media;
 using Avalonia.Media.TextFormatting;
 using Avalonia.Media.TextFormatting.Unicode;
