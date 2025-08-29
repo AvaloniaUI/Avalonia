34621efddb4d91dd45f26ad594e358ddf2ccc050
Benedikt Stebner <Gillibald@users.noreply.github.com>
2025-08-29 10:58:38 +0200

    Fix rename

--- tests/Avalonia.Skia.UnitTests/Media/TextFormatting/TextFormatterTests.cs
+++ tests/Avalonia.Skia.UnitTests/Media/TextFormatting/TextFormatterTests.cs
@@ -1097,7 +1097,7 @@ namespace Avalonia.Skia.UnitTests.Media.TextFormatting
 
                 var typeface = new Typeface(new FontFamily(new Uri("resm:Avalonia.Skia.UnitTests.Assets?assembly=Avalonia.Skia.UnitTests"), "Noto Mono"));
                 var defaultRunProperties = new GenericTextRunProperties(typeface);
-                var paragraphProperties = new GenericTextParagraphProperties(defaultRunProperties, textWrapping: TextWrapping.Wrap);
+                var paragraphProperties = new GenericTextParagraphProperties(defaultRunProperties, textWrap: TextWrapping.Wrap);
                 var textLine = TextFormatter.Current.FormatLine(new SimpleTextSource(text, defaultRunProperties), 0, 120, paragraphProperties);
 
                 Assert.NotNull(textLine);
