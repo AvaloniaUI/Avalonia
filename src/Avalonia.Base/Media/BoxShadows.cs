cdb4a0abfcc7373b13eda1acbb895c5b310c8aa1
robloo <robloo@users.noreply.github.com>
2025-09-14 15:33:35 -0400

    Fix formatting errors/warnings in BoxShadows

--- src/Avalonia.Base/Media/BoxShadows.cs
+++ src/Avalonia.Base/Media/BoxShadows.cs
@@ -10,7 +10,7 @@ namespace Avalonia.Media
         private readonly BoxShadow _first;
         private readonly BoxShadow[]? _list;
         public int Count { get; }
-        
+
         public BoxShadows(BoxShadow shadow)
         {
             _first = shadow;
@@ -30,9 +30,15 @@ namespace Avalonia.Media
             get
             {
                 if (c< 0 || c >= Count)
+                {
                     throw new IndexOutOfRangeException();
+                }
+
                 if (c == 0)
+                {
                     return _first;
+                }
+
                 return _list![c - 1];
             }
         }
@@ -89,15 +95,22 @@ namespace Avalonia.Media
                 || (sp.Length == 1 &&
                     (string.IsNullOrWhiteSpace(sp[0])
                      || sp[0] == "none")))
+            {
                 return new BoxShadows();
+            }
 
             var first = BoxShadow.Parse(sp[0]);
             if (sp.Length == 1)
+            {
                 return new BoxShadows(first);
+            }
 
             var rest = new BoxShadow[sp.Length - 1];
             for (var c = 0; c < rest.Length; c++)
+            {
                 rest[c] = BoxShadow.Parse(sp[c + 1]);
+            }
+
             return new BoxShadows(first, rest);
         }
 
@@ -105,28 +118,44 @@ namespace Avalonia.Media
         {
             var final = rect;
             foreach (var shadow in this)
+            {
                 final = final.Union(shadow.TransformBounds(rect));
+            }
+
             return final;
         }
-        
+
         public bool HasInsetShadows
         {
             get
             {
                 foreach(var boxShadow in this)
+                {
                     if (boxShadow != default && boxShadow.IsInset)
+                    {
                         return true;
+                    }
+                }
+
                 return false;
             }
         }
-        
+
         public bool Equals(BoxShadows other)
         {
             if (other.Count != Count)
+            {
                 return false;
-            for(var c=0; c<Count ; c++)
+            }
+
+            for (var c = 0; c < Count; c++)
+            {
                 if (!this[c].Equals(other[c]))
+                {
                     return false;
+                }
+            }
+
             return true;
         }
 
@@ -141,7 +170,10 @@ namespace Avalonia.Media
             {
                 int hashCode = 0;
                 foreach (var s in this)
+                {
                     hashCode = (hashCode * 397) ^ s.GetHashCode();
+                }
+
                 return hashCode;
             }
         }
