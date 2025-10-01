faee457d3f189c66628588d1a34c0c6c92c442df
Emmanuel Hansen <emmausssss@gmail.com>
2025-10-01 12:16:48 +0000

    Add xaml composition implicit animations

---
+++ src/Avalonia.Base/Animation/ImplicitAnimations/ExpressionKeyFrame.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Animation.Easings;
+
+namespace Avalonia.Animation
+{
+    public class ExpressionKeyFrame : AvaloniaObject
+    {
+        public float NormalizedProgressKey { get; set; }
+        public string? Value { get; set; }
+        public IEasing? Easing { get; set; }
+    }
+}
