8d2550780aef9d546eb9b11172ed5093168cf4d9
fgsfds <4870330+fgsfds@users.noreply.github.com>
2025-10-07 09:27:21 +0500

    Add unit test for notifications

---
+++ tests/Avalonia.Controls.UnitTests/NotificationsTests.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia.Controls.Notifications;
+using Avalonia.UnitTests;
+using Xunit;
+using Notification = Avalonia.Controls.Notifications.Notification;
+
+namespace Avalonia.Controls.UnitTests
+{
+    public class WindowNotificationManagerTests : ScopedTestBase
+    {
+        [Fact]
+        public void Show_Notifications_With_Same_String()
+        {
+            WindowNotificationManager manager = new();
+
+            manager.Show("Notification text");
+            manager.Show("Notification text");
+            manager.Show("Notification text");
+        }
+    }
+
+    public class INotificationManagerTests : ScopedTestBase
+    {
+        [Fact]
+        public void Show_Notifications_With_Same_Content()
+        {
+            INotificationManager manager = new WindowNotificationManager();
+
+            Notification notification = new()
+            {
+                Message = "Notification text"
+            };
+
+            manager.Show(notification);
+            manager.Show(notification);
+            manager.Show(notification);
+        }
+    }
+}
