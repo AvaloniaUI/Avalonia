2c540873ec2f2dc4feaa10be8aef7d8d183e1a16
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2025-10-27 11:43:09 +0800

    Add Impl for SaveFilePickerWithResultAsync API (#19783)
    * Add new SaveFilePickerWithResultAsync API to the base
    
    * Add SaveFilePickerWithResultAsync stub impl
    
    * Control Catalog SaveFilePickerWithResultAsync sample with XML and JSON types
    
    * Make SaveFilePickerResult a struct
    
    * Add managed picker implementation
    
    * Make SaveFilePickerResult a readonly struct
    
    * Windows implementation of SaveFilePickerWithResultAsync
    
    * Test impl for dbus
    
    * Reuse the file type object (FTO) so StorageFile consumer can match exactly the right FTO when receiving the SaveFilePicker's result.
    
    * Add Gtk impl
    
    * Avalonia.Native: surface selected save dialog filters
    
    * macOS: report selected NSSavePanel filter
    
    * Modify the conditional in case there's duplicate descriptions of FTO's in DBusSystemDialog.cs
    
    * Instantiate FPFT as fallback
    
    * Pass the mime/pattern to instantiated FPFT
    
    * Update API diff
    
    * Fix review comments
    
    ---------
    
    Co-authored-by: Max Katz <maxkatz6@outlook.com>

---
+++ src/Avalonia.Base/Platform/Storage/SaveFilePickerResult.cs
@@ -0,0 +1,22 @@
+namespace Avalonia.Platform.Storage;
+
+/// <summary>
+/// Extended result of the <see cref="IStorageProvider.SaveFilePickerWithResultAsync(FilePickerSaveOptions)"/> operation.
+/// </summary>
+public readonly struct SaveFilePickerResult
+{
+    internal SaveFilePickerResult(IStorageFile? file)
+    {
+        File = file;
+    }
+
+    /// <summary>
+    /// Saved <see cref="IStorageFile"/> or null if user canceled the dialog.
+    /// </summary>
+    public IStorageFile? File { get; init; }
+
+    /// <summary>
+    /// Selected file type or null if not supported.
+    /// </summary>
+    public FilePickerFileType? SelectedFileType { get; init; }
+}
