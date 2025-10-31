6edfba7defb63f846d00d3ce928605153ea8b6ab
davidw <davidw@icselectronics.co.uk>
2025-10-31 13:07:08 +0000

    Fix for Issue#16451

--- src/Avalonia.Base/Rendering/Composition/Drawing/ServerCompositionSimplePen.cs
+++ src/Avalonia.Base/Rendering/Composition/Drawing/ServerCompositionSimplePen.cs
@@ -1,12 +1,18 @@
-using System;
 using Avalonia.Media;
-using Avalonia.Media.Immutable;
-using Avalonia.Rendering.Composition.Server;
-using Avalonia.Rendering.Composition.Transport;
 
 namespace Avalonia.Rendering.Composition.Server;
 
 internal partial class ServerCompositionSimplePen : IPen
 {
     IDashStyle? IPen.DashStyle => DashStyle;
+
+    /// <inheritdoc/>
+    public override void Dispose()
+    {
+        // Dispose of the brush resource, this will remove the pen from the brush observers.
+        // This was causing the pen to be retained in memory by long lived brush resources (e.g. those defined in
+        // the theme or app resources), hence was causing memory leaks; see Issue #16451
+        SetValue(ref _brush, null!);
+        base.Dispose();
+    }
 }
