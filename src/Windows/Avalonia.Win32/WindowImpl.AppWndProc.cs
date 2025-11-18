4612173f67767b273a5f8a87f13bda91e2dfedc5
Emmanuel Hansen <emmausssss@gmail.com>
2025-11-18 11:09:38 +0000

    fix popups not closing in embedded view in winforms

--- src/Windows/Avalonia.Win32/WindowImpl.AppWndProc.cs
+++ src/Windows/Avalonia.Win32/WindowImpl.AppWndProc.cs
@@ -900,7 +900,7 @@ namespace Avalonia.Win32
             return DefWindowProc(hWnd, msg, wParam, lParam);
         }
 
-        private bool IsOurWindow(IntPtr hwnd)
+        internal bool IsOurWindow(IntPtr hwnd)
         {
             if (hwnd == IntPtr.Zero)
                 return false;
