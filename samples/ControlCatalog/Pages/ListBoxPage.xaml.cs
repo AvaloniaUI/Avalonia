grokys/Perspex

Added ItemSourceViewLayer Supports filter invalidation via changes to a "State" property, or via PropertyChanged notifications on individual items Fixed AvaloniaListAttribute passing invalid options to string.Split Added ItemFilter Added ItemSorter (abstract class only)

samples/ControlCatalog/Pages/ListBoxPage.xaml.cs
@@ -6,18 +6,14 @@ namespace ControlCatalog.Pages
 {
     public class ListBoxPage : UserControl
     {
-        private readonly ListBox _listBox;
         private readonly TextBox _searchBox;
 
         public ListBoxPage()
         {
             InitializeComponent();
-            DataContext = new ListBoxPageViewModel();
-
-            _listBox = this.Get<ListBox>("ListBox");
-
             _searchBox = this.Get<TextBox>("SearchBox");
-            _searchBox.TextChanged += OnSearchTextChanged;
+
+            DataContext = new ListBoxPageViewModel();
         }
 
         private void InitializeComponent()
@@ -25,14 +21,17 @@ namespace ControlCatalog.Pages
             AvaloniaXamlLoader.Load(this);
         }
 
-        private void OnSearchTextChanged(object? sender, TextChangedEventArgs e)
+        private void FilterItem(object? sender, FunctionItemFilter.FilterEventArgs e)
         {
-            _listBox.Items.Filter = string.IsNullOrEmpty(_searchBox.Text) ? null : FilterItem;
-        }
-
-        private void FilterItem(object? sender, ItemSourceViewFilterEventArgs e)
-        {
-            e.Accept = ((ItemModel)e.Item!).ToString().Contains(_searchBox.Text!);
+            if (string.IsNullOrEmpty(_searchBox.Text))
+            {
+                e.Accept = true;
+            }
+            else
+            {
+                var item = (ItemModel)e.Item!;
+                e.Accept = item.IsFavorite || item.ID.ToString().Contains(_searchBox.Text!);
+            }
         }
     }
 }

src/Avalonia.Controls/ItemCollection.cs
@@ -18,8 +18,8 @@ namespace Avalonia.Controls
 
         private Mode _mode;
 
-        internal ItemCollection()
-            : base(s_uninitialized)
+        internal ItemCollection(AvaloniaObject? owner)
+            : base(owner, s_uninitialized)
         {
         }
 

AvaloniaUI/Avalonia

samples/ControlCatalog/Pages/ListBoxPage.xaml.cs
@@ -11,14 +11,17 @@ namespace ControlCatalog.Pages
             InitializeComponent();
         }
 
-        private void OnSearchTextChanged(object? sender, TextChangedEventArgs e)
+        private void FilterItem(object? sender, FunctionItemFilter.FilterEventArgs e)
         {
-            ListBox.Items.Filter = string.IsNullOrEmpty(SearchBox.Text) ? null : FilterItem;
-        }
-
-        private void FilterItem(object? sender, ItemSourceViewFilterEventArgs e)
-        {
-            e.Accept = ((ItemModel)e.Item!).ToString().Contains(SearchBox.Text!);
+            if (string.IsNullOrEmpty(SearchBox.Text))
+            {
+                e.Accept = true;
+            }
+            else
+            {
+                var item = (ItemModel)e.Item!;
+                e.Accept = item.IsFavorite || item.ID.ToString().Contains(SearchBox.Text!);
+            }
         }
     }
 }

src/Avalonia.Controls/ItemCollection.cs
@@ -13,8 +13,8 @@ namespace Avalonia.Controls
     {
         private Mode _mode;
 
-        internal ItemCollection()
-            : base(UninitializedSource)
+        internal ItemCollection(AvaloniaObject? owner)
+            : base(owner, UninitializedSource)
         {
         }
 