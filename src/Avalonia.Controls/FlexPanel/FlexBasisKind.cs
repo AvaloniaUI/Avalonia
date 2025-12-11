23b6aa0e620f50f69adaa1a9dd412ac7c9acbc45
Emmanuel Hansen <emmausssss@gmail.com>
2025-12-11 13:34:16 +0000

    update namespace style to block namespaces

--- src/Avalonia.Controls/FlexPanel/FlexBasisKind.cs
+++ src/Avalonia.Controls/FlexPanel/FlexBasisKind.cs
@@ -1,22 +1,23 @@
-﻿namespace Avalonia.Controls;
-
-/// <summary>
-/// Determines how <see cref="FlexBasis"/> affects the size of the flex item
-/// </summary>
-public enum FlexBasisKind
+﻿namespace Avalonia.Controls
 {
     /// <summary>
-    /// Uses the measured Width and Height of the <see cref="FlexPanel"/> to determine the initial size of the item.
+    /// Determines how <see cref="FlexBasis"/> affects the size of the flex item
     /// </summary>
-    Auto,
+    public enum FlexBasisKind
+    {
+        /// <summary>
+        /// Uses the measured Width and Height of the <see cref="FlexPanel"/> to determine the initial size of the item.
+        /// </summary>
+        Auto,
 
-    /// <summary>
-    /// The initial size of the item is set to the <see cref="FlexBasis"/> value.
-    /// </summary>
-    Absolute,
+        /// <summary>
+        /// The initial size of the item is set to the <see cref="FlexBasis"/> value.
+        /// </summary>
+        Absolute,
 
-    /// <summary>
-    /// Indicates the <see cref="FlexBasis"/> value is a percentage, and the size of the flex item is scaled by it.
-    /// </summary>
-    Relative,
+        /// <summary>
+        /// Indicates the <see cref="FlexBasis"/> value is a percentage, and the size of the flex item is scaled by it.
+        /// </summary>
+        Relative,
+    }
 }
