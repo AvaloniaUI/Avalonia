d81edfdae09cc4d5a8088d497d8fb55db434ebb0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-12-16 12:06:17 +0100

    perf(Panel): remove unused System.Linq import

--- src/Avalonia.Controls/Panel.cs
+++ src/Avalonia.Controls/Panel.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Linq;
 using Avalonia.Controls.Presenters;
 using Avalonia.LogicalTree;
 using Avalonia.Media;
