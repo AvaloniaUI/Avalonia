85603eae41c49ad1cc9e0ee0890918b19b369a22
Melissa Geels <melissa@nimble.tools>
2026-01-17 12:56:02 +0100

    Added arrow key scroll navigation to ScrollViewer control

--- src/Avalonia.Controls/ScrollViewer.cs
+++ src/Avalonia.Controls/ScrollViewer.cs
@@ -786,6 +786,26 @@ namespace Avalonia.Controls
                 PageDown();
                 e.Handled = true;
             }
+            else if (e.Key == Key.Left)
+            {
+                LineLeft();
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Right)
+            {
+                LineRight();
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Up)
+            {
+                LineUp();
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Down)
+            {
+                LineDown();
+                e.Handled = true;
+            }
         }
 
         /// <summary>
