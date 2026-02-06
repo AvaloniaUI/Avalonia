87a7134d341023d26bdd9449d99ca00aa9bf83c2
grokys <grokys@users.noreply.github.com>
2026-02-06 10:05:39 +0100

    Make binding plugin API internal.
    Making everything pluggable like has performance implications even if the plugins are never customized (which will be the 99% case).
    
    `IPropertyAccessor` is used in `CompiledBindingPathBuilder` so needs to be kept public for the moment.

--- src/Avalonia.Base/Data/Core/Plugins/BindingPlugins.cs
+++ src/Avalonia.Base/Data/Core/Plugins/BindingPlugins.cs
@@ -7,7 +7,7 @@ namespace Avalonia.Data.Core.Plugins
     /// Holds a registry of plugins used for bindings.
     /// </summary>
     [RequiresUnreferencedCode(TrimmingMessages.PropertyAccessorsRequiresUnreferencedCodeMessage)]
-    public static class BindingPlugins
+    internal static class BindingPlugins
     {
         internal static readonly List<IPropertyAccessorPlugin> s_propertyAccessors = new()
         {
