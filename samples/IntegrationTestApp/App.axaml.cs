cefdae2a7ee346f1517886824d267db7b466c1ad
Timothy Miller <innerlogic4321@gmail.com>
2026-02-08 19:27:11 +0900

    Properly set the button for the checkbox

--- samples/IntegrationTestApp/App.axaml.cs
+++ samples/IntegrationTestApp/App.axaml.cs
@@ -1,8 +1,10 @@
+using System.Linq;
 using System.Windows.Input;
 
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
 using MiniMvvm;
 
@@ -20,7 +22,10 @@ namespace IntegrationTestApp
             });
             DockMenuCommand = MiniCommand.Create<string>(name =>
             {
-                _mainWindow!.Get<CheckBox>(name).IsChecked = true;
+                // This is for the "Show Main Window" dock menu item in the test.
+                // It doesn't actually show the main window, but sets the checkbox to true in the page.
+                var checkbox = _mainWindow!.GetLogicalDescendants().OfType<CheckBox>().FirstOrDefault(x => x.Name == name); 
+                if (checkbox != null) checkbox.IsChecked = true;
             });
             DataContext = this;
         }
