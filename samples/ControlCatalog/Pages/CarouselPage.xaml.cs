ae76a0ad4376b009c723e02cbb0f086a52e55679
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2026-02-10 08:49:11 +0100

    Format Carousel sample UI

--- samples/ControlCatalog/Pages/CarouselPage.xaml.cs
+++ samples/ControlCatalog/Pages/CarouselPage.xaml.cs
@@ -1,6 +1,8 @@
 using System;
+using Avalonia;
 using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 
 namespace ControlCatalog.Pages
 {
@@ -9,28 +11,83 @@ namespace ControlCatalog.Pages
         public CarouselPage()
         {
             InitializeComponent();
+            
             left.Click += (s, e) => carousel.Previous();
             right.Click += (s, e) => carousel.Next();
             transition.SelectionChanged += TransitionChanged;
             orientation.SelectionChanged += TransitionChanged;
+            
+            carousel.PropertyChanged += (s, e) =>
+            {
+                if (e.Property == SelectingItemsControl.SelectedIndexProperty)
+                {
+                    UpdateButtonState();
+                }
+            };
+
+            UpdateButtonState();
+        }
+
+        private void UpdateButtonState()
+        {
+            itemsCountIndicator.Text = carousel.ItemCount.ToString();
+            selectedIndexIndicator.Text = carousel.SelectedIndex.ToString();
+
+            left.IsEnabled = carousel.SelectedIndex > 0;
+            right.IsEnabled = carousel.SelectedIndex < carousel.ItemCount - 1;
         }
 
         private void TransitionChanged(object? sender, SelectionChangedEventArgs e)
         {
+            var isVertical = orientation.SelectedIndex == 1;
+            var axis = isVertical ? PageSlide.SlideAxis.Vertical : PageSlide.SlideAxis.Horizontal;
+            
             switch (transition.SelectedIndex)
             {
                 case 0:
                     carousel.PageTransition = null;
                     break;
                 case 1:
-                    carousel.PageTransition = new PageSlide(TimeSpan.FromSeconds(0.25), orientation.SelectedIndex == 0 ? PageSlide.SlideAxis.Horizontal : PageSlide.SlideAxis.Vertical);
+                    carousel.PageTransition = new PageSlide(TimeSpan.FromSeconds(0.25), axis);
                     break;
                 case 2:
                     carousel.PageTransition = new CrossFade(TimeSpan.FromSeconds(0.25));
                     break;
                 case 3:
-                    carousel.PageTransition = new Rotate3DTransition(TimeSpan.FromSeconds(0.5), orientation.SelectedIndex == 0 ? PageSlide.SlideAxis.Horizontal : PageSlide.SlideAxis.Vertical);
+                    carousel.PageTransition = new Rotate3DTransition(TimeSpan.FromSeconds(0.5), axis);
                     break;
+            }
+            
+            UpdateLayoutForOrientation(isVertical);
+        }
+
+        private void UpdateLayoutForOrientation(bool isVertical)
+        {
+            if (isVertical)
+            {
+                Grid.SetColumn(left, 1);
+                Grid.SetRow(left, 0);
+                Grid.SetColumn(right, 1);
+                Grid.SetRow(right, 2);
+                
+                left.Padding = new Thickness(20, 10);
+                right.Padding = new Thickness(20, 10);
+
+                leftArrow.RenderTransform = new Avalonia.Media.RotateTransform(90);
+                rightArrow.RenderTransform = new Avalonia.Media.RotateTransform(90);
+            }
+            else
+            {
+                Grid.SetColumn(left, 0);
+                Grid.SetRow(left, 1);
+                Grid.SetColumn(right, 2);
+                Grid.SetRow(right, 1);
+
+                left.Padding = new Thickness(10, 20);
+                right.Padding = new Thickness(10, 20);
+
+                leftArrow.RenderTransform = null;
+                rightArrow.RenderTransform = null;
             }
         }
     }
