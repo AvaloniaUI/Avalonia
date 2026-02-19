b1676dfb5fcc55d44a7c757d2e833bf2171e1505
rabbitism <dongbin@irihi.tech>
2026-02-19 19:50:37 +0800

    feat: add empty string handling and unit tests for HarfBuzzTextShaper

--- src/HarfBuzz/Avalonia.HarfBuzz/HarfBuzzTextShaper.cs
+++ src/HarfBuzz/Avalonia.HarfBuzz/HarfBuzzTextShaper.cs
@@ -24,6 +24,9 @@ namespace Avalonia.Harfbuzz
         {
             var textSpan = text.Span;
 
+            if (text.Length == 0)
+                return new ShapedBuffer(text, 0, options.GlyphTypeface, options.FontRenderingEmSize, options.BidiLevel);
+
             var glyphTypeface = options.GlyphTypeface;
 
             if (glyphTypeface.TextShaperTypeface is not HarfBuzzTypeface harfBuzzTypeface)
