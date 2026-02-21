a6b56377f65e8aff8b81e47d521dd00ae3a3c939
Adam Demasi <adam@hbang.com.au>
2026-02-21 15:59:00 +1100

    Implement virtualized automation children for ListBox
    This creates an automation peer for ListBox that returns actual or virtual
    automation peers for the items in the list. This allows VoiceOver to be
    aware of the number of items, and be able to navigate between them,
    even if not visible in the current viewport.
    
    Fixes #20685.

---
+++ src/Avalonia.Controls/Automation/Peers/ListBoxAutomationPeer.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Avalonia.Controls;
+
+namespace Avalonia.Automation.Peers
+{
+    public class ListBoxAutomationPeer : SelectingItemsControlAutomationPeer
+    {
+        public ListBoxAutomationPeer(ListBox owner)
+            : base(owner)
+        {
+        }
+
+        public new ListBox Owner => (ListBox)base.Owner;
+
+        protected override IReadOnlyList<AutomationPeer>? GetChildrenCore()
+        {
+            if (Owner.ItemCount == 0)
+            {
+                return null;
+            }
+
+            var children = new List<AutomationPeer>();
+
+            for (var i = 0; i < Owner.ItemCount; i++)
+            {
+                var container = Owner.ContainerFromIndex(i);
+
+                if (container is Control control)
+                {
+                    children.Add(GetOrCreate(control));
+                }
+                else
+                {
+                    children.Add(new VirtualListItemAutomationPeer(Owner, i));
+                }
+            }
+
+            return children;
+        }
+    }
+}
