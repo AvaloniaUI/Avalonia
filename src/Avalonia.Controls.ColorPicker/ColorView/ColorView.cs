b28138e4b707368f3d1e2417be4fd2ee0c5f316a
timunie <tim.uniewski@gmail.com>
2026-03-04 13:19:16 +0100

    propose: Remove redundant logic from ColorView
    The TabItem now handles the correct selection of only visible items

--- src/Avalonia.Controls.ColorPicker/ColorView/ColorView.cs
+++ src/Avalonia.Controls.ColorPicker/ColorView/ColorView.cs
@@ -4,7 +4,6 @@ using Avalonia.Controls.Converters;
 using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
 using Avalonia.Media;
-using Avalonia.Threading;
 
 namespace Avalonia.Controls
 {
@@ -12,7 +11,6 @@ namespace Avalonia.Controls
     /// Presents a color for user editing using a spectrum, palette and component sliders.
     /// </summary>
     [TemplatePart("PART_HexTextBox", typeof(TextBox))]
-    [TemplatePart("PART_TabControl", typeof(TabControl))]
     public partial class ColorView : TemplatedControl
     {
         /// <summary>
@@ -22,7 +20,6 @@ namespace Avalonia.Controls
 
         // XAML template parts
         private TextBox?    _hexTextBox;
-        private TabControl? _tabControl;
 
         protected bool _ignorePropertyChanged = false;
 
@@ -68,110 +65,7 @@ namespace Avalonia.Controls
                     includeSymbol: false);
             }
         }
-
-        /// <summary>
-        /// Validates the tab/panel/page selection taking into account the visibility of each item
-        /// as well as the current selection.
-        /// </summary>
-        /// <remarks>
-        /// Derived controls may re-implement this based on their default style / control template
-        /// and any specialized selection needs.
-        /// </remarks>
-        protected virtual void ValidateSelection()
-        {
-            if (_tabControl != null &&
-                _tabControl.Items != null)
-            {
-                // Determine the number of visible tab items
-                int numVisibleItems = 0;
-                foreach (var item in _tabControl.Items)
-                {
-                    if (item is Control control &&
-                        control.IsVisible)
-                    {
-                        numVisibleItems++;
-                    }
-                }
-
-                // Verify the selection
-                if (numVisibleItems > 0)
-                {
-                    object? selectedItem = null;
-
-                    if (_tabControl.SelectedItem == null &&
-                        _tabControl.ItemCount > 0)
-                    {
-                        // As a failsafe, forcefully select the first item
-                        foreach (var item in _tabControl.Items)
-                        {
-                            selectedItem = item;
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        selectedItem = _tabControl.SelectedItem;
-                    }
-
-                    if (selectedItem is Control selectedControl &&
-                        selectedControl.IsVisible == false)
-                    {
-                        // Select the first visible item instead
-                        foreach (var item in _tabControl.Items)
-                        {
-                            if (item is Control control &&
-                                control.IsVisible)
-                            {
-                                selectedItem = item;
-                                break;
-                            }
-                        }
-                    }
-
-                    _tabControl.SelectedItem = selectedItem;
-                    _tabControl.IsVisible = true;
-                }
-                else
-                {
-                    // Special case when all items are hidden
-                    // If TabControl ever properly supports no selected item /
-                    // all items hidden this can be removed
-                    _tabControl.SelectedItem = null;
-                    _tabControl.IsVisible = false;
-                }
-
-                // Hide the "tab strip" if there is only one tab
-                // This allows, for example, to view only the palette
-                /*
-                var itemsPresenter = _tabControl.FindDescendantOfType<ItemsPresenter>();
-                if (itemsPresenter != null)
-                {
-                    if (numVisibleItems == 1)
-                    {
-                        itemsPresenter.IsVisible = false;
-                    }
-                    else
-                    {
-                        itemsPresenter.IsVisible = true;
-                    }
-                }
-                */
-
-                // Note that if externally the SelectedIndex is set to 4 or something
-                // outside the valid range, the TabControl will ignore it and replace it
-                // with a valid SelectedIndex. This however is not propagated back through
-                // the TwoWay binding in the control template so the SelectedIndex and
-                // SelectedIndex become out of sync.
-                //
-                // The work-around for this is done here where SelectedIndex is forcefully
-                // synchronized with whatever the TabControl property value is. This is
-                // possible since selection validation is already done by this method.
-                SetCurrentValue(SelectedIndexProperty, _tabControl.SelectedIndex);
-            }
-
-            return;
-        }
-
+        
         /// <inheritdoc/>
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
@@ -182,7 +76,6 @@ namespace Avalonia.Controls
             }
 
             _hexTextBox = e.NameScope.Find<TextBox>("PART_HexTextBox");
-            _tabControl = e.NameScope.Find<TabControl>("PART_TabControl");
 
             SetColorToHexTextBox();
 
@@ -193,7 +86,6 @@ namespace Avalonia.Controls
             }
 
             base.OnApplyTemplate(e);
-            ValidateSelection();
         }
 
         /// <inheritdoc/>
@@ -259,27 +151,6 @@ namespace Avalonia.Controls
                 // Manually coerce the HsvColor value
                 // (Color will be coerced automatically if HsvColor changes)
                 SetCurrentValue(HsvColorProperty,  OnCoerceHsvColor(HsvColor));
-            }
-            else if (change.Property == IsColorComponentsVisibleProperty ||
-                     change.Property == IsColorPaletteVisibleProperty ||
-                     change.Property == IsColorSpectrumVisibleProperty)
-            {
-                // When the property changed notification is received here the visibility
-                // of individual tab items has not yet been updated through the bindings.
-                // Therefore, the validation is delayed until after bindings update.
-                Dispatcher.UIThread.Post(() =>
-                {
-                    ValidateSelection();
-                }, DispatcherPriority.Background);
-            }
-            else if (change.Property == SelectedIndexProperty)
-            {
-                // Again, it is necessary to wait for the SelectedIndex value to
-                // be applied to the TabControl through binding before validation occurs.
-                Dispatcher.UIThread.Post(() =>
-                {
-                    ValidateSelection();
-                }, DispatcherPriority.Background);
             }
 
             base.OnPropertyChanged(change);
