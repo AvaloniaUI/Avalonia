f3df9b1f30e8ed15a3b3fc5157f0b8c183297219
Tim Miller <drasticactions@users.noreply.github.com>
2026-03-09 18:41:53 +0900

    [Metal] Dispose GRBackendRenderTarget and @autoreleasepool for metal objects (#20815)
    * Add @autoreleasepool to release memory
    
    * Pass in and dispose GRBackendRenderTarget
    
    * Use Macros

--- src/Skia/Avalonia.Skia/Gpu/Metal/SkiaMetalGpu.cs
+++ src/Skia/Avalonia.Skia/Gpu/Metal/SkiaMetalGpu.cs
@@ -105,7 +105,7 @@ internal class SkiaMetalGpu : ISkiaGpu
                 session.IsYFlipped ? GRSurfaceOrigin.BottomLeft : GRSurfaceOrigin.TopLeft,
                 SKColorType.Bgra8888);
 
-            return new SkiaMetalRenderSession(_gpu, surface, session);
+            return new SkiaMetalRenderSession(_gpu, surface, session, backendTarget);
         }
 
         public bool IsCorrupted => false;
@@ -118,14 +118,17 @@ internal class SkiaMetalGpu : ISkiaGpu
         private readonly SkiaMetalGpu _gpu;
         private SKSurface? _surface;
         private IMetalPlatformSurfaceRenderingSession? _session;
+        private GRBackendRenderTarget? _backendTarget;
 
-        public SkiaMetalRenderSession(SkiaMetalGpu gpu, 
+        public SkiaMetalRenderSession(SkiaMetalGpu gpu,
             SKSurface surface,
-            IMetalPlatformSurfaceRenderingSession session)
+            IMetalPlatformSurfaceRenderingSession session,
+            GRBackendRenderTarget backendTarget)
         {
             _gpu = gpu;
             _surface = surface;
             _session = session;
+            _backendTarget = backendTarget;
         }
 
         public void Dispose()
@@ -133,11 +136,13 @@ internal class SkiaMetalGpu : ISkiaGpu
             _surface?.Canvas.Flush();
             _surface?.Flush();
             _gpu._context?.Flush();
-            
+
             _surface?.Dispose();
             _surface = null;
             _session?.Dispose();
             _session = null;
+            _backendTarget?.Dispose();
+            _backendTarget = null;
         }
 
         public GRContext GrContext => _gpu._context!;
