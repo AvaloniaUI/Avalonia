d73ffba95fe705807233929a8d9832c83a6785dd
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2026-03-10 08:58:26 +0100

    Improve CarouselPage Transitions sample

--- samples/ControlCatalog/Pages/CarouselPage/CarouselPageTransitionsPage.xaml.cs
+++ samples/ControlCatalog/Pages/CarouselPage/CarouselPageTransitionsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using ControlCatalog.Pages.Transitions;
 
 namespace ControlCatalog.Pages
 {
@@ -24,6 +25,8 @@ namespace ControlCatalog.Pages
                 1 => new CrossFade(TimeSpan.FromMilliseconds(300)),
                 2 => new PageSlide(TimeSpan.FromMilliseconds(300), PageSlide.SlideAxis.Horizontal),
                 3 => new PageSlide(TimeSpan.FromMilliseconds(300), PageSlide.SlideAxis.Vertical),
+                4 => new CardStackPageTransition(TimeSpan.FromMilliseconds(400)),
+                5 => new WaveRevealPageTransition(TimeSpan.FromMilliseconds(600)),
                 _ => null
             };
 
@@ -53,7 +56,13 @@ namespace ControlCatalog.Pages
             if (StatusText == null)
                 return;
             var pageCount = (DemoCarousel.Pages as IList)?.Count ?? 0;
-            var modeName = DemoCarousel.PageTransition == null ? "None" : DemoCarousel.PageTransition.GetType().Name;
+            var modeName = DemoCarousel.PageTransition switch
+            {
+                null => "None",
+                CardStackPageTransition => "Card Stack",
+                WaveRevealPageTransition => "Wave Reveal",
+                { } t => t.GetType().Name
+            };
             StatusText.Text = $"Page {DemoCarousel.SelectedIndex + 1} of {pageCount} | Transition: {modeName}";
         }
     }
