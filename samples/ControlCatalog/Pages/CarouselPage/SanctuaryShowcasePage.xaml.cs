c5df58ca5e94fb74e81a8c0c7faadd29ec7550d4
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2026-03-11 13:53:49 +0100

    More changes in showcases

--- samples/ControlCatalog/Pages/CarouselPage/SanctuaryShowcasePage.xaml.cs
+++ samples/ControlCatalog/Pages/CarouselPage/SanctuaryShowcasePage.xaml.cs
@@ -1,6 +1,9 @@
 using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media;
 using Avalonia.VisualTree;
 
 namespace ControlCatalog.Pages;
@@ -29,8 +32,38 @@ public partial class SanctuaryShowcasePage : UserControl
             return;
 
         var previousPages = nav.NavigationStack.ToList();
-        var mainPage = new ContentPage { Content = new SanctuaryMainPage() };
-        NavigationPage.SetHasNavigationBar(mainPage, false);
+
+        var headerGrid = new Grid { ColumnDefinitions = new ColumnDefinitions("*, Auto") };
+        headerGrid.Children.Add(new TextBlock
+        {
+            Text = "Sanctuary",
+            VerticalAlignment = VerticalAlignment.Center
+        });
+        var closeIcon = Geometry.Parse(
+            "M4.397 4.397a1 1 0 0 1 1.414 0L12 10.585l6.19-6.188a1 1 0 0 1 1.414 1.414L13.413 12l6.19 6.189a1 1 0 0 1-1.414 1.414L12 13.413l-6.189 6.19a1 1 0 0 1-1.414-1.414L10.585 12 4.397 5.811a1 1 0 0 1 0-1.414z");
+        var closeBtn = new Button
+        {
+            Content = new PathIcon { Data = closeIcon },
+            Background = Brushes.Transparent,
+            BorderThickness = new Thickness(0),
+            Padding = new Thickness(8, 4),
+            VerticalAlignment = VerticalAlignment.Center
+        };
+        Grid.SetColumn(closeBtn, 1);
+        headerGrid.Children.Add(closeBtn);
+
+        var mainPage = new ContentPage
+        {
+            Header = headerGrid,
+            Content = new SanctuaryMainPage()
+        };
+        NavigationPage.SetHasBackButton(mainPage, false);
+
+        closeBtn.Click += async (_, _) =>
+        {
+            nav.RemovePage(mainPage);
+        };
+
         await nav.PushAsync(mainPage);
         foreach (var page in previousPages)
             nav.RemovePage(page);
