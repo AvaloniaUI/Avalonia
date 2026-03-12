075ef1eb2a67d2ef83fff754d15eb0b7396290ff
Nikita Tsukanov <keks9n@gmail.com>
2026-03-12 15:05:19 +0500

    XML docs

--- src/Avalonia.Base/Rendering/IRenderTimer.cs
+++ src/Avalonia.Base/Rendering/IRenderTimer.cs
@@ -10,13 +10,14 @@ namespace Avalonia.Rendering
     public interface IRenderTimer
     {
         /// <summary>
-        /// Set by the render loop to control ticking.
-        /// Setting a non-null value starts the timer; setting null stops it.
-        /// Called under a lock by the render loop — will not be set concurrently.
+        /// Gets or sets the callback to be invoked when the timer ticks.
+        /// This property can be set from any thread, but it's guaranteed that it's not set concurrently
+        /// (i. e. render loop always does it under a lock).
+        /// Setting the value to null suggests the timer to stop ticking, however
+        /// timer is allowed to produce ticks on the previously set value as long as it stops doing so
         /// </summary>
         /// <remarks>
-        /// The callback can be invoked on any thread; it is the responsibility of the subscriber to
-        /// switch execution to the right thread.
+        /// The callback can be invoked on any thread
         /// </remarks>
         Action<TimeSpan>? Tick { get; set; }
 
