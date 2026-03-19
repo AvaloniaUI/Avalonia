2ce213b9d5e50d2bba895e5512bfeed1d1478307
Emmanuel Hansen <emmausssss@gmail.com>
2026-03-19 12:49:37 +0000

    remove unused usings

--- src/Avalonia.Controls/Converters/BorderGapMaskConverter.cs
+++ src/Avalonia.Controls/Converters/BorderGapMaskConverter.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using Avalonia.Controls.Shapes;
 using Avalonia.Data.Converters;
 using Avalonia.Media;
