6fb4a3586d70388bae4cda8f1d5bed50a07c2c11
Julien Lebosquain <julien@lebosquain.net>
2026-03-20 12:25:48 +0100

    Remove Unstable from several RoutedEventArgs

--- src/Avalonia.Base/Input/PointerDeltaEventArgs.cs
+++ src/Avalonia.Base/Input/PointerDeltaEventArgs.cs
@@ -1,7 +1,4 @@
-using System;
 using Avalonia.Interactivity;
-using Avalonia.Metadata;
-using Avalonia.VisualTree;
 
 namespace Avalonia.Input
 {
@@ -9,7 +6,6 @@ namespace Avalonia.Input
     {
         public Vector Delta { get; }
 
-        [Unstable("This constructor might be removed in 12.0.")]
         public PointerDeltaEventArgs(RoutedEvent? routedEvent, object? source,
             IPointer pointer, Visual rootVisual, Point rootVisualPosition, ulong timestamp,
             PointerPointProperties properties, KeyModifiers modifiers, Vector delta) 
