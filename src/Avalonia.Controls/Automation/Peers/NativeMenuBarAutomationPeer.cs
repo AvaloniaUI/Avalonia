d8de5556b9f41a8944e4849c507796a71617f7ea
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2026-05-07 20:01:36 +0800

    Add back a thin automation peer for native menu bar.

---
+++ src/Avalonia.Controls/Automation/Peers/NativeMenuBarAutomationPeer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.LogicalTree;
+using Avalonia.VisualTree;
+
+namespace Avalonia.Automation.Peers
+{
+    public class NativeMenuBarAutomationPeer(NativeMenuBar owner) : ControlAutomationPeer(owner)
+    {
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.MenuBar;
+        }
+    }
+}
