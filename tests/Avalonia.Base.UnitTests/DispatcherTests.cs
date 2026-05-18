aa7dcf1a028883439231f4636fb3d5e920109eb0
Tom Edwards <109803929+TomEdwardsEnscape@users.noreply.github.com>
2026-05-18 10:56:40 +0200

    Allow `Dispatcher` to be converted to a `TaskScheduler` (#21032)
    * Allow Dispatcher to be implicitly cast to TaskScheduler
    
    * API change requests
    
    * Cache one scheduler per priority
    
    * Cache a TaskScheduler in each sync context

--- tests/Avalonia.Base.UnitTests/DispatcherTests.cs
+++ tests/Avalonia.Base.UnitTests/DispatcherTests.cs
@@ -848,4 +848,15 @@ public partial class DispatcherTests
         }
     }
 
+    [Fact]
+    public async Task Dispatcher_Can_Act_As_TaskScheduler()
+    {
+        var impl = new SimpleDispatcherImpl();
+        Dispatcher.InitializeUIThreadDispatcher(impl);
+        Thread? continuationThread = null;
+        _ = Task.CompletedTask.ContinueWith(t => continuationThread = Thread.CurrentThread, Dispatcher.UIThread.ToTaskScheduler());
+        Assert.True(impl.AskedForSignal);
+        impl.ExecuteSignal();
+        Assert.Equal(Dispatcher.UIThread.Thread, continuationThread);
+    }
 }
