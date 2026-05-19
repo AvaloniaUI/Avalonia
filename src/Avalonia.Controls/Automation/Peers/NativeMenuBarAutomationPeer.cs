a9989bcc21ee4ab7aeb97641c3b7b79d18a746a4
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2026-05-20 02:13:53 +0800

    Drop NativeMenuBarAutomationPeer.GetChildrenCore override
    Bisect confirmed this override caused 5 Windows IT tests (Slider, Screen, DragDrop) to fail by producing an inconsistent UIA tree where MenuItem peers list NativeMenuBar as parent while their visual parent is the inner Menu. Reverted to the thin peer that only reports ControlType=MenuBar, and dropped the now-irrelevant children assertion test.

--- src/Avalonia.Controls/Automation/Peers/NativeMenuBarAutomationPeer.cs
+++ src/Avalonia.Controls/Automation/Peers/NativeMenuBarAutomationPeer.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-using Avalonia.LogicalTree;
-using Avalonia.VisualTree;
 
 namespace Avalonia.Automation.Peers
 {
@@ -12,23 +7,6 @@ namespace Avalonia.Automation.Peers
         protected override AutomationControlType GetAutomationControlTypeCore()
         {
             return AutomationControlType.MenuBar;
-        }
-
-        protected override IReadOnlyList<AutomationPeer>? GetChildrenCore()
-        {
-            var menu = Owner.GetVisualDescendants().OfType<MenuBase>().FirstOrDefault()
-                       ?? Owner.GetLogicalDescendants().OfType<MenuBase>().FirstOrDefault();
-            if (menu is null)
-                return base.GetChildrenCore();
-
-            var result = new List<AutomationPeer>();
-            foreach (var logicalChild in menu.LogicalChildren)
-            {
-                if (logicalChild is MenuItem { IsVisible: true } menuItem)
-                    result.Add(GetOrCreate(menuItem));
-            }
-
-            return result;
         }
     }
 }
