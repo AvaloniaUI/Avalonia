0d139ca47edbf704b83e6c19394c0dc4d800a3e0
Timothy Miller <innerlogic4321@gmail.com>
2026-05-20 15:00:27 +0900

    Update sample

--- samples/WinUIEmbedSample/EmbeddedView.axaml.cs
+++ samples/WinUIEmbedSample/EmbeddedView.axaml.cs
@@ -1,12 +1,19 @@
 using global::Avalonia;
 using global::Avalonia.Controls;
+using global::Avalonia.Controls.Shapes;
+using global::Avalonia.Input;
 using global::Avalonia.Interactivity;
+using global::Avalonia.Markup.Xaml;
+using global::Avalonia.Markup.Xaml.MarkupExtensions;
+using global::Avalonia.Media;
 
 namespace WinUIEmbedSample;
 
 public partial class EmbeddedView : UserControl
 {
     private int _clicks;
+    private int _pointCount;
+    private int _intermediateCount;
 
     public EmbeddedView()
     {
@@ -23,5 +30,61 @@ public partial class EmbeddedView : UserControl
     {
         if (e.Property == Slider.ValueProperty)
             AvSliderValue.Text = $"Slider: {AvSlider.Value:F0}";
+    }
+
+    private void OnInkPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        AddDot(e.GetPosition(InkCanvas), Brushes.Red);
+        _pointCount++;
+        e.Pointer.Capture(InkCanvas);
+        UpdateStats();
+        e.Handled = true;
+    }
+
+    private void OnInkPointerMoved(object? sender, PointerEventArgs e)
+    {
+        var intermediates = e.GetIntermediatePoints(InkCanvas);
+        for (var i = 0; i < intermediates.Count; i++)
+            AddDot(intermediates[i].Position, Brushes.Orange);
+        _intermediateCount += intermediates.Count;
+
+        AddDot(e.GetPosition(InkCanvas), Brushes.Green);
+        _pointCount++;
+        UpdateStats();
+    }
+
+    private void OnInkPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        AddDot(e.GetPosition(InkCanvas), Brushes.Blue);
+        _pointCount++;
+        UpdateStats();
+        e.Handled = true;
+    }
+
+    private void AddDot(Point position, IBrush brush)
+    {
+        const double size = 6;
+        var dot = new Ellipse
+        {
+            Width = size,
+            Height = size,
+            Fill = brush,
+        };
+        Canvas.SetLeft(dot, position.X - size / 2);
+        Canvas.SetTop(dot, position.Y - size / 2);
+        InkCanvas.Children.Add(dot);
+    }
+
+    private void OnClearInk(object? sender, RoutedEventArgs e)
+    {
+        InkCanvas.Children.Clear();
+        _pointCount = 0;
+        _intermediateCount = 0;
+        UpdateStats();
+    }
+
+    private void UpdateStats()
+    {
+        InkStats.Text = $"{_pointCount} points / {_intermediateCount} intermediate";
     }
 }
