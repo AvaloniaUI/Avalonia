325d89442144a957940304eed0f35939e661b24c
Benedikt Stebner <Gillibald@users.noreply.github.com>
2026-06-03 09:41:58 +0200

    [Text] Fixes culture aware font matching (#21435)
    * Make font matching inside the FontCollectionBase implementation culture aware
    
    * Aligning `RefineWithCulture` with major text-stack implementations
    
    * Revert submodule change
    
    * Fix BuildTypefaceWithSynthesis
    
    * Add a better unit test coverage for TryMatchCharacter that covers the tiered algorithm
    
    * Add some comments to MetaTable parsing
    Use Span helpers
    Optimize ScriptExtensions.data

--- src/Avalonia.Base/Media/TextFormatting/Unicode/UnicodeData.cs
+++ src/Avalonia.Base/Media/TextFormatting/Unicode/UnicodeData.cs
@@ -15,6 +15,7 @@ namespace Avalonia.Media.TextFormatting.Unicode
         internal const int WORDBREAK_BITS = 5;
         internal const int GRAPHEMEBREAK_BITS = 5;
         internal const int INDICCONJUNCTBREAK_BITS = 2;
+        internal const int SCRIPTEXTENSIONS_BITS = 7;
 
         internal const int BIDIPAIREDBRACKED_BITS = 16;
         internal const int BIDIPAIREDBRACKEDTYPE_BITS = 2;
@@ -23,6 +24,7 @@ namespace Avalonia.Media.TextFormatting.Unicode
         internal const int SCRIPT_SHIFT = CATEGORY_BITS;
         internal const int LINEBREAK_SHIFT = CATEGORY_BITS + SCRIPT_BITS;
         internal const int WORDBREAK_SHIFT = CATEGORY_BITS + SCRIPT_BITS + LINEBREAK_BITS;
+        internal const int SCRIPTEXTENSIONS_SHIFT = CATEGORY_BITS + SCRIPT_BITS + LINEBREAK_BITS + WORDBREAK_BITS;
         internal const int INDICCONJUNCTBREAK_SHIFT = GRAPHEMEBREAK_BITS;
 
         internal const int BIDIPAIREDBRACKEDTYPE_SHIFT = BIDIPAIREDBRACKED_BITS;
@@ -34,6 +36,7 @@ namespace Avalonia.Media.TextFormatting.Unicode
         internal const int WORDBREAK_MASK = (1 << WORDBREAK_BITS) - 1;
         internal const int GRAPHEMEBREAK_MASK = (1 << GRAPHEMEBREAK_BITS) - 1;
         internal const int INDICCONJUNCTBREAK_MASK = (1 << INDICCONJUNCTBREAK_BITS) - 1;
+        internal const int SCRIPTEXTENSIONS_MASK = (1 << SCRIPTEXTENSIONS_BITS) - 1;
 
         internal const int BIDIPAIREDBRACKED_MASK = (1 << BIDIPAIREDBRACKED_BITS) - 1;
         internal const int BIDIPAIREDBRACKEDTYPE_MASK = (1 << BIDIPAIREDBRACKEDTYPE_BITS) - 1;
