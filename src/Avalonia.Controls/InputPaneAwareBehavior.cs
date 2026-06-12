be84b540e015388139fc0348513baba2b5c5182a
Emmanuel Hansen <emmausssss@gmail.com>
2026-06-12 11:06:46 +0000

    add doc comment

--- src/Avalonia.Controls/InputPaneAwareBehavior.cs
+++ src/Avalonia.Controls/InputPaneAwareBehavior.cs
@@ -1,9 +1,23 @@
 ﻿namespace Avalonia.Controls
 {
+    /// <summary>
+    /// Specifies the behavior <see cref="InputPaneAwareView"/> takes when input pane state changes.
+    /// </summary>
     public enum InputPaneAwareBehavior
     {
+        /// <summary>
+        /// The view doesn't resize or move the content when input pane state changes.
+        /// </summary>
         None = 0,
+
+        /// <summary>
+        /// The view moves the content vertically when input pane states changes. The content is not resized.
+        /// </summary>
         Pan,
+
+        /// <summary>
+        /// The view resizes the content when input pane state changes.
+        /// </summary>
         Resize
     }
 }
