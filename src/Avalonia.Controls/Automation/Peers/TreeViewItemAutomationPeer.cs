4658d3c7062acce214da424023d522ba892d45a6
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2026-06-17 03:47:25 +0800

    Expose tree item selection to macOS accessibility (#21598)
    TreeViewItemAutomationPeer implements ISelectionItemProvider, and AvnAccessibilityElement maps setAccessibilitySelected to it, so AX clients can select a TreeViewItem.

--- src/Avalonia.Controls/Automation/Peers/TreeViewItemAutomationPeer.cs
+++ src/Avalonia.Controls/Automation/Peers/TreeViewItemAutomationPeer.cs
@@ -1,14 +1,59 @@
+using Avalonia.Automation.Provider;
 using Avalonia.Controls;
 
 namespace Avalonia.Automation.Peers;
 
-public class TreeViewItemAutomationPeer : ItemsControlAutomationPeer
+public class TreeViewItemAutomationPeer : ItemsControlAutomationPeer,
+    ISelectionItemProvider
 {
     public TreeViewItemAutomationPeer(TreeViewItem owner)
         : base(owner)
     {
     }
-   
+
+    public bool IsSelected => Owner.GetValue(TreeViewItem.IsSelectedProperty);
+
+    public ISelectionProvider? SelectionContainer
+    {
+        get
+        {
+            if (Owner is TreeViewItem { TreeViewOwner: { } treeView })
+            {
+                var parentPeer = GetOrCreate(treeView);
+                return parentPeer.GetProvider<ISelectionProvider>();
+            }
+
+            return null;
+        }
+    }
+
+    public void Select()
+    {
+        EnsureEnabled();
+
+        if (Owner is TreeViewItem item)
+        {
+            item.TreeViewOwner?.SelectedItems.Clear();
+            item.IsSelected = true;
+        }
+    }
+
+    void ISelectionItemProvider.AddToSelection()
+    {
+        EnsureEnabled();
+
+        if (Owner is TreeViewItem item)
+            item.IsSelected = true;
+    }
+
+    void ISelectionItemProvider.RemoveFromSelection()
+    {
+        EnsureEnabled();
+
+        if (Owner is TreeViewItem item)
+            item.IsSelected = false;
+    }
+
     protected override AutomationControlType GetAutomationControlTypeCore()
     {
         return AutomationControlType.TreeItem;
