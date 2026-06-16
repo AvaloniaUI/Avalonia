f02835308e7804d45b5fdd05d1858f82ce3a51d6
yoyo <143886016+zrt2399@users.noreply.github.com>
2026-06-16 21:22:28 +0800

    fix(TextBox): avoid read-only editing side effects in keyboard handlers (#21587)

--- src/Avalonia.Controls/TextBox.cs
+++ src/Avalonia.Controls/TextBox.cs
@@ -1038,6 +1038,10 @@ namespace Avalonia.Controls
                 UpdatePseudoclasses();
                 UpdateCommandStates();
             }
+            else if (change.Property == IsReadOnlyProperty || change.Property == PasswordCharProperty)
+            {
+                UpdateCommandStates();
+            }
             else if (change.Property == CaretIndexProperty)
             {
                 OnCaretIndexChanged(change);
@@ -1371,7 +1375,7 @@ namespace Avalonia.Controls
             }
             else if (Match(keymap.Copy))
             {
-                if (!IsPasswordBox)
+                if (CanCopy)
                 {
                     Copy();
                 }
@@ -1380,7 +1384,7 @@ namespace Avalonia.Controls
             }
             else if (Match(keymap.Cut))
             {
-                if (!IsPasswordBox)
+                if (CanCut)
                 {
                     Cut();
                 }
@@ -1389,18 +1393,28 @@ namespace Avalonia.Controls
             }
             else if (Match(keymap.Paste))
             {
-                Paste();
+                if (CanPaste)
+                {
+                    Paste();
+                }
+
                 handled = true;
             }
             else if (Match(keymap.Undo) && IsUndoEnabled)
             {
-                Undo();
+                if (!IsReadOnly)
+                {
+                    Undo();
+                }
 
                 handled = true;
             }
             else if (Match(keymap.Redo) && IsUndoEnabled)
             {
-                Redo();
+                if (!IsReadOnly)
+                {
+                    Redo();
+                }
 
                 handled = true;
             }
@@ -1544,6 +1558,7 @@ namespace Avalonia.Controls
                         break;
 
                     case Key.Back:
+                        if (!IsReadOnly)
                         {
                             SnapshotUndoRedo();
 
@@ -1589,38 +1604,42 @@ namespace Avalonia.Controls
                             }
 
                             SnapshotUndoRedo();
-
-                            handled = true;
-                            break;
                         }
+
+                        handled = true;
+                        break;
+
                     case Key.Delete:
-                        SnapshotUndoRedo();
-
-                        if (hasWholeWordModifiers && SelectionStart == SelectionEnd)
+                        if (!IsReadOnly)
                         {
-                            SetSelectionForControlDelete();
-                        }
+                            SnapshotUndoRedo();
 
-                        if (!DeleteSelection())
-                        {
-                            var characterHit = _presenter.GetNextCharacterHit();
-
-                            var nextPosition = characterHit.FirstCharacterIndex + characterHit.TrailingLength;
-
-                            if (nextPosition != caretIndex)
+                            if (hasWholeWordModifiers && SelectionStart == SelectionEnd)
                             {
-                                var start = Math.Min(nextPosition, caretIndex);
-                                var end = Math.Max(nextPosition, caretIndex);
-
-                                var sb = StringBuilderCache.Acquire(text.Length);
-                                sb.Append(text);
-                                sb.Remove(start, end - start);
-
-                                SetCurrentValue(TextProperty, StringBuilderCache.GetStringAndRelease(sb));
+                                SetSelectionForControlDelete();
                             }
-                        }
 
-                        SnapshotUndoRedo();
+                            if (!DeleteSelection())
+                            {
+                                var characterHit = _presenter.GetNextCharacterHit();
+
+                                var nextPosition = characterHit.FirstCharacterIndex + characterHit.TrailingLength;
+
+                                if (nextPosition != caretIndex)
+                                {
+                                    var start = Math.Min(nextPosition, caretIndex);
+                                    var end = Math.Max(nextPosition, caretIndex);
+
+                                    var sb = StringBuilderCache.Acquire(text.Length);
+                                    sb.Append(text);
+                                    sb.Remove(start, end - start);
+
+                                    SetCurrentValue(TextProperty, StringBuilderCache.GetStringAndRelease(sb));
+                                }
+                            }
+
+                            SnapshotUndoRedo();
+                        }
 
                         handled = true;
                         break;
@@ -1628,8 +1647,12 @@ namespace Avalonia.Controls
                     case Key.Enter:
                         if (AcceptsReturn)
                         {
-                            SnapshotUndoRedo();
-                            HandleTextInput(NewLine);
+                            if (!IsReadOnly)
+                            {
+                                SnapshotUndoRedo();
+                                HandleTextInput(NewLine);
+                            }
+
                             handled = true;
                         }
 
@@ -1638,8 +1661,12 @@ namespace Avalonia.Controls
                     case Key.Tab:
                         if (AcceptsTab)
                         {
-                            SnapshotUndoRedo();
-                            HandleTextInput("\t");
+                            if (!IsReadOnly)
+                            {
+                                SnapshotUndoRedo();
+                                HandleTextInput("\t");
+                            }
+
                             handled = true;
                         }
                         else
@@ -1650,7 +1677,10 @@ namespace Avalonia.Controls
                         break;
 
                     case Key.Space:
-                        SnapshotUndoRedo(); // always snapshot in between words
+                        if (!IsReadOnly)
+                        {
+                            SnapshotUndoRedo(); // always snapshot in between words
+                        }
                         break;
 
                     default:
