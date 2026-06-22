b33c49d28bb5b8af3a64081bae77cef7ac168466
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2026-06-22 15:05:34 +0800

    Use file-scoped namespace in ListBoxAutomationPeer

--- src/Avalonia.Controls/Automation/Peers/ListBoxAutomationPeer.cs
+++ src/Avalonia.Controls/Automation/Peers/ListBoxAutomationPeer.cs
@@ -1,12 +1,11 @@
 using Avalonia.Controls;
 
-namespace Avalonia.Automation.Peers
+namespace Avalonia.Automation.Peers;
+
+public class ListBoxAutomationPeer : SelectingItemsControlAutomationPeer
 {
-    public class ListBoxAutomationPeer : SelectingItemsControlAutomationPeer
+    public ListBoxAutomationPeer(ListBox owner)
+        : base(owner)
     {
-        public ListBoxAutomationPeer(ListBox owner)
-            : base(owner)
-        {
-        }
     }
 }
