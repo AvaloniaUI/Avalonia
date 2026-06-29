bff9bbb057e463ada3e209cbf304a1c995df2db9
Symbai <14368203+Symbai@users.noreply.github.com>
2026-06-29 05:30:52 +0200

    Update SelectableTextBlock.cs

--- src/Avalonia.Controls/SelectableTextBlock.cs
+++ src/Avalonia.Controls/SelectableTextBlock.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Avalonia.Controls.Documents;
 using Avalonia.Controls.Utils;
 using Avalonia.Input;
