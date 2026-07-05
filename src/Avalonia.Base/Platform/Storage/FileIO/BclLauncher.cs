5ccd9c9c1299b343333332ac37de91f008ae7ab3
Misaka_L <lipww1234@foxmail.com>
2026-07-05 17:48:09 +0800

    Fix BclLauncher throw exception instead of return false when Process.Start thrown

--- src/Avalonia.Base/Platform/Storage/FileIO/BclLauncher.cs
+++ src/Avalonia.Base/Platform/Storage/FileIO/BclLauncher.cs
@@ -38,30 +38,37 @@ internal class BclLauncher : ILauncher
     
     private static bool Exec(string urlOrFile)
     {
-        if (OperatingSystem.IsLinux())
+        try
         {
-            // If no associated application/json MimeType is found xdg-open opens return error
-            // but it tries to open it anyway using the console editor (nano, vim, other..)
-            var args = EscapeForShell(urlOrFile);
-            ShellExecRaw($"xdg-open \\\"{args}\\\"", waitForExit: false);
-            return true;
-        }
-        else if (OperatingSystem.IsWindows() || OperatingSystem.IsMacOS())
-        {
-            var info = new ProcessStartInfo
+            if (OperatingSystem.IsLinux())
             {
-                FileName = OperatingSystem.IsWindows() ? urlOrFile : "open",
-                CreateNoWindow = true,
-                UseShellExecute = OperatingSystem.IsWindows()
-            };
-            // Using the argument list avoids having to escape spaces and other special 
-            // characters that are part of valid macos file and folder paths.
-            if (OperatingSystem.IsMacOS())
-                info.ArgumentList.Add(urlOrFile);
-            using var process = Process.Start(info);
-            return true;
+                // If no associated application/json MimeType is found xdg-open opens return error
+                // but it tries to open it anyway using the console editor (nano, vim, other..)
+                var args = EscapeForShell(urlOrFile);
+                ShellExecRaw($"xdg-open \\\"{args}\\\"", waitForExit: false);
+                return true;
+            }
+            else if (OperatingSystem.IsWindows() || OperatingSystem.IsMacOS())
+            {
+                var info = new ProcessStartInfo
+                {
+                    FileName = OperatingSystem.IsWindows() ? urlOrFile : "open",
+                    CreateNoWindow = true,
+                    UseShellExecute = OperatingSystem.IsWindows()
+                };
+                // Using the argument list avoids having to escape spaces and other special 
+                // characters that are part of valid macos file and folder paths.
+                if (OperatingSystem.IsMacOS())
+                    info.ArgumentList.Add(urlOrFile);
+                using var process = Process.Start(info);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
-        else
+        catch
         {
             return false;
         }
