28d6775b1d2080d375faf894478f364c33121fdc
Nathan Nguyen <146415969+NathanDrake2406@users.noreply.github.com>
2026-07-10 02:32:00 +1000

    Add regression test for UnsetValue converter precedence

--- tests/Avalonia.Markup.UnitTests/Data/BindingTests_Converters.cs
+++ tests/Avalonia.Markup.UnitTests/Data/BindingTests_Converters.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 using Avalonia.Controls;
 using Avalonia.Data;
 using Avalonia.Data.Converters;
 using Avalonia.Data.Core;
+using Avalonia.Media;
 using Avalonia.UnitTests;
 using Moq;
 using Xunit;
@@ -124,9 +127,75 @@ namespace Avalonia.Markup.UnitTests.Data
                 Times.Once);
         }
 
+        [Fact]
+        public void Converter_UnsetValue_Should_Reveal_Lower_Priority_Value()
+        {
+            var source = new BrushSource(-1);
+            var styleBrush = Brushes.White;
+            var localBrush = Brushes.Red;
+            var textBlock = new TextBlock
+            {
+                DataContext = source,
+            };
+
+            textBlock.SetValue(TextBlock.ForegroundProperty, styleBrush, BindingPriority.Style);
+            textBlock.Bind(
+                TextBlock.ForegroundProperty,
+                new Binding(nameof(BrushSource.Value))
+                {
+                    Converter = new IntToBrushConverter(localBrush),
+                });
+
+            Assert.Same(styleBrush, textBlock.Foreground);
+
+            source.Value = 1;
+            Assert.Same(localBrush, textBlock.Foreground);
+
+            source.Value = -1;
+            Assert.Same(styleBrush, textBlock.Foreground);
+
+            source.Value = 2;
+            Assert.Same(localBrush, textBlock.Foreground);
+        }
+
         private class Class1
         {
             public string Foo { get; set; } = "foo";
+        }
+
+        private class BrushSource(int value) : INotifyPropertyChanged
+        {
+            private int _value = value;
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+
+            public int Value
+            {
+                get => _value;
+                set
+                {
+                    _value = value;
+                    RaisePropertyChanged();
+                }
+            }
+
+            private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private class IntToBrushConverter(IBrush brush) : IValueConverter
+        {
+            public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            {
+                return value is int { } i && i >= 0 ? brush : AvaloniaProperty.UnsetValue;
+            }
+
+            public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            {
+                throw new NotSupportedException();
+            }
         }
     }
 }
