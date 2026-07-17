807d294a7858e8b546580050c1cecfd8df7d2c03
Max Katz <maxkatz6@outlook.com>
2026-07-16 19:03:03 -0700

    Move AvaloniaPropertyAnalyzer ids to DiagnosticIds

--- src/tools/Avalonia.Analyzers.CSharp/DiagnosticIds.cs
+++ src/tools/Avalonia.Analyzers.CSharp/DiagnosticIds.cs
@@ -4,4 +4,21 @@ public static class DiagnosticIds
 {
     public const string OnPropertyChangedOverride = "AVA2001";
     public const string Bitmap = "AVA2002";
+
+    // Avalonia property diagnostics.
+    public const string AvaloniaPropertyAssociatedProperty = "AVP0001";
+    public const string AvaloniaPropertyInappropriateAssignment = "AVP1000";
+    public const string AvaloniaPropertyInappropriateRegistration = "AVP1001";
+    public const string AvaloniaPropertyOwnedByGenericType = "AVP1002";
+    public const string AvaloniaPropertyOwnerDoesNotMatchOuterType = "AVP1010";
+    public const string AvaloniaPropertyUnexpectedAccess = "AVP1011";
+    public const string AvaloniaPropertySettingOwnStyledValue = "AVP1012";
+    public const string AvaloniaPropertySuperfluousAddOwnerCall = "AVP1013";
+    public const string AvaloniaPropertyDuplicateName = "AVP1020";
+    public const string AvaloniaPropertyAmbiguousName = "AVP1021";
+    public const string AvaloniaPropertyNameMismatch = "AVP1022";
+    public const string AvaloniaPropertyAccessorSideEffects = "AVP1030";
+    public const string AvaloniaPropertyMissingAccessor = "AVP1031";
+    public const string AvaloniaPropertyInconsistentAccessibility = "AVP1032";
+    public const string AvaloniaPropertyTypeMismatch = "AVP1040";
 }
