e325e70c860509056cf59a2ae3cc8ee207267327
Max Katz <maxkatz6@outlook.com>
2026-08-26 04:25:18 -0700

    Include page section in search

--- samples/ControlCatalog/Models/PageItem.cs
+++ samples/ControlCatalog/Models/PageItem.cs
@@ -6,12 +6,12 @@ using Avalonia.Media;
 
 namespace ControlCatalog.Models;
 
-public class PageItem(string header, Func<Page> factory, StreamGeometry iconData, string description)
+public class PageItem(string header, Func<Page> factory, StreamGeometry iconData, string description, string section)
 {
     public string Header { get; } = header;
     public StreamGeometry? IconData { get; } = iconData;
     public string? Description { get; } = description;
-    private string SearchKey { get; } = CreateSearchKey(header);
+    private string SearchKey { get; } = CreateSearchKey(header, section);
 
     public bool IsVisible { get; set; } = true;
 
@@ -22,25 +22,29 @@ public class PageItem(string header, Func<Page> factory, StreamGeometry iconData
         return SearchKey.Contains(searchKey, StringComparison.Ordinal);
     }
 
-    public static string CreateSearchKey(string value)
+    public static string CreateSearchKey(params ReadOnlySpan<string> values)
     {
-        var normalizedValue = value.Normalize(NormalizationForm.FormKD);
-        var builder = new StringBuilder(normalizedValue.Length);
+        var builder = new StringBuilder(256);
 
-        foreach (var c in normalizedValue)
+        foreach (var value in values)
         {
-            var category = CharUnicodeInfo.GetUnicodeCategory(c);
+            var normalizedValue = value.Normalize(NormalizationForm.FormKD);
 
-            if (category is UnicodeCategory.NonSpacingMark or
-                UnicodeCategory.SpacingCombiningMark or
-                UnicodeCategory.EnclosingMark)
+            foreach (var c in normalizedValue)
             {
-                continue;
-            }
+                var category = CharUnicodeInfo.GetUnicodeCategory(c);
 
-            if (char.IsLetterOrDigit(c))
-            {
-                builder.Append(char.ToUpperInvariant(c));
+                if (category is UnicodeCategory.NonSpacingMark or
+                    UnicodeCategory.SpacingCombiningMark or
+                    UnicodeCategory.EnclosingMark)
+                {
+                    continue;
+                }
+
+                if (char.IsLetterOrDigit(c))
+                {
+                    builder.Append(char.ToUpperInvariant(c));
+                }
             }
         }
 
