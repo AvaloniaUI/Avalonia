e5dbb5aef4fd09f0297d1f23d81bd4c844814058
Max Katz <maxkatz6@outlook.com>
2026-09-11 03:03:54 -0700

    Add missing build.cs RunCoreTest("Avalonia.Themes.UnitTests")

--- nukebuild/Build.cs
+++ nukebuild/Build.cs
@@ -282,6 +282,7 @@ partial class Build : NukeBuild
             RunCoreTest("Avalonia.Markup.UnitTests");
             RunCoreTest("Avalonia.Markup.Xaml.UnitTests");
             RunCoreTest("Avalonia.Skia.UnitTests");
+            RunCoreTest("Avalonia.Themes.UnitTests");
             RunCoreTest("Avalonia.Headless.NUnit.PerAssembly.UnitTests");
             RunCoreTest("Avalonia.Headless.NUnit.PerTest.UnitTests");
             RunCoreTest("Avalonia.Headless.XUnit.PerAssembly.UnitTests");
