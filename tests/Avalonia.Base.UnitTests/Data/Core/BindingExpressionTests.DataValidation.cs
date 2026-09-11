2bacf31c01db31648dfb4c173e568c2fcf5ee026
grokys <grokys@users.noreply.github.com>
2026-09-11 09:59:56 +0200

    Add additional failing tests.
    There are more `BindingNotification` cases to handle.

--- tests/Avalonia.Base.UnitTests/Data/Core/BindingExpressionTests.DataValidation.cs
+++ tests/Avalonia.Base.UnitTests/Data/Core/BindingExpressionTests.DataValidation.cs
@@ -578,10 +578,11 @@ public partial class BindingExpressionTests
         }
     }   
 
-    private class InvalidIdConverter : IValueConverter
+    private class InvalidIdConverter(BindingErrorType errorType) : IValueConverter
     {
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => value;
+        private readonly BindingErrorType _errorType = errorType;
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => value;
 
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
@@ -593,7 +594,7 @@ public partial class BindingExpressionTests
 
             return new BindingNotification(
                 new FormatException($"'{value}' is not a valid ID."),
-                BindingErrorType.DataValidationError);
+                _errorType);
         }
     }
 
@@ -604,7 +605,7 @@ public partial class BindingExpressionTests
         var target = CreateTargetWithSource(
             data,
             o => o.IntValue,
-            converter: new InvalidIdConverter(),
+            converter: new InvalidIdConverter(BindingErrorType.DataValidationError),
             enableDataValidation: true,
             mode: BindingMode.TwoWay,
             targetProperty: TargetClass.TagProperty);
@@ -623,6 +624,67 @@ public partial class BindingExpressionTests
             BindingErrorType.DataValidationError);
 
         GC.KeepAlive(data);
+    }
+
+    [Fact]
+    public void ConvertBack_Error_Updates_Data_Validation()
+    {
+        var data = new ViewModel { IntValue = 1 };
+        var target = CreateTargetWithSource(
+            data,
+            o => o.IntValue,
+            converter: new InvalidIdConverter(BindingErrorType.Error),
+            enableDataValidation: true,
+            mode: BindingMode.TwoWay,
+            targetProperty: TargetClass.TagProperty);
+
+        target.Tag = "42";
+
+        AssertNoError(target, TargetClass.TagProperty);
+
+        target.Tag = "0x555g";
+
+        Assert.Equal(42, data.IntValue);
+        AssertBindingError(
+            target,
+            TargetClass.TagProperty,
+            new FormatException("'0x555g' is not a valid ID."),
+            BindingErrorType.Error);
+
+        GC.KeepAlive(data);
+    }
+
+    [Fact]
+    public void ConvertBack_Notification_With_Value_Writes_Value()
+    {
+        var data = new ViewModel { IntValue = 1 };
+        var target = CreateTargetWithSource(
+            data,
+            o => o.IntValue,
+            converter: new FuncValueConverter(v => new BindingNotification(42)),
+            enableDataValidation: true,
+            mode: BindingMode.TwoWay,
+            targetProperty: TargetClass.TagProperty);
+
+        target.Tag = "foo";
+
+        Assert.Equal(42, data.IntValue);
+        AssertNoError(target, TargetClass.TagProperty);
+
+        GC.KeepAlive(data);
+    }
+
+    private class FuncValueConverter : IValueConverter
+    {
+        private readonly Func<object?, object?> _convertBack;
+
+        public FuncValueConverter(Func<object?, object?> convertBack) => _convertBack = convertBack;
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => value;
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => _convertBack(value);
     }
 
     private static void AssertNoError(TargetClass target, AvaloniaProperty property)
